090f127dd3de59aca9fa1d76ce16b3e9d5b01dd3
Ryan Nowak <nowakra@gmail.com>
2019-01-23 18:04:12 -0800

    Straw man of project-system hookup API for Razor

---
+++ src/Razor/src/Microsoft.VisualStudio.LanguageServices.Razor/NotifyRazorEditorCreated.cs
@@ -0,0 +1,19 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.Shell.Interop;
+
+namespace Microsoft.VisualStudio.LanguageServices.Razor
+{
+    /// <summary>
+    /// Used for internal Visual Studio infrastructure. Not for public consumption.
+    /// </summary>
+    public abstract class NotifyRazorEditorCreated
+    {
+        /// <summary>
+        /// Used for internal Visual Studio infrastructure. Not for public consumption.
+        /// </summary>
+        public abstract Task AddToWorkspaceAsync(IVsHierarchy hierarchy, uint itemId);
+    }
+}
