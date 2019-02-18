85e8f097aa18b94edcfdc2d2dc07162c512425d1
Ryan Nowak <nowakra@gmail.com>
2019-02-18 12:47:23 -0800

    Errata for Runtime APIs

--- src/Razor/test/Microsoft.AspNetCore.Razor.Test.ComponentShim/Microsoft.AspNetCore.Components/EventCallbackFactory.cs
+++ src/Razor/test/Microsoft.AspNetCore.Razor.Test.ComponentShim/Microsoft.AspNetCore.Components/EventCallbackFactory.cs
@@ -8,6 +8,8 @@ namespace Microsoft.AspNetCore.Components
 {
     public sealed class EventCallbackFactory
     {
+        public EventCallback Create(object receiver, EventCallback callback) => default;
+
         public EventCallback Create(object receiver, Action callback) => default;
 
         public EventCallback Create(object receiver, Action<object> callback) => default;
@@ -16,6 +18,10 @@ namespace Microsoft.AspNetCore.Components
 
         public EventCallback Create(object receiver, Func<object, Task> callback) => default;
 
+        public EventCallback<T> Create<T>(object receiver, string callback) => default;
+
+        public EventCallback<T> Create<T>(object receiver, EventCallback<T> callback) => default;
+
         public EventCallback<T> Create<T>(object receiver, Action callback) => default;
 
         public EventCallback<T> Create<T>(object receiver, Action<T> callback) => default;
@@ -23,5 +29,15 @@ namespace Microsoft.AspNetCore.Components
         public EventCallback<T> Create<T>(object receiver, Func<Task> callback) => default;
 
         public EventCallback<T> Create<T>(object receiver, Func<T, Task> callback) => default;
+
+        public EventCallback<T> CreateInferred<T>(object receiver, Action<T> callback, T value)
+        {
+            return Create(receiver, callback);
+        }
+
+        public EventCallback<T> CreateInferred<T>(object receiver, Func<T, Task> callback, T value)
+        {
+            return Create(receiver, callback);
+        }
     }
 }
