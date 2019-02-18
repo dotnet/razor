c86b67ea9b1c717e059abff39e140ddc2ef25fa8
Ryan Nowak <nowakra@gmail.com>
2019-02-18 10:40:23 -0800

    Add new APIs to ComponentShim

---
+++ src/Razor/test/Microsoft.AspNetCore.Razor.Test.ComponentShim/Microsoft.AspNetCore.Components/EventCallbackFactory.cs
@@ -0,0 +1,27 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using System.Threading.Tasks;
+
+namespace Microsoft.AspNetCore.Components
+{
+    public sealed class EventCallbackFactory
+    {
+        public EventCallback Create(object receiver, Action callback) => default;
+
+        public EventCallback Create(object receiver, Action<object> callback) => default;
+
+        public EventCallback Create(object receiver, Func<Task> callback) => default;
+
+        public EventCallback Create(object receiver, Func<object, Task> callback) => default;
+
+        public EventCallback<T> Create<T>(object receiver, Action callback) => default;
+
+        public EventCallback<T> Create<T>(object receiver, Action<T> callback) => default;
+
+        public EventCallback<T> Create<T>(object receiver, Func<Task> callback) => default;
+
+        public EventCallback<T> Create<T>(object receiver, Func<T, Task> callback) => default;
+    }
+}
