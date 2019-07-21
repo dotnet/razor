c303ec678131f1816abe7753dfaa5c50204bf167
Pranav K <prkrishn@hotmail.com>
2019-07-21 16:37:25 -0700

    Remove preview7 DesignTime tests

--- src/Razor/test/Microsoft.NET.Sdk.Razor.Test/IntegrationTests/BuildIntrospectionTest.cs
+++ src/Razor/test/Microsoft.NET.Sdk.Razor.Test/IntegrationTests/BuildIntrospectionTest.cs
@@ -194,38 +194,5 @@ namespace Microsoft.AspNetCore.Razor.Design.IntegrationTests
             Assert.BuildPassed(result);
             Assert.BuildOutputContainsLine(result, "UpToDateReloadFileTypes: ;.cs;.razor;.resx;");
         }
-
-        [Fact]
-        [InitializeTestProject("SimpleMvc")]
-        public async Task UpToDateReloadFileTypes_UsingPreview7DesignTimeTargets()
-        {
-            AddProjectFileContent(
-@"
-<PropertyGroup>
-    <RazorDesignTimeTargets>$(MSBuildProjectDirectory)\Microsoft.NET.Sdk.Razor.DesignTime.targets</RazorDesignTimeTargets>
-</PropertyGroup>");
-
-            var result = await DotnetMSBuild("_IntrospectUpToDateReloadFileTypes");
-
-            Assert.BuildPassed(result);
-            Assert.BuildOutputContainsLine(result, "UpToDateReloadFileTypes: .cs;.razor;.resx;.cshtml");
-        }
-
-        [Fact]
-        [InitializeTestProject("SimpleMvc")]
-        public async Task UpToDateReloadFileTypes_UsingPreview7DesignTimeTargets_AndRuntimeCompilation()
-        {
-            AddProjectFileContent(
-@"
-<PropertyGroup>
-    <RazorUpToDateReloadFileTypes>$(RazorUpToDateReloadFileTypes.Replace('.cshtml', ''))</RazorUpToDateReloadFileTypes>
-    <RazorDesignTimeTargets>$(MSBuildProjectDirectory)\Microsoft.NET.Sdk.Razor.DesignTime.targets</RazorDesignTimeTargets>
-</PropertyGroup>");
-
-            var result = await DotnetMSBuild("_IntrospectUpToDateReloadFileTypes");
-
-            Assert.BuildPassed(result);
-            Assert.BuildOutputContainsLine(result, "UpToDateReloadFileTypes: .cs;.razor;.resx;");
-        }
     }
 }
