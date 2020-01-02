9c5b00a4fea09c9e003894159040209b5e1c46b0
Ajay Bhargav Baaskaran <ajbaaska@microsoft.com>
2020-01-02 13:32:51 -0800

    LSP Razor formatting for Razor code block directives

---
+++ src/Razor/src/Microsoft.AspNetCore.Razor.LanguageServer/Formatting/FormattingContext.cs
@@ -0,0 +1,46 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Razor.Language;
+using Microsoft.AspNetCore.Razor.LanguageServer.Common;
+using Microsoft.CodeAnalysis.Text;
+using OmniSharp.Extensions.LanguageServer.Protocol.Models;
+using Range = OmniSharp.Extensions.LanguageServer.Protocol.Models.Range;
+
+namespace Microsoft.AspNetCore.Razor.LanguageServer.Formatting
+{
+    internal class FormattingContext
+    {
+        public Uri Uri { get; set; }
+
+        public RazorCodeDocument CodeDocument { get; set; }
+
+        public SourceText SourceText => CodeDocument?.GetSourceText();
+
+        public FormattingOptions Options { get; set; }
+
+        public Range Range { get; set; }
+
+        public Dictionary<int, IndentationContext> Indentations { get; } = new Dictionary<int, IndentationContext>();
+    }
+
+    internal class IndentationContext
+    {
+        public int Line { get; set; }
+
+        public int IndentationLevel { get; set; }
+
+        public int RelativeIndentationLevel { get; set; }
+
+        public int ExistingIndentation { get; set; }
+
+        public FormattingSpan FirstSpan { get; set; }
+
+        public override string ToString()
+        {
+            return $"Line: {Line}, IndentationLevel: {IndentationLevel}, RelativeIndentationLevel: {RelativeIndentationLevel}, ExistingIndentation: {ExistingIndentation}";
+        }
+    }
+}
---
+++ src/Razor/test/Microsoft.AspNetCore.Razor.LanguageServer.Test/Formatting/DefaultRazorFormattingServiceTest.cs
@@ -0,0 +1,383 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Microsoft.AspNetCore.Razor.LanguageServer.Formatting
+{
+    public class DefaultRazorFormattingServiceTest : FormattingTestBase
+    {
+        [Fact]
+        public async Task FormatsCodeBlockDirective()
+        {
+            await RunFormattingTestAsync(
+input: @"
+|@functions {
+ public class Foo{}
+        public interface Bar {
+}
+}|
+",
+expected: @"
+@functions {
+    public class Foo { }
+    public interface Bar
+    {
+    }
+}
+");
+        }
+
+        [Fact]
+        public async Task DoesNotFormat_NonCodeBlockDirectives()
+        {
+            await RunFormattingTestAsync(
+input: @"
+|@{
+var x = ""foo"";
+}
+<div>
+        </div>|
+",
+expected: @"
+@{
+var x = ""foo"";
+}
+<div>
+        </div>
+");
+        }
+
+        [Fact]
+        public async Task DoesNotFormat_CodeBlockDirectiveWithMarkup()
+        {
+            await RunFormattingTestAsync(
+input: @"
+|@functions {
+ public class Foo{
+void Method() { <div></div> }
+}
+}|
+",
+expected: @"
+@functions {
+ public class Foo{
+void Method() { <div></div> }
+}
+}
+");
+        }
+
+        [Fact]
+        public async Task DoesNotFormat_CodeBlockDirectiveWithImplicitExpressions()
+        {
+            await RunFormattingTestAsync(
+input: @"
+|@functions {
+ public class Foo{
+void Method() { @DateTime.Now }
+}
+}|
+",
+expected: @"
+@functions {
+ public class Foo{
+void Method() { @DateTime.Now }
+}
+}
+");
+        }
+
+        [Fact]
+        public async Task DoesNotFormat_CodeBlockDirectiveWithExplicitExpressions()
+        {
+            await RunFormattingTestAsync(
+input: @"
+|@functions {
+ public class Foo{
+void Method() { @(DateTime.Now) }
+}
+}|
+",
+expected: @"
+@functions {
+ public class Foo{
+void Method() { @(DateTime.Now) }
+}
+}
+");
+        }
+
+        [Fact]
+        public async Task DoesNotFormat_CodeBlockDirectiveWithRazorComments()
+        {
+            await RunFormattingTestAsync(
+input: @"
+|@functions {
+ public class Foo{
+@* This is a Razor Comment *@
+void Method() {  }
+}
+}|
+",
+expected: @"
+@functions {
+ public class Foo{
+@* This is a Razor Comment *@
+void Method() {  }
+}
+}
+");
+        }
+
+        [Fact]
+        public async Task DoesNotFormat_CodeBlockDirectiveWithRazorStatements()
+        {
+            await RunFormattingTestAsync(
+input: @"
+|@functions {
+ public class Foo{
+@* This is a Razor Comment *@
+void Method() { @if (true) {} }
+}
+}|
+",
+expected: @"
+@functions {
+ public class Foo{
+@* This is a Razor Comment *@
+void Method() { @if (true) {} }
+}
+}
+");
+        }
+
+        [Fact]
+        public async Task OnlyFormatsWithinRange()
+        {
+            await RunFormattingTestAsync(
+input: @"
+@functions {
+ public class Foo{}
+        |public interface Bar {
+}|
+}
+",
+expected: @"
+@functions {
+ public class Foo{}
+    public interface Bar
+    {
+    }
+}
+");
+        }
+
+        [Fact]
+        public async Task MultipleCodeBlockDirectives()
+        {
+            await RunFormattingTestAsync(
+input: @"
+|@functions {
+ public class Foo{}
+        public interface Bar {
+}
+}
+Hello World
+@functions {
+      public class Baz    {
+          void Method ( )
+          { }
+          }
+}|
+",
+expected: @"
+@functions {
+    public class Foo { }
+    public interface Bar
+    {
+    }
+}
+Hello World
+@functions {
+    public class Baz
+    {
+        void Method()
+        { }
+    }
+}
+");
+        }
+
+        [Fact]
+        public async Task MultipleCodeBlockDirectives2()
+        {
+            await RunFormattingTestAsync(
+input: @"|
+Hello World
+@functions {
+public class HelloWorld
+{
+}
+}
+
+@functions{
+    
+ public class Bar {}
+}
+|",
+expected: @"
+Hello World
+@functions {
+    public class HelloWorld
+    {
+    }
+}
+
+@functions{
+    
+    public class Bar { }
+}
+");
+        }
+
+        [Fact]
+        public async Task CodeOnTheSameLineAsCodeBlockDirectiveStart()
+        {
+            await RunFormattingTestAsync(
+input: @"
+|@functions {public class Foo{
+}
+}|
+",
+expected: @"
+@functions {
+    public class Foo
+    {
+    }
+}
+");
+        }
+
+        [Fact]
+        public async Task CodeOnTheSameLineAsCodeBlockDirectiveEnd()
+        {
+            await RunFormattingTestAsync(
+input: @"
+|@functions {
+public class Foo{
+}}|
+",
+expected: @"
+@functions {
+    public class Foo
+    {
+    }
+}
+");
+        }
+
+        [Fact]
+        public async Task SingleLineCodeBlockDirective()
+        {
+            await RunFormattingTestAsync(
+input: @"
+|@functions {public class Foo{}}|
+",
+expected: @"
+@functions {
+    public class Foo { }
+}
+");
+        }
+
+        [Fact]
+        public async Task IndentsCodeBlockDirectiveStart()
+        {
+            await RunFormattingTestAsync(
+input: @"|
+Hello World
+     @functions {public class Foo{}
+}|
+",
+expected: @"
+Hello World
+@functions {
+    public class Foo { }
+}
+");
+        }
+
+        [Fact]
+        public async Task IndentsCodeBlockDirectiveEnd()
+        {
+            await RunFormattingTestAsync(
+input: @"|
+ @functions {
+public class Foo{}
+     }|
+",
+expected: @"
+@functions {
+    public class Foo { }
+}
+");
+        }
+
+        [Fact]
+        public async Task ComplexCodeBlockDirective()
+        {
+            await RunFormattingTestAsync(
+input: @"
+|@functions{
+     public class Foo
+            {
+                public Foo()
+                {
+                    var arr = new string[ ] {
+""One"", ""two"",
+""three""
+                    };
+                }
+public int MyProperty { get
+{
+return 0 ;
+} set {} }
+
+void Method(){
+
+}
+                    }
+}|
+",
+expected: @"
+@functions{
+    public class Foo
+    {
+        public Foo()
+        {
+            var arr = new string[] {
+""One"", ""two"",
+""three""
+                };
+        }
+        public int MyProperty
+        {
+            get
+            {
+                return 0;
+            }
+            set { }
+        }
+
+        void Method()
+        {
+
+        }
+    }
+}
+");
+        }
+    }
+}
