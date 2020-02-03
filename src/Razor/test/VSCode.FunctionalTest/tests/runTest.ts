a0b37786fbfc037206476b0207304d55daad346b
N. Taylor Mullen <nimullen@microsoft.com>
2020-02-03 13:53:36 -0800

    Add more logs

--- src/Razor/test/VSCode.FunctionalTest/tests/runTest.ts
+++ src/Razor/test/VSCode.FunctionalTest/tests/runTest.ts
@@ -6,14 +6,28 @@
 // This file is used at the command line to download VSCode insiders and run all of our functional tests.
 
 import * as cp from 'child_process';
+import * as fs from 'fs';
 import * as path from 'path';
 import { downloadAndUnzipVSCode, resolveCliPathFromVSCodeExecutablePath, runTests } from 'vscode-test';
 
 async function main() {
     try {
         const extensionDevelopmentPath = path.resolve(__dirname, '../../../src/Microsoft.AspNetCore.Razor.VSCode.Extension/');
+        console.log(`EXPERIMENTAL - Extension Development Path: ${extensionDevelopmentPath}`);
+        console.log(`EXPERIMENTAL - Existence check for Extension Development Path: ${fs.existsSync(extensionDevelopmentPath)}`);
+
+        const directoryContent = fs.readdirSync(extensionDevelopmentPath);
+        console.log('EXPERIMENTAL - Directory content for development path:');
+        for (const content of directoryContent) {
+            console.log(`- ${content}`);
+        }
+
         const extensionTestsPath = path.resolve(__dirname, './index');
+        console.log(`EXPERIMENTAL - Extension Test Path: ${extensionTestsPath}`);
+        console.log(`EXPERIMENTAL - Existence check for Test Path: ${fs.existsSync(extensionTestsPath)}`);
         const testAppFolder = path.resolve(__dirname, '../../testapps');
+        console.log(`EXPERIMENTAL - Test App Folder: ${testAppFolder}`);
+        console.log(`EXPERIMENTAL - Existence check for Test App Folder: ${fs.existsSync(testAppFolder)}`);
 
         const vscodeExecutablePath = await downloadAndUnzipVSCode('stable');
         const cliPath = resolveCliPathFromVSCodeExecutablePath(vscodeExecutablePath);
