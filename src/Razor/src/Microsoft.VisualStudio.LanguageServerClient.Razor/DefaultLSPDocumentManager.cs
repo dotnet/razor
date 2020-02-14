ec6eb477892372be6534b6e1eb8db0045d8fddca
N. Taylor Mullen <nimullen@microsoft.com>
2020-02-13 17:35:05 -0800

    WIP

---
+++ src/Razor/src/Microsoft.VisualStudio.LanguageServerClient.Razor/DefaultLSPDocumentManager.cs
@@ -0,0 +1,106 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using System.Collections.Generic;
+using System.Composition;
+using System.Diagnostics;
+using Microsoft.VisualStudio.Shell;
+using Microsoft.VisualStudio.Text;
+using Microsoft.VisualStudio.Text.Editor;
+
+namespace Microsoft.VisualStudio.LanguageServerClient.Razor
+{
+    [Shared]
+    [Export(typeof(LSPDocumentManager))]
+    internal class DefaultLSPDocumentManager : LSPDocumentManager
+    {
+        private readonly FileUriProvider _fileUriProvider;
+        private readonly LSPDocumentFactory _documentFactory;
+        private readonly Dictionary<Uri, DocumentTracker> _documents;
+
+        [ImportingConstructor]
+        public DefaultLSPDocumentManager(
+            FileUriProvider fileUriProvider,
+            LSPDocumentFactory documentFactory)
+        {
+            if (fileUriProvider is null)
+            {
+                throw new ArgumentNullException(nameof(fileUriProvider));
+            }
+
+            if (documentFactory is null)
+            {
+                throw new ArgumentNullException(nameof(documentFactory));
+            }
+
+            _fileUriProvider = fileUriProvider;
+            _documentFactory = documentFactory;
+            _documents = new Dictionary<Uri, DocumentTracker>();
+        }
+
+        public void TrackDocumentView(ITextBuffer buffer, ITextView textView)
+        {
+            if (buffer is null)
+            {
+                throw new ArgumentNullException(nameof(buffer));
+            }
+
+            ThreadHelper.CheckAccess();
+
+            var uri = _fileUriProvider.GetOrCreate(buffer);
+            if (!_documents.TryGetValue(uri, out var documentTracker))
+            {
+                var lspDocument = _documentFactory.Create(buffer);
+                documentTracker = new DocumentTracker(lspDocument);
+                _documents[uri] = documentTracker;
+            }
+
+            documentTracker.TextViews.Add(textView);
+        }
+
+        public void UntrackDocumentView(ITextBuffer buffer, ITextView textView)
+        {
+            ThreadHelper.CheckAccess();
+
+            var uri = _fileUriProvider.GetOrCreate(buffer);
+            if (!_documents.TryGetValue(uri, out var documentTracker))
+            {
+                // We don't know about this document, noop.
+                return;
+            }
+
+            documentTracker.TextViews.Remove(textView);
+
+            if (documentTracker.TextViews.Count == 0)
+            {
+                _documents.Remove(uri);
+            }
+        }
+
+        public override LSPDocument GetDocument(Uri uri)
+        {
+            ThreadHelper.CheckAccess();
+
+            if (!_documents.TryGetValue(uri, out var documentTracker))
+            {
+                Debug.Fail("We should always know about the documents we're asked for.");
+            }
+
+            return documentTracker.Document;
+        }
+
+        private class DocumentTracker
+        {
+            public DocumentTracker(LSPDocument document)
+            {
+                Document = document;
+                TextViews = new HashSet<ITextView>();
+            }
+
+            public LSPDocument Document { get; }
+
+            public HashSet<ITextView> TextViews { get; }
+        }
+    }
+}
---
+++ src/Razor/src/Microsoft.VisualStudio.LanguageServerClient.Razor/LSPDocumentExtensions.cs
@@ -0,0 +1,50 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+
+namespace Microsoft.VisualStudio.LanguageServerClient.Razor
+{
+    internal static class LSPDocumentExtensions
+    {
+        public static bool TryGetVirtualCSharpDocument(this LSPDocument hostDocument, out VirtualDocument virtualDocument)
+        {
+            if (hostDocument is null)
+            {
+                throw new ArgumentNullException(nameof(hostDocument));
+            }
+
+            for (var i = 0; i < hostDocument.VirtualDocuments.Count; i++)
+            {
+                if (hostDocument.VirtualDocuments[i] is CSharpVirtualDocument csharpDocument)
+                {
+                    virtualDocument = csharpDocument;
+                    return true;
+                }
+            }
+
+            virtualDocument = null;
+            return false;
+        }
+
+        public static bool TryGetVirtualHTMLDocument(this LSPDocument hostDocument, out VirtualDocument virtualDocument)
+        {
+            if (hostDocument is null)
+            {
+                throw new ArgumentNullException(nameof(hostDocument));
+            }
+
+            for (var i = 0; i < hostDocument.VirtualDocuments.Count; i++)
+            {
+                if (hostDocument.VirtualDocuments[i] is HtmlVirtualDocument htmlDocument)
+                {
+                    virtualDocument = htmlDocument;
+                    return true;
+                }
+            }
+
+            virtualDocument = null;
+            return false;
+        }
+    }
+}
