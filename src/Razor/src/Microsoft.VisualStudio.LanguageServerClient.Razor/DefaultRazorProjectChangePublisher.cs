16945ec90543406b95b6a899766379ba9e9b9c57
Ryan Brandenburg <rybrande@microsoft.com>
2020-04-07 15:00:54 -0700

    Clean up file writing

--- src/Razor/src/Microsoft.VisualStudio.LanguageServerClient.Razor/DefaultRazorProjectChangePublisher.cs
+++ src/Razor/src/Microsoft.VisualStudio.LanguageServerClient.Razor/DefaultRazorProjectChangePublisher.cs
@@ -209,9 +209,12 @@ namespace Microsoft.VisualStudio.LanguageServerClient.Razor
 
         protected virtual void SerializeToFile(ProjectSnapshot projectSnapshot, string publishFilePath)
         {
+            var stringWriter = new StringWriter();
+            _serializer.Serialize(stringWriter, projectSnapshot);
+
             var fileInfo = new FileInfo(publishFilePath);
-            using var writer = fileInfo.CreateText();
-            _serializer.Serialize(writer, projectSnapshot);
+            using var fileWriter = fileInfo.CreateText();
+            fileWriter.Write(stringWriter.ToString());
         }
 
         private async Task PublishAfterDelayAsync(string projectFilePath)
