e9931defee73a2ae796c19a417833ab76c9e1ba3
Tanay Parikh <taparik@microsoft.com>
2020-05-04 13:59:33 -0700

    Check if read/write/flush happen across multiple threads
    Write/Flush share a thread, Read happens on a distinct thread. A read thread doesn't write, and vice versa.
    
    There aren't multiple threads which read, or multiple threads performing write/flush functionality.

--- src/Razor/src/Microsoft.VisualStudio.LanguageServerClient.Razor/AutoFlushingStream.cs
+++ src/Razor/src/Microsoft.VisualStudio.LanguageServerClient.Razor/AutoFlushingStream.cs
@@ -1,6 +1,7 @@
 ﻿// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
+using System.Collections.Generic;
 using System.IO;
 using System.Threading;
 using System.Threading.Tasks;
@@ -10,6 +11,8 @@ namespace Microsoft.VisualStudio.LanguageServerClient.Razor
     internal class AutoFlushingStream : Stream
     {
         private readonly Stream _inner;
+
+        private Dictionary<(string, int), int> prevThreads = new Dictionary<(string, int), int>();
 
         public AutoFlushingStream(Stream inner)
         {
@@ -30,24 +33,51 @@ namespace Microsoft.VisualStudio.LanguageServerClient.Razor
 
         public override void SetLength(long value) => _inner.SetLength(value);
 
-        public override void Flush() => FlushAsync(CancellationToken.None).GetAwaiter().GetResult();
+        public override void Flush()
+        {
+            FlushAsync(CancellationToken.None).GetAwaiter().GetResult();
+        }
 
-        public override int Read(byte[] buffer, int offset, int count) => ReadAsync(buffer, offset, count, CancellationToken.None).GetAwaiter().GetResult();
+        public override int Read(byte[] buffer, int offset, int count)
+        {
+            return ReadAsync(buffer, offset, count, CancellationToken.None).GetAwaiter().GetResult();
+        }
 
-        public override void Write(byte[] buffer, int offset, int count) => WriteAsync(buffer, offset, count, CancellationToken.None).GetAwaiter().GetResult();
+        public override void Write(byte[] buffer, int offset, int count)
+        {
+            WriteAsync(buffer, offset, count, CancellationToken.None).GetAwaiter().GetResult();
+        }
 
         public override async Task FlushAsync(CancellationToken cancellationToken)
         {
+            CheckThreads("flush");
+
             await _inner.FlushAsync(cancellationToken).ConfigureAwait(false);
+        }
+
+        private void CheckThreads(string operation)
+        {
+            if (prevThreads.TryGetValue((operation, Thread.CurrentThread.ManagedThreadId), out _))
+            {
+                prevThreads[(operation, Thread.CurrentThread.ManagedThreadId)] += 1;
+            }
+            else
+            {
+                prevThreads[(operation, Thread.CurrentThread.ManagedThreadId)] = 1;
+            }
         }
 
         public override async Task<int> ReadAsync(byte[] buffer, int offset, int count, CancellationToken cancellationToken)
         {
+            CheckThreads("read");
+
             return await _inner.ReadAsync(buffer, offset, count).ConfigureAwait(false);
         }
 
         public override async Task WriteAsync(byte[] buffer, int offset, int count, CancellationToken cancellationToken)
         {
+            CheckThreads("write");
+
             await _inner.WriteAsync(buffer, offset, count).ConfigureAwait(false);
             await FlushAsync().ConfigureAwait(false);
         }
