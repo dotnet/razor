2567c384492724c05017a4d1b2bc81a7c5ae2418
N. Taylor Mullen <nimullen@microsoft.com>
2020-05-21 17:26:54 -0700

    Add smart indenting support for HTML + TagHelper scenarios.
    - I utilized an `OnTypeFormattingProvider` specifically for newlines to handle translating tags into better formatted segments after entering a newline.
    - Moved some indentation generation methods onto `FormattingContext` so they could be usable from outside of the formatting service.
    - Added tests to validate HTML/TagHelper scenarios.
    
    dotnet/aspnetcore#22119

--- src/Razor/src/Microsoft.AspNetCore.Razor.LanguageServer/Formatting/FormattingContext.cs
+++ src/Razor/src/Microsoft.AspNetCore.Razor.LanguageServer/Formatting/FormattingContext.cs
@@ -24,5 +24,35 @@ namespace Microsoft.AspNetCore.Razor.LanguageServer.Formatting
         public Range Range { get; set; }
 
         public Dictionary<int, IndentationContext> Indentations { get; } = new Dictionary<int, IndentationContext>();
+
+        public string GetIndentationLevelString(int indentationLevel)
+        {
+            var indentation = indentationLevel;
+            if (Options.InsertSpaces)
+            {
+                indentation *= (int)Options.TabSize;
+            }
+            var indentationString = GetIndentationString(indentation);
+            return indentationString;
+        }
+
+        public string GetIndentationString(int indentation)
+        {
+            if (Options.InsertSpaces)
+            {
+                return new string(' ', indentation);
+            }
+            else
+            {
+                var tabs = indentation / Options.TabSize;
+                var tabPrefix = new string('\t', (int)tabs);
+
+                var spaces = indentation % Options.TabSize;
+                var spaceSuffix = new string(' ', (int)spaces);
+
+                var combined = string.Concat(tabPrefix, spaceSuffix);
+                return combined;
+            }
+        }
     }
 }
