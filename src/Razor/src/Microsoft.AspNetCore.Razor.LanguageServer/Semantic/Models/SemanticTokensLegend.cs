e998120f236d1128166cb5dc1e8b588a67c9dd7c
Ryan Brandenburg <rybrande@microsoft.com>
2020-06-12 14:40:16 -0700

    Add models

---
+++ src/Razor/src/Microsoft.AspNetCore.Razor.LanguageServer/Semantic/Models/SemanticTokensLegend.cs
@@ -0,0 +1,14 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using OmniSharp.Extensions.LanguageServer.Protocol.Models;
+
+namespace Microsoft.AspNetCore.Razor.LanguageServer.Semantic.Models
+{
+    public class SemanticTokensLegend
+    {
+        public Container<string> TokenTypes { get; set; }
+
+        public Container<string> TokenModifiers { get; set; }
+    }
+}
