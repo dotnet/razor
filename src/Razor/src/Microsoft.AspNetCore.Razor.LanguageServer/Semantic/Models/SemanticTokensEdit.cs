73616dd491c29a165cb88f7cdf125ad8dd3fb32b
Ryan Brandenburg <rybrande@microsoft.com>
2020-06-18 16:26:20 -0700

    Cleanup

--- src/Razor/src/Microsoft.AspNetCore.Razor.LanguageServer/Semantic/Models/SemanticTokensEdit.cs
+++ src/Razor/src/Microsoft.AspNetCore.Razor.LanguageServer/Semantic/Models/SemanticTokensEdit.cs
@@ -2,8 +2,6 @@
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using System.Collections.Generic;
-using System.Linq;
-using Microsoft.Extensions.Internal;
 
 namespace Microsoft.AspNetCore.Razor.LanguageServer.Semantic.Models
 {
@@ -12,33 +10,5 @@ namespace Microsoft.AspNetCore.Razor.LanguageServer.Semantic.Models
         public int Start { get; set; }
         public int DeleteCount { get; set; }
         public IEnumerable<uint> Data { get; set; }
-
-        public override bool Equals(object obj)
-        {
-            if (!(obj is SemanticTokensEdit other))
-            {
-                return false;
-            }
-
-            if (Data is null || other.Data is null)
-            {
-                return Data is null && other.Data is null;
-            }
-
-            return Enumerable.SequenceEqual(Data, other.Data)
-                && DeleteCount.Equals(other.DeleteCount)
-                && Start.Equals(other.Start);
-        }
-
-        public override int GetHashCode()
-        {
-            var combiner = HashCodeCombiner.Start();
-
-            combiner.Add(Start);
-            combiner.Add(DeleteCount);
-            combiner.Add(Data);
-
-            return combiner.CombinedHash;
-        }
     }
 }
