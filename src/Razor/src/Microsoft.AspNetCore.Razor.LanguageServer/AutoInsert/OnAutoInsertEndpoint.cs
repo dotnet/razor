zhamppx97/aspnetcore-tooling

Move from OnTypeFormatting to OnAutoInsert.
- Removed all of our hacks to enable our old `OnTypeFormatting` `__placeholder__` tech that helped us control cursors. This involved removing:
    - `OnTypeFormatting` middlelayer, this would tell the language server to insert placeholders and also move the cursor
    - `LSPEditorService` & `DefaultLSPEditorService`, this provided utilities to apply text edits and move the cursor
    - Placeholder constants
    - All of our existing placeholder insertion logic in our old on type formatting providers
    - `RazorOnTypeFormattingProvider`, the base class for on type formatting providers
    - `RazorOnTypeFormattingEndpoint`, endpoint for `OnTypeFormatting`
    - On-type formatting within the existing Razor formatting service.
    - Logic from the `RazorContentTypeChangeListener` to move the cursor on LiveShare guests.
- Added a new `OnAutoInsert` endpoint & corresponding registration types.
- Created a `RazorOnAutoInsertProvider` to server as the base class for our pre-existing `RazorOnTypeFormattingProvider`s
    - The logic to consume all of our insert providers now lives in the endpoint (instead of formatting service).
- Updated existing tests
- Moved compatible tests from the formatting service's on type formatting section into the OnAutoInsertEndpoint tests.

Fixes dotnet/aspnetcore#22716

src/Razor/src/Microsoft.AspNetCore.Razor.LanguageServer/AutoInsert/OnAutoInsertEndpoint.cs
@@ -1,0 +1,124 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Razor.LanguageServer.Common;
+using Microsoft.AspNetCore.Razor.LanguageServer.Formatting;
+using Microsoft.AspNetCore.Razor.LanguageServer.ProjectSystem;
+using Microsoft.CodeAnalysis.Razor;
+using OmniSharp.Extensions.LanguageServer.Protocol.Models;
+
+namespace Microsoft.AspNetCore.Razor.LanguageServer.AutoInsert
+{
+    internal class OnAutoInsertEndpoint : IOnAutoInsertHandler
+    {
+        private readonly ForegroundDispatcher _foregroundDispatcher;
+        private readonly DocumentResolver _documentResolver;
+        private readonly IReadOnlyList<RazorOnAutoInsertProvider> _onAutoInsertProvider;
+        private readonly Container<string> _formatOnTypeTriggerCharacters;
+
+        public OnAutoInsertEndpoint(
+            ForegroundDispatcher foregroundDispatcher,
+            DocumentResolver documentResolver,
+            IEnumerable<RazorOnAutoInsertProvider> onAutoInsertProvider)
+        {
+            if (foregroundDispatcher is null)
+            {
+                throw new ArgumentNullException(nameof(foregroundDispatcher));
+            }
+
+            if (documentResolver is null)
+            {
+                throw new ArgumentNullException(nameof(documentResolver));
+            }
+
+            if (onAutoInsertProvider is null)
+            {
+                throw new ArgumentNullException(nameof(onAutoInsertProvider));
+            }
+
+            _foregroundDispatcher = foregroundDispatcher;
+            _documentResolver = documentResolver;
+            _onAutoInsertProvider = onAutoInsertProvider.ToList();
+            _formatOnTypeTriggerCharacters = _onAutoInsertProvider.Select(provider => provider.TriggerCharacter).ToList();
+        }
+
+        public RegistrationExtensionResult GetRegistration()
+        {
+            const string AssociatedServerCapability = "_ms_onAutoInsertProvider";
+
+            var registrationOptions = new OnAutoInsertRegistrationOptions()
+            {
+                DocumentSelector = RazorDefaults.Selector,
+                TriggerCharacters = _formatOnTypeTriggerCharacters,
+            };
+
+            return new RegistrationExtensionResult(AssociatedServerCapability, registrationOptions);
+        }
+
+        public async Task<OnAutoInsertResponse> Handle(OnAutoInsertParams request, CancellationToken cancellationToken)
+        {
+            var document = await Task.Factory.StartNew(() =>
+            {
+                _documentResolver.TryResolveDocument(request.TextDocument.Uri.GetAbsoluteOrUNCPath(), out var documentSnapshot);
+
+                return documentSnapshot;
+            }, cancellationToken, TaskCreationOptions.None, _foregroundDispatcher.ForegroundScheduler);
+
+            if (document is null || cancellationToken.IsCancellationRequested)
+            {
+                return null;
+            }
+
+            var codeDocument = await document.GetGeneratedOutputAsync();
+            if (codeDocument.IsUnsupported())
+            {
+                return null;
+            }
+
+            cancellationToken.ThrowIfCancellationRequested();
+
+            var character = request.Character;
+
+            var applicableProviders = new List<RazorOnAutoInsertProvider>();
+            for (var i = 0; i < _onAutoInsertProvider.Count; i++)
+            {
+                var formatOnTypeProvider = _onAutoInsertProvider[i];
+                if (formatOnTypeProvider.TriggerCharacter == character)
+                {
+                    applicableProviders.Add(formatOnTypeProvider);
+                }
+            }
+
+            if (applicableProviders.Count == 0)
+            {
+                // There's currently a bug in the LSP platform where other language clients OnAutoInsert trigger characters influence every language clients trigger characters.
+                // To combat this we need to pre-emptively return so we don't try having our providers handle characters that they can't.
+                return null;
+            }
+
+            var uri = request.TextDocument.Uri;
+            var position = request.Position;
+
+            var formattingContext = FormattingContext.Create(uri, codeDocument, new Range(position, position), request.Options);
+            for (var i = 0; i < applicableProviders.Count; i++)
+            {
+                if (applicableProviders[i].TryResolveInsertion(position, formattingContext, out var textEdit, out var format))
+                {
+                    return new OnAutoInsertResponse()
+                    {
+                        TextEdit = textEdit,
+                        TextEditFormat = format,
+                    };
+                }
+            }
+
+            // No provider could handle the text edit.
+            return null;
+        }
+    }
+}

dotnet/razor

src/Razor/src/Microsoft.AspNetCore.Razor.LanguageServer/AutoInsert/OnAutoInsertEndpoint.cs
@@ -1,0 +1,124 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Razor.LanguageServer.Common;
+using Microsoft.AspNetCore.Razor.LanguageServer.Formatting;
+using Microsoft.AspNetCore.Razor.LanguageServer.ProjectSystem;
+using Microsoft.CodeAnalysis.Razor;
+using OmniSharp.Extensions.LanguageServer.Protocol.Models;
+
+namespace Microsoft.AspNetCore.Razor.LanguageServer.AutoInsert
+{
+    internal class OnAutoInsertEndpoint : IOnAutoInsertHandler
+    {
+        private readonly ForegroundDispatcher _foregroundDispatcher;
+        private readonly DocumentResolver _documentResolver;
+        private readonly IReadOnlyList<RazorOnAutoInsertProvider> _onAutoInsertProviders;
+        private readonly Container<string> _formatOnTypeTriggerCharacters;
+
+        public OnAutoInsertEndpoint(
+            ForegroundDispatcher foregroundDispatcher,
+            DocumentResolver documentResolver,
+            IEnumerable<RazorOnAutoInsertProvider> onAutoInsertProvider)
+        {
+            if (foregroundDispatcher is null)
+            {
+                throw new ArgumentNullException(nameof(foregroundDispatcher));
+            }
+
+            if (documentResolver is null)
+            {
+                throw new ArgumentNullException(nameof(documentResolver));
+            }
+
+            if (onAutoInsertProvider is null)
+            {
+                throw new ArgumentNullException(nameof(onAutoInsertProvider));
+            }
+
+            _foregroundDispatcher = foregroundDispatcher;
+            _documentResolver = documentResolver;
+            _onAutoInsertProviders = onAutoInsertProvider.ToList();
+            _formatOnTypeTriggerCharacters = _onAutoInsertProviders.Select(provider => provider.TriggerCharacter).ToList();
+        }
+
+        public RegistrationExtensionResult GetRegistration()
+        {
+            const string AssociatedServerCapability = "_ms_onAutoInsertProvider";
+
+            var registrationOptions = new OnAutoInsertRegistrationOptions()
+            {
+                DocumentSelector = RazorDefaults.Selector,
+                TriggerCharacters = _formatOnTypeTriggerCharacters,
+            };
+
+            return new RegistrationExtensionResult(AssociatedServerCapability, registrationOptions);
+        }
+
+        public async Task<OnAutoInsertResponse> Handle(OnAutoInsertParams request, CancellationToken cancellationToken)
+        {
+            var document = await Task.Factory.StartNew(() =>
+            {
+                _documentResolver.TryResolveDocument(request.TextDocument.Uri.GetAbsoluteOrUNCPath(), out var documentSnapshot);
+
+                return documentSnapshot;
+            }, cancellationToken, TaskCreationOptions.None, _foregroundDispatcher.ForegroundScheduler);
+
+            if (document is null || cancellationToken.IsCancellationRequested)
+            {
+                return null;
+            }
+
+            var codeDocument = await document.GetGeneratedOutputAsync();
+            if (codeDocument.IsUnsupported())
+            {
+                return null;
+            }
+
+            cancellationToken.ThrowIfCancellationRequested();
+
+            var character = request.Character;
+
+            var applicableProviders = new List<RazorOnAutoInsertProvider>();
+            for (var i = 0; i < _onAutoInsertProviders.Count; i++)
+            {
+                var formatOnTypeProvider = _onAutoInsertProviders[i];
+                if (formatOnTypeProvider.TriggerCharacter == character)
+                {
+                    applicableProviders.Add(formatOnTypeProvider);
+                }
+            }
+
+            if (applicableProviders.Count == 0)
+            {
+                // There's currently a bug in the LSP platform where other language clients OnAutoInsert trigger characters influence every language clients trigger characters.
+                // To combat this we need to pre-emptively return so we don't try having our providers handle characters that they can't.
+                return null;
+            }
+
+            var uri = request.TextDocument.Uri;
+            var position = request.Position;
+
+            var formattingContext = FormattingContext.Create(uri, codeDocument, new Range(position, position), request.Options);
+            for (var i = 0; i < applicableProviders.Count; i++)
+            {
+                if (applicableProviders[i].TryResolveInsertion(position, formattingContext, out var textEdit, out var format))
+                {
+                    return new OnAutoInsertResponse()
+                    {
+                        TextEdit = textEdit,
+                        TextEditFormat = format,
+                    };
+                }
+            }
+
+            // No provider could handle the text edit.
+            return null;
+        }
+    }
+}