23681ad23017f0407de05f3482677abb3188a887
Tanay Parikh <taparik@microsoft.com>
2020-08-18 18:48:37 -0700

    ExtendableClientCapabilities
    Support for experimental capabilities

--- src/Razor/src/Microsoft.AspNetCore.Razor.LanguageServer/RazorLanguageServer.cs
+++ src/Razor/src/Microsoft.AspNetCore.Razor.LanguageServer/RazorLanguageServer.cs
@@ -30,6 +30,8 @@ using ILanguageServer = OmniSharp.Extensions.LanguageServer.Server.ILanguageServ
 using System.Threading;
 using Microsoft.AspNetCore.Razor.LanguageServer.Refactoring;
 using Microsoft.AspNetCore.Razor.LanguageServer.Definition;
+using Microsoft.AspNetCore.Razor.LanguageServer.Serialization;
+using OmniSharp.Extensions.LanguageServer.Protocol.Models;
 
 namespace Microsoft.AspNetCore.Razor.LanguageServer
 {
@@ -60,6 +62,9 @@ namespace Microsoft.AspNetCore.Razor.LanguageServer
         {
             Serializer.Instance.Settings.Converters.Add(SemanticTokensOrSemanticTokensEditsConverter.Instance);
             Serializer.Instance.JsonSerializer.Converters.RegisterRazorConverters();
+
+            // Custom ClientCapabilities deserializer to extract experimental capabilities
+            Serializer.Instance.JsonSerializer.Converters.Add(ExtendableClientCapabilitiesJsonConverter.Instance);
 
             ILanguageServer server = null;
             server = OmniSharp.Extensions.LanguageServer.Server.LanguageServer.PreInit(options =>
