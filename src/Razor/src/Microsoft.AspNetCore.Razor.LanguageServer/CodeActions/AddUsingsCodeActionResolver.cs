7baaf0a49705f8c14e3a6dac59f6ab0a77bcbb03
Tanay Parikh <taparik@microsoft.com>
2020-08-19 13:31:36 -0700

    PR Feedback

--- src/Razor/src/Microsoft.AspNetCore.Razor.LanguageServer/CodeActions/AddUsingsCodeActionResolver.cs
+++ src/Razor/src/Microsoft.AspNetCore.Razor.LanguageServer/CodeActions/AddUsingsCodeActionResolver.cs
@@ -157,8 +157,9 @@ namespace Microsoft.AspNetCore.Razor.LanguageServer.CodeActions
             VersionedTextDocumentIdentifier codeDocumentIdentifier,
             string newUsingNamespace)
         {
-            // If we don't have usings, insert after the last namespace or page directive, which ever comes later
             var head = new Position(0, 0);
+
+            // If we don't have usings, insert after the last namespace or page directive, which ever comes later
             var syntaxTreeRoot = codeDocument.GetSyntaxTree().Root;
             var lastNamespaceOrPageDirective = syntaxTreeRoot
                 .DescendantNodes()
