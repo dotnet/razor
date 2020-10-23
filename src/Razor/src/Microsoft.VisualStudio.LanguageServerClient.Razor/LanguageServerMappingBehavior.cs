136ef1e0a3fc2f123734e8abc19f0093a8edc429
N. Taylor Mullen <nimullen@microsoft.com>
2020-10-22 20:25:34 -0700

    Update src/Razor/src/Microsoft.VisualStudio.LanguageServerClient.Razor/LanguageServerMappingBehavior.cs
    Co-authored-by: Tanay Parikh <TanayParikh@users.noreply.github.com>

--- src/Razor/src/Microsoft.VisualStudio.LanguageServerClient.Razor/LanguageServerMappingBehavior.cs
+++ src/Razor/src/Microsoft.VisualStudio.LanguageServerClient.Razor/LanguageServerMappingBehavior.cs
@@ -16,7 +16,7 @@ namespace Microsoft.VisualStudio.LanguageServerClient.Razor
         ///     - Overlaps > 1 generated range = No mapping
         ///     - Intersects > 1 generated range = No mapping
         ///     - Overlaps 1 generated range = Will reduce the provided range down to the generated range.
-        ///     - Intersects 1 generated range = Will use the generated ranges mapping
+        ///     - Intersects 1 generated range = Will use the generated range mappings
         /// </summary>
         Inclusive
     }
