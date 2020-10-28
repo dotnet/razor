ddd8e9ab19b43c7568a342dae877187b2e83a1c4
Ryan Brandenburg <rybrande@microsoft.com>
2020-10-28 14:15:28 -0700

    Cleanup and add tests

--- src/Razor/src/Microsoft.AspNetCore.Razor.LanguageServer/ClientNotifierService.cs
+++ src/Razor/src/Microsoft.AspNetCore.Razor.LanguageServer/ClientNotifierService.cs
@@ -8,32 +8,41 @@ using OmniSharp.Extensions.LanguageServer.Protocol.Server;
 
 namespace Microsoft.AspNetCore.Razor.LanguageServer
 {
-    public class ClientNotifierService
+    // The VSCode OmniSharp client starts the RazorServer before all of its handlers are registered
+    // because of this we need to wait until everthing is initialized to make some client requests like
+    // razor\serverReady. This class takes a TCS which will complete when everything is initialized
+    // ensuring that no requests are sent before the client is ready.
+    internal class ClientNotifierService
     {
-        private TaskCompletionSource<bool> _taskCompletionSource;
+        private TaskCompletionSource<bool> _initializedCompletionSource;
         private IClientLanguageServer _languageServer;
 
-        public ClientNotifierService(IClientLanguageServer languageServer, TaskCompletionSource<bool> taskCompletionSource)
+        public ClientNotifierService(IClientLanguageServer languageServer, TaskCompletionSource<bool> initializedCompletionSource)
         {
             if (languageServer is null)
             {
                 throw new ArgumentNullException(nameof(languageServer));
             }
 
+            if (initializedCompletionSource is null)
+            {
+                throw new ArgumentNullException(nameof(initializedCompletionSource));
+            }
+
             _languageServer = languageServer;
-            _taskCompletionSource = taskCompletionSource;
+            _initializedCompletionSource = initializedCompletionSource;
         }
 
         public async Task<IResponseRouterReturns> SendRequestAsync(string method)
         {
-            await _taskCompletionSource.Task;
+            await _initializedCompletionSource.Task;
 
             return _languageServer.SendRequest(method);
         }
 
         public async Task<IResponseRouterReturns> SendRequestAsync<T>(string method, T @params)
         {
-            await _taskCompletionSource.Task;
+            await _initializedCompletionSource.Task;
 
             return _languageServer.SendRequest<T>(method, @params);
         }
