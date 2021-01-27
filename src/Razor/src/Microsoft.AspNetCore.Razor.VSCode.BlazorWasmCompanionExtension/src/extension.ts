04b8d28264fa7f5041b235157fcdeca378070786
Safia Abdalla <safia@microsoft.com>
2021-01-27 11:33:01 -0800

    Add license header to new source files

--- src/Razor/src/Microsoft.AspNetCore.Razor.VSCode.BlazorWasmCompanionExtension/src/extension.ts
+++ src/Razor/src/Microsoft.AspNetCore.Razor.VSCode.BlazorWasmCompanionExtension/src/extension.ts
@@ -1,3 +1,9 @@
+/* --------------------------------------------------------------------------------------------
+ * Copyright (c) Microsoft Corporation. All rights reserved.
+ * Licensed under the MIT License. See License.txt in the project root for license information.
+ * ------------------------------------------------------------------------------------------ */
+
+
 import * as vscode from 'vscode';
 import { LocalDebugProxyManager } from './LocalDebugProxyManager';
 import { spawn } from 'child_process';
