2091aaa73a34ee7ea6f1df84fce6214782733ff1
Allison Chou <allichou@microsoft.com>
2021-03-24 00:54:30 -0700

    Add options monitor file

---
+++ src/Razor/src/Microsoft.VisualStudio.LanguageServerClient.Razor/RazorLSPClientOptionsMonitor.cs
@@ -0,0 +1,21 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System.ComponentModel.Composition;
+
+namespace Microsoft.VisualStudio.LanguageServerClient.Razor
+{
+    [Export(typeof(RazorLSPClientOptionsMonitor))]
+    internal class RazorLSPClientOptionsMonitor
+    {
+        public bool InsertSpaces { get; private set; }
+
+        public int TabSize { get; private set; }
+
+        public void UpdateOptions(bool insertSpaces, int tabSize)
+        {
+            InsertSpaces = insertSpaces;
+            TabSize = tabSize;
+        }
+    }
+}
