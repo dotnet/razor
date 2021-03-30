998c13532564b6f7dbe6b762ab642a5f15a4da52
Tanay Parikh <taparik@microsoft.com>
2021-03-29 22:45:28 -0700

    Update RazorCodeAction.cs

--- src/Razor/src/Microsoft.AspNetCore.Razor.LanguageServer/CodeActions/Models/RazorCodeAction.cs
+++ src/Razor/src/Microsoft.AspNetCore.Razor.LanguageServer/CodeActions/Models/RazorCodeAction.cs
@@ -14,28 +14,6 @@ namespace Microsoft.AspNetCore.Razor.LanguageServer.CodeActions.Models
     [DebuggerDisplay("{Title,nq}")]
     internal class RazorCodeAction : CodeAction, IRequest<RazorCodeAction>, IBaseRequest
     {
-        // RazorCodeAction inherits from CodeAction to ensure we can utilize additional properties
-        // Re-implements the properties to ensure we have direct control over property (de)serialization. 
-        // Extensive platform compatibility issues occur without this.
-
-        [JsonProperty(PropertyName = "title", Required = Required.Always)]
-        public new string Title { get; set; }
-
-        [JsonProperty(PropertyName = "kind", NullValueHandling = NullValueHandling.Ignore)]
-        public new CodeActionKind Kind { get; set; }
-
-        [JsonProperty(PropertyName = "diagnostics", NullValueHandling = NullValueHandling.Ignore)]
-        public new Container<Diagnostic> Diagnostics { get; set; }
-
-        [JsonProperty(PropertyName = "edit", NullValueHandling = NullValueHandling.Ignore)]
-        public new WorkspaceEdit Edit { get; set; }
-
-        [JsonProperty(PropertyName = "command", NullValueHandling = NullValueHandling.Ignore)]
-        public new Command Command { get; set; }
-
-        [JsonProperty(PropertyName = "data", NullValueHandling = NullValueHandling.Ignore)]
-        public new object Data { get; set; }
-
         [JsonProperty(PropertyName = "children")]
         public RazorCodeAction[] Children { get; set; } = Array.Empty<RazorCodeAction>();
 
