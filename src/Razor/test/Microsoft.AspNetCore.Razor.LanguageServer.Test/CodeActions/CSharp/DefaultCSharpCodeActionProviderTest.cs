952bb311c0b835ca43881aa136a4d6cf113ece5c
Tanay Parikh <taparik@microsoft.com>
2021-03-30 17:14:42 -0700

    Update DefaultCSharpCodeActionProviderTest.cs

--- src/Razor/test/Microsoft.AspNetCore.Razor.LanguageServer.Test/CodeActions/CSharp/DefaultCSharpCodeActionProviderTest.cs
+++ src/Razor/test/Microsoft.AspNetCore.Razor.LanguageServer.Test/CodeActions/CSharp/DefaultCSharpCodeActionProviderTest.cs
@@ -17,166 +17,167 @@ using Range = OmniSharp.Extensions.LanguageServer.Protocol.Models.Range;
 
 namespace Microsoft.AspNetCore.Razor.LanguageServer.CodeActions
 {
-   public class DefaultCSharpCodeActionProviderTest : LanguageServerTestBase
-   {
-       private readonly RazorCodeAction[] SupportedCodeActions;
+    public class DefaultCSharpCodeActionProviderTest : LanguageServerTestBase
+    {
+        private readonly RazorCodeAction[] SupportedCodeActions;
 
-       public DefaultCSharpCodeActionProviderTest()
-       {
+        public DefaultCSharpCodeActionProviderTest()
+        {
             SupportedCodeActions = DefaultCSharpCodeActionProvider
                 .SupportedDefaultCodeActionNames
                 .Select(name => new RazorCodeAction() { Name = name })
                 .ToArray();
-       }
+        }
 
-       [Fact]
-       public async Task ProvideAsync_ValidCodeActions_ReturnsProvidedCodeAction()
-       {
-           // Arrange
-           var documentPath = "c:/Test.razor";
-           var contents = "@code { Path; }";
-           var request = new CodeActionParams()
-           {
-               TextDocument = new TextDocumentIdentifier(new Uri(documentPath)),
-               Range = new Range(),
-               Context = new CodeActionContext()
-           };
+        [Fact]
+        public async Task ProvideAsync_ValidCodeActions_ReturnsProvidedCodeAction()
+        {
+            // Arrange
+            var documentPath = "c:/Test.razor";
+            var contents = "@code { Path; }";
+            var request = new CodeActionParams()
+            {
+                TextDocument = new TextDocumentIdentifier(new Uri(documentPath)),
+                Range = new Range(),
+                Context = new CodeActionContext()
+            };
 
-           var location = new SourceLocation(8, -1, -1);
-           var context = CreateRazorCodeActionContext(request, location, documentPath, contents, new SourceSpan(8, 4));
-           context.CodeDocument.SetFileKind(FileKinds.Legacy);
+            var location = new SourceLocation(8, -1, -1);
+            var context = CreateRazorCodeActionContext(request, location, documentPath, contents, new SourceSpan(8, 4));
+            context.CodeDocument.SetFileKind(FileKinds.Legacy);
 
-           var provider = new DefaultCSharpCodeActionProvider();
+            var provider = new DefaultCSharpCodeActionProvider();
 
-           // Act
-           var providedCodeActions = await provider.ProvideAsync(context, SupportedCodeActions, default);
+            // Act
+            var providedCodeActions = await provider.ProvideAsync(context, SupportedCodeActions, default);
 
-           // Assert
-           Assert.Equal(SupportedCodeActions.Length, providedCodeActions.Count);
-           var providedNames = providedCodeActions.Select(action => action.Name);
-           var expectedNames = SupportedCodeActions.Select(action => action.Name);
-           Assert.Equal(expectedNames, providedNames);
-       }
+            // Assert
+            Assert.Equal(SupportedCodeActions.Length, providedCodeActions.Count);
+            var providedNames = providedCodeActions.Select(action => action.Name);
+            var expectedNames = SupportedCodeActions.Select(action => action.Name);
+            Assert.Equal(expectedNames, providedNames);
+        }
 
-       [Fact]
-       public async Task ProvideAsync_SupportsCodeActionResolveFalse_ValidCodeActions_ReturnsEmpty()
-       {
-           // Arrange
-           var documentPath = "c:/Test.razor";
-           var contents = "@code { Path; }";
-           var request = new CodeActionParams()
-           {
-               TextDocument = new TextDocumentIdentifier(new Uri(documentPath)),
-               Range = new Range(),
-               Context = new CodeActionContext()
-           };
+        [Fact]
+        public async Task ProvideAsync_SupportsCodeActionResolveFalse_ValidCodeActions_ReturnsEmpty()
+        {
+            // Arrange
+            var documentPath = "c:/Test.razor";
+            var contents = "@code { Path; }";
+            var request = new CodeActionParams()
+            {
+                TextDocument = new TextDocumentIdentifier(new Uri(documentPath)),
+                Range = new Range(),
+                Context = new CodeActionContext()
+            };
 
-           var location = new SourceLocation(8, -1, -1);
-           var context = CreateRazorCodeActionContext(request, location, documentPath, contents, new SourceSpan(8, 4), supportsCodeActionResolve: false);
-           context.CodeDocument.SetFileKind(FileKinds.Legacy);
+            var location = new SourceLocation(8, -1, -1);
+            var context = CreateRazorCodeActionContext(request, location, documentPath, contents, new SourceSpan(8, 4), supportsCodeActionResolve: false);
+            context.CodeDocument.SetFileKind(FileKinds.Legacy);
 
-           var provider = new DefaultCSharpCodeActionProvider();
+            var provider = new DefaultCSharpCodeActionProvider();
 
-           // Act
-           var providedCodeActions = await provider.ProvideAsync(context, SupportedCodeActions, default);
+            // Act
+            var providedCodeActions = await provider.ProvideAsync(context, SupportedCodeActions, default);
 
-           // Assert
-           Assert.Empty(providedCodeActions);
-       }
+            // Assert
+            Assert.Empty(providedCodeActions);
+        }
 
-       [Fact]
-       public async Task ProvideAsync_FunctionsBlock_ValidCodeActions_ReturnsEmpty()
-       {
-           // Arrange
-           var documentPath = "c:/Test.razor";
-           var contents = "@functions { Path; }";
-           var request = new CodeActionParams()
-           {
-               TextDocument = new TextDocumentIdentifier(new Uri(documentPath)),
-               Range = new Range(),
-               Context = new CodeActionContext()
-           };
+        [Fact]
+        public async Task ProvideAsync_FunctionsBlock_ValidCodeActions_ReturnsEmpty()
+        {
+            // Arrange
+            var documentPath = "c:/Test.razor";
+            var contents = "@functions { Path; }";
+            var request = new CodeActionParams()
+            {
+                TextDocument = new TextDocumentIdentifier(new Uri(documentPath)),
+                Range = new Range(),
+                Context = new CodeActionContext()
+            };
 
-           var location = new SourceLocation(13, -1, -1);
-           var context = CreateRazorCodeActionContext(request, location, documentPath, contents, new SourceSpan(13, 4));
-           context.CodeDocument.SetFileKind(FileKinds.Legacy);
+            var location = new SourceLocation(13, -1, -1);
+            var context = CreateRazorCodeActionContext(request, location, documentPath, contents, new SourceSpan(13, 4));
+            context.CodeDocument.SetFileKind(FileKinds.Legacy);
 
-           var provider = new DefaultCSharpCodeActionProvider();
+            var provider = new DefaultCSharpCodeActionProvider();
 
-           // Act
-           var providedCodeActions = await provider.ProvideAsync(context, SupportedCodeActions, default);
+            // Act
+            var providedCodeActions = await provider.ProvideAsync(context, SupportedCodeActions, default);
 
-           // Assert
-           Assert.Empty(providedCodeActions);
-       }
+            // Assert
+            Assert.Empty(providedCodeActions);
+        }
 
-       [Fact]
-       public async Task ProvideAsync_InvalidCodeActions_ReturnsNoCodeActions()
-       {
-           // Arrange
-           var documentPath = "c:/Test.razor";
-           var contents = "@code { Path; }";
-           var request = new CodeActionParams()
-           {
-               TextDocument = new TextDocumentIdentifier(new Uri(documentPath)),
-               Range = new Range(),
-               Context = new CodeActionContext()
-           };
+        [Fact]
+        public async Task ProvideAsync_InvalidCodeActions_ReturnsNoCodeActions()
+        {
+            // Arrange
+            var documentPath = "c:/Test.razor";
+            var contents = "@code { Path; }";
+            var request = new CodeActionParams()
+            {
+                TextDocument = new TextDocumentIdentifier(new Uri(documentPath)),
+                Range = new Range(),
+                Context = new CodeActionContext()
+            };
 
-           var location = new SourceLocation(8, -1, -1);
-           var context = CreateRazorCodeActionContext(request, location, documentPath, contents, new SourceSpan(8, 4));
-           context.CodeDocument.SetFileKind(FileKinds.Legacy);
+            var location = new SourceLocation(8, -1, -1);
+            var context = CreateRazorCodeActionContext(request, location, documentPath, contents, new SourceSpan(8, 4));
+            context.CodeDocument.SetFileKind(FileKinds.Legacy);
 
-           var provider = new DefaultCSharpCodeActionProvider();
+            var provider = new DefaultCSharpCodeActionProvider();
 
-           var codeActions = new RazorCodeAction[]
-           {
+            var codeActions = new RazorCodeAction[]
+            {
                new RazorCodeAction()
                {
                    Title = "Do something not really supported in razor",
                    Name = "Non-existant name"
                }
-           };
+            };
 
-           // Act
-           var providedCodeActions = await provider.ProvideAsync(context, codeActions, default);
+            // Act
+            var providedCodeActions = await provider.ProvideAsync(context, codeActions, default);
 
-           // Assert
-           Assert.Empty(providedCodeActions);
-       }
+            // Assert
+            Assert.Empty(providedCodeActions);
+        }
 
-       private static RazorCodeActionContext CreateRazorCodeActionContext(
-           CodeActionParams request,
-           SourceLocation location,
-           string filePath,
-           string text,
-           SourceSpan componentSourceSpan,
-           bool supportsFileCreation = true,
-           bool supportsCodeActionResolve = true)
-       {
-           var tagHelpers = Array.Empty<TagHelperDescriptor>();
-           var sourceDocument = TestRazorSourceDocument.Create(text, filePath: filePath, relativePath: filePath);
-           var projectEngine = RazorProjectEngine.Create(builder => {
-               builder.AddTagHelpers(tagHelpers);
-           });
-           var codeDocument = projectEngine.ProcessDesignTime(sourceDocument, FileKinds.Component, Array.Empty<RazorSourceDocument>(), tagHelpers);
+        private static RazorCodeActionContext CreateRazorCodeActionContext(
+            CodeActionParams request,
+            SourceLocation location,
+            string filePath,
+            string text,
+            SourceSpan componentSourceSpan,
+            bool supportsFileCreation = true,
+            bool supportsCodeActionResolve = true)
+        {
+            var tagHelpers = Array.Empty<TagHelperDescriptor>();
+            var sourceDocument = TestRazorSourceDocument.Create(text, filePath: filePath, relativePath: filePath);
+            var projectEngine = RazorProjectEngine.Create(builder =>
+            {
+                builder.AddTagHelpers(tagHelpers);
+            });
+            var codeDocument = projectEngine.ProcessDesignTime(sourceDocument, FileKinds.Component, Array.Empty<RazorSourceDocument>(), tagHelpers);
 
-           var cSharpDocument = codeDocument.GetCSharpDocument();
-           var diagnosticDescriptor = new RazorDiagnosticDescriptor("RZ10012", () => "", RazorDiagnosticSeverity.Error);
-           var diagnostic = RazorDiagnostic.Create(diagnosticDescriptor, componentSourceSpan);
-           var cSharpDocumentWithDiagnostic = RazorCSharpDocument.Create(cSharpDocument.GeneratedCode, cSharpDocument.Options, new[] { diagnostic });
-           codeDocument.SetCSharpDocument(cSharpDocumentWithDiagnostic);
+            var cSharpDocument = codeDocument.GetCSharpDocument();
+            var diagnosticDescriptor = new RazorDiagnosticDescriptor("RZ10012", () => "", RazorDiagnosticSeverity.Error);
+            var diagnostic = RazorDiagnostic.Create(diagnosticDescriptor, componentSourceSpan);
+            var cSharpDocumentWithDiagnostic = RazorCSharpDocument.Create(cSharpDocument.GeneratedCode, cSharpDocument.Options, new[] { diagnostic });
+            codeDocument.SetCSharpDocument(cSharpDocumentWithDiagnostic);
 
-           var documentSnapshot = Mock.Of<DocumentSnapshot>(document =>
-               document.GetGeneratedOutputAsync() == Task.FromResult(codeDocument) &&
-               document.GetTextAsync() == Task.FromResult(codeDocument.GetSourceText()) &&
-               document.Project.TagHelpers == tagHelpers, MockBehavior.Strict);
+            var documentSnapshot = Mock.Of<DocumentSnapshot>(document =>
+                document.GetGeneratedOutputAsync() == Task.FromResult(codeDocument) &&
+                document.GetTextAsync() == Task.FromResult(codeDocument.GetSourceText()) &&
+                document.Project.TagHelpers == tagHelpers, MockBehavior.Strict);
 
-           var sourceText = SourceText.From(text);
+            var sourceText = SourceText.From(text);
 
-           var context = new RazorCodeActionContext(request, documentSnapshot, codeDocument, location, sourceText, supportsFileCreation, supportsCodeActionResolve);
+            var context = new RazorCodeActionContext(request, documentSnapshot, codeDocument, location, sourceText, supportsFileCreation, supportsCodeActionResolve);
 
-           return context;
-       }
-   }
+            return context;
+        }
+    }
 }
