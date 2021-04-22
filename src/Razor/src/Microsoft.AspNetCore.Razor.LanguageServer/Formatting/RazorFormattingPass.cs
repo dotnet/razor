70e33d45c61779bb87ed3d7011b97103914e54e8
David Wengier <git@wengier.com>
2021-04-22 14:30:41 +1000

    Format razor code blocks

--- src/Razor/src/Microsoft.AspNetCore.Razor.LanguageServer/Formatting/RazorFormattingPass.cs
+++ src/Razor/src/Microsoft.AspNetCore.Razor.LanguageServer/Formatting/RazorFormattingPass.cs
@@ -58,6 +58,94 @@ namespace Microsoft.AspNetCore.Razor.LanguageServer.Formatting
             }
 
             cancellationToken.ThrowIfCancellationRequested();
+
+            // Format the razor bits of the file
+            var syntaxTree = changedContext.CodeDocument.GetSyntaxTree();
+            var edits = FormatRazor(changedContext, syntaxTree);
+
+            // Compute the final combined set of edits
+            var formattingChanges = edits.Select(e => e.AsTextChange(changedText));
+            changedText = changedText.WithChanges(formattingChanges);
+            var finalChanges = SourceTextDiffer.GetMinimalTextChanges(originalText, changedText, lineDiffOnly: false);
+            var finalEdits = finalChanges.Select(f => f.AsTextEdit(originalText)).ToArray();
+
+            return new FormattingResult(finalEdits);
+        }
+
+        private IEnumerable<TextEdit> FormatRazor(FormattingContext context, RazorSyntaxTree syntaxTree)
+        {
+            var edits = new List<TextEdit>();
+            var source = syntaxTree.Source;
+
+            foreach (var node in syntaxTree.Root.DescendantNodes())
+            {
+                // Disclaimer: CSharpCodeBlockSyntax is used a _lot_ in razor so I'm being overly careful to only try to
+                // format syntax forms we care about.
+                //
+                // We're looking for a code block like this:
+                //
+                // @code {
+                //    var x = 1;
+                // }
+                //
+                // The nodes will be a grandchild of a RazorDirective (the "@code") and we expect there to be
+                // at least three children, being:
+                // 1. Optional whitespace
+                // 2. The opening brace
+                // 3. The C# code
+                // 4. The closing brace
+                if (node is CSharpCodeBlockSyntax code &&
+                    node.Parent?.Parent is RazorDirectiveSyntax directive &&
+                    directive.DirectiveDescriptor?.Kind == DirectiveKind.CodeBlock &&
+                    code.Children.Count >= 3)
+                {
+                    if (code.Children[0] is UnclassifiedTextLiteralSyntax literal)
+                    {
+                        // For whitespace we normalize it differently depending on if its multi-line or not
+                        FormatWhitespaceBetweenDirectiveAndBrace(literal, directive, edits, source, context);
+                    }
+                    else if (code.Children[0] is RazorMetaCodeSyntax metaCode &&
+                        metaCode.MetaCode.Count == 1 &&
+                        metaCode.MetaCode[0].Content.Equals("{"))
+                    {
+                        // If there is no whitespace at all we normalize to a single space
+                        var start = metaCode.GetRange(source).Start;
+                        edits.Add(new TextEdit
+                        {
+                            Range = new Range(start, start),
+                            NewText = " "
+                        });
+                    }
+                }
+            }
+
+            return edits;
+        }
+
+        private static void FormatWhitespaceBetweenDirectiveAndBrace(UnclassifiedTextLiteralSyntax literal, RazorDirectiveSyntax directive, List<TextEdit> edits, RazorSourceDocument source, FormattingContext context)
+        {
+            if (literal.LiteralTokens.Any(t => t.Kind == SyntaxKind.NewLine))
+            {
+                // If there is a newline then we want to have just one newline after the directive
+                // and indent the { to match the @
+                edits.Add(new TextEdit
+                {
+                    Range = literal.GetRange(source),
+                    NewText = context.NewLineString + context.GetIndentationString(directive.GetLinePositionSpan(source).Start.Character)
+                });
+            }
+            else if (literal.Width > 1 ||
+                literal.LiteralTokens[0].Content.Equals("\t"))
+            {
+                // If there is anything other than one single space then we replace with one space between directive and brace.
+                //
+                // ie, "@code     {" will become "@code {"
+                edits.Add(new TextEdit
+                {
+                    Range = literal.GetRange(source),
+                    NewText = " "
+                });
+            }
         }
     }
 }
