3bc1404cbadc975b33d1317a7d640066482c6c83
David Wengier <git@wengier.com>
2021-04-22 14:27:03 +1000

    Create a basic Razor formatting pass

---
+++ src/Razor/src/Microsoft.AspNetCore.Razor.LanguageServer/Formatting/RazorFormattingPass.cs
@@ -0,0 +1,63 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Razor.Language;
+using Microsoft.AspNetCore.Razor.Language.Syntax;
+using Microsoft.AspNetCore.Razor.LanguageServer.Common;
+using Microsoft.Extensions.Logging;
+using OmniSharp.Extensions.LanguageServer.Protocol.Models;
+
+namespace Microsoft.AspNetCore.Razor.LanguageServer.Formatting
+{
+    internal class RazorFormattingPass : FormattingPassBase
+    {
+        private readonly ILogger _logger;
+
+        public RazorFormattingPass(
+            RazorDocumentMappingService documentMappingService,
+            FilePathNormalizer filePathNormalizer,
+            ClientNotifierServiceBase server,
+            ILoggerFactory loggerFactory)
+            : base(documentMappingService, filePathNormalizer, server)
+        {
+            if (loggerFactory is null)
+            {
+                throw new ArgumentNullException(nameof(loggerFactory));
+            }
+
+            _logger = loggerFactory.CreateLogger<CSharpFormattingPass>();
+        }
+
+        // Run after the C# formatter pass.
+        public override int Order => DefaultOrder - 3;
+
+        public override bool IsValidationPass => false;
+
+        public async override Task<FormattingResult> ExecuteAsync(FormattingContext context, FormattingResult result, CancellationToken cancellationToken)
+        {
+            if (context.IsFormatOnType)
+            {
+                // We don't want to handle OnTypeFormatting here.
+                return result;
+            }
+
+            // Apply previous edits if any.
+            var originalText = context.SourceText;
+            var changedText = originalText;
+            var changedContext = context;
+            if (result.Edits.Length > 0)
+            {
+                var changes = result.Edits.Select(e => e.AsTextChange(originalText)).ToArray();
+                changedText = changedText.WithChanges(changes);
+                changedContext = await context.WithTextAsync(changedText);
+            }
+
+            cancellationToken.ThrowIfCancellationRequested();
+        }
+    }
+}
