d1cd8d3ce52019f2216d00f3bbe05209dc259d8d
N. Taylor Mullen <nimullen@microsoft.com>
2021-06-08 21:04:44 -0700

    Turn Razor LSP editor on-by-default.
    - Removed the experimental editor feature flag and all the implications it imposed in our feature detector in regards to LiveShare.
    - Updated tests to verify only what's still valuable.
    
    Fixes dotnet/aspnetcore#33295

--- src/Razor/test/Microsoft.VisualStudio.LanguageServerClient.Razor.Test/DefaultLSPEditorFeatureDetectorTest.cs
+++ src/Razor/test/Microsoft.VisualStudio.LanguageServerClient.Razor.Test/DefaultLSPEditorFeatureDetectorTest.cs
@@ -9,14 +9,29 @@ namespace Microsoft.VisualStudio.LanguageServerClient.Razor
     public class DefaultLSPEditorFeatureDetectorTest
     {
         [Fact]
-        public void IsLSPEditorAvailable_ShouldUseLegacyEditorTrue_ReturnsFalse()
+        public void IsLSPEditorAvailable_ProjectSupported_ReturnsTrue()
+        {
+            // Arrange
+            var featureDetector = new TestLSPEditorFeatureDetector()
+            {
+                ProjectSupportsLSPEditorValue = true,
+            };
+
+            // Act
+            var result = featureDetector.IsLSPEditorAvailable("testMoniker", hierarchy: null);
+
+            // Assert
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void IsLSPEditorAvailable_LegacyEditorEnabled_ReturnsFalse()
         {
             // Arrange
             var featureDetector = new TestLSPEditorFeatureDetector()
             {
                 UseLegacyEditor = true,
-                IsFeatureFlagEnabledValue = true,
-                ProjectSupportsRazorLSPEditorValue = true,
+                ProjectSupportsLSPEditorValue = true,
             };
 
             // Act
@@ -24,57 +39,6 @@ namespace Microsoft.VisualStudio.LanguageServerClient.Razor
 
             // Assert
             Assert.False(result);
-        }
-
-        [Fact]
-        public void IsLSPEditorAvailable_EnvironmentVariableTrue_ReturnsTrue()
-        {
-            // Arrange
-            var featureDetector = new TestLSPEditorFeatureDetector()
-            {
-                EnvironmentFeatureEnabledValue = true,
-                ProjectSupportsRazorLSPEditorValue = true,
-            };
-
-            // Act
-            var result = featureDetector.IsLSPEditorAvailable("testMoniker", hierarchy: null);
-
-            // Assert
-            Assert.True(result);
-        }
-
-        [Fact]
-        public void IsLSPEditorAvailable_FeatureFlagEnabled_ReturnsTrue()
-        {
-            // Arrange
-            var featureDetector = new TestLSPEditorFeatureDetector()
-            {
-                IsFeatureFlagEnabledValue = true,
-                ProjectSupportsRazorLSPEditorValue = true,
-            };
-
-            // Act
-            var result = featureDetector.IsLSPEditorAvailable("testMoniker", hierarchy: null);
-
-            // Assert
-            Assert.True(result);
-        }
-
-        [Fact]
-        public void IsLSPEditorAvailable_IsVSServer_ReturnsTrue()
-        {
-            // Arrange
-            var featureDetector = new TestLSPEditorFeatureDetector()
-            {
-                IsVSServerValue = true,
-                ProjectSupportsRazorLSPEditorValue = true,
-            };
-
-            // Act
-            var result = featureDetector.IsLSPEditorAvailable("testMoniker", hierarchy: null);
-
-            // Assert
-            Assert.True(result);
         }
 
         [Fact]
@@ -84,7 +48,7 @@ namespace Microsoft.VisualStudio.LanguageServerClient.Razor
             var featureDetector = new TestLSPEditorFeatureDetector()
             {
                 IsVSRemoteClientValue = true,
-                ProjectSupportsRazorLSPEditorValue = true,
+                ProjectSupportsLSPEditorValue = true,
             };
 
             // Act
@@ -95,13 +59,12 @@ namespace Microsoft.VisualStudio.LanguageServerClient.Razor
         }
 
         [Fact]
-        public void IsLSPEditorAvailable_IsLiveShareHost_ReturnsFalse()
+        public void IsLSPEditorAvailable_UnsupportedProject_ReturnsFalse()
         {
             // Arrange
             var featureDetector = new TestLSPEditorFeatureDetector()
             {
-                IsLiveShareHostValue = true,
-                ProjectSupportsRazorLSPEditorValue = true,
+                ProjectSupportsLSPEditorValue = false,
             };
 
             // Act
@@ -109,70 +72,6 @@ namespace Microsoft.VisualStudio.LanguageServerClient.Razor
 
             // Assert
             Assert.False(result);
-        }
-
-        [Fact]
-        public void IsLSPEditorAvailable_IsLiveShareGuest_ReturnsFalse()
-        {
-            // Arrange
-            var featureDetector = new TestLSPEditorFeatureDetector()
-            {
-                IsLiveShareGuestValue = true,
-                ProjectSupportsRazorLSPEditorValue = true,
-            };
-
-            // Act
-            var result = featureDetector.IsLSPEditorAvailable("testMoniker", hierarchy: null);
-
-            // Assert
-            Assert.False(result);
-        }
-
-        [Fact]
-        public void IsLSPEditorAvailable_UnknownEnvironment_ReturnsFalse()
-        {
-            // Arrange
-            var featureDetector = new TestLSPEditorFeatureDetector();
-
-            // Act
-            var result = featureDetector.IsLSPEditorAvailable("testMoniker", hierarchy: null);
-
-            // Assert
-            Assert.False(result);
-        }
-
-        [Fact]
-        public void IsLSPEditorAvailable_FeatureFlagEnabled_UnsupportedProject_ReturnsFalse()
-        {
-            // Arrange
-            var featureDetector = new TestLSPEditorFeatureDetector()
-            {
-                IsFeatureFlagEnabledValue = true,
-                ProjectSupportsRazorLSPEditorValue = false,
-            };
-
-            // Act
-            var result = featureDetector.IsLSPEditorAvailable("testMoniker", hierarchy: null);
-
-            // Assert
-            Assert.False(result);
-        }
-
-        [Fact]
-        public void IsLSPEditorAvailable_FeatureFlagEnabled_SupportedProject_ReturnsTrue()
-        {
-            // Arrange
-            var featureDetector = new TestLSPEditorFeatureDetector()
-            {
-                IsFeatureFlagEnabledValue = true,
-                ProjectSupportsRazorLSPEditorValue = true,
-            };
-
-            // Act
-            var result = featureDetector.IsLSPEditorAvailable("testMoniker", hierarchy: null);
-
-            // Assert
-            Assert.True(result);
         }
 
         [Fact]
@@ -208,22 +107,6 @@ namespace Microsoft.VisualStudio.LanguageServerClient.Razor
         }
 
         [Fact]
-        public void IsRemoteClient_FeatureFlagEnabled_ReturnsFalse()
-        {
-            // Arrange
-            var featureDetector = new TestLSPEditorFeatureDetector()
-            {
-                IsFeatureFlagEnabledValue = true,
-            };
-
-            // Act
-            var result = featureDetector.IsRemoteClient();
-
-            // Assert
-            Assert.False(result);
-        }
-
-        [Fact]
         public void IsRemoteClient_UnknownEnvironment_ReturnsFalse()
         {
             // Arrange
@@ -240,35 +123,23 @@ namespace Microsoft.VisualStudio.LanguageServerClient.Razor
         {
             public bool UseLegacyEditor { get; set; }
 
-            public bool EnvironmentFeatureEnabledValue { get; set; }
-
-            public bool IsFeatureFlagEnabledValue { get; set; }
-
             public bool IsLiveShareGuestValue { get; set; }
 
             public bool IsLiveShareHostValue { get; set; }
 
             public bool IsVSRemoteClientValue { get; set; }
 
-            public bool IsVSServerValue { get; set; }
+            public bool ProjectSupportsLSPEditorValue { get; set; }
 
-            public bool ProjectSupportsRazorLSPEditorValue { get; set; }
-
-            private protected override bool ShouldUseLegacyEditor() => UseLegacyEditor;
+            public override bool IsLSPEditorAvailable() => !UseLegacyEditor;
 
             public override bool IsLiveShareHost() => IsLiveShareHostValue;
-
-            private protected override bool EnvironmentFeatureEnabled() => EnvironmentFeatureEnabledValue;
-
-            private protected override bool IsFeatureFlagEnabledCached() => IsFeatureFlagEnabledValue;
 
             private protected override bool IsLiveShareGuest() => IsLiveShareGuestValue;
 
             private protected override bool IsVSRemoteClient() => IsVSRemoteClientValue;
 
-            private protected override bool IsVSServer() => IsVSServerValue;
-
-            private protected override bool ProjectSupportsRazorLSPEditor(string documentMoniker, IVsHierarchy hierarchy) => ProjectSupportsRazorLSPEditorValue;
+            private protected override bool ProjectSupportsLSPEditor(string documentMoniker, IVsHierarchy hierarchy) => ProjectSupportsLSPEditorValue;
         }
     }
 }
