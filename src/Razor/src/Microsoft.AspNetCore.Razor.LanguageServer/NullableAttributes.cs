83a6b2b42e176966ec281c748d0554f148ade68f
David Wengier <david.wengier@microsoft.com>
2021-06-24 21:34:52 +1000

    Fix file header

--- src/Razor/src/Microsoft.AspNetCore.Razor.LanguageServer/NullableAttributes.cs
+++ src/Razor/src/Microsoft.AspNetCore.Razor.LanguageServer/NullableAttributes.cs
@@ -1,6 +1,5 @@
-﻿// Licensed to the .NET Foundation under one or more agreements.
-// The .NET Foundation licenses this file to you under the MIT license.
-// See the LICENSE file in the project root for more information.
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 // This was copied from https://github.com/dotnet/runtime/blob/39b9607807f29e48cae4652cd74735182b31182e/src/libraries/System.Private.CoreLib/src/System/Diagnostics/CodeAnalysis/NullableAttributes.cs
 // and updated to have the scope of the attributes be internal.
