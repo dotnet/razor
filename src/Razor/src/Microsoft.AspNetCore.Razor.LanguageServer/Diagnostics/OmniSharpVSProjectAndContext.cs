37865920f124634e5df354220f07d46145345a93
N. Taylor Mullen <nimullen@microsoft.com>
2021-07-01 10:16:20 -0700

    Properly serialize VS diagnostics during translation.
    - We were attempting to translate VS diagnostics into LSP bound diagnostics. The problem with this is that VSDiagnostics have many more fields than the normal LSP spec'd diagnostic type. Therefore, when we'd attempt to translate VSDiagnostics to their corresponding Razor positions we'd lose all of the VSDiagnostic data. One of the fields we lost was "Project", bringing all of the VS properties over to our O# implementation allows us to have a higher fidelity of diagnostic translation presenting things like the project for a diagnostic. To prevent future mismatches I brought over more than just the project members I brought over everything. This means that C# can build diagnostics however they want and we'll "do the right thing".
    - Also found that when requesting diagnostics from C# we were similarly failing to deserialize a diagnostic into a VSDiagnostic due to lacking extension converters. Was able to replace a pre-existing custom `AddVSExtensionConverters` method with an official one.
    
    ![image](https://user-images.githubusercontent.com/2008729/124187311-7e98c480-da72-11eb-8bd0-16b58b2a787c.png)
    
    Fixes dotnet/aspnetcore#33793

---
+++ src/Razor/src/Microsoft.AspNetCore.Razor.LanguageServer/Diagnostics/OmniSharpVSProjectAndContext.cs
@@ -0,0 +1,26 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+#nullable enable
+
+namespace Microsoft.AspNetCore.Razor.LanguageServer.Diagnostics
+{
+    internal class OmniSharpVSProjectAndContext
+    {
+        /// <summary>
+        /// Gets or sets a human-readable identifier for the project in which the diagnostic was generated.
+        /// </summary>
+        public string? ProjectName { get; set; }
+
+        /// <summary>
+        /// Gets or sets A human-readable identifier for the build context (e.g. Win32 or MacOS)
+        /// in which the diagnostic was generated.
+        /// </summary>
+        public string? Context { get; set; }
+
+        /// <summary>
+        /// Gets or sets the unique identifier for the project in which the diagnostic was generated.
+        /// </summary>
+        public string? ProjectIdentifier { get; set; }
+    }
+}
