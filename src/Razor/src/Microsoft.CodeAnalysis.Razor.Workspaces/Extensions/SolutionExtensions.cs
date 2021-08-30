8fce504a08dfa6b5cac43f5dbef0e41b28ccfae3
Ryan Brandenburg <rybrande@microsoft.com>
2021-08-30 14:09:00 -0700

    Update dependent projects when razor files are added or removed (#4146)
    * Enqueue dependent project updates in more scenarios

---
+++ src/Razor/src/Microsoft.CodeAnalysis.Razor.Workspaces/Extensions/SolutionExtensions.cs
@@ -0,0 +1,34 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the MIT license. See License.txt in the project root for license information.
+
+using System;
+
+#nullable enable
+
+namespace Microsoft.CodeAnalysis.Razor.Workspaces.Extensions
+{
+    internal static class SolutionExtensions
+    {
+        internal static Project GetRequiredProject(this Solution solution, ProjectId? projectId)
+        {
+            if (solution is null)
+            {
+                throw new ArgumentNullException(nameof(solution));
+            }
+
+            if (projectId is null)
+            {
+                throw new ArgumentNullException(nameof(projectId));
+            }
+
+            var project = solution.GetProject(projectId);
+
+            if (project is null)
+            {
+                throw new InvalidOperationException($"The projectId {projectId} did not exist in {solution}.");
+            }
+
+            return project;
+        }
+    }
+}
