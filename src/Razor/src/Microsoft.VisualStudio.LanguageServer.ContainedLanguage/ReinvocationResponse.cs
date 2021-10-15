39323af1fe71da46e000946215e927bab415a3ef
N. Taylor Mullen <nimullen@microsoft.com>
2021-10-15 11:00:21 -0700

    Add LSP request invoker APIs to interact with new delegation APIs.
    - The new APIs now take `ITextBuffer`s as a route to resolve what language servers to talk to.
      - New APIs are nullable decorated to ensure that callers do the right thing.
    - Added new extension methods to help acquire `ITextBuffer`s for consumers. This way they can easily pass in the right buffer based on the language they're operating in.
    
    Part of #5017

---
+++ src/Razor/src/Microsoft.VisualStudio.LanguageServer.ContainedLanguage/ReinvocationResponse.cs
@@ -0,0 +1,21 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the MIT license. See License.txt in the project root for license information.
+
+
+#nullable enable
+
+namespace Microsoft.VisualStudio.LanguageServer.ContainedLanguage
+{
+    internal class ReinvocationResponse<TResponseType>
+    {
+        public ReinvocationResponse(string languageClientName, TResponseType? response)
+        {
+            LanguageClientName = languageClientName;
+            Response = response;
+        }
+
+        public string LanguageClientName { get; }
+
+        public TResponseType? Response { get; }
+    }
+}
