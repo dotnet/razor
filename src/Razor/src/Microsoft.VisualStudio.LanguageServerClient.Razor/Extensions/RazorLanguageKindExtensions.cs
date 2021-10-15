23f6ed3873e0921dbe5a281b736522f0393cea6c
N. Taylor Mullen <nimullen@microsoft.com>
2021-10-15 11:00:21 -0700

    Add LSP request invoker APIs to interact with new delegation APIs.
    - The new APIs now take `ITextBuffer`s as a route to resolve what language servers to talk to.
      - New APIs are nullable decorated to ensure that callers do the right thing.
    - Added new extension methods to help acquire `ITextBuffer`s for consumers. This way they can easily pass in the right buffer based on the language they're operating in.
    
    Part of #5017

--- src/Razor/src/Microsoft.VisualStudio.LanguageServerClient.Razor/Extensions/RazorLanguageKindExtensions.cs
+++ src/Razor/src/Microsoft.VisualStudio.LanguageServerClient.Razor/Extensions/RazorLanguageKindExtensions.cs
@@ -2,6 +2,7 @@
 // Licensed under the MIT license. See License.txt in the project root for license information.
 
 using System;
+using Microsoft.VisualStudio.LanguageServerClient.Razor.HtmlCSharp;
 
 namespace Microsoft.VisualStudio.LanguageServerClient.Razor.Extensions
 {
@@ -18,6 +19,17 @@ namespace Microsoft.VisualStudio.LanguageServerClient.Razor.Extensions
                 RazorLanguageKind.Html => RazorLSPConstants.HtmlLanguageServerName,
                 RazorLanguageKind.Razor => RazorLSPConstants.RazorLanguageServerName,
                 _ => throw new NotImplementedException("A RazorLanguageKind did not have a corresponding ClientName"),
+            };
+        }
+
+        public static LanguageServerKind ToLanguageServerKind(this RazorLanguageKind razorLanguageKind)
+        {
+            return razorLanguageKind switch
+            {
+                RazorLanguageKind.CSharp => LanguageServerKind.CSharp,
+                RazorLanguageKind.Html => LanguageServerKind.Html,
+                RazorLanguageKind.Razor => LanguageServerKind.Razor,
+                _ => throw new NotImplementedException("A RazorLanguageKind did not have a corresponding language server kind"),
             };
         }
     }
