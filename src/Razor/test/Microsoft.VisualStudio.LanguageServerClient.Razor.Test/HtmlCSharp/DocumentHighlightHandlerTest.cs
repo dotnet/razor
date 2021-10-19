ddd2a659fc852da5ec14cd74cca75bdfe8ce1682
N. Taylor Mullen <nimullen@microsoft.com>
2021-10-19 12:36:50 -0700

    Migrate `textDocument/highlight` to new use delegation APIs.
    - Updated the tests to reflect the new dependency on document snapshots and their virtual documents.
    
    Part of #5017

--- src/Razor/test/Microsoft.VisualStudio.LanguageServerClient.Razor.Test/HtmlCSharp/DocumentHighlightHandlerTest.cs
+++ src/Razor/test/Microsoft.VisualStudio.LanguageServerClient.Razor.Test/HtmlCSharp/DocumentHighlightHandlerTest.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 using Microsoft.VisualStudio.LanguageServer.Client;
 using Microsoft.VisualStudio.LanguageServer.ContainedLanguage;
 using Microsoft.VisualStudio.LanguageServer.Protocol;
+using Microsoft.VisualStudio.Text;
 using Moq;
 using Xunit;
 using Range = Microsoft.VisualStudio.LanguageServer.Protocol.Range;
@@ -18,21 +19,34 @@ namespace Microsoft.VisualStudio.LanguageServerClient.Razor.HtmlCSharp
         public DocumentHighlightHandlerTest()
         {
             Uri = new Uri("C:/path/to/file.razor");
+
+            var csharpVirtualDocument = new CSharpVirtualDocumentSnapshot(
+                new Uri("C:/path/to/file.razor.g.cs"),
+                new StringTextSnapshot(String.Empty),
+                hostDocumentSyncVersion: 0);
+            var htmlVirtualDocument = new HtmlVirtualDocumentSnapshot(
+                new Uri("C:/path/to/file.razor__virtual.html"),
+                new StringTextSnapshot(String.Empty),
+                hostDocumentSyncVersion: 0);
+            DocumentSnapshot = new TestLSPDocumentSnapshot(Uri, version: 0, "Some Content", csharpVirtualDocument, htmlVirtualDocument);
+            DocumentManager = new TestDocumentManager();
+            DocumentManager.AddDocument(Uri, DocumentSnapshot);
         }
 
         private Uri Uri { get; }
 
-        private readonly ILanguageClient _languageClient = Mock.Of<ILanguageClient>(MockBehavior.Strict);
+        private LSPDocumentSnapshot DocumentSnapshot { get; }
+
+        private TestDocumentManager DocumentManager { get; }
 
         [Fact]
         public async Task HandleRequestAsync_DocumentNotFound_ReturnsNull()
         {
             // Arrange
-            var documentManager = new TestDocumentManager();
             var requestInvoker = Mock.Of<LSPRequestInvoker>(MockBehavior.Strict);
             var projectionProvider = Mock.Of<LSPProjectionProvider>(MockBehavior.Strict);
             var documentMappingProvider = Mock.Of<LSPDocumentMappingProvider>(MockBehavior.Strict);
-            var highlightHandler = new DocumentHighlightHandler(requestInvoker, documentManager, projectionProvider, documentMappingProvider, LoggerProvider);
+            var highlightHandler = new DocumentHighlightHandler(requestInvoker, new TestDocumentManager(), projectionProvider, documentMappingProvider, LoggerProvider);
             var highlightRequest = new DocumentHighlightParams()
             {
                 TextDocument = new TextDocumentIdentifier() { Uri = Uri },
@@ -50,14 +64,12 @@ namespace Microsoft.VisualStudio.LanguageServerClient.Razor.HtmlCSharp
         public async Task HandleRequestAsync_ProjectionNotFound_ReturnsNull()
         {
             // Arrange
-            var documentManager = new TestDocumentManager();
-            documentManager.AddDocument(Uri, Mock.Of<LSPDocumentSnapshot>(MockBehavior.Strict));
             var requestInvoker = Mock.Of<LSPRequestInvoker>(MockBehavior.Strict);
             var projectionProvider = new Mock<LSPProjectionProvider>(MockBehavior.Strict).Object;
             Mock.Get(projectionProvider).Setup(projectionProvider => projectionProvider.GetProjectionAsync(It.IsAny<LSPDocumentSnapshot>(), It.IsAny<Position>(), CancellationToken.None))
                 .Returns(Task.FromResult<ProjectionResult>(null));
             var documentMappingProvider = Mock.Of<LSPDocumentMappingProvider>(MockBehavior.Strict);
-            var highlightHandler = new DocumentHighlightHandler(requestInvoker, documentManager, projectionProvider, documentMappingProvider, LoggerProvider);
+            var highlightHandler = new DocumentHighlightHandler(requestInvoker, DocumentManager, projectionProvider, documentMappingProvider, LoggerProvider);
             var highlightRequest = new DocumentHighlightParams()
             {
                 TextDocument = new TextDocumentIdentifier() { Uri = Uri },
@@ -77,13 +89,10 @@ namespace Microsoft.VisualStudio.LanguageServerClient.Razor.HtmlCSharp
             // Arrange
             var called = false;
             var expectedHighlight = GetHighlight(5, 5, 5, 5);
-            var documentManager = new TestDocumentManager();
-            documentManager.AddDocument(Uri, Mock.Of<LSPDocumentSnapshot>(d => d.Version == 0, MockBehavior.Strict));
-
             var csharpHighlight = GetHighlight(100, 100, 100, 100);
             var requestInvoker = GetRequestInvoker<DocumentHighlightParams, DocumentHighlight[]>(
                 new[] { csharpHighlight },
-                (method, clientName, highlightParams, ct) =>
+                (textBuffer, method, clientName, highlightParams, ct) =>
                 {
                     Assert.Equal(Methods.TextDocumentDocumentHighlightName, method);
                     Assert.Equal(RazorLSPConstants.RazorCSharpLanguageServerName, clientName);
@@ -98,7 +107,7 @@ namespace Microsoft.VisualStudio.LanguageServerClient.Razor.HtmlCSharp
 
             var documentMappingProvider = GetDocumentMappingProvider(expectedHighlight.Range, 0, RazorLanguageKind.CSharp);
 
-            var highlightHandler = new DocumentHighlightHandler(requestInvoker, documentManager, projectionProvider, documentMappingProvider, LoggerProvider);
+            var highlightHandler = new DocumentHighlightHandler(requestInvoker, DocumentManager, projectionProvider, documentMappingProvider, LoggerProvider);
             var highlightRequest = new DocumentHighlightParams()
             {
                 TextDocument = new TextDocumentIdentifier() { Uri = Uri },
@@ -120,13 +129,11 @@ namespace Microsoft.VisualStudio.LanguageServerClient.Razor.HtmlCSharp
             // Arrange
             var called = false;
             var expectedHighlight = GetHighlight(5, 5, 5, 5);
-            var documentManager = new TestDocumentManager();
-            documentManager.AddDocument(Uri, Mock.Of<LSPDocumentSnapshot>(d => d.Version == 0, MockBehavior.Strict));
 
             var htmlHighlight = GetHighlight(100, 100, 100, 100);
             var requestInvoker = GetRequestInvoker<DocumentHighlightParams, DocumentHighlight[]>(
                 new[] { htmlHighlight },
-                (method, clientName, highlightParams, ct) =>
+                (textBuffer, method, clientName, highlightParams, ct) =>
                 {
                     Assert.Equal(Methods.TextDocumentDocumentHighlightName, method);
                     Assert.Equal(RazorLSPConstants.HtmlLanguageServerName, clientName);
@@ -141,7 +148,7 @@ namespace Microsoft.VisualStudio.LanguageServerClient.Razor.HtmlCSharp
 
             var documentMappingProvider = GetDocumentMappingProvider(expectedHighlight.Range, 0, RazorLanguageKind.Html);
 
-            var highlightHandler = new DocumentHighlightHandler(requestInvoker, documentManager, projectionProvider, documentMappingProvider, LoggerProvider);
+            var highlightHandler = new DocumentHighlightHandler(requestInvoker, DocumentManager, projectionProvider, documentMappingProvider, LoggerProvider);
             var highlightRequest = new DocumentHighlightParams()
             {
                 TextDocument = new TextDocumentIdentifier() { Uri = Uri },
@@ -163,13 +170,11 @@ namespace Microsoft.VisualStudio.LanguageServerClient.Razor.HtmlCSharp
             // Arrange
             var called = false;
             var expectedHighlight = GetHighlight(5, 5, 5, 5);
-            var documentManager = new TestDocumentManager();
-            documentManager.AddDocument(Uri, Mock.Of<LSPDocumentSnapshot>(d => d.Version == 1, MockBehavior.Strict));
 
             var csharpHighlight = GetHighlight(100, 100, 100, 100);
             var requestInvoker = GetRequestInvoker<DocumentHighlightParams, DocumentHighlight[]>(
                 new[] { csharpHighlight },
-                (method, clientName, highlightParams, ct) =>
+                (textBuffer, method, clientName, highlightParams, ct) =>
                 {
                     Assert.Equal(Methods.TextDocumentDocumentHighlightName, method);
                     Assert.Equal(RazorLSPConstants.RazorCSharpLanguageServerName, clientName);
@@ -182,9 +187,9 @@ namespace Microsoft.VisualStudio.LanguageServerClient.Razor.HtmlCSharp
             };
             var projectionProvider = GetProjectionProvider(projectionResult);
 
-            var documentMappingProvider = GetDocumentMappingProvider(expectedHighlight.Range, 0 /* Different from document version (1) */, RazorLanguageKind.CSharp);
+            var documentMappingProvider = GetDocumentMappingProvider(expectedHighlight.Range, 1 /* Different from document version (0) */, RazorLanguageKind.CSharp);
 
-            var highlightHandler = new DocumentHighlightHandler(requestInvoker, documentManager, projectionProvider, documentMappingProvider, LoggerProvider);
+            var highlightHandler = new DocumentHighlightHandler(requestInvoker, DocumentManager, projectionProvider, documentMappingProvider, LoggerProvider);
             var highlightRequest = new DocumentHighlightParams()
             {
                 TextDocument = new TextDocumentIdentifier() { Uri = Uri },
@@ -205,13 +210,11 @@ namespace Microsoft.VisualStudio.LanguageServerClient.Razor.HtmlCSharp
             // Arrange
             var called = false;
             var expectedHighlight = GetHighlight(5, 5, 5, 5);
-            var documentManager = new TestDocumentManager();
-            documentManager.AddDocument(Uri, Mock.Of<LSPDocumentSnapshot>(d => d.Version == 0, MockBehavior.Strict));
 
             var csharpHighlight = GetHighlight(100, 100, 100, 100);
             var requestInvoker = GetRequestInvoker<DocumentHighlightParams, DocumentHighlight[]>(
                 new[] { csharpHighlight },
-                (method, clientName, highlightParams, ct) =>
+                (textBuffer, method, clientName, highlightParams, ct) =>
                 {
                     Assert.Equal(Methods.TextDocumentDocumentHighlightName, method);
                     Assert.Equal(RazorLSPConstants.RazorCSharpLanguageServerName, clientName);
@@ -228,7 +231,7 @@ namespace Microsoft.VisualStudio.LanguageServerClient.Razor.HtmlCSharp
             Mock.Get(documentMappingProvider).Setup(p => p.MapToDocumentRangesAsync(RazorLanguageKind.CSharp, Uri, It.IsAny<Range[]>(), CancellationToken.None))
                 .Returns(Task.FromResult<RazorMapToDocumentRangesResponse>(null));
 
-            var highlightHandler = new DocumentHighlightHandler(requestInvoker, documentManager, projectionProvider, documentMappingProvider, LoggerProvider);
+            var highlightHandler = new DocumentHighlightHandler(requestInvoker, DocumentManager, projectionProvider, documentMappingProvider, LoggerProvider);
             var highlightRequest = new DocumentHighlightParams()
             {
                 TextDocument = new TextDocumentIdentifier() { Uri = Uri },
@@ -251,13 +254,14 @@ namespace Microsoft.VisualStudio.LanguageServerClient.Razor.HtmlCSharp
             return projectionProvider.Object;
         }
 
-        private LSPRequestInvoker GetRequestInvoker<TParams, TResult>(TResult expectedResponse, Action<string, string, TParams, CancellationToken> callback)
+        private LSPRequestInvoker GetRequestInvoker<TParams, TResult>(TResult expectedResponse, Action<ITextBuffer, string, string, TParams, CancellationToken> callback)
         {
             var requestInvoker = new Mock<LSPRequestInvoker>(MockBehavior.Strict);
             requestInvoker
-                .Setup(r => r.ReinvokeRequestOnServerAsync<TParams, TResult>(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<TParams>(), It.IsAny<CancellationToken>()))
+                .Setup(r => r.ReinvokeRequestOnServerAsync<TParams, TResult>(
+                    It.IsAny<ITextBuffer>(), It.IsAny<string>(), It.IsAny<string>(), It.IsAny<TParams>(), It.IsAny<CancellationToken>()))
                 .Callback(callback)
-                .Returns(Task.FromResult(new ReinvokeResponse<TResult>(_languageClient, expectedResponse)));
+                .Returns(Task.FromResult(new ReinvocationResponse<TResult>("LanguageClient", expectedResponse)));
 
             return requestInvoker.Object;
         }
