9baa9e7c9e9322ce62a348358f19db5261bb9d0e
N. Taylor Mullen <nimullen@microsoft.com>
2021-11-17 10:25:32 -0800

    Update src/Razor/src/Microsoft.VisualStudio.LanguageServer.ContainedLanguage/LSPDocumentSynchronizer.cs
    Co-authored-by: Allison Chou <allichou@microsoft.com>

--- src/Razor/src/Microsoft.VisualStudio.LanguageServer.ContainedLanguage/LSPDocumentSynchronizer.cs
+++ src/Razor/src/Microsoft.VisualStudio.LanguageServer.ContainedLanguage/LSPDocumentSynchronizer.cs
@@ -18,7 +18,7 @@ namespace Microsoft.VisualStudio.LanguageServer.ContainedLanguage
         /// <param name="virtualDocument">A generated document to correlate <paramref name="requiredHostDocumentVersion"/>'s for.</param>
         /// <param name="rejectOnNewerParallelRequest">
         /// When <c>true</c> if a second synchronization request for the same virtual document comes in with a newer required host document version all pending synchronization requests will be rejected.
-        /// If <c>false</c>, active synchronization requests will be fulfilled as virtul document buffers get updated. Fulfillment could be a rejection or an approval.
+        /// If <c>false</c>, active synchronization requests will be fulfilled as virtual document buffers get updated. Fulfillment could be a rejection or an approval.
         /// </param>
         /// <param name="cancellationToken"></param>
         /// <returns><c>true</c> if we were able to successfully synchronize; <c>false</c> otherwise.</returns>
