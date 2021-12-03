46ec872da7c0324c62a1df55dc6c944b13a7db13
David Wengier <git@wengier.com>
2021-12-03 15:01:36 +1100

    Nullable

--- src/Razor/src/Microsoft.AspNetCore.Razor.LanguageServer/WrapWithDiv/IWrapWithTagHandler.cs
+++ src/Razor/src/Microsoft.AspNetCore.Razor.LanguageServer/WrapWithDiv/IWrapWithTagHandler.cs
@@ -1,6 +1,8 @@
 ﻿// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the MIT license. See License.txt in the project root for license information.
 
+#nullable enable
+
 using Microsoft.AspNetCore.Razor.LanguageServer.Common;
 using Microsoft.VisualStudio.LanguageServerClient.Razor.WrapWithDiv;
 using OmniSharp.Extensions.JsonRpc;
@@ -8,7 +10,7 @@ using OmniSharp.Extensions.JsonRpc;
 namespace Microsoft.AspNetCore.Razor.LanguageServer.AutoInsert
 {
     [Parallel, Method(LanguageServerConstants.RazorWrapWithTagEndpoint)]
-    internal interface IWrapWithTagHandler : IJsonRpcRequestHandler<WrapWithTagParams, WrapWithTagResponse>
+    internal interface IWrapWithTagHandler : IJsonRpcRequestHandler<WrapWithTagParams, WrapWithTagResponse?>
     {
     }
 }
