98234cfbf9f12303ccd3497dd492cca83d47aeec
Tanay Parikh <taparik@microsoft.com>
2021-12-17 13:53:08 -0800

    Revert "Convert retrieveDebugProxy to ES Module"
    This reverts commit e9bfb0360199f2e6b6c428d6e23532065ad7c438.

--- src/Razor/src/Microsoft.AspNetCore.Razor.VSCode.BlazorWasmDebuggingExtension/scripts/retrieveDebugProxy.js
+++ src/Razor/src/Microsoft.AspNetCore.Razor.VSCode.BlazorWasmDebuggingExtension/scripts/retrieveDebugProxy.js
@@ -3,15 +3,16 @@
  * Licensed under the MIT License. See License.txt in the project root for license information.
  * ------------------------------------------------------------------------------------------ */
 
-import fetch from 'node-fetch';
-import { finished as _finished } from 'stream';
-import extract from 'extract-zip';
-import { existsSync, mkdirSync, createWriteStream, readdirSync, copyFileSync } from 'fs';
-import { promisify } from 'util';
-import { join } from 'path';
-import { tmpdir } from 'os';
+const fetch = require('node-fetch');
+const stream = require('stream');
+const extract = require('extract-zip');
+const fs = require('fs');
+const util = require('util');
+const path = require('path');
+const { spawn } = require("child_process");
+const os = require('os');
 
-var finished = promisify(_finished);
+var finished = util.promisify(stream.finished);
 
 const formatLog = (text) => `[${new Date()}] ${text}`;
 const log = (text) => console.log(formatLog(text));
@@ -21,16 +22,16 @@ async function downloadProxyPackage(version) {
     var nugetUrl = 'https://api.nuget.org/v3-flatcontainer';
     var packageName = 'Microsoft.AspNetCore.Components.WebAssembly.DevServer';
 
-    const tmpDirectory = join(tmpdir(), 'blazorwasm-companion-tmp');
-    if (!existsSync(tmpDirectory)) {
-        mkdirSync(tmpDirectory);
+    const tmpDirectory = path.join(os.tmpdir(), 'blazorwasm-companion-tmp');
+    if (!fs.existsSync(tmpDirectory)){
+        fs.mkdirSync(tmpDirectory);
     }
-    const extractTarget = join(tmpDirectory, `extracted-${packageName}.${version}`);
+    const extractTarget = path.join(tmpDirectory, `extracted-${packageName}.${version}`);
 
     const versionedPackageName = `${packageName.toLowerCase()}.${version}.nupkg`;
     // nuget.org requires the package name be lower-case
     const downloadUrl = `${nugetUrl}/${packageName.toLowerCase()}/${version}/${versionedPackageName}`;
-    const downloadPath = join(tmpDirectory, versionedPackageName);
+    const downloadPath = path.join(tmpDirectory, versionedPackageName);
 
     // Download and save nupkg to disk
     log(`Fetching package from ${downloadUrl}...`);
@@ -40,7 +41,7 @@ async function downloadProxyPackage(version) {
         logError(`Failed to download ${downloadUrl}`);
         return null;
     }
-    const outputStream = createWriteStream(downloadPath);
+    const outputStream = fs.createWriteStream(downloadPath);
     response.body.pipe(outputStream);
 
     // Extract nupkg to extraction directory
@@ -56,19 +57,19 @@ async function copyDebugProxyAssets(version) {
         return;
     }
 
-    var srcDirectory = join(extracted, 'tools', 'BlazorDebugProxy');
+    var srcDirectory = path.join(extracted, 'tools', 'BlazorDebugProxy');
     log(`Looking for installed BlazorDebugProxy in ${srcDirectory}...`);
-    var targetDirectory = join(__dirname, '..', 'BlazorDebugProxy');
+    var targetDirectory = path.join(__dirname, '..', 'BlazorDebugProxy');
     log(`Using ${targetDirectory} as targetDirectory...`);
-    var exists = existsSync(srcDirectory);
+    var exists = fs.existsSync(srcDirectory);
     if (exists) {
         log(`Copying BlazorDebugProxy assets from ${srcDirectory} to bundle...`);
-        readdirSync(srcDirectory).forEach(function(file) {
+        fs.readdirSync(srcDirectory).forEach(function(file) {
             log(`Copying ${file} to target directory...`);
-            copyFileSync(join(srcDirectory, file), join(targetDirectory, file));
+            fs.copyFileSync(path.join(srcDirectory, file), path.join(targetDirectory, file));
         });
     }
 }
 
-import { debugProxyVersion } from '../package.json';
+const debugProxyVersion = require('../package.json').debugProxyVersion;
 copyDebugProxyAssets(debugProxyVersion);
