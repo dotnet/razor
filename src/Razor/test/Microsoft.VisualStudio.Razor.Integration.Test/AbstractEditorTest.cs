7d7a876b3d4a0ef7d4d2a728331c8b55c423f467
Ryan Brandenburg <rybrande@microsoft.com>
2022-01-07 16:23:41 -0800

    Remove unused using

--- src/Razor/test/Microsoft.VisualStudio.Razor.Integration.Test/AbstractEditorTest.cs
+++ src/Razor/test/Microsoft.VisualStudio.Razor.Integration.Test/AbstractEditorTest.cs
@@ -1,9 +1,7 @@
 ﻿// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the MIT license. See License.txt in the project root for license information.
 
-using System.Threading;
 using System.Threading.Tasks;
-using Microsoft.VisualStudio.Razor.Integration.Test.InProcess;
 
 namespace Microsoft.VisualStudio.Razor.Integration.Test
 {
