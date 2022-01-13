76b816b2485f53a6458eccd672ee61467d12e691
Ryan Brandenburg <rybrande@microsoft.com>
2022-01-13 13:58:48 -0800

    Add pipelines for Integration tests on VSMain

---
+++ src/Razor/test/Microsoft.VisualStudio.Razor.IntegrationTests/Extensions/IVsTextViewExtensions.cs
@@ -0,0 +1,23 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the MIT license. See License.txt in the project root for license information.
+
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.Editor;
+using Microsoft.VisualStudio.Text.Editor;
+using Microsoft.VisualStudio.TextManager.Interop;
+using Microsoft.VisualStudio.Threading;
+
+namespace Microsoft.VisualStudio.Razor.IntegrationTests.Extensions
+{
+    internal static class IVsTextViewExtensions
+    {
+        public static async Task<IWpfTextViewHost> GetTextViewHostAsync(this IVsTextView textView, JoinableTaskFactory joinableTaskFactory, CancellationToken cancellationToken)
+        {
+            await joinableTaskFactory.SwitchToMainThreadAsync(cancellationToken);
+
+            ErrorHandler.ThrowOnFailure(((IVsUserData)textView).GetData(DefGuidList.guidIWpfTextViewHost, out var wpfTextViewHost));
+            return (IWpfTextViewHost)wpfTextViewHost;
+        }
+    }
+}
---
+++ src/Razor/test/Microsoft.VisualStudio.Razor.IntegrationTests/IntializeTestFileAttribute.cs
@@ -0,0 +1,28 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the MIT license. See License.txt in the project root for license information.
+
+using System.Reflection;
+using Xunit.Sdk;
+
+namespace Microsoft.VisualStudio.Razor.IntegrationTests
+{
+    public class IntializeTestFileAttribute : BeforeAfterTestAttribute
+    {
+        public override void Before(MethodInfo methodUnderTest)
+        {
+            var typeName = methodUnderTest.ReflectedType.Name;
+            if (typeof(RazorSemanticTokensTests).GetTypeInfo().IsAssignableFrom(methodUnderTest.DeclaringType.GetTypeInfo()))
+            {
+                RazorSemanticTokensTests.FileName = $"Semantic/TestFiles/{typeName}/{methodUnderTest.Name}";
+            }
+        }
+
+        public override void After(MethodInfo methodUnderTest)
+        {
+            if (typeof(RazorSemanticTokensTests).GetTypeInfo().IsAssignableFrom(methodUnderTest.DeclaringType.GetTypeInfo()))
+            {
+                RazorSemanticTokensTests.FileName = null;
+            }
+        }
+    }
+}
