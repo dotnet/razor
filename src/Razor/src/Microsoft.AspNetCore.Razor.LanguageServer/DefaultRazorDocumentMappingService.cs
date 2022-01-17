a5a369a2425eccbd8b3cffe7adbb6417c5e25ff3
David Wengier <git@wengier.com>
2022-01-18 08:07:11 +1100

    Add new mapping function
    For breakpoint spans, we want to map to the next C# position available, if the one passed in is not a valid position. Fortunately this doesn't change the characteristics of the code much, because we were already using "passed the right point" to exit the loop, now we just return information then too.

--- src/Razor/src/Microsoft.AspNetCore.Razor.LanguageServer/DefaultRazorDocumentMappingService.cs
+++ src/Razor/src/Microsoft.AspNetCore.Razor.LanguageServer/DefaultRazorDocumentMappingService.cs
@@ -323,6 +323,12 @@ namespace Microsoft.AspNetCore.Razor.LanguageServer
         }
 
         public override bool TryMapToProjectedDocumentPosition(RazorCodeDocument codeDocument, int absoluteIndex, [NotNullWhen(true)] out Position? projectedPosition, out int projectedIndex)
+            => TryMapToProjectedDocumentPositionInternal(codeDocument, absoluteIndex, nextCSharpPositionOnFailure: false, out projectedPosition, out projectedIndex);
+
+        public override bool TryMapToProjectedDocumentOrNextCSharpPosition(RazorCodeDocument codeDocument, int absoluteIndex, [NotNullWhen(true)] out Position? projectedPosition, out int projectedIndex)
+            => TryMapToProjectedDocumentPositionInternal(codeDocument, absoluteIndex, nextCSharpPositionOnFailure: true, out projectedPosition, out projectedIndex);
+
+        private static bool TryMapToProjectedDocumentPositionInternal(RazorCodeDocument codeDocument, int absoluteIndex, bool nextCSharpPositionOnFailure, [NotNullWhen(true)] out Position? projectedPosition, out int projectedIndex)
         {
             if (codeDocument is null)
             {
@@ -341,18 +347,30 @@ namespace Microsoft.AspNetCore.Razor.LanguageServer
                     var distanceIntoOriginalSpan = absoluteIndex - originalAbsoluteIndex;
                     if (distanceIntoOriginalSpan <= originalSpan.Length)
                     {
-                        var generatedSource = codeDocument.GetCSharpSourceText();
                         projectedIndex = mapping.GeneratedSpan.AbsoluteIndex + distanceIntoOriginalSpan;
-                        var generatedLinePosition = generatedSource.Lines.GetLinePosition(projectedIndex);
-                        projectedPosition = new Position(generatedLinePosition.Line, generatedLinePosition.Character);
+                        projectedPosition = GetProjectedPosition(codeDocument, projectedIndex);
                         return true;
                     }
+                }
+                else if (nextCSharpPositionOnFailure)
+                {
+                    projectedIndex = mapping.GeneratedSpan.AbsoluteIndex;
+                    projectedPosition = GetProjectedPosition(codeDocument, projectedIndex);
+                    return true;
                 }
             }
 
             projectedPosition = default;
             projectedIndex = default;
             return false;
+
+            static Position GetProjectedPosition(RazorCodeDocument codeDocument, int projectedIndex)
+            {
+                var generatedSource = codeDocument.GetCSharpSourceText();
+                var generatedLinePosition = generatedSource.Lines.GetLinePosition(projectedIndex);
+                var projectedPosition = new Position(generatedLinePosition.Line, generatedLinePosition.Character);
+                return projectedPosition;
+            }
         }
 
         public override RazorLanguageKind GetLanguageKind(RazorCodeDocument codeDocument, int originalIndex)
