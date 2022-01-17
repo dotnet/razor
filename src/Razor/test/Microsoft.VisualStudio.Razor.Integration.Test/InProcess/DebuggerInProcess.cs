12dc6834233e9f10f4ac4a2c8d06a1cc8437640b
David Wengier <git@wengier.com>
2022-01-18 10:12:02 +1100

    Add integrtaion tests

---
+++ src/Razor/test/Microsoft.VisualStudio.Razor.Integration.Test/InProcess/DebuggerInProcess.cs
@@ -0,0 +1,53 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the MIT license. See License.txt in the project root for license information.
+
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.Shell.Interop;
+
+namespace Microsoft.VisualStudio.Extensibility.Testing
+{
+    [TestService]
+    internal partial class DebuggerInProcess
+    {
+        public async Task<bool> SetBreakpointAsync(string fileName, int line, int character, CancellationToken cancellationToken)
+        {
+            await JoinableTaskFactory.SwitchToMainThreadAsync(cancellationToken);
+
+            var dte = await GetRequiredGlobalServiceAsync<SDTE, EnvDTE.DTE>(cancellationToken);
+            var debugger = dte.Debugger;
+
+            try
+            {
+                debugger.Breakpoints.Add(File: fileName, Line: line, Column: character);
+            }
+            catch (COMException)
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        public async Task<bool> VerifyBreakpointAsync(string fileName, int line, int character, CancellationToken cancellationToken)
+        {
+            await JoinableTaskFactory.SwitchToMainThreadAsync(cancellationToken);
+
+            var dte = await GetRequiredGlobalServiceAsync<SDTE, EnvDTE.DTE>(cancellationToken);
+            var debugger = dte.Debugger;
+
+            foreach (EnvDTE.Breakpoint breakpoint in debugger.Breakpoints)
+            {
+                if (breakpoint.File == fileName &&
+                    breakpoint.FileLine == line &&
+                    breakpoint.FileColumn == character)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
+}
