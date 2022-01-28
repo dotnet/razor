f50497b2d657d35234f9724b6ad3ac44240f1de9
David Barbet <dabarbet@microsoft.com>
2022-01-27 17:23:18 -0800

    add missing file

---
+++ src/Razor/src/Microsoft.AspNetCore.Razor.LanguageServer/InlineCompletion/RazorInlineCompletionRequest.cs
@@ -0,0 +1,16 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the MIT license. See License.txt in the project root for license information.
+
+#nullable disable
+
+using System.Runtime.Serialization;
+using Newtonsoft.Json;
+
+namespace Microsoft.AspNetCore.Razor.LanguageServer;
+
+internal class RazorInlineCompletionRequest : InlineCompletionRequest
+{
+    [DataMember(Name = "razorLanguageKind")]
+    [JsonProperty(Required = Required.Always)]
+    public RazorLanguageKind Kind { get; set; }
+}
