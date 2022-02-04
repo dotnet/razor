98505faec750de8673c76278a4da903eeed43bba
David Wengier <git@wengier.com>
2022-02-04 16:31:01 +1100

    Integeration tests

---
+++ src/Razor/test/Microsoft.VisualStudio.Razor.IntegrationTests/GoToImplementationTests.cs
@@ -0,0 +1,44 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the MIT license. See License.txt in the project root for license information.
+
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis.Host;
+using Xunit;
+
+namespace Microsoft.VisualStudio.Razor.IntegrationTests
+{
+    public class GoToImplementationTests : AbstractRazorEditorTest
+    {
+        [IdeFact]
+        public async Task GoToImplementation_SameFile()
+        {
+            // Open the file
+            await TestServices.SolutionExplorer.OpenFileAsync(BlazorProjectName, CounterRazorFile, HangMitigatingCancellationToken);
+
+            await TestServices.Editor.PlaceCaretAsync("IncrementCount", charsOffset: -1, HangMitigatingCancellationToken);
+
+            // Act
+            await TestServices.Editor.InvokeGoToImplementationAsync(HangMitigatingCancellationToken);
+
+            // Assert
+            await TestServices.Editor.WaitForCurrentLineTextAsync("private void IncrementCount()", HangMitigatingCancellationToken);
+        }
+
+        [IdeFact]
+        public async Task GoToImplementation_CSharpClass()
+        {
+            // Open the file
+            await TestServices.SolutionExplorer.OpenFileAsync(BlazorProjectName, IndexRazorFile, HangMitigatingCancellationToken);
+
+            // Change text to refer back to Program class
+            await TestServices.Editor.SetTextAsync(@"<SurveyPrompt Title=""@nameof(Program)", HangMitigatingCancellationToken);
+            await TestServices.Editor.PlaceCaretAsync("Program", charsOffset: -1, HangMitigatingCancellationToken);
+
+            // Act
+            await TestServices.Editor.InvokeGoToImplementationAsync(HangMitigatingCancellationToken);
+
+            // Assert
+            await TestServices.Editor.WaitForActiveWindowAsync("Program.cs", HangMitigatingCancellationToken);
+        }
+    }
+}
