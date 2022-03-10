77752069a7f43b3ce121449345237020903bd87e
Andrew Hall <ryzngard@live.com>
2022-03-09 16:42:52 -0800

    Update tests to use lines

--- src/Razor/test/Microsoft.VisualStudio.Razor.IntegrationTests/CodeFoldingTests.cs
+++ src/Razor/test/Microsoft.VisualStudio.Razor.IntegrationTests/CodeFoldingTests.cs
@@ -4,6 +4,7 @@
 using System.Collections.Generic;
 using System.Collections.Immutable;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using Microsoft.CodeAnalysis.Host;
 using Microsoft.VisualStudio.Text;
@@ -15,6 +16,12 @@ namespace Microsoft.VisualStudio.Razor.IntegrationTests
 {
     public class CodeFoldingTests : AbstractRazorEditorTest
     {
+        private struct CollapsibleLines
+        {
+            public int Start { get; set; }
+            public int End { get; set; }
+        }
+
         private async Task<ICollapsible[]> GetOutlineRegionsAsync(Text.Editor.IWpfTextView textView)
         {
             await TestServices.JoinableTaskFactory.SwitchToMainThreadAsync();
@@ -41,53 +48,115 @@ namespace Microsoft.VisualStudio.Razor.IntegrationTests
                 return new Span(start, blockText.Length);
             }).ToImmutableArray();
 
+            var foldableLines = foldableSpans.Select(s => ConvertToLineNumbers(s, textView)).ToImmutableArray();
+
             //
             // Built in retry logic because getting spans can take time.
             //
             var tries = 0;
             const int MaxTries = 20;
-            ImmutableArray<Span> missingSpans;
+            ImmutableArray<CollapsibleLines> missingLines;
+            var outlines = new ICollapsible[0];
             while (tries++ < MaxTries)
             {
-                var outlines = await GetOutlineRegionsAsync(textView);
+                outlines = await GetOutlineRegionsAsync(textView);
 
-                missingSpans = GetMissingExpectedSpans(outlines, foldableSpans, textView);
-                if (missingSpans.Length == 0)
+                (missingLines, var extraLines) = GetOutlineDiff(outlines, foldableSpans, textView);
+                if (missingLines.Length == 0)
                 {
+                    if (extraLines.Length > 0)
+                    {
+                        var extraLineText = PrintLines(extraLines, textView);
+                        var lineText = PrintLines(foldableLines, textView);
+
+                        Assert.False(true, $"Extra Lines: {extraLineText}Expected Lines: {lineText}");
+                    }
+
                     return;
                 }
 
                 await Task.Delay(500);
             }
 
-            Assert.Empty(missingSpans);
-
-            static ImmutableArray<Span> GetMissingExpectedSpans(ICollapsible[] outlines, ImmutableArray<Span> foldableSpans, ITextView textView)
+            if (missingLines.Length > 0)
             {
-                if (outlines.Length < foldableSpans.Length)
-                {
-                    return foldableSpans.ToImmutableArray();
-                }
-
-                var builder = new List<Span>();
-
+                var missingSpanText = PrintLines(missingLines, textView);
                 var spans = outlines.Select(o => o.Extent.GetSpan(textView.TextSnapshot).Span).ToImmutableArray();
-                foreach (var foldableSpan in foldableSpans)
+                var lines = spans.Select(s => ConvertToLineNumbers(s, textView)).ToImmutableArray();
+                var linesText = PrintLines(lines, textView);
+
+                Assert.False(true, $"Missing Lines: {missingSpanText}Actual Lines: {linesText}");
+            }
+            
+            Assert.Empty(missingLines);
+
+            static (ImmutableArray<CollapsibleLines> missingSpans, ImmutableArray<CollapsibleLines> extraSpans) GetOutlineDiff(ICollapsible[] outlines, ImmutableArray<Span> foldableSpans, ITextView textView)
+            {
+                var spans = outlines.Select(o => o.Extent.GetSpan(textView.TextSnapshot).Span).ToImmutableArray();
+                var lines = spans.Select(s => ConvertToLineNumbers(s, textView));
+
+                var foldableLines = foldableSpans.Select(s => ConvertToLineNumbers(s, textView));
+
+                var missingSpans = foldableLines.Except(lines).ToImmutableArray();
+                var extraSpans = lines.Except(foldableLines).ToImmutableArray();
+
+                return (missingSpans, extraSpans);
+            }
+
+            static string PrintLines(ImmutableArray<CollapsibleLines> lines, ITextView textView)
+            {
+                var sb = new StringBuilder();
+                foreach (var line in lines)
                 {
-                    if (!spans.Contains(foldableSpan))
-                    {
-                        builder.Add(foldableSpan);
-                    }
+                    sb.AppendLine();
+
+                    var startLine = textView.TextSnapshot.GetLineFromLineNumber(line.Start);
+                    var endLine = textView.TextSnapshot.GetLineFromLineNumber(line.End);
+                    var span = Span.FromBounds(startLine.Start, endLine.End);
+                    var text = textView.TextSnapshot.GetText(span);
+
+                    sb.AppendLine(span.ToString());
+                    sb.AppendLine(text);
+                    sb.AppendLine();
                 }
 
-                return builder.ToImmutableArray();
+                return sb.ToString();
             }
+
+            static CollapsibleLines ConvertToLineNumbers(Span span, ITextView textView)
+            {
+                return new CollapsibleLines()
+                {
+                    Start = textView.TextSnapshot.GetLineNumberFromPosition(span.Start),
+                    End = textView.TextSnapshot.GetLineNumberFromPosition(span.End)
+                };
+            }
+            
         }
 
         [IdeFact]
-        public async Task CodeFolding_CodeBlock_Default()
+        public async Task CodeFolding_CodeBlock()
         {
-            await TestServices.SolutionExplorer.OpenFileAsync(BlazorProjectName, CounterRazorFile, HangMitigatingCancellationToken);
+            await TestServices.SolutionExplorer.AddFileAsync(
+                BlazorProjectName,
+                "Test.razor",
+                @"
+@page ""/Test""
+
+<PageTitle>Test</PageTitle>
+
+<h1>Test</h1>
+
+@code {
+    private int currentCount = 0;
+
+    private void IncrementCount()
+    {
+        currentCount++;
+    }
+}",
+                open: true,
+                HangMitigatingCancellationToken);
 
             await AssertFoldableBlocksAsync(
 @"@code {
@@ -97,7 +166,11 @@ namespace Microsoft.VisualStudio.Razor.IntegrationTests
     {
         currentCount++;
     }
-}");
+}",
+@"private void IncrementCount()
+    {
+        currentCount++;
+    }");
         }
 
         [IdeFact]
@@ -135,12 +208,12 @@ namespace Microsoft.VisualStudio.Razor.IntegrationTests
     {
         M();
     }
-}
-",
-@"if (true)
-{
-    M();
-}
+}",
+@"
+    if (true)
+    {
+        M();
+    }
 ",
 @"@code {
     string M() => ""M"";
@@ -176,6 +249,9 @@ namespace Microsoft.VisualStudio.Razor.IntegrationTests
 @"@foreach (var s in GetStuff())
 {
     <h2>s</h2>
+}",
+@"@code {
+    string[] GetStuff() => new string[0];
 }");
         }
 
@@ -202,10 +278,17 @@ namespace Microsoft.VisualStudio.Razor.IntegrationTests
                 open: true,
                 HangMitigatingCancellationToken);
 
-            await AssertFoldableBlocksAsync(@"#region Methods
+            await AssertFoldableBlocksAsync(
+@"#region Methods
     void M1() { }
     void M2() { }
-    #endregion");
+    #endregion",
+@"@code {
+    #region Methods
+    void M1() { }
+    void M2() { }
+    #endregion
+}");
         }
 
     }
