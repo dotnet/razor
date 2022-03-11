88fa191d06e017a1ae2af66cb6666fc87838a46e
N. Taylor Mullen <nimullen@microsoft.com>
2022-03-10 16:19:21 -0800

    Add VS4Mac LSP Editor feature detector.
    - The LSP editor feature detector is the key component in the system that decides "should we turn on the LSP editor". Therefore we have to make a VS4mac specific version of the detector because the project types are different (`DotNetProject` instead of `IVsHierarchy`) and the settings / feature flags are different.
    - Added a feature switch for the VS4Mac LSP editor. This doesn't come with UI because adding UI has to be done in the VSMac platform itself + is an entirely different system (preview features). To unblock our inability to add UI / preview features the feature switch should enable us to turn on the new editor. Once we get the green light to add a preview feature we'll add the UI / understanding.
    - Updated the existing `TextBufferProjectService` to understand resolving a documents project. The underlying code for this was already written I just needed to refactor it.
    - Added tests.
    
    #6038

---
+++ src/Razor/src/Microsoft.VisualStudio.Mac.LanguageServices.Razor/VisualStudioMacLSPEditorFeatureDetector.cs
@@ -0,0 +1,116 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the MIT license. See License.txt in the project root for license information.
+
+using System;
+using System.Composition;
+using Microsoft.CodeAnalysis.Razor.Workspaces;
+using Microsoft.VisualStudio.Editor.Razor;
+using MonoDevelop.Core.FeatureConfiguration;
+using MonoDevelop.Ide;
+using MonoDevelop.Projects;
+
+namespace Microsoft.VisualStudio.LanguageServices.Razor
+{
+    [Shared]
+    [Export(typeof(LSPEditorFeatureDetector))]
+    internal class VisualStudioMacLSPEditorFeatureDetector : LSPEditorFeatureDetector
+    {
+        private const string RazorLSPEditorFeatureFlag = "Razor.LSP.Editor";
+        private const string DotNetCoreCSharpCapability = "CSharp&CPS";
+        private const string LegacyRazorEditorCapability = "LegacyRazorEditor";
+
+        private readonly AggregateProjectCapabilityResolver _projectCapabilityResolver;
+        private readonly TextBufferProjectService _textBufferProjectService;
+        private readonly Lazy<bool> _useLegacyEditor;
+
+        [ImportingConstructor]
+        public VisualStudioMacLSPEditorFeatureDetector(
+            AggregateProjectCapabilityResolver projectCapabilityResolver,
+            TextBufferProjectService textBufferProjectService)
+        {
+            _projectCapabilityResolver = projectCapabilityResolver;
+            _textBufferProjectService = textBufferProjectService;
+
+            _useLegacyEditor = new Lazy<bool>(() =>
+            {
+                // TODO: Pull from preview features collection
+
+                if (FeatureSwitchService.IsFeatureEnabled(RazorLSPEditorFeatureFlag) == true)
+                {
+                    return false;
+                }
+
+                return true;
+            });
+        }
+
+        [Obsolete("Test constructor")]
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+        internal VisualStudioMacLSPEditorFeatureDetector()
+#pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+        {
+        }
+
+        public override bool IsLSPEditorAvailable(string documentFilePath, object hierarchy)
+        {
+            if (documentFilePath is null)
+            {
+                return false;
+            }
+
+            if (!IsLSPEditorAvailable())
+            {
+                return false;
+            }
+
+            var dotnetProject = hierarchy as DotNetProject;
+            if (!ProjectSupportsLSPEditor(documentFilePath, dotnetProject))
+            {
+                // Current project hierarchy doesn't support the LSP Razor editor
+                return false;
+            }
+
+            return true;
+        }
+
+        public override bool IsLSPEditorAvailable() => !_useLegacyEditor.Value;
+
+        // LiveShare / CodeSpaces is not supported in VS4Mac
+        public override bool IsRemoteClient() => false;
+
+        // LiveShare / CodeSpaces is not supported in VS4Mac
+        public override bool IsLiveShareHost() => false;
+
+        // Private protected virtual for testing
+        private protected virtual bool ProjectSupportsLSPEditor(string documentFilePath, DotNetProject? project)
+        {
+            if (project is null)
+            {
+                project = _textBufferProjectService.GetHostProject(documentFilePath) as DotNetProject;
+
+                if (project is null)
+                {
+                    return false;
+                }
+            }
+
+            // We alow projects to specifically opt-out of the legacy Razor editor because there are legacy scenarios which would rely on behind-the-scenes
+            // opt-out mechanics to enable the .NET Core editor in non-.NET Core scenarios. Therefore, we need a similar mechanic to continue supporting
+            // those types of scenarios for the new .NET Core Razor editor.
+            if (_projectCapabilityResolver.HasCapability(documentFilePath, project, LegacyRazorEditorCapability))
+            {
+                // CPS project that requires the legacy editor
+                return false;
+            }
+
+            if (_projectCapabilityResolver.HasCapability(documentFilePath, project, DotNetCoreCSharpCapability))
+            {
+                // .NET Core project that supports C#
+                return true;
+            }
+
+            // Not a C# .NET Core project. This typically happens for legacy Razor scenarios
+            return false;
+        }
+    }
+}
---
+++ src/Razor/test/Microsoft.VisualStudio.Mac.LanguageServices.Razor.Test/VisualStudioMacLSPEditorFeatureDetectorTest.cs
@@ -0,0 +1,129 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the MIT license. See License.txt in the project root for license information.
+
+#nullable disable
+
+using MonoDevelop.Projects;
+using Xunit;
+
+namespace Microsoft.VisualStudio.LanguageServices.Razor
+{
+    public class VisualStudioMacLSPEditorFeatureDetectorTest
+    {
+        [Fact]
+        public void IsLSPEditorAvailable_ProjectSupported_ReturnsTrue()
+        {
+            // Arrange
+            var featureDetector = new TestLSPEditorFeatureDetector()
+            {
+                ProjectSupportsLSPEditorValue = true,
+            };
+
+            // Act
+            var result = featureDetector.IsLSPEditorAvailable("testMoniker", hierarchy: null);
+
+            // Assert
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void IsLSPEditorAvailable_LegacyEditorEnabled_ReturnsFalse()
+        {
+            // Arrange
+            var featureDetector = new TestLSPEditorFeatureDetector()
+            {
+                UseLegacyEditor = true,
+                ProjectSupportsLSPEditorValue = true,
+            };
+
+            // Act
+            var result = featureDetector.IsLSPEditorAvailable("testMoniker", hierarchy: null);
+
+            // Assert
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void IsLSPEditorAvailable_IsVSRemoteClient_ReturnsTrue()
+        {
+            // Arrange
+            var featureDetector = new TestLSPEditorFeatureDetector()
+            {
+                IsRemoteClientValue = true,
+                ProjectSupportsLSPEditorValue = true,
+            };
+
+            // Act
+            var result = featureDetector.IsLSPEditorAvailable("testMoniker", hierarchy: null);
+
+            // Assert
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void IsLSPEditorAvailable_UnsupportedProject_ReturnsFalse()
+        {
+            // Arrange
+            var featureDetector = new TestLSPEditorFeatureDetector()
+            {
+                ProjectSupportsLSPEditorValue = false,
+            };
+
+            // Act
+            var result = featureDetector.IsLSPEditorAvailable("testMoniker", hierarchy: null);
+
+            // Assert
+            Assert.False(result);
+        }
+
+        [Fact]
+        public void IsRemoteClient_VSRemoteClient_ReturnsTrue()
+        {
+            // Arrange
+            var featureDetector = new TestLSPEditorFeatureDetector()
+            {
+                IsRemoteClientValue = true,
+            };
+
+            // Act
+            var result = featureDetector.IsRemoteClient();
+
+            // Assert
+            Assert.True(result);
+        }
+
+        [Fact]
+        public void IsRemoteClient_UnknownEnvironment_ReturnsFalse()
+        {
+            // Arrange
+            var featureDetector = new TestLSPEditorFeatureDetector();
+
+            // Act
+            var result = featureDetector.IsRemoteClient();
+
+            // Assert
+            Assert.False(result);
+        }
+
+#pragma warning disable CS0618 // Type or member is obsolete (Test constructor)
+        private class TestLSPEditorFeatureDetector : VisualStudioMacLSPEditorFeatureDetector
+        {
+            public bool UseLegacyEditor { get; set; }
+
+            public bool IsLiveShareHostValue { get; set; }
+
+            public bool IsRemoteClientValue { get; set; }
+
+            public bool ProjectSupportsLSPEditorValue { get; set; }
+
+            public override bool IsLSPEditorAvailable() => !UseLegacyEditor;
+
+            public override bool IsLiveShareHost() => IsLiveShareHostValue;
+
+            public override bool IsRemoteClient() => IsRemoteClientValue;
+
+            private protected override bool ProjectSupportsLSPEditor(string documentFilePath, DotNetProject project) => ProjectSupportsLSPEditorValue;
+        }
+#pragma warning restore CS0618 // Type or member is obsolete (Test constructor)
+    }
+}
