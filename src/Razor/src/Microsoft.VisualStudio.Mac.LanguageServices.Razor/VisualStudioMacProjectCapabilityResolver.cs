c1d89951ae60d53bf2b62121482c9ac1474e8aa0
N. Taylor Mullen <nimullen@microsoft.com>
2022-03-11 10:57:16 -0800

    Add VS4Mac LSP Editor feature detector. (#6170)
    * Add VS4Mac LSP Editor feature detector.
    
    - The LSP editor feature detector is the key component in the system that decides "should we turn on the LSP editor". Therefore we have to make a VS4mac specific version of the detector because the project types are different (`DotNetProject` instead of `IVsHierarchy`) and the settings / feature flags are different.
    - Added a feature switch for the VS4Mac LSP editor. This doesn't come with UI because adding UI has to be done in the VSMac platform itself + is an entirely different system (preview features). To unblock our inability to add UI / preview features the feature switch should enable us to turn on the new editor. Once we get the green light to add a preview feature we'll add the UI / understanding.
    - Updated the existing `TextBufferProjectService` to understand resolving a documents project. The underlying code for this was already written I just needed to refactor it.
    - Added tests.
    
    #6038
    
    * Address code review comments.

---
+++ src/Razor/src/Microsoft.VisualStudio.Mac.LanguageServices.Razor/VisualStudioMacProjectCapabilityResolver.cs
@@ -0,0 +1,52 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the MIT license. See License.txt in the project root for license information.
+
+using System;
+using System.ComponentModel.Composition;
+using Microsoft.CodeAnalysis.Razor;
+using Microsoft.VisualStudio.Editor.Razor;
+using MonoDevelop.Projects;
+
+namespace Microsoft.VisualStudio.LanguageServices.Razor
+{
+    [Export(typeof(ProjectCapabilityResolver))]
+    internal class VisualStudioMacProjectCapabilityResolver : ProjectCapabilityResolver
+    {
+        private readonly RazorLogger _razorLogger;
+
+        [ImportingConstructor]
+        public VisualStudioMacProjectCapabilityResolver(RazorLogger razorLogger)
+        {
+            _razorLogger = razorLogger;
+        }
+
+        public override bool HasCapability(object project, string capability)
+        {
+            if (project is not DotNetProject dotnetProject)
+            {
+                return false;
+            }
+
+            try
+            {
+                var hasCapability = dotnetProject.IsCapabilityMatch(capability);
+                return hasCapability;
+            }
+            catch (NotSupportedException)
+            {
+                // IsCapabilityMatch throws a NotSupportedException if it can't create a
+                // BooleanSymbolExpressionEvaluator COM object
+                _razorLogger.LogWarning("Could not resolve project capability for hierarchy due to NotSupportedException.");
+                return false;
+            }
+            catch (ObjectDisposedException)
+            {
+                // IsCapabilityMatch throws an ObjectDisposedException if the underlying hierarchy has been disposed
+                _razorLogger.LogWarning("Could not resolve project capability for hierarchy due to hierarchy being disposed.");
+                return false;
+            }
+        }
+
+        public override bool HasCapability(string documentFilePath, object project, string capability) => HasCapability(project, capability);
+    }
+}
