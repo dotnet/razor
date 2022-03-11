d3bb22c1798da85e903a725a7a2824f57da78ecd
N. Taylor Mullen <nimullen@microsoft.com>
2022-03-10 16:04:56 -0800

    Add ability to resolve capabilities for projects without document.
    - LiveShare is the only project capability resolver that actually requires documents and I didn't want to put the expectation on all capability resolution mechanisms to require documents. Therefore I added a new overload of the `HasCapability` check that only requires a project.
    - In VS4Mac landscape we also don't need document file paths so this also sets us up for being able to resolve capabilities there.
    
    Part of #6038

--- src/Razor/src/Microsoft.VisualStudio.Editor.Razor/ProjectCapabilityResolver.cs
+++ src/Razor/src/Microsoft.VisualStudio.Editor.Razor/ProjectCapabilityResolver.cs
@@ -5,6 +5,8 @@ namespace Microsoft.VisualStudio.Editor.Razor
 {
     internal abstract class ProjectCapabilityResolver
     {
+        public abstract bool HasCapability(object project, string capability);
+
         public abstract bool HasCapability(string documentFilePath, object project, string capability);
     }
 }
