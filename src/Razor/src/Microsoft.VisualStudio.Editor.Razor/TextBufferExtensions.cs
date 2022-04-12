1337f3dd84d9f9d3b6140d964ee6313cf1ca4229
N. Taylor Mullen <nimullen@microsoft.com>
2022-04-12 13:21:25 -0700

    Add some comments

--- src/Razor/src/Microsoft.VisualStudio.Editor.Razor/TextBufferExtensions.cs
+++ src/Razor/src/Microsoft.VisualStudio.Editor.Razor/TextBufferExtensions.cs
@@ -10,11 +10,22 @@ namespace Microsoft.VisualStudio.Text
 {
     internal static class TextBufferExtensions
     {
+        /// <summary>
+        /// Indicates if a <paramref name="textBuffer"/> has the legacy Razor content type. This is represented by the projection based ASP.NET Core Razor editor.
+        /// </summary>
+        /// <param name="textBuffer">The text buffer to inspect</param>
+        /// <returns><c>true</c> if the text buffers content type represents an ASP.NET Core projection based Razor editor content type.</returns>
         public static bool IsLegacyCoreRazorBuffer(this ITextBuffer textBuffer!!)
         {
             return textBuffer.ContentType.IsOfType(RazorLanguage.CoreContentType) || textBuffer.ContentType.IsOfType(RazorConstants.LegacyCoreContentType);
         }
 
+
+        /// <summary>
+        /// Indicates if a <paramref name="textBuffer"/> has the LSP Razor content type. This is represented by the LSP based ASP.NET Core Razor editor.
+        /// </summary>
+        /// <param name="textBuffer">The text buffer to inspect</param>
+        /// <returns><c>true</c> if the text buffers content type represents an ASP.NET Core LSP based Razor editor content type.
         public static bool IsRazorLSPBuffer(this ITextBuffer textBuffer!!)
         {
             var matchesContentType = textBuffer.ContentType.IsOfType(RazorConstants.RazorLSPContentTypeName);
