2bb71f7f0c04a5b38dbf6b68b94cab208fa262f9
N. Taylor Mullen <nimullen@microsoft.com>
2022-04-12 14:17:00 -0700

    Make ViewCodeCommand work in VS4Mac. (#6271)
    * Make ViewCodeCommand work in VS4Mac.
    
    - The view code command is equivalent to hitting F7 in the Razor editor to view the C# code behind file. The existing implementation relied on shell APIs to open the code behind file where in this PR I abstracted the open document logic to be implemented by both VSWin and VSMac respectively.
    
    Part of #6038
    
    * Update src/Razor/src/Microsoft.VisualStudio.Mac.LanguageServices.Razor/VisualStudioMacDocumentInteractionManager.cs

---
+++ src/Razor/src/Microsoft.VisualStudio.LanguageServices.Razor/VisualStudioWindowsDocumentInteractionManager.cs
@@ -0,0 +1,23 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the MIT license. See License.txt in the project root for license information.
+
+using System.Composition;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.Editor.Razor;
+using Microsoft.VisualStudio.Shell;
+
+namespace Microsoft.VisualStudio.LanguageServices.Razor
+{
+    [Shared]
+    [Export(typeof(DocumentInteractionManager))]
+    internal class VisualStudioWindowsDocumentInteractionManager : DocumentInteractionManager
+    {
+        public override Task OpenDocumentAsync(string filePath, CancellationToken cancellationToken)
+        {
+            VsShellUtilities.OpenDocument(ServiceProvider.GlobalProvider, filePath);
+
+            return Task.CompletedTask;
+        }
+    }
+}
