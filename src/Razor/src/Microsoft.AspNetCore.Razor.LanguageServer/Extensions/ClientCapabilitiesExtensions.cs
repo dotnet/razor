ffbd8de69bdc9cc45a0ff8e41dd84358d232bfdc
N. Taylor Mullen <nimullen@microsoft.com>
2022-04-29 11:09:08 -0700

    Use VS Protocol types for VS extensions.
    - Today we have the `IRegistrationExtension` concept which is used to extend Razor's server capabilities to communicate with VS on what "extra" capabilities are enabled. Examples of this are OnAutoInsert and VS' InlineCompletion for snippets. As we're moving more towards consuming VS' protocol types directly it naturally makes sense to flow VS' client capabilities into these extensions so they can make decisions utilizing VS' types which are a superset of proper LSP.
    - Changed the `Microsoft.AspnetCore.Razor.LanguageServer.Protocol` package to utilize VS' protocol types and not O#'s. This is a big move because it means all of our "shared" implementation types wont leak O# into the client but then also requires the server to have moved to VS protocol types.
    - Added VS internal serializers to the LSP serializer collection. In the end this was harder than I initially thought because the LSP platform APIs don't have a way to add to a list of converters, they only have a way to add to a serializer; unfortunately O# has two sets of converters it looks at so I had to do some hacks to work around this.
    - Now that we're passing VS' capabilities into registration extensions I changed the way we consumed the result of a `GetRegistration` call and if it happens to be `null` we now no-op to not add junk to our server capability collection.
    - Added tests to validate that we convert capabilities correctly.
    
    Fixes #6332

---
+++ src/Razor/src/Microsoft.AspNetCore.Razor.LanguageServer/Extensions/ClientCapabilitiesExtensions.cs
@@ -0,0 +1,19 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the MIT license. See License.txt in the project root for license information.
+
+using Microsoft.VisualStudio.LanguageServer.Protocol;
+using OmniSharp.Extensions.LanguageServer.Protocol.Serialization;
+using OmniSharpClientCapabilities = OmniSharp.Extensions.LanguageServer.Protocol.Client.Capabilities.ClientCapabilities;
+
+namespace Microsoft.AspNetCore.Razor.LanguageServer.Extensions
+{
+    internal static class ClientCapabilitiesExtensions
+    {
+        public static VSInternalClientCapabilities ToVSClientCapabilities(this OmniSharpClientCapabilities omniSharpClientCapabilities, LspSerializer serializer)
+        {
+            var serializedOmniSharpClientCapabilities = serializer.SerializeObject(omniSharpClientCapabilities);
+            var vsClientCapabilities = serializer.DeserializeObject<VSInternalClientCapabilities>(serializedOmniSharpClientCapabilities);
+            return vsClientCapabilities;
+        }
+    }
+}
