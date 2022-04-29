40b1a8e6fdb54d91bd73c17c066b657d61ffaf1b
N. Taylor Mullen <nimullen@microsoft.com>
2022-04-29 15:11:36 -0700

    Migrate colorized tooltip generation to VS protocol types.
    - Prior to this change colorized tooltips were implemented by faking the existing of VS types in our language server (see all of the deleted files in the `Tooltip` directory in this changeset) and those fakes would then be serialized to look like VS types. This changeset moves from the fake types to the actual types.
    - Our tooltip generation factories went away from O# types entirely in favor of protocol types. In reaction the completion resolve and hover endpoints needed to change since those are consumers of the tooltips.
        - In each of the endpoints we needed to convert from VS -> O# types. This is only temporary until we move the endpoints to also consume VS protocol types.
        - One extra change that I found important as part of the consumption process was to pull the client capabilities introspection from inside of the tooltip service into the endpoint.
    - Updated existing tests to utilize the new protocol types.
    
    Fixes #6331

--- src/Razor/src/Microsoft.AspNetCore.Razor.LanguageServer/Hover/OmniSharpVSHover.cs
+++ src/Razor/src/Microsoft.AspNetCore.Razor.LanguageServer/Hover/OmniSharpVSHover.cs
@@ -1,6 +1,7 @@
 ﻿// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the MIT license. See License.txt in the project root for license information.
 
+using Microsoft.VisualStudio.LanguageServer.Protocol;
 using Newtonsoft.Json;
 using HoverModel = OmniSharp.Extensions.LanguageServer.Protocol.Models.Hover;
 
@@ -9,6 +10,7 @@ namespace Microsoft.AspNetCore.Razor.LanguageServer.Hover
     internal record OmniSharpVSHover : HoverModel
     {
         [JsonProperty("_vs_rawContent")]
+        [JsonConverter(typeof(ObjectContentConverter))]
         public object? RawContent { get; init; }
     }
 }
