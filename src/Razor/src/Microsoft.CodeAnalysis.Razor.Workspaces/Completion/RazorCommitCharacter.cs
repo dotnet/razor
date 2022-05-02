9396c2191687cc52b778984aa66688416a273ba4
N. Taylor Mullen <nimullen@microsoft.com>
2022-05-02 10:44:55 -0700

    Add VS "like" commit characters to Razor's completion item. (#6342)
    * Add VS "like" commit characters to Razor's completion item.
    
    - The new `RazorCommitCharacter` construct allows us to specify whether or not to insert the commit character on insert. Unfortunately this PR doesn't go alllll the way and remove the hacked code to inject VS commit characters onto our O# completion items yet (that's coming).
    - Updated call sites to take in the new Razor commit character construct
    - Added a new `RazorCompletionOptions` record struct that can indicate when snippest are supported. In all relevant scenarios they are; however, in the legacy editor they're not so we need an option to know if we should use snippet based commit characters or non-snippet based commit characters.
    - Updated tests
    
    Fixes #6339
    
    * Add helper method for going from string[] -> RCC[]

---
+++ src/Razor/src/Microsoft.CodeAnalysis.Razor.Workspaces/Completion/RazorCommitCharacter.cs
@@ -0,0 +1,24 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the MIT license. See License.txt in the project root for license information.
+
+using System.Collections.Generic;
+
+namespace Microsoft.CodeAnalysis.Razor.Completion
+{
+    internal sealed record RazorCommitCharacter(string Character, bool Insert = true)
+    {
+        public static IReadOnlyList<RazorCommitCharacter> FromArray(IReadOnlyList<string> characters) => FromArray(characters, insert: true);
+
+        public static IReadOnlyList<RazorCommitCharacter> FromArray(IReadOnlyList<string> characters, bool insert)
+        {
+            var converted = new RazorCommitCharacter[characters.Count];
+
+            for (var i = 0; i < characters.Count; i++)
+            {
+                converted[i] = new RazorCommitCharacter(characters[i], insert);
+            }
+
+            return converted;
+        }
+    }
+}
