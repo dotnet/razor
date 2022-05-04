886268230f5bf47c89cdd2ff27b26da7d627988b
N. Taylor Mullen <nimullen@microsoft.com>
2022-05-04 16:29:27 -0700

    Properly capture all VS client capabilities.
    - Prior to this I hadn't realized that we were only capturing VS items that were specifically "overloaded" in our "Platform agnostic XYZ" types. This meant that when we were trying to inspect other items on the client capability collection we'd fail to understand them because O# would see the initialize request, deserialize into their own ClientCapabilities at which point all data representing VS' would be lost. To overcome this I created a new `ICaptureJson` type that informs our platform extension converters to maintain the JToken representation of objects.
    - Updated our test to be pretty exhaustive to ensure we're capturing all information.
    
    Part of #6328

---
+++ src/Razor/src/Microsoft.AspNetCore.Razor.LanguageServer/Serialization/ICaptureJson.cs
@@ -0,0 +1,17 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the MIT license. See License.txt in the project root for license information.
+
+using Newtonsoft.Json.Linq;
+
+namespace Microsoft.AspNetCore.Razor.LanguageServer.Serialization
+{
+    /// <summary>
+    /// Used by an interface to capture the <see cref="JToken"/> representation of a request so no data loss occurs. This should be used sparringly
+    /// because converting to a <see cref="JToken"/> and then an actual type is not as efficient.
+    /// </summary>
+    internal interface ICaptureJson
+    {
+        public JToken Json { get; set; }
+    }
+}
+
