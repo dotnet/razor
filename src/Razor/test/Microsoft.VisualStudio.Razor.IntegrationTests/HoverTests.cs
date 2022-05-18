a85b786a9b35d3efb4ed52bc89da874999428b70
Ryan Brandenburg <rybrande@microsoft.com>
2022-05-18 09:30:16 -0700

    Add Hover Integration Test

---
+++ src/Razor/test/Microsoft.VisualStudio.Razor.IntegrationTests/HoverTests.cs
@@ -0,0 +1,30 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the MIT license. See License.txt in the project root for license information.
+
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Microsoft.VisualStudio.Razor.IntegrationTests
+{
+    public class HoverTests : AbstractRazorEditorTest
+    {
+        [IdeFact]
+        public async Task Hover_OverTagHelperElementAsync()
+        {
+            // Open the file
+            await TestServices.SolutionExplorer.OpenFileAsync(RazorProjectConstants.BlazorProjectName, RazorProjectConstants.CounterRazorFile, ControlledHangMitigatingCancellationToken);
+
+            await TestServices.Editor.PlaceCaretAsync("PageTitle", charsOffset: -1, ControlledHangMitigatingCancellationToken);
+
+            var position = await TestServices.Editor.GetCaretPositionAsync(ControlledHangMitigatingCancellationToken);
+
+            // Act
+            var hoverResults = await TestServices.Editor.HoverAsync(position, ControlledHangMitigatingCancellationToken);
+
+            // Assert
+            var hoverResult = Assert.Single(hoverResults);
+            var expectedResult = hoverResult;
+            Assert.Equal(expectedResult, hoverResult);
+        }
+    }
+}
---
+++ src/Razor/test/Microsoft.VisualStudio.Razor.IntegrationTests/InProcess/EditorInProcess_Hover.cs
@@ -0,0 +1,34 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the MIT license. See License.txt in the project root for license information.
+
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.Language.Intellisense;
+using Microsoft.VisualStudio.Text;
+
+namespace Microsoft.VisualStudio.Extensibility.Testing
+{
+    internal partial class EditorInProcess
+    {
+        public async Task<IEnumerable<object>> HoverAsync(int position, CancellationToken cancellationToken)
+        {
+            var hoverService = await GetComponentModelServiceAsync<IAsyncQuickInfoBroker>(cancellationToken);
+
+            var view = await GetActiveTextViewAsync(cancellationToken);
+
+            var trackingPoint = view.TextSnapshot.CreateTrackingPoint(position, PointTrackingMode.Negative);
+            var quickInfoSession = await hoverService.TriggerQuickInfoAsync(view, trackingPoint, QuickInfoSessionOptions.TrackMouse, cancellationToken);
+            quickInfoSession.StateChanged += QuickInfoSession_StateChanged;
+
+            return quickInfoSession.Content;
+
+            void QuickInfoSession_StateChanged(object sender, QuickInfoSessionStateChangedEventArgs e)
+            {
+                throw new NotImplementedException();
+            }
+        }
+    }
+}
+
