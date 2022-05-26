0d3bf763066ecc6ca28d6cd7013423f5affe6508
Ryan Brandenburg <rybrande@microsoft.com>
2022-05-25 17:00:28 -0700

    Add extra files

---
+++ src/Razor/src/Microsoft.AspNetCore.Razor.LanguageServer/Extensions/SumTypeExtensions.cs
@@ -0,0 +1,60 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the MIT license. See License.txt in the project root for license information.
+
+using System;
+using System.Linq;
+using Microsoft.VisualStudio.LanguageServer.Protocol;
+
+namespace Microsoft.AspNetCore.Razor.LanguageServer.Extensions
+{
+    internal static class SumTypeExtensions
+    {
+        internal static int Count(this SumType<TextDocumentEdit[], SumType<TextDocumentEdit, CreateFile, RenameFile, DeleteFile>[]> sumType)
+        {
+            if (sumType.TryGetFirst(out var textDocumentEdit))
+            {
+                return textDocumentEdit.Length;
+            }
+            else if (sumType.TryGetSecond(out var edits))
+            {
+                return edits.Length;
+            }
+            else
+            {
+                throw new NotImplementedException("Shouldn't be possible");
+            }
+        }
+
+        internal static SumType<TextDocumentEdit, CreateFile, RenameFile, DeleteFile> First(this SumType<TextDocumentEdit[], SumType<TextDocumentEdit, CreateFile, RenameFile, DeleteFile>[]> sumType)
+        {
+            if (sumType.TryGetFirst(out var textDocumentEdits))
+            {
+                return textDocumentEdits.First();
+            }
+            else if (sumType.TryGetSecond(out var edits))
+            {
+                return edits.First();
+            }
+            else
+            {
+                throw new NotImplementedException("Shouldn't be possible");
+            }
+        }
+
+        internal static SumType<TextDocumentEdit, CreateFile, RenameFile, DeleteFile> Last(this SumType<TextDocumentEdit[], SumType<TextDocumentEdit, CreateFile, RenameFile, DeleteFile>[]> sumType)
+        {
+            if (sumType.TryGetFirst(out var textDocumentEdits))
+            {
+                return textDocumentEdits.Last();
+            }
+            else if (sumType.TryGetSecond(out var edits))
+            {
+                return edits.Last();
+            }
+            else
+            {
+                throw new NotImplementedException("Shouldn't be possible");
+            }
+        }
+    }
+}
