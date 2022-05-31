5a16d035ba3d2cf263ac8d6dd005857aafe56c93
Ryan Brandenburg <rybrande@microsoft.com>
2022-05-31 15:27:16 -0700

    Move IDidChangeConfigurationHandler to VSContracts

--- src/Razor/src/Microsoft.AspNetCore.Razor.LanguageServer/IRazorMapToDocumentRangesHandler.cs
+++ src/Razor/src/Microsoft.AspNetCore.Razor.LanguageServer/IRazorMapToDocumentRangesHandler.cs
@@ -1,15 +1,13 @@
 ﻿// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the MIT license. See License.txt in the project root for license information.
 
-#nullable disable
-
 using Microsoft.AspNetCore.Razor.LanguageServer.Common;
 using OmniSharp.Extensions.JsonRpc;
 
 namespace Microsoft.AspNetCore.Razor.LanguageServer
 {
     [Parallel, Method(LanguageServerConstants.RazorMapToDocumentRangesEndpoint)]
-    internal interface IRazorMapToDocumentRangesHandler : IJsonRpcRequestHandler<RazorMapToDocumentRangesParams, RazorMapToDocumentRangesResponse>
+    internal interface IRazorMapToDocumentRangesHandler : IJsonRpcRequestHandler<RazorMapToDocumentRangesParams, RazorMapToDocumentRangesResponse?>
     {
     }
 }
