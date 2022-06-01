6f9352b718b53fa7fcf42212cbde574af4901793
Ryan Brandenburg <rybrande@microsoft.com>
2022-06-01 15:15:45 -0700

    Move Definition to VS Contracts (#6476)
    * Move Definition to VS Contracts

---
+++ src/Razor/src/Microsoft.AspNetCore.Razor.LanguageServer/EndpointContracts/IDefinitionEndpoint.cs
@@ -0,0 +1,18 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the MIT license. See License.txt in the project root for license information.
+
+using Microsoft.VisualStudio.LanguageServer.Protocol;
+using OmniSharp.Extensions.JsonRpc;
+using DefinitionResult = Microsoft.VisualStudio.LanguageServer.Protocol.SumType<
+    Microsoft.VisualStudio.LanguageServer.Protocol.VSInternalLocation,
+    Microsoft.VisualStudio.LanguageServer.Protocol.VSInternalLocation[],
+    Microsoft.VisualStudio.LanguageServer.Protocol.DocumentLink[]>;
+
+namespace Microsoft.AspNetCore.Razor.LanguageServer.EndpointContracts
+{
+    [Parallel, Method(Methods.TextDocumentDefinitionName)]
+    internal interface IDefinitionEndpoint : IJsonRpcRequestHandler<DefinitionParamsBridge, DefinitionResult?>,
+        IRegistrationExtension
+    {
+    }
+}
