e64118652343390983289d020839dc3269838fa2
Ryan Brandenburg <rybrande@microsoft.com>
2022-06-01 10:37:56 -0700

    Add missing attributes

--- src/Razor/src/Microsoft.AspNetCore.Razor.LanguageServer/EndpointContracts/IDefinitionEndpoint.cs
+++ src/Razor/src/Microsoft.AspNetCore.Razor.LanguageServer/EndpointContracts/IDefinitionEndpoint.cs
@@ -1,6 +1,7 @@
 ﻿// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the MIT license. See License.txt in the project root for license information.
 
+using Microsoft.VisualStudio.LanguageServer.Protocol;
 using OmniSharp.Extensions.JsonRpc;
 using DefinitionResult = Microsoft.VisualStudio.LanguageServer.Protocol.SumType<
     Microsoft.VisualStudio.LanguageServer.Protocol.VSInternalLocation,
@@ -9,6 +10,7 @@ using DefinitionResult = Microsoft.VisualStudio.LanguageServer.Protocol.SumType<
 
 namespace Microsoft.AspNetCore.Razor.LanguageServer.EndpointContracts
 {
+    [Parallel, Method(Methods.TextDocumentDefinitionName)]
     internal interface IDefinitionEndpoint : IJsonRpcRequestHandler<DefinitionParams, DefinitionResult?>,
         IRegistrationExtension
     {
