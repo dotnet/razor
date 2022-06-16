d5f2ce7d5a05ab2447af212ab8beb480d0bec335
Fredric Silberberg <frsilb@microsoft.com>
2022-06-16 15:08:07 -0700

    Pull TagHelperDescriptor hashcode calculation into the tooling layer
    The TagHelperDescriptorCache uses the hashcode of TagHelperDescriptors as a means of avoiding unnecesary deserialization of types. However, this means that the hashcode of the tag itself is used as an object equality check, and is a significant performance bottleneck in the compiler when tags are stored in a hashtable. This pulls the calculation of a cache ID into a dedicated method in the tooling layer, so we can remove the unnecessary elements from hashcode calculation for taghelpers for the general case.

--- src/Razor/src/Microsoft.CodeAnalysis.Razor.Workspaces/TagHelperDescriptorCache.cs
+++ src/Razor/src/Microsoft.CodeAnalysis.Razor.Workspaces/TagHelperDescriptorCache.cs
@@ -3,7 +3,10 @@
 
 #nullable disable
 
+using System;
+using System.Collections.Generic;
 using Microsoft.AspNetCore.Razor.Language;
+using Microsoft.Extensions.Internal;
 
 namespace Microsoft.CodeAnalysis.Razor
 {
@@ -17,5 +20,117 @@ namespace Microsoft.CodeAnalysis.Razor
 
         internal static void Set(int hashCode, TagHelperDescriptor descriptor) =>
             s_cachedTagHelperDescriptors.Set(hashCode, descriptor);
+
+#nullable enable
+        internal static int GetTagHelperDescriptorCacheId(TagHelperDescriptor descriptor)
+        {
+            var hash = HashCodeCombiner.Start();
+            hash.Add(descriptor.Kind, StringComparer.Ordinal);
+            hash.Add(descriptor.AssemblyName, StringComparer.Ordinal);
+            hash.Add(descriptor.Name, StringComparer.Ordinal);
+            hash.Add(descriptor.DisplayName, StringComparer.Ordinal);
+            hash.Add(descriptor.CaseSensitive ? 1 : 0);
+
+            CalculateForProperty(ref hash, descriptor.BoundAttributes, CalculateBoundAttributeCacheId);
+            CalculateForProperty(ref hash, descriptor.TagMatchingRules, CalculateTagMatchingRuleCacheId);
+            CalculateForProperty(ref hash, descriptor.AllowedChildTags, CalculateAllowedChildTagsCacheId);
+
+            if (descriptor.Diagnostics != null)
+            {
+                for (var i = 0; i < descriptor.Diagnostics.Count; i++)
+                {
+                    hash.Add(descriptor.Diagnostics[i]);
+                }
+            }
+
+            if (descriptor.Metadata != null)
+            {
+                foreach (var kvp in descriptor.Metadata)
+                {
+                    hash.Add(kvp.Value, StringComparer.Ordinal);
+                }
+            }
+
+            return hash.CombinedHash;
+        }
+
+        private static void CalculateForProperty<T>(ref HashCodeCombiner hash, IReadOnlyList<T> property, CalculationDelegate<T> calculationDelegate)
+        {
+            if (property != null)
+            {
+                for (var i = 0; i < property.Count; i++)
+                {
+                    calculationDelegate(ref hash, property[i]);
+                }
+            }
+        }
+
+        private delegate void CalculationDelegate<T>(ref HashCodeCombiner hash, T? value);
+
+        private static void CalculateBoundAttributeCacheId(ref HashCodeCombiner hash, BoundAttributeDescriptor? descriptor)
+        {
+            if (descriptor is null)
+            {
+                return;
+            }
+
+            hash.Add(descriptor.Kind, StringComparer.Ordinal);
+            hash.Add(descriptor.Name, StringComparer.Ordinal);
+            hash.Add(descriptor.IsEditorRequired);
+            hash.Add(descriptor.TypeName, comparer: StringComparer.Ordinal);
+            hash.Add(descriptor.Documentation, StringComparer.Ordinal);
+
+            CalculateForProperty(ref hash, descriptor.BoundAttributeParameters, CalculateBoundAttributeParameterCacheId);
+            ComparerUtilities.AddToHash(ref hash, descriptor.Metadata, StringComparer.Ordinal, StringComparer.Ordinal);
+        }
+
+        private static void CalculateBoundAttributeParameterCacheId(ref HashCodeCombiner hash, BoundAttributeParameterDescriptor? descriptor)
+        {
+            if (descriptor is null)
+            {
+                return;
+            }
+
+            hash.Add(descriptor.Kind, StringComparer.Ordinal);
+            hash.Add(descriptor.Name, StringComparer.Ordinal);
+            hash.Add(descriptor.TypeName, StringComparer.Ordinal);
+            hash.Add(descriptor.Metadata?.Count);
+        }
+
+        private static void CalculateTagMatchingRuleCacheId(ref HashCodeCombiner hash, TagMatchingRuleDescriptor? rule)
+        {
+            if (rule is null)
+            {
+                return;
+            }
+
+            hash.Add(rule.TagName, StringComparer.Ordinal);
+            hash.Add(rule.ParentTag, StringComparer.Ordinal);
+
+            CalculateForProperty(ref hash, rule.Attributes, CalculateRequiredAttributeDescriptorCacheId);
+        }
+
+        private static void CalculateRequiredAttributeDescriptorCacheId(ref HashCodeCombiner combiner, RequiredAttributeDescriptor? descriptor)
+        {
+            if (descriptor is null)
+            {
+                return;
+            }
+
+            var hash = HashCodeCombiner.Start();
+            hash.Add(descriptor.Name, StringComparer.Ordinal);
+            hash.Add(descriptor.Value, StringComparer.Ordinal);
+        }
+
+        private static void CalculateAllowedChildTagsCacheId(ref HashCodeCombiner hash, AllowedChildTagDescriptor? descriptor)
+        {
+            if (descriptor is null)
+            {
+                return;
+            }
+
+            hash.Add(descriptor.Name, StringComparer.Ordinal);
+        }
     }
 }
+
