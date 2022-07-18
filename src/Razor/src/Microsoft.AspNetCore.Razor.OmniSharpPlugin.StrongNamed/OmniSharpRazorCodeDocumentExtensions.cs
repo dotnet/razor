5d8c3448eac7e3e116822ae4b56019b40df36745
N. Taylor Mullen <nimullen@microsoft.com>
2022-07-18 13:04:01 -0700

    Replace GeneratedDocumentContainer & its dependencies. (#6602)
    * Replace GeneratedDocumentContainer & deps.
    
    - This changeset effectively removes all `GeneratedDocumentContainer` assets. Let me explain what effectively this means. Prior to this whenever a Document was parsed there was a decoupled system that attempted to understand the "latest document" and capture its generated output in one of these generated containers. That decoupled system was also responsibel for publishing generated C#/HTML to the client. Meaning, whenever it saw a change to its generated C# container or HTML container it'd push a notification to the client to update C#/HTML buffers. The issue with the old system is that it was so decoupled from the rest of the tooling system that things like "open/closing" files couldn't be understood meaning we couldn't reset document content when closing an unsaved document. For another in-depth understanding of this see [this](https://github.com/dotnet/razor-tooling/issues/6043#issuecomment-1185969172).
        - The new system presents itself as a service (the `GeneratedDocumentSynchronizer`) and as documents change via project updates, document additions or document changes this synchronizer is notified via the `DocumentProccessedListener` infra. Upon notification it can then re-dispatch those "processed" updates to a C#/HTML publisher. This new path enables us to be open/close document aware (document changes) so that upon closing/open we can re-push C#/HTML to the client effectively fixing this issue (#6043).
    - One huge benefit of moving this way is the sheer amount of code we can delete. Was able to remove our generated document containers, the factory for them, the store for them, their tests, and all their old fragile consumption bits.
    - A piece that's missing from this PR is that we used to capture references to generated C# computations via the `ReferenceOutputCapturingContainer` which is now removed. It had to be removed because it can no longer hook into these generated document stores (they don't exist). I plan to replace this in follow up PRs. As a point-in-time measurement the perf may still be better due to how much less work we need to do; however, we do cache miss more so that's debatable. In my follow ups we are actually able to significantly exceed pre-existing perf measurements.
    - Had to update our O# plugin handling of document processed listeners upon removal of the generated containers.
    - Added new tests and updated pre-existing ones.
    
    Mostly fixes #6043 with the exception of the slight cache miss regression (coming soon)
    
    * Address code review comments.
    
    - Removes even more code!

---
+++ src/Razor/src/Microsoft.AspNetCore.Razor.OmniSharpPlugin.StrongNamed/OmniSharpRazorCodeDocumentExtensions.cs
@@ -0,0 +1,17 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the MIT license. See License.txt in the project root for license information.
+
+using Microsoft.AspNetCore.Razor.Language;
+using Microsoft.CodeAnalysis.Razor.Workspaces.Extensions;
+using Microsoft.CodeAnalysis.Text;
+
+namespace Microsoft.AspNetCore.Razor.OmniSharpPlugin
+{
+    public static class OmniSharpRazorCodeDocumentExtensions
+    {
+        public static SourceText GetInternalCSharpSourceText(this RazorCodeDocument codeDocument)
+        {
+            return codeDocument.GetCSharpSourceText();
+        }
+    }
+}
