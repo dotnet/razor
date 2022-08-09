5ba235563d229c2e43a21acb2fa233cda0824ba7
Andrew Hall <ryzngard@live.com>
2022-08-09 12:42:08 -0700

    Move delegated types to a single file

--- src/Razor/src/Microsoft.AspNetCore.Razor.LanguageServer.Protocol/DelegatedHoverParams.cs
+++
@@ -1,8 +0,0 @@
-﻿// Copyright (c) .NET Foundation. All rights reserved.
-// Licensed under the MIT license. See License.txt in the project root for license information.
-
-using Microsoft.VisualStudio.LanguageServer.Protocol;
-
-namespace Microsoft.AspNetCore.Razor.LanguageServer.Protocol;
-
-internal record DelegatedHoverParams(VersionedTextDocumentIdentifier HostDocument, Position ProjectedPosition, RazorLanguageKind ProjectedKind);
