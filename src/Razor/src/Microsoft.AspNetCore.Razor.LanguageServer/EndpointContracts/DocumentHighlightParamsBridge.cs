656d78cf8f1ed2f6cb5c81ea1c070e80fcd71cbd
David Wengier <david.wengier@microsoft.com>
2022-08-19 13:14:27 +1000

    Permalink

--- src/Razor/src/Microsoft.AspNetCore.Razor.LanguageServer/EndpointContracts/DocumentHighlightParamsBridge.cs
+++ src/Razor/src/Microsoft.AspNetCore.Razor.LanguageServer/EndpointContracts/DocumentHighlightParamsBridge.cs
@@ -17,10 +17,10 @@ namespace Microsoft.AspNetCore.Razor.LanguageServer.EndpointContracts;
 /// supported in the middle layer. No, I don't know what "the middle layer" is either. I guess we're in
 /// the middle layer right now? No idea. Taylor probably knows, I'm just guessing based on this comment:
 /// <![CDATA[
-/// https://devdiv.visualstudio.com/DevDiv/_git/VSLanguageServerClient?path=/src/product/RemoteLanguage/Impl/JsonRpcExtensionMethods.cs&version=GBdevelop&line=142&lineEnd=143&lineStartColumn=1&lineEndColumn=1&lineStyle=plain&_a=contents
+/// https://devdiv.visualstudio.com/DevDiv/_git/VSLanguageServerClient?path=/src/product/RemoteLanguage/Impl/JsonRpcExtensionMethods.cs&version=GC2d1fe0c5ab668d49b0404bed9cd658a78f696165&line=142&lineEnd=143&lineStartColumn=1&lineEndColumn=1&lineStyle=plain&_a=contents
 /// ]]>
 /// In case that link goes somewhere bad now, the comment says "IProgress&lt;T&gt; is not supported in MiddleLayer".
-/// 
+///
 /// I should mention, its not a complete guess, because we can't ignore the fact that things don't work if
 /// we inherit DocumentHighlightParams,  and they do work if we don't.
 /// As I always say: "If it hurts when you do that, don't do that".
