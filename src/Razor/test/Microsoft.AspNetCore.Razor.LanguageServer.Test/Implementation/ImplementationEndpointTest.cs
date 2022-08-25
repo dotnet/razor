a93a0a960e5b3592c87edf0e86c50d59cda18e74
David Wengier <david.wengier@microsoft.com>
2022-08-25 17:10:56 +1000

    Extract common test base
    Apparently we don't even need capabilities??

--- src/Razor/test/Microsoft.AspNetCore.Razor.LanguageServer.Test/Implementation/ImplementationEndpointTest.cs
+++ src/Razor/test/Microsoft.AspNetCore.Razor.LanguageServer.Test/Implementation/ImplementationEndpointTest.cs
@@ -8,29 +8,18 @@ using System.Collections.Immutable;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Razor.Language;
-using Microsoft.AspNetCore.Razor.LanguageServer.Common;
-using Microsoft.AspNetCore.Razor.LanguageServer.Completion;
 using Microsoft.AspNetCore.Razor.LanguageServer.EndpointContracts;
 using Microsoft.AspNetCore.Razor.LanguageServer.Extensions;
-using Microsoft.AspNetCore.Razor.LanguageServer.Protocol;
-using Microsoft.AspNetCore.Razor.LanguageServer.Test;
-using Microsoft.AspNetCore.Razor.LanguageServer.Test.Common;
 using Microsoft.AspNetCore.Razor.Test.Common;
-using Microsoft.CodeAnalysis.Razor.Workspaces;
 using Microsoft.CodeAnalysis.Razor.Workspaces.Extensions;
 using Microsoft.CodeAnalysis.Testing;
 using Microsoft.CodeAnalysis.Text;
 using Microsoft.VisualStudio.LanguageServer.Protocol;
-using Moq;
-using OmniSharp.Extensions.JsonRpc;
-using OmniSharp.Extensions.LanguageServer.Protocol.Server;
 using Xunit;
 
 namespace Microsoft.AspNetCore.Razor.LanguageServer.Implementation
 {
-    [UseExportProvider]
-    public class ImplementationEndpointTest : TagHelperServiceTestBase
+    public class ImplementationEndpointTest : SingleServerDelegatingEndpointTestBase
     {
         [Fact]
         public async Task Handle_SingleServer_CSharp_Method()
@@ -106,8 +95,22 @@ namespace Microsoft.AspNetCore.Razor.LanguageServer.Implementation
             var codeDocument = CreateCodeDocument(output);
             var razorFilePath = "C:/path/to/file.razor";
 
+            await CreateLanguageServerAsync(codeDocument, razorFilePath).ConfigureAwait(false);
+
+            var endpoint = new ImplementationEndpoint(DocumentContextFactory, LanguageServerFeatureOptions, DocumentMappingService, LanguageServer, TestLoggerFactory.Instance);
+
+            codeDocument.GetSourceText().GetLineAndOffset(cursorPosition, out var line, out var offset);
+            var request = new ImplementationParamsBridge
+            {
+                TextDocument = new TextDocumentIdentifier
+                {
+                    Uri = new Uri(razorFilePath)
+                },
+                Position = new Position(line, offset)
+            };
+
             // Act
-            var result = await GetImplementationResultAsync(codeDocument, razorFilePath, cursorPosition);
+            var result = await endpoint.Handle(request, CancellationToken.None);
 
             // Assert
             Assert.NotNull(result.First);
@@ -124,87 +127,6 @@ namespace Microsoft.AspNetCore.Razor.LanguageServer.Implementation
                 Assert.Equal(expectedRange, location.Range);
 
                 i++;
-            }
-        }
-
-        private async Task<SumType<Location[], VSInternalReferenceItem[]>> GetImplementationResultAsync(RazorCodeDocument codeDocument, string razorFilePath, int cursorPosition)
-        {
-            var realLanguageServerFeatureOptions = new DefaultLanguageServerFeatureOptions();
-
-            var csharpSourceText = codeDocument.GetCSharpSourceText();
-            var serverCapabilities = new ServerCapabilities()
-            {
-                ImplementationProvider = true
-            };
-            var csharpDocumentUri = new Uri(realLanguageServerFeatureOptions.GetRazorCSharpFilePath(razorFilePath));
-            var csharpServer = await CSharpTestLspServerHelpers.CreateCSharpLspServerAsync(csharpSourceText, csharpDocumentUri, serverCapabilities, razorSpanMappingService: null).ConfigureAwait(false);
-            await csharpServer.OpenDocumentAsync(csharpDocumentUri, csharpSourceText.ToString()).ConfigureAwait(false);
-
-            var documentContextFactory = new TestDocumentContextFactory(razorFilePath, codeDocument, version: 1337);
-            var languageServerFeatureOptions = Mock.Of<LanguageServerFeatureOptions>(options =>
-                options.SupportsFileManipulation == true &&
-                options.SingleServerSupport == true &&
-                options.CSharpVirtualDocumentSuffix == realLanguageServerFeatureOptions.CSharpVirtualDocumentSuffix &&
-                options.HtmlVirtualDocumentSuffix == realLanguageServerFeatureOptions.HtmlVirtualDocumentSuffix
-                , MockBehavior.Strict);
-            var languageServer = new ImplementationLanguageServer(csharpServer, csharpDocumentUri);
-            var documentMappingService = new DefaultRazorDocumentMappingService(languageServerFeatureOptions, documentContextFactory, LoggerFactory);
-
-            var endpoint = new ImplementationEndpoint(documentContextFactory, languageServerFeatureOptions, documentMappingService, languageServer, TestLoggerFactory.Instance);
-
-            codeDocument.GetSourceText().GetLineAndOffset(cursorPosition, out var line, out var offset);
-            var request = new ImplementationParamsBridge
-            {
-                TextDocument = new TextDocumentIdentifier
-                {
-                    Uri = new Uri(razorFilePath)
-                },
-                Position = new Position(line, offset)
-            };
-
-            return await endpoint.Handle(request, CancellationToken.None);
-        }
-
-        private class ImplementationLanguageServer : ClientNotifierServiceBase
-        {
-            private readonly CSharpTestLspServer _csharpServer;
-            private readonly Uri _csharpDocumentUri;
-
-            public ImplementationLanguageServer(CSharpTestLspServer csharpServer, Uri csharpDocumentUri)
-            {
-                _csharpServer = csharpServer;
-                _csharpDocumentUri = csharpDocumentUri;
-            }
-
-            public override OmniSharp.Extensions.LanguageServer.Protocol.Models.InitializeParams ClientSettings { get; }
-
-            public override Task OnStarted(ILanguageServer server, CancellationToken cancellationToken)
-            {
-                return Task.CompletedTask;
-            }
-
-            public override Task<IResponseRouterReturns> SendRequestAsync(string method)
-            {
-                throw new NotImplementedException();
-            }
-
-            public async override Task<IResponseRouterReturns> SendRequestAsync<T>(string method, T @params)
-            {
-                Assert.Equal(RazorLanguageServerCustomMessageTargets.RazorImplementationEndpointName, method);
-                var implementationParams = Assert.IsType<DelegatedPositionParams>(@params);
-
-                var implementationRequest = new TextDocumentPositionParams()
-                {
-                    TextDocument = new TextDocumentIdentifier()
-                    {
-                        Uri = _csharpDocumentUri
-                    },
-                    Position = implementationParams.ProjectedPosition
-                };
-
-                var result = await _csharpServer.ExecuteRequestAsync<TextDocumentPositionParams, SumType<Location[], VSInternalReferenceItem[]>>(Methods.TextDocumentImplementationName, implementationRequest, CancellationToken.None);
-
-                return new TestResponseRouterReturn(result);
             }
         }
     }
---
+++ src/Razor/test/Microsoft.AspNetCore.Razor.LanguageServer.Test/SingleServerDelegatingEndpointTestBase.cs
@@ -0,0 +1,103 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the MIT license. See License.txt in the project root for license information.
+
+#nullable disable
+
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Razor.Language;
+using Microsoft.AspNetCore.Razor.LanguageServer.Common;
+using Microsoft.AspNetCore.Razor.LanguageServer.Protocol;
+using Microsoft.AspNetCore.Razor.LanguageServer.Test;
+using Microsoft.AspNetCore.Razor.LanguageServer.Test.Common;
+using Microsoft.AspNetCore.Razor.Test.Common;
+using Microsoft.CodeAnalysis.Razor.Workspaces;
+using Microsoft.CodeAnalysis.Razor.Workspaces.Extensions;
+using Microsoft.VisualStudio.LanguageServer.Protocol;
+using Moq;
+using OmniSharp.Extensions.JsonRpc;
+using OmniSharp.Extensions.LanguageServer.Protocol.Server;
+using Xunit;
+
+namespace Microsoft.AspNetCore.Razor.LanguageServer
+{
+    [UseExportProvider]
+    public abstract class SingleServerDelegatingEndpointTestBase : LanguageServerTestBase
+    {
+        internal DocumentContextFactory DocumentContextFactory { get; private set; }
+        internal LanguageServerFeatureOptions LanguageServerFeatureOptions { get; private set; }
+        internal ClientNotifierServiceBase LanguageServer { get; private set; }
+        internal RazorDocumentMappingService DocumentMappingService { get; private set; }
+
+        protected async Task CreateLanguageServerAsync(RazorCodeDocument codeDocument, string razorFilePath)
+        {
+            var realLanguageServerFeatureOptions = new DefaultLanguageServerFeatureOptions();
+
+            var csharpSourceText = codeDocument.GetCSharpSourceText();
+            var csharpDocumentUri = new Uri(realLanguageServerFeatureOptions.GetRazorCSharpFilePath(razorFilePath));
+            var csharpServer = await CSharpTestLspServerHelpers.CreateCSharpLspServerAsync(csharpSourceText, csharpDocumentUri, new ServerCapabilities(), razorSpanMappingService: null).ConfigureAwait(false);
+            await csharpServer.OpenDocumentAsync(csharpDocumentUri, csharpSourceText.ToString()).ConfigureAwait(false);
+
+            DocumentContextFactory = new TestDocumentContextFactory(razorFilePath, codeDocument, version: 1337);
+            LanguageServerFeatureOptions = Mock.Of<LanguageServerFeatureOptions>(options =>
+                options.SupportsFileManipulation == true &&
+                options.SingleServerSupport == true &&
+                options.CSharpVirtualDocumentSuffix == realLanguageServerFeatureOptions.CSharpVirtualDocumentSuffix &&
+                options.HtmlVirtualDocumentSuffix == realLanguageServerFeatureOptions.HtmlVirtualDocumentSuffix
+                , MockBehavior.Strict);
+            LanguageServer = new TestLanguageServer(csharpServer, csharpDocumentUri);
+            DocumentMappingService = new DefaultRazorDocumentMappingService(LanguageServerFeatureOptions, DocumentContextFactory, LoggerFactory);
+        }
+
+        private class TestLanguageServer : ClientNotifierServiceBase
+        {
+            private readonly CSharpTestLspServer _csharpServer;
+            private readonly Uri _csharpDocumentUri;
+
+            public TestLanguageServer(CSharpTestLspServer csharpServer, Uri csharpDocumentUri)
+            {
+                _csharpServer = csharpServer;
+                _csharpDocumentUri = csharpDocumentUri;
+            }
+
+            public override OmniSharp.Extensions.LanguageServer.Protocol.Models.InitializeParams ClientSettings { get; }
+
+            public override Task OnStarted(ILanguageServer server, CancellationToken cancellationToken)
+            {
+                return Task.CompletedTask;
+            }
+
+            public override Task<IResponseRouterReturns> SendRequestAsync(string method)
+            {
+                throw new NotImplementedException();
+            }
+
+            public async override Task<IResponseRouterReturns> SendRequestAsync<T>(string method, T @params)
+            {
+                return await (method switch
+                {
+                    RazorLanguageServerCustomMessageTargets.RazorImplementationEndpointName => HandleImplementationAsync(@params),
+                    _ => throw new NotImplementedException($"I don't know how to handle the '{method}' method.")
+                });
+            }
+
+            private async Task<IResponseRouterReturns> HandleImplementationAsync<T>(T @params)
+            {
+                var delegatedParams = Assert.IsType<DelegatedPositionParams>(@params);
+                var delegatedRequest = new TextDocumentPositionParams()
+                {
+                    TextDocument = new TextDocumentIdentifier()
+                    {
+                        Uri = _csharpDocumentUri
+                    },
+                    Position = delegatedParams.ProjectedPosition
+                };
+
+                var result = await _csharpServer.ExecuteRequestAsync<TextDocumentPositionParams, SumType<Location[], VSInternalReferenceItem[]>>(Methods.TextDocumentImplementationName, delegatedRequest, CancellationToken.None);
+
+                return new TestResponseRouterReturn(result);
+            }
+        }
+    }
+}
