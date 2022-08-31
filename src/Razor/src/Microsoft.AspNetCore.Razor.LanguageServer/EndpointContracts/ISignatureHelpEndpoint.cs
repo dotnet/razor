e1dbfc7ccb7d21cae7fdce10834e100510b7a627
David Wengier <david.wengier@microsoft.com>
2022-08-31 11:44:06 +1000

    Actually listen to LSP requests (and tell the server that we can) (#6765)

---
+++ src/Razor/src/Microsoft.AspNetCore.Razor.LanguageServer/EndpointContracts/ISignatureHelpEndpoint.cs
@@ -0,0 +1,15 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the MIT license. See License.txt in the project root for license information.
+
+using Microsoft.VisualStudio.LanguageServer.Protocol;
+using OmniSharp.Extensions.JsonRpc;
+using LS = Microsoft.VisualStudio.LanguageServer.Protocol;
+
+namespace Microsoft.AspNetCore.Razor.LanguageServer.EndpointContracts
+{
+    [Parallel, Method(Methods.TextDocumentSignatureHelpName)]
+    internal interface ISignatureHelpEndpoint : IJsonRpcRequestHandler<SignatureHelpParamsBridge, LS.SignatureHelp?>,
+        IRegistrationExtension
+    {
+    }
+}
