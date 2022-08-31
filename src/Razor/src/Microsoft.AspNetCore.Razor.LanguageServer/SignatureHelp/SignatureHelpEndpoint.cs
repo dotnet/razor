4ba5619e3bcc7093377470d86848ac0bc45adbb1
David Wengier <david.wengier@microsoft.com>
2022-08-31 11:06:07 +1000

    Actually listen to LSP requests (and tell the server that we can)

--- src/Razor/src/Microsoft.AspNetCore.Razor.LanguageServer/SignatureHelp/SignatureHelpEndpoint.cs
+++ src/Razor/src/Microsoft.AspNetCore.Razor.LanguageServer/SignatureHelp/SignatureHelpEndpoint.cs
@@ -1,17 +1,18 @@
 ﻿// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the MIT license. See License.txt in the project root for license information.
 
+using System.Threading;
+using Microsoft.AspNetCore.Razor.LanguageServer.Common;
 using Microsoft.AspNetCore.Razor.LanguageServer.EndpointContracts;
+using Microsoft.AspNetCore.Razor.LanguageServer.Protocol;
 using Microsoft.CodeAnalysis.Razor.Workspaces;
 using Microsoft.Extensions.Logging;
-using Microsoft.AspNetCore.Razor.LanguageServer.Common;
-using Microsoft.AspNetCore.Razor.LanguageServer.Protocol;
-using System.Threading;
+using Microsoft.VisualStudio.LanguageServer.Protocol;
 using LS = Microsoft.VisualStudio.LanguageServer.Protocol;
 
 namespace Microsoft.AspNetCore.Razor.LanguageServer.SignatureHelp
 {
-    internal class SignatureHelpEndpoint : AbstractRazorDelegatingEndpoint<SignatureHelpParamsBridge, LS.SignatureHelp>
+    internal class SignatureHelpEndpoint : AbstractRazorDelegatingEndpoint<SignatureHelpParamsBridge, LS.SignatureHelp>, ISignatureHelpEndpoint
     {
         public SignatureHelpEndpoint(
             DocumentContextFactory documentContextFactory,
@@ -21,11 +22,22 @@ namespace Microsoft.AspNetCore.Razor.LanguageServer.SignatureHelp
             ILoggerFactory loggerProvider)
             : base(documentContextFactory, languageServerFeatureOptions, documentMappingService, languageServer, loggerProvider.CreateLogger<SignatureHelpEndpoint>())
         {
-
         }
 
         /// <inheritdoc />
         protected override string CustomMessageTarget => RazorLanguageServerCustomMessageTargets.RazorSignatureHelpEndpointName;
+
+        public RegistrationExtensionResult? GetRegistration(VSInternalClientCapabilities clientCapabilities)
+        {
+            const string ServerCapability = "signatureHelpProvider";
+            var option = new SignatureHelpOptions()
+            {
+                TriggerCharacters = new[] { "(", ",", "<" },
+                RetriggerCharacters = new[] { ">", ")" }
+            };
+
+            return new RegistrationExtensionResult(ServerCapability, option);
+        }
 
         /// <inheritdoc />
         protected override IDelegatedParams CreateDelegatedParams(SignatureHelpParamsBridge request, DocumentContext documentContext, Projection projection, CancellationToken cancellationToken)
