dotnet/razor

commit 04e076ecb67e5f8e5bdca3661266c96533365444

Generate Sbom for VS insertion (#6545)

build.cmd
@@ -1,3 +1,8 @@
 @echo off
-powershell -ExecutionPolicy ByPass -NoProfile -command "& """%~dp0eng\common\Build.ps1""" -restore -build -pack %*"
-exit /b %ErrorLevel%
+setlocal
+
+set _args=%*
+if "%~1"=="-?" set _args=-help
+
+powershell -ExecutionPolicy ByPass -NoProfile -Command "& '%~dp0eng\common\build.ps1'" %_args%
+exit /b %ERRORLEVEL%

eng/common/restore-internal-tools.yml
@@ -1,0 +1,15 @@
+steps:
+  - task: NuGetAuthenticate@0
+    inputs:
+      nuGetServiceConnections: 'devdiv/dotnet-core-internal-tooling'
+      forceReinstallCredentialProvider: true
+
+  - script: $(Build.SourcesDirectory)\build.cmd
+            -ci
+            -restore
+            -projects $(Build.SourcesDirectory)/eng/common/internal/Tools.csproj
+            /bl:$(Build.SourcesDirectory)/artifacts/log/$(_BuildConfig)/RestoreInternal.binlog
+            /v:normal
+    displayName: Restore internal tools
+    condition: and(succeeded(), ne(variables['_skipRestoreInternalTools'], 'true'))
+

azure-pipelines.yml
@@ -153,6 +153,7 @@ stages:
               /p:ManifestBuildBranch=$(Build.SourceBranchName)
               /p:ManifestBuildNumber=$(Build.BuildNumber)
               /p:VisualStudioDropName=Products/dotnet/razor-tooling/$(Build.SourceBranchName)/$(Build.BuildNumber)
+              /p:GenerateSbom=true
 
         - ${{ if and(ne(variables['System.TeamProject'], 'public'), notin(variables['Build.Reason'], 'PullRequest')) }}:
           - group: DotNet-Blob-Feed
@@ -171,6 +172,8 @@ stages:
           inputs:
             command: custom
             arguments: 'locals all -clear'
+        - ${{ if ne(variables['System.TeamProject'], 'public') }}:
+          - template: /eng/common/restore-internal-tools.yml
 
         - powershell: ./eng/scripts/InstallProcDump.ps1
           displayName: Install ProcDump

commit b12c5c66abf14969fbf65de56252dba354cfca34

Fix build.cmd to always pass -restore and -build
Fixes #6790

This fixes an issue that was introduced with #6545 where `build.cmd` would no
longer pass the `-restore` and `-build` arguments by default. Previously,
`build.cmd` also passed `-pack`, but that appears to be failing, so I've
chosen to leave that out for now.

build.cmd
@@ -1,7 +1,7 @@
 @echo off
 setlocal
 
-set _args=%*
+set _args=-restore -build %*
 if "%~1"=="-?" set _args=-help
 
 powershell -ExecutionPolicy ByPass -NoProfile -Command "& '%~dp0eng\common\build.ps1'" %_args%

eng/restore-internal-tools.yml
@@ -4,8 +4,7 @@ steps:
       nuGetServiceConnections: 'devdiv/dotnet-core-internal-tooling'
       forceReinstallCredentialProvider: true
 
-  - script: $(Build.SourcesDirectory)\build.cmd
-            -ci
+  - script: $(Build.SourcesDirectory)/eng/CIBuild.cmd
             -restore
             -projects $(Build.SourcesDirectory)/eng/common/internal/Tools.csproj
             /bl:$(Build.SourcesDirectory)/artifacts/log/$(_BuildConfig)/RestoreInternal.binlog