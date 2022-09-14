e95d525fc5179972c87fa0c65ee5ed633c2293d4
David Wengier <david.wengier@microsoft.com>
2022-09-14 15:21:14 +1000

    Rename method to match behaviour
    Now the method name, the comment, and the actual logic, are all in wonderful harmony

--- src/Razor/src/Microsoft.AspNetCore.Razor.LanguageServer/Formatting/CSharpFormattingPassBase.cs
+++ src/Razor/src/Microsoft.AspNetCore.Razor.LanguageServer/Formatting/CSharpFormattingPassBase.cs
@@ -324,7 +324,7 @@ namespace Microsoft.AspNetCore.Razor.LanguageServer.Formatting
             }
 
             if (IsRazorComment() ||
-                IsInHtmlTag() ||
+                IsInHtmlAttributeValue() ||
                 IsInDirectiveWithNoKind() ||
                 IsInSingleLineDirective() ||
                 IsImplicitExpression() ||
@@ -367,7 +367,7 @@ namespace Microsoft.AspNetCore.Razor.LanguageServer.Formatting
                 return false;
             }
 
-            bool IsInHtmlTag()
+            bool IsInHtmlAttributeValue()
             {
                 // E.g, (| is position)
                 //
