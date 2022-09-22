3129ca899fc547427a7e32ab800added6e2429f3
Dustin Campbell <dustin@teamcampbell.org>
2022-09-22 13:38:12 -0700

    Introduce TestBase abstract class that provides common services
    This change introduces an abstract base class to be used by all
    test classes. This base class provides support for many common
    test needs, including:
    
        * A JoinableTaskFactory that uses the xUnit test thread as the main
          thread.
        * A CancellationToken that signals when the test has finished
          running and xUnit disposes the test class.
        * An ILoggerFactory implementation that writes to an xUnit
          ITestOutputHelper.
        * An easy way to register IDisposable objects that should be
          disposed when the test completes.
        * An easy way to register IAsyncDisposable objects that should be
          disposed when the test completes.
        * An implementation of IAsyncLifetime that test classes can
          override to provide custom initialization and disposal for tests.

---
+++ src/Razor/test/Microsoft.AspNetCore.Razor.Test.Common/Logging/TestOutputLogger.NoOpDisposable.cs
@@ -0,0 +1,15 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the MIT license. See License.txt in the project root for license information.
+
+using System;
+
+namespace Microsoft.AspNetCore.Razor.Test.Common.Logging;
+
+internal sealed class NoOpDisposable : IDisposable
+{
+    public static IDisposable Instance { get; } = new NoOpDisposable();
+
+    public void Dispose()
+    {
+    }
+}
