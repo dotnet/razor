4e478a1a95c09f567eb57860bdfc9f6bd0737df8
Ryan Brandenburg <rybrande@microsoft.com>
2022-09-22 11:48:53 -0700

    Cleanup

--- src/Razor/test/Microsoft.AspNetCore.Razor.LanguageServer.Test/RazorLanguageEndpointTest.cs
+++ src/Razor/test/Microsoft.AspNetCore.Razor.LanguageServer.Test/RazorLanguageEndpointTest.cs
@@ -24,7 +24,6 @@ namespace Microsoft.AspNetCore.Razor.LanguageServer
         public RazorLanguageEndpointTest()
         {
             MappingService = new DefaultRazorDocumentMappingService(TestLanguageServerFeatureOptions.Instance, new TestDocumentContextFactory(), LoggerFactory);
-
         }
 
         private RazorDocumentMappingService MappingService { get; }
