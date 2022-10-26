2d9669179b452250d30cd895adf5c7cc5a90cdcb
Ryan Brandenburg <rybrande@microsoft.com>
2022-10-26 10:04:37 -0700

    More Codebase

--- src/Razor/src/Microsoft.VisualStudio.RazorExtension.Dependencies/AssemblyCodeBases.cs
+++ src/Razor/src/Microsoft.VisualStudio.RazorExtension.Dependencies/AssemblyCodeBases.cs
@@ -52,6 +52,12 @@ using Microsoft.VisualStudio.Shell;
     OldVersionUpperBound = "4.5.0.0",
     NewVersion = "4.5.0.0")]
 [assembly: ProvideBindingRedirection(
+    AssemblyName = "Microsoft.CodeAnalysis.ExternalAccess.FSharp",
+    GenerateCodeBase = true,
+    OldVersionLowerBound = "4.4.0.0",
+    OldVersionUpperBound = "4.5.0.0",
+    NewVersion = "4.5.0.0")]
+[assembly: ProvideBindingRedirection(
     AssemblyName = "Microsoft.CodeAnalysis.InteractiveHost",
     GenerateCodeBase = true,
     OldVersionLowerBound = "4.4.0.0",
@@ -77,6 +83,12 @@ using Microsoft.VisualStudio.Shell;
     NewVersion = "4.5.0.0")]
 [assembly: ProvideBindingRedirection(
     AssemblyName = "Microsoft.CodeAnalysis.VisualBasic.Features",
+    GenerateCodeBase = true,
+    OldVersionLowerBound = "4.4.0.0",
+    OldVersionUpperBound = "4.5.0.0",
+    NewVersion = "4.5.0.0")]
+[assembly: ProvideBindingRedirection(
+    AssemblyName = "Microsoft.CodeAnalysis.VisualBasic.Workspaces",
     GenerateCodeBase = true,
     OldVersionLowerBound = "4.4.0.0",
     OldVersionUpperBound = "4.5.0.0",
