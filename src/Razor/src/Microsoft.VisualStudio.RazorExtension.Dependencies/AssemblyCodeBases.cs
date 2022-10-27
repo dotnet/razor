b32b88a4475b3c68c3580068229a6b0d8bfb37cf
Ryan Brandenburg <rybrande@microsoft.com>
2022-10-27 13:50:00 -0700

    Turn off Roslyn OOP for test scenarios

--- src/Razor/src/Microsoft.VisualStudio.RazorExtension.Dependencies/AssemblyCodeBases.cs
+++ src/Razor/src/Microsoft.VisualStudio.RazorExtension.Dependencies/AssemblyCodeBases.cs
@@ -52,12 +52,6 @@ using Microsoft.VisualStudio.Shell;
     OldVersionUpperBound = "4.5.0.0",
     NewVersion = "4.5.0.0")]
 [assembly: ProvideBindingRedirection(
-    AssemblyName = "Microsoft.CodeAnalysis.ExternalAccess.FSharp",
-    GenerateCodeBase = true,
-    OldVersionLowerBound = "4.4.0.0",
-    OldVersionUpperBound = "4.5.0.0",
-    NewVersion = "4.5.0.0")]
-[assembly: ProvideBindingRedirection(
     AssemblyName = "Microsoft.CodeAnalysis.InteractiveHost",
     GenerateCodeBase = true,
     OldVersionLowerBound = "4.4.0.0",
@@ -83,12 +77,6 @@ using Microsoft.VisualStudio.Shell;
     NewVersion = "4.5.0.0")]
 [assembly: ProvideBindingRedirection(
     AssemblyName = "Microsoft.CodeAnalysis.VisualBasic.Features",
-    GenerateCodeBase = true,
-    OldVersionLowerBound = "4.4.0.0",
-    OldVersionUpperBound = "4.5.0.0",
-    NewVersion = "4.5.0.0")]
-[assembly: ProvideBindingRedirection(
-    AssemblyName = "Microsoft.CodeAnalysis.VisualBasic.Workspaces",
     GenerateCodeBase = true,
     OldVersionLowerBound = "4.4.0.0",
     OldVersionUpperBound = "4.5.0.0",
