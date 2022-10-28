adc1a5cea32731c2592d77d0635e9d15741ae0d7
Ryan Brandenburg <rybrande@microsoft.com>
2022-10-28 11:18:37 -0700

    Retry hive creation and move to script

---
+++ eng/scripts/CreateVSHive.ps1
@@ -0,0 +1,27 @@
+#requires -version 5
+
+param(
+  [Parameter(Mandatory = $true)]
+  [ValidateNotNullOrEmpty()]
+  [string]
+  $rootSuffix
+)
+
+$ErrorActionPreference = 'Stop'
+Set-StrictMode -Version 1
+
+$success=$false
+for($i=0; $i -le 3; $i++)
+{
+  & "C:\\Test\\VisualStudio\\Common7\\IDE\\devenv.exe" /rootsuffix $rootSuffix /updateConfiguration
+  if(Test-Path -Path $env:LocalAppData\Microsoft\VisualStudio\17.0*RoslynDev)
+  {
+    Write-Host "The hive 'RoslynDev' exists"
+    $success=$true
+    break
+  }
+}
+
+if($success -eq $false){
+  throw "Failed to create hive"
+}
