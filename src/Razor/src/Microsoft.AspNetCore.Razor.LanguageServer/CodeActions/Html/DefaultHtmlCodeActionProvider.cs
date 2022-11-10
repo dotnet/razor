ac2154fa0d0faf8492a2d73534718f4c4f7eb892
David Wengier <david.wengier@microsoft.com>
2022-11-10 16:37:31 +1100

    Fix up Html edits when necessary

--- src/Razor/src/Microsoft.AspNetCore.Razor.LanguageServer/CodeActions/Html/DefaultHtmlCodeActionProvider.cs
+++ src/Razor/src/Microsoft.AspNetCore.Razor.LanguageServer/CodeActions/Html/DefaultHtmlCodeActionProvider.cs
@@ -1,11 +1,15 @@
 ﻿// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the MIT license. See License.txt in the project root for license information.
 
+using System;
 using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Razor.LanguageServer.CodeActions.Models;
 using Microsoft.AspNetCore.Razor.LanguageServer.Common;
+using Microsoft.AspNetCore.Razor.LanguageServer.Extensions;
+using Microsoft.AspNetCore.Razor.LanguageServer.Formatting;
+using Microsoft.CodeAnalysis.Razor.Workspaces.Extensions;
 
 namespace Microsoft.AspNetCore.Razor.LanguageServer.CodeActions
 {
@@ -29,8 +33,23 @@ namespace Microsoft.AspNetCore.Razor.LanguageServer.CodeActions
             {
                 if (codeAction.Edit is not null)
                 {
-                    // TODO: Do the ~ rewriting from WrapWithTag handler
                     codeAction.Edit = await _documentMappingService.RemapWorkspaceEditAsync(codeAction.Edit, cancellationToken).ConfigureAwait(false);
+
+                    if (codeAction.Edit.TryGetDocumentChanges(out var documentEdits) == true)
+                    {
+                        var htmlSourceText = context.CodeDocument.GetHtmlSourceText();
+
+                        foreach (var edit in documentEdits)
+                        {
+                            edit.Edits = HtmlFormatter.FixHtmlTestEdits(htmlSourceText, edit.Edits);
+                        }
+
+                        codeAction.Edit = new VisualStudio.LanguageServer.Protocol.WorkspaceEdit
+                        {
+                            DocumentChanges = documentEdits
+                        };
+                    }
+
                     results.Add(codeAction);
                 }
                 else
