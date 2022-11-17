4b6dc10e2364d13c57ca4723f0efa8fb0ac68523
David Wengier <david.wengier@microsoft.com>
2022-11-17 11:20:07 +1100

    Doc

--- src/Razor/src/Microsoft.AspNetCore.Razor.LanguageServer/CodeActions/ICodeActionProvider.cs
+++ src/Razor/src/Microsoft.AspNetCore.Razor.LanguageServer/CodeActions/ICodeActionProvider.cs
@@ -10,5 +10,12 @@ namespace Microsoft.AspNetCore.Razor.LanguageServer.CodeActions;
 
 internal interface ICodeActionProvider
 {
+    /// <summary>
+    /// Takes code actions provided by a child language, and provides code actions that should be returned to the LSP client.
+    /// </summary>
+    /// <remarks>
+    /// The list of code actions returned from all providers will be combined together in a list. A null result and an empty
+    /// result are effectively the same.
+    /// </remarks>
     Task<IReadOnlyList<RazorVSInternalCodeAction>?> ProvideAsync(RazorCodeActionContext context, IEnumerable<RazorVSInternalCodeAction> codeActions, CancellationToken cancellationToken);
 }
