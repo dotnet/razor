06ebb909c529dcf7de02ba08fd7cef64a89b6f62
David Wengier <david.wengier@microsoft.com>
2022-11-28 13:17:18 +1100

    Update test snapshots

--- src/Compiler/Microsoft.AspNetCore.Razor.Language/test/TestFiles/IntegrationTests/ComponentDesignTimeCodeGenerationTest/BodyAndAttributeChildContent/TestComponent.codegen.cs
+++ src/Compiler/Microsoft.AspNetCore.Razor.Language/test/TestFiles/IntegrationTests/ComponentDesignTimeCodeGenerationTest/BodyAndAttributeChildContent/TestComponent.codegen.cs
@@ -52,6 +52,15 @@ namespace Test
 #line hidden
 #nullable disable
             );
+            ((global::Test.MyComponent)null).
+#nullable restore
+#line 2 "x:\dir\subdir\Test\TestComponent.cshtml"
+             Header
+
+#line default
+#line hidden
+#nullable disable
+            = default;
             __builder.AddAttribute(-1, "ChildContent", (global::Microsoft.AspNetCore.Components.RenderFragment)((__builder2) => {
             }
             ));
--- src/Compiler/Microsoft.AspNetCore.Razor.Language/test/TestFiles/IntegrationTests/ComponentDesignTimeCodeGenerationTest/ComponentWithBooleanParameter_Minimized/TestComponent.codegen.cs
+++ src/Compiler/Microsoft.AspNetCore.Razor.Language/test/TestFiles/IntegrationTests/ComponentDesignTimeCodeGenerationTest/ComponentWithBooleanParameter_Minimized/TestComponent.codegen.cs
@@ -28,6 +28,15 @@ namespace Test
 #line hidden
 #nullable disable
             __o = true;
+            ((global::Test.TestComponent)null).
+#nullable restore
+#line 3 "x:\dir\subdir\Test\TestComponent.cshtml"
+               TestBool
+
+#line default
+#line hidden
+#nullable disable
+            = default;
             __builder.AddAttribute(-1, "ChildContent", (global::Microsoft.AspNetCore.Components.RenderFragment)((__builder2) => {
             }
             ));
