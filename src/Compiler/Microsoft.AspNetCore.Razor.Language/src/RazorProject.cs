cce5a469ee953cf7af5ee796d257bf3f5ed51669
Jan Jones <janjones@microsoft.com>
2022-12-06 17:19:02 +0100

    Handle files in root dir

--- src/Compiler/Microsoft.AspNetCore.Razor.Language/src/RazorProject.cs
+++ src/Compiler/Microsoft.AspNetCore.Razor.Language/src/RazorProject.cs
@@ -97,12 +97,19 @@ public abstract class RazorProject
 
         StringBuilder builder;
         var fileNameIndex = path.LastIndexOf('/');
-        var length = path.Length;
+
+        // Handle paths like `C:file.ext`.
+        if (fileNameIndex == -1)
+        {
+            fileNameIndex = path.LastIndexOf(':');
+        }
 
         if (fileNameIndex == -1)
         {
-            throw new InvalidOperationException($"Cannot find '/' in path '{path}'");
+            throw new InvalidOperationException($"Cannot find file name in path '{path}'");
         }
+
+        var length = path.Length;
 
         if (string.Compare(path, fileNameIndex + 1, fileName, 0, fileName.Length, StringComparison.Ordinal) == 0)
         {
