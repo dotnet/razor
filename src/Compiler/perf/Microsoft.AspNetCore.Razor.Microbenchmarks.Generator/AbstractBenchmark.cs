11edc15efe6475433798bab7c7c9e30be8134c5a
Chris Sienkiewicz <chsienki@microsoft.com>
2022-12-12 12:33:28 -0800

    Benchmark updates (#8001)
    * Factor out the razor benchmarks into mostly manually listed ones
    * Add ETW and memory diagnostics when available
    * Rename benchmarks
    * Add unit tests for the benchmarks
    * Return generatordriver from benchmarks
    * Simplify benchmark construction
    * Add more benchmark tests

---
+++ src/Compiler/perf/Microsoft.AspNetCore.Razor.Microbenchmarks.Generator/AbstractBenchmark.cs
@@ -0,0 +1,39 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System.Runtime.CompilerServices;
+using BenchmarkDotNet.Attributes;
+using Microsoft.Build.Locator;
+using Microsoft.CodeAnalysis;
+
+namespace Microsoft.AspNetCore.Razor.Microbenchmarks.Generator;
+
+public abstract class AbstractBenchmark
+{
+    private ProjectSetup.RazorProject? _project;
+
+    internal ProjectSetup.RazorProject? Project => _project;
+
+    public enum StartupKind { Warm, Cold };
+    [ParamsAllUnlessDebug(StartupKind.Warm)]
+    public StartupKind Startup { get; set; }
+
+    [ModuleInitializer]
+    public static void LoadMSBuild() => MSBuildLocator.RegisterDefaults();
+
+    [GlobalSetup]
+    public void Setup()
+    {
+        _project = ProjectSetup.GetRazorProject(cold: Startup == StartupKind.Cold);
+    }
+
+    protected GeneratorDriver RunBenchmark(Func<ProjectSetup.RazorProject, GeneratorDriver> updateDriver)
+    {
+        var compilation = _project!.Compilation;
+        var driver = _project!.GeneratorDriver;
+        driver = updateDriver(_project!);
+
+        driver = driver.RunGenerators(compilation);
+        return driver;
+    }
+}
