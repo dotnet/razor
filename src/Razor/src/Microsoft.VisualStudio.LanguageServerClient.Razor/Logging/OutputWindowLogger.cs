a6b6f08845b33867f8008e773c6be32a3ab6487e
Ryan Brandenburg <rybrande@microsoft.com>
2022-12-30 10:01:52 -0800

    Fix tests

--- src/Razor/src/Microsoft.VisualStudio.LanguageServerClient.Razor/Logging/OutputWindowLogger.cs
+++ src/Razor/src/Microsoft.VisualStudio.LanguageServerClient.Razor/Logging/OutputWindowLogger.cs
@@ -23,6 +23,13 @@ internal class OutputWindowLogger : ILogger
         _outputPane = new OutputPane(joinableTaskContext);
     }
 
+    [Obsolete("Exists only for Mock.")]
+#pragma warning disable CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+    internal OutputWindowLogger()
+#pragma warning restore CS8618 // Non-nullable field must contain a non-null value when exiting constructor. Consider declaring as nullable.
+    {
+    }
+
     public IDisposable BeginScope<TState>(TState state)
     {
         throw new NotImplementedException();
