c5e416b6b21f8f823d457f8b51a35ab1735d6d69
Ryan Brandenburg <rybrande@microsoft.com>
2022-12-29 16:31:38 -0800

    Cleanup

--- src/Shared/LanguageSupport/IsExternalInit.cs
+++ src/Shared/LanguageSupport/IsExternalInit.cs
@@ -6,6 +6,7 @@
 using System.ComponentModel;
 
 namespace System.Runtime.CompilerServices;
+
 /// <summary>
 /// Reserved to be used by the compiler for tracking metadata.
 /// This class should not be used by developers in source code.
