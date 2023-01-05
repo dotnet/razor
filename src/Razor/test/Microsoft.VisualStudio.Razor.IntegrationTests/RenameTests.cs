1ddc7751873bc31f7247abf96edd020a81d71302
Ryan Brandenburg <rybrande@microsoft.com>
2023-01-05 14:03:41 -0800

    Rename cleanup

--- src/Razor/test/Microsoft.VisualStudio.Razor.IntegrationTests/RenameTests.cs
+++ src/Razor/test/Microsoft.VisualStudio.Razor.IntegrationTests/RenameTests.cs
@@ -1,13 +1,8 @@
 ﻿// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the MIT license. See License.txt in the project root for license information.
 
-using System;
-using System.IO;
 using System.Threading.Tasks;
-using Microsoft.VisualStudio.Shell.TableControl;
-using Microsoft.VisualStudio.Shell.TableManager;
 using Xunit;
-using Xunit.Abstractions;
 
 namespace Microsoft.VisualStudio.Razor.IntegrationTests;
 
@@ -104,6 +99,7 @@ public class RenameTests : AbstractRazorEditorTest
         await TestServices.SolutionExplorer.OpenFileAsync(RazorProjectConstants.BlazorProjectName, "MyComponent.razor.cs", ControlledHangMitigatingCancellationToken);
 
         await TestServices.Editor.PlaceCaretAsync("MyProperty", charsOffset: 0, occurrence: 2, extendSelection: false, selectBlock: false, ControlledHangMitigatingCancellationToken);
+        await TestServices.SolutionExplorer.BuildSolutionAndWaitAsync(ControlledHangMitigatingCancellationToken);
 
         // Act
         await TestServices.Editor.InvokeRenameAsync(ControlledHangMitigatingCancellationToken);
