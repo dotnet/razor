37e3f80ee47add42602a90a973d556792e7f4fc5
Dustin Campbell <dustin@teamcampbell.org>
2023-01-10 12:05:08 -0800

    Update tooling micro benchmark runner
    When originally created, the tooling micro benchmarks lifted a small
    bit of infrastructure from the aspnetcore repo to run BenchmarkDotNet.
    This change removes that infrastructure and adds simpler logic to run
    tests that is based on code from https://github.com/dotnet/performance.
    In addition, BDN has been updated and a package reference has been added
    to BenchmarkDotNet.Performance.Windows. This makes it easy to capture an
    ETW profile on Windows.

---
+++ src/Razor/benchmarks/Microsoft.AspNetCore.Razor.Microbenchmarks/Extensions.cs
@@ -0,0 +1,36 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the MIT license. See License.txt in the project root for license information.
+
+using System.Collections.Generic;
+using System.Linq;
+using BenchmarkDotNet.Reports;
+
+namespace Microsoft.AspNetCore.Razor.Microbenchmarks;
+
+internal static class SummaryExtensions
+{
+    public static int ToExitCode(this IEnumerable<Summary> summaries)
+    {
+        // an empty summary means that initial filtering and validation did not allow to run
+        if (!summaries.Any())
+        {
+            return 1;
+        }
+
+        // if anything has failed, it's an error
+        if (summaries.Any(summary => summary.HasAnyErrors()))
+        {
+            return 1;
+        }
+
+        return 0;
+    }
+
+    public static bool HasAnyErrors(this Summary summary)
+        => summary.HasCriticalValidationErrors ||
+           summary.Reports.Any(report => report.HasAnyErrors());
+
+    public static bool HasAnyErrors(this BenchmarkReport report)
+        => !report.BuildResult.IsBuildSuccess ||
+           !report.AllMeasurements.Any();
+}
