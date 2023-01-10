73b95627abcd53ffa8e9a1296a5b77912e11ff74
Chris Sienkiewicz <chsienki@microsoft.com>
2023-01-10 14:09:53 -0800

    Add symbol extensions

---
+++ src/Compiler/Microsoft.CodeAnalysis.Razor/src/SymbolExtensions.cs
@@ -0,0 +1,19 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System;
+
+namespace Microsoft.CodeAnalysis.Razor;
+
+internal static class SymbolExtensions
+{
+    internal static readonly SymbolDisplayFormat FullNameTypeDisplayFormat =
+        SymbolDisplayFormat.FullyQualifiedFormat
+            .WithGlobalNamespaceStyle(SymbolDisplayGlobalNamespaceStyle.Omitted)
+            .WithMiscellaneousOptions(SymbolDisplayFormat.FullyQualifiedFormat.MiscellaneousOptions & (~SymbolDisplayMiscellaneousOptions.UseSpecialTypes));
+
+    internal static bool HasFullName(this ISymbol symbol, string fullName)
+    {
+        return symbol.ToDisplayString(FullNameTypeDisplayFormat).Equals(fullName, StringComparison.OrdinalIgnoreCase);
+    }
+}
