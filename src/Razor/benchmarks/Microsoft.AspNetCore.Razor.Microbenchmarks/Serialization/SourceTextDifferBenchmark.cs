b524a8a94b99322a4252c1082663d82aa2e91451
Dustin Campbell <dustin@teamcampbell.org>
2023-01-20 09:59:50 -0800

    Refactor SourceTextDifferBenchmark

--- src/Razor/benchmarks/Microsoft.AspNetCore.Razor.Microbenchmarks/Serialization/SourceTextDifferBenchmark.cs
+++ src/Razor/benchmarks/Microsoft.AspNetCore.Razor.Microbenchmarks/Serialization/SourceTextDifferBenchmark.cs
@@ -1,10 +1,7 @@
 ﻿// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the MIT license. See License.txt in the project root for license information.
 
-#nullable disable
-
 using System;
-using System.IO;
 using System.Linq;
 using BenchmarkDotNet.Attributes;
 using Microsoft.AspNetCore.Razor.LanguageServer;
@@ -14,20 +11,14 @@ namespace Microsoft.AspNetCore.Razor.Microbenchmarks.Serialization;
 
 public class SourceTextDifferBenchmark
 {
-    private readonly SourceText _largeFileOriginal;
-    private readonly SourceText _largeFileMinimalChanges;
-    private readonly SourceText _largeFileSignificantChanges;
+    private SourceText? _largeFileOriginal;
+    private SourceText? _largeFileMinimalChanges;
+    private SourceText? _largeFileSignificantChanges;
 
-    public SourceTextDifferBenchmark()
+    [GlobalSetup]
+    public void GlobalSetup()
     {
-        var current = new DirectoryInfo(AppContext.BaseDirectory);
-        while (current != null && !File.Exists(Path.Combine(current.FullName, "MSN.cshtml")))
-        {
-            current = current.Parent;
-        }
-
-        var largeFilePath = Path.Combine(current.FullName, "MSN.cshtml");
-        var largeFileText = File.ReadAllText(largeFilePath);
+        var largeFileText = Resources.GetResourceText("MSN.cshtml");
 
         _largeFileOriginal = SourceText.From(largeFileText);
 
@@ -41,18 +32,18 @@ public class SourceTextDifferBenchmark
     [Benchmark(Description = "Line Diff - One line change (Typing)")]
     public void LineDiff_LargeFile_OneLineChanged()
     {
-        SourceTextDiffer.GetMinimalTextChanges(_largeFileOriginal, _largeFileMinimalChanges, lineDiffOnly: true);
+        SourceTextDiffer.GetMinimalTextChanges(_largeFileOriginal!, _largeFileMinimalChanges!, lineDiffOnly: true);
     }
 
     [Benchmark(Description = "Line Diff - Significant Changes (Copy-paste)")]
     public void LineDiff_LargeFile_SignificantlyDifferent()
     {
-        SourceTextDiffer.GetMinimalTextChanges(_largeFileOriginal, _largeFileSignificantChanges, lineDiffOnly: true);
+        SourceTextDiffer.GetMinimalTextChanges(_largeFileOriginal!, _largeFileSignificantChanges!, lineDiffOnly: true);
     }
 
     [Benchmark(Description = "Character Diff - One character change (Typing)")]
     public void CharDiff_LargeFile_OneCharChanged()
     {
-        SourceTextDiffer.GetMinimalTextChanges(_largeFileOriginal, _largeFileMinimalChanges, lineDiffOnly: false);
+        SourceTextDiffer.GetMinimalTextChanges(_largeFileOriginal!, _largeFileMinimalChanges!, lineDiffOnly: false);
     }
 }
