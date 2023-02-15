91727a05ba08cc86bd7a18793ae570b727d70436
Dustin Campbell <dustin@teamcampbell.org>
2023-02-15 14:58:24 -0800

    Move helpers and utilities from MS.ANC.Razor.Common to MS.ANC.Razor.Utilities.Shared

--- src/Shared/Microsoft.AspNetCore.Razor.Utilities.Shared/ImmutableArrayExtensions.cs
+++ src/Shared/Microsoft.AspNetCore.Razor.Utilities.Shared/ImmutableArrayExtensions.cs
@@ -1,9 +1,7 @@
 ﻿// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the MIT license. See License.txt in the project root for license information.
 
-using System.Collections.Immutable;
-
-namespace Microsoft.AspNetCore.Razor;
+namespace System.Collections.Immutable;
 
 /// <summary>
 /// <see cref="ImmutableArray{T}"/> extension methods
