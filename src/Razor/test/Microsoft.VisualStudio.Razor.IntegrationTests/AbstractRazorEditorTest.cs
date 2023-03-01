ed5d166bc3607ca02f4ee59b2e6bef3cc5af74da
David Wengier <git@wengier.com>
2023-03-02 08:37:37 +1100

    Remove obsolete attribute and fix test

--- src/Razor/test/Microsoft.VisualStudio.Razor.IntegrationTests/AbstractRazorEditorTest.cs
+++ src/Razor/test/Microsoft.VisualStudio.Razor.IntegrationTests/AbstractRazorEditorTest.cs
@@ -53,7 +53,7 @@ public abstract class AbstractRazorEditorTest : AbstractEditorTest
         await TestServices.Editor.WaitForComponentClassificationAsync(ControlledHangMitigatingCancellationToken, count: 3);
 
         // Close the file we opened, just in case, so the test can start with a clean slate
-        await TestServices.Editor.CloseDocumentWindowAsync(ControlledHangMitigatingCancellationToken);
+        await TestServices.Editor.CloseCodeFileAsync(RazorProjectConstants.BlazorProjectName, RazorProjectConstants.IndexRazorFile, saveFile: false, ControlledHangMitigatingCancellationToken);
     }
 
     private static void EnsureLSPEditorEnabled()
