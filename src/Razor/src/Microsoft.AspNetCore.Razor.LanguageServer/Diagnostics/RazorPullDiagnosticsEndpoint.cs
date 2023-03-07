4ca1126fe0b496448a49568b6a2b9312cde7e783
David Wengier <david.wengier@microsoft.com>
2023-03-07 15:06:40 +1100

    Clean up endpoint

--- src/Razor/src/Microsoft.AspNetCore.Razor.LanguageServer/Diagnostics/RazorPullDiagnosticsEndpoint.cs
+++ src/Razor/src/Microsoft.AspNetCore.Razor.LanguageServer/Diagnostics/RazorPullDiagnosticsEndpoint.cs
@@ -2,6 +2,7 @@
 // Licensed under the MIT license. See License.txt in the project root for license information.
 
 using System;
+using System.Buffers;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
@@ -10,6 +11,7 @@ using Microsoft.AspNetCore.Razor.Language;
 using Microsoft.AspNetCore.Razor.LanguageServer.Common;
 using Microsoft.AspNetCore.Razor.LanguageServer.EndpointContracts;
 using Microsoft.AspNetCore.Razor.LanguageServer.Protocol;
+using Microsoft.AspNetCore.Razor.PooledObjects;
 using Microsoft.CodeAnalysis.Razor.Workspaces;
 using Microsoft.VisualStudio.LanguageServer.Protocol;
 
@@ -20,17 +22,14 @@ internal class RazorPullDiagnosticsEndpoint : IRazorPullDiagnosticsEndpoint
     private readonly LanguageServerFeatureOptions _languageServerFeatureOptions;
     private readonly ClientNotifierServiceBase _languageServer;
     private readonly RazorTranslateDiagnosticsService _translateDiagnosticsService;
-    protected readonly RazorDocumentMappingService DocumentMappingService;
 
     public RazorPullDiagnosticsEndpoint(
         LanguageServerFeatureOptions languageServerFeatureOptions,
         RazorTranslateDiagnosticsService translateDiagnosticsService,
-        RazorDocumentMappingService documentMappingService,
         ClientNotifierServiceBase languageServer)
     {
         _languageServerFeatureOptions = languageServerFeatureOptions ?? throw new ArgumentNullException(nameof(languageServerFeatureOptions));
         _translateDiagnosticsService = translateDiagnosticsService ?? throw new ArgumentNullException(nameof(translateDiagnosticsService));
-        DocumentMappingService = documentMappingService ?? throw new ArgumentNullException(nameof(documentMappingService));
         _languageServer = languageServer ?? throw new ArgumentNullException(nameof(languageServer));
     }
 
@@ -60,10 +59,65 @@ internal class RazorPullDiagnosticsEndpoint : IRazorPullDiagnosticsEndpoint
 
         var documentContext = context.GetRequiredDocumentContext();
 
+        var razorDiagnostics = await GetRazorDiagnosticsAsync(documentContext, cancellationToken).ConfigureAwait(false);
+
+        var (csharpDiagnostics, htmlDiagnostics) = await GetHtmlCSharpDiagnosticsAsync(documentContext, cancellationToken).ConfigureAwait(false);
+
+        using var _ = ListPool<VSInternalDiagnosticReport>.GetPooledObject(out var allDiagnostics);
+        allDiagnostics.SetCapacityIfLarger(
+            (razorDiagnostics?.Length ?? 0) +
+            (csharpDiagnostics?.Length ?? 0) +
+            (htmlDiagnostics?.Length ?? 0));
+
+        if (razorDiagnostics is not null)
+        {
+            // No extra work to do for Razor diagnostics
+            allDiagnostics.AddRange(razorDiagnostics);
+        }
+
+        if (csharpDiagnostics is not null)
+        {
+            foreach (var report in csharpDiagnostics)
+            {
+                if (report.Diagnostics is not null)
+                {
+                    var mappedDiagnostics = await _translateDiagnosticsService.TranslateAsync(RazorLanguageKind.CSharp, report.Diagnostics, documentContext, cancellationToken);
+                    report.Diagnostics = mappedDiagnostics;
+                }
+
+                allDiagnostics.Add(report);
+            }
+        }
+
+        if (htmlDiagnostics is not null)
+        {
+            foreach (var report in htmlDiagnostics)
+            {
+                if (report.Diagnostics is not null)
+                {
+                    var mappedDiagnostics = await _translateDiagnosticsService.TranslateAsync(RazorLanguageKind.Html, report.Diagnostics, documentContext, cancellationToken);
+                    report.Diagnostics = mappedDiagnostics;
+                }
+
+                allDiagnostics.Add(report);
+            }
+        }
+
+        return allDiagnostics.ToArray();
+    }
+
+    private static async Task<VSInternalDiagnosticReport[]?> GetRazorDiagnosticsAsync(VersionedDocumentContext documentContext, CancellationToken cancellationToken)
+    {
         var codeDocument = await documentContext.GetCodeDocumentAsync(cancellationToken).ConfigureAwait(false);
         var sourceText = await documentContext.GetSourceTextAsync(cancellationToken).ConfigureAwait(false);
         var csharpDocument = codeDocument.GetCSharpDocument();
         var diagnostics = csharpDocument.Diagnostics;
+
+        if (diagnostics.Count == 0)
+        {
+            return null;
+        }
+
         var convertedDiagnostics = RazorDiagnosticConverter.Convert(diagnostics, sourceText);
 
         var razorDiagnostics = new VSInternalDiagnosticReport[]
@@ -74,37 +128,22 @@ internal class RazorPullDiagnosticsEndpoint : IRazorPullDiagnosticsEndpoint
                 ResultId = Guid.NewGuid().ToString()
             }
         };
+        return razorDiagnostics;
+    }
 
+    private async Task<(VSInternalDiagnosticReport[]? CSharpDiagnostics, VSInternalDiagnosticReport[]? HtmlDiagnostics)> GetHtmlCSharpDiagnosticsAsync(VersionedDocumentContext documentContext, CancellationToken cancellationToken)
+    {
         var delegatedParams = new DelegatedDiagnosticParams(documentContext.Identifier);
-
         var delegatedResponse = await _languageServer.SendRequestAsync<DelegatedDiagnosticParams, RazorPullDiagnosticResponse?>(
             RazorLanguageServerCustomMessageTargets.RazorPullDiagnosticEndpointName,
             delegatedParams,
             cancellationToken).ConfigureAwait(false);
 
-        if (delegatedResponse is not null)
+        if (delegatedResponse is null)
         {
-            foreach (var report in delegatedResponse.CSharpDiagnostics)
-            {
-                if (report.Diagnostics is not null)
-                {
-                    var mappedDiagnostics = await _translateDiagnosticsService.TranslateAsync(RazorLanguageKind.CSharp, report.Diagnostics, documentContext, cancellationToken);
-                    report.Diagnostics = mappedDiagnostics;
-                }
-            }
-
-            foreach (var report in delegatedResponse.HtmlDiagnostics)
-            {
-                if (report.Diagnostics is not null)
-                {
-                    var mappedDiagnostics = await _translateDiagnosticsService.TranslateAsync(RazorLanguageKind.Html, report.Diagnostics, documentContext, cancellationToken);
-                    report.Diagnostics = mappedDiagnostics;
-                }
-            }
-
-            razorDiagnostics = razorDiagnostics.Concat(delegatedResponse.CSharpDiagnostics).Concat(delegatedResponse.HtmlDiagnostics).ToArray();
+            return (null, null);
         }
 
-        return razorDiagnostics;
+        return (delegatedResponse.CSharpDiagnostics, delegatedResponse.HtmlDiagnostics);
     }
 }
