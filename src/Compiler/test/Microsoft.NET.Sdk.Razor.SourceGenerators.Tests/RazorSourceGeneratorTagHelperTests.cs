5adf42417cf34dfeca39034d19d3eefa616ac58a
Jan Jones <janjones@microsoft.com>
2023-03-28 10:39:04 +0200

    Add Encoders test

--- src/Compiler/test/Microsoft.NET.Sdk.Razor.SourceGenerators.Tests/RazorSourceGeneratorTagHelperTests.cs
+++ src/Compiler/test/Microsoft.NET.Sdk.Razor.SourceGenerators.Tests/RazorSourceGeneratorTagHelperTests.cs
@@ -52,6 +52,218 @@ public sealed class RazorSourceGeneratorTagHelperTests : RazorSourceGeneratorTes
         await VerifyRazorPageMatchesBaselineAsync(compilation, "Views_Home_Index");
     }
 
+    [Theory]
+    [InlineData("Index")]
+    [InlineData("CustomEncoder")]
+    [InlineData("NullEncoder")]
+    [InlineData("TwoEncoders")]
+    [InlineData("ThreeEncoders")]
+    public async Task Encoders(string name)
+    {
+        // Arrange
+        var project = CreateTestProject(new()
+        {
+            // https://github.com/dotnet/aspnetcore/tree/b40cc0b/src/Mvc/test/WebSites/TagHelpersWebSite/Views/Encoders
+            ["Views/Home/Index.cshtml"] = """
+                @addTagHelper TestProject.TagHelpers.DefaultEncoderTagHelper, TestProject
+
+                @{
+                    var count = 0;
+                    var innerCount = 0;
+                }
+
+                <pre>@($"Outer knows <b>{ ++count } < 4</b>")
+                <inner>@($"Inner knows <b>{ ++innerCount } < 4</b>")</inner></pre>
+                """,
+            ["Views/Home/CustomEncoder.cshtml"] = """
+                @addTagHelper TestProject.TagHelpers.CustomEncoderTagHelper, TestProject
+
+                @{
+                    var count = 0;
+                    var innerCount = 0;
+                }
+
+                <pre>@($"Outer knows <b>{ ++count } < 4</b>")
+                <inner>@($"Inner knows <b>{ ++innerCount } < 4</b>")</inner></pre>
+                """,
+            ["Views/Home/NullEncoder.cshtml"] = """
+                @addTagHelper TestProject.TagHelpers.NullEncoderTagHelper, TestProject
+
+                @{
+                    var count = 0;
+                    var innerCount = 0;
+                }
+
+                <pre>@($"Outer knows <b>{ ++count } < 4</b>")
+                <inner>@($"Inner knows <b>{ ++innerCount } < 4</b>")</inner></pre>
+                """,
+            ["Views/Home/TwoEncoders.cshtml"] = """
+                @addTagHelper TestProject.TagHelpers.CustomEncoderTagHelper, TestProject
+                @addTagHelper TestProject.TagHelpers.NullEncoderTagHelper, TestProject
+
+                @{
+                    var count = 0;
+                    var innerCount = 0;
+                }
+
+                <pre>@($"Outer knows <b>{ ++count } < 4</b>")
+                <inner>@($"Inner knows <b>{ ++innerCount } < 4</b>")</inner></pre>
+                """,
+            ["Views/Home/ThreeEncoders.cshtml"] = """
+                @addTagHelper TestProject.TagHelpers.CustomEncoderTagHelper, TestProject
+                @addTagHelper TestProject.TagHelpers.DefaultEncoderTagHelper, TestProject
+                @addTagHelper TestProject.TagHelpers.NullEncoderTagHelper, TestProject
+
+                @{
+                    var count = 0;
+                    var innerCount = 0;
+                }
+
+                <pre>@($"Outer knows <b>{ ++count } < 4</b>")
+                <inner>@($"Inner knows <b>{ ++innerCount } < 4</b>")</inner></pre>
+                """,
+        }, new()
+        {
+            ["GlobalUsings.g.cs"] = """
+                global using System.IO;
+                global using System.Threading.Tasks;
+                """,
+            ["DefaultEncoderTagHelper.cs"] = """
+                using Microsoft.AspNetCore.Razor.TagHelpers;
+
+                namespace TestProject.TagHelpers;
+
+                [HtmlTargetElement("pre")]
+                [HtmlTargetElement("inner")]
+                [OutputElementHint("pre")]
+                public class DefaultEncoderTagHelper : TagHelper
+                {
+                    public override int Order => 2;
+
+                    public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+                    {
+                        var defaultContent = await output.GetChildContentAsync();
+
+                        output.Content
+                            .SetHtmlContent("Default encoder: ")
+                            .AppendHtml(defaultContent);
+                        output.TagName = "pre";
+                    }
+                }
+                """,
+            ["CustomEncoderTagHelper.cs"] = """
+                using System.Text.Encodings.Web;
+                using Microsoft.AspNetCore.Razor.TagHelpers;
+
+                namespace TestProject.TagHelpers;
+
+                [HtmlTargetElement("pre")]
+                public class CustomEncoderTagHelper : TagHelper
+                {
+                    public override int Order => 1;
+
+                    public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+                    {
+                        var encoder = new CustomEncoder();
+                        var customContent = await output.GetChildContentAsync(encoder);
+
+                        // Note this is very unsafe. Should always post-process content that may not be fully HTML encoded before
+                        // writing it into a response. Here for example, could pass SetContent() a string and that would be
+                        // HTML encoded later.
+                        output.PreContent
+                            .SetHtmlContent("Custom encoder: ")
+                            .AppendHtml(customContent)
+                            .AppendHtml("<br />");
+                    }
+
+                    // Simple encoder that just wraps "string" as "Custom[[string]]". Note: Lacks all parameter checks.
+                    private class CustomEncoder : HtmlEncoder
+                    {
+                        public CustomEncoder()
+                        {
+                        }
+
+                        public override int MaxOutputCharactersPerInputCharacter => 1;
+
+                        public override string Encode(string value) => $"Custom[[{ value }]]";
+
+                        public override void Encode(TextWriter output, char[] value, int startIndex, int characterCount)
+                        {
+                            if (characterCount == 0)
+                            {
+                                return;
+                            }
+
+                            output.Write("Custom[[");
+                            output.Write(value, startIndex, characterCount);
+                            output.Write("]]");
+                        }
+
+                        public override void Encode(TextWriter output, string value, int startIndex, int characterCount)
+                        {
+                            if (characterCount == 0)
+                            {
+                                return;
+                            }
+
+                            output.Write("Custom[[");
+                            output.Write(value.Substring(startIndex, characterCount));
+                            output.Write("]]");
+                        }
+
+                        public override unsafe int FindFirstCharacterToEncode(char* text, int textLength) => -1;
+
+                        public override unsafe bool TryEncodeUnicodeScalar(
+                            int unicodeScalar,
+                            char* buffer,
+                            int bufferLength,
+                            out int numberOfCharactersWritten)
+                        {
+                            numberOfCharactersWritten = 0;
+
+                            return false;
+                        }
+
+                        public override bool WillEncode(int unicodeScalar) => false;
+                    }
+                }
+                """,
+            ["NullEncoderTagHelper.cs"] = """
+                using Microsoft.AspNetCore.Razor.TagHelpers;
+
+                namespace TestProject.TagHelpers;
+
+                [HtmlTargetElement("pre")]
+                public class NullEncoderTagHelper : TagHelper
+                {
+                    public override int Order => 3;
+
+                    public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+                    {
+                        var nullContent = await output.GetChildContentAsync(NullHtmlEncoder.Default);
+
+                        // Note this is very unsafe. Should always post-process content that may not be fully HTML encoded before
+                        // writing it into a response. Here for example, could pass SetContent() a string and that would be
+                        // HTML encoded later.
+                        output.PostContent
+                            .SetHtmlContent("<br />Null encoder: ")
+                            .AppendHtml(nullContent);
+                    }
+                }
+                """,
+        });
+        project = project.WithCompilationOptions(((CSharpCompilationOptions)project.CompilationOptions!)
+            .WithAllowUnsafe(true));
+        var compilation = await project.GetCompilationAsync();
+        var driver = await GetDriverAsync(project);
+
+        // Act
+        RunGenerator(compilation!, ref driver, out compilation);
+
+        // Assert
+        await VerifyRazorPageMatchesBaselineAsync(compilation, $"Views_Home_{name}");
+    }
+
     [Fact]
     public async Task TagHelpersWebSite()
     {
