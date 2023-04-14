918f8ee4d53a0155b1087292502bc0fb7a95af28
Dustin Campbell <dustin@teamcampbell.org>
2023-04-14 12:02:02 -0700

    Performance and allocation tweaks in ComponentTagHelperDescriptorProvider

---
+++ src/Shared/Microsoft.AspNetCore.Razor.Utilities.Shared/PooledObjects/StringHashSetPool.Policy.cs
@@ -0,0 +1,36 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the MIT license. See License.txt in the project root for license information.
+
+using System.Collections.Generic;
+using Microsoft.Extensions.ObjectPool;
+
+namespace Microsoft.AspNetCore.Razor.PooledObjects;
+
+internal static partial class StringHashSetPool
+{
+    private class Policy : IPooledObjectPolicy<HashSet<string>>
+    {
+        private readonly IEqualityComparer<string>? _comparer;
+
+        public Policy(IEqualityComparer<string>? comparer = null)
+        {
+            _comparer = comparer;
+        }
+
+        public HashSet<string> Create() => new(_comparer);
+
+        public bool Return(HashSet<string> set)
+        {
+            var count = set.Count;
+
+            set.Clear();
+
+            if (count > DefaultPool.MaximumObjectSize)
+            {
+                set.TrimExcess();
+            }
+
+            return true;
+        }
+    }
+}
