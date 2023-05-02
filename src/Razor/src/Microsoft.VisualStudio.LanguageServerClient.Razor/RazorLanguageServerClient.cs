4b65c55dac10078ffcf2cbd64dba64a8583df505
Maryam Ariyan <maariyan@microsoft.com>
2023-05-02 13:58:47 -0700

    bringing back binding redirect update

--- src/Razor/src/Microsoft.VisualStudio.LanguageServerClient.Razor/RazorLanguageServerClient.cs
+++ src/Razor/src/Microsoft.VisualStudio.LanguageServerClient.Razor/RazorLanguageServerClient.cs
@@ -7,6 +7,7 @@ using System.ComponentModel.Composition;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Razor;
 using Microsoft.AspNetCore.Razor.LanguageServer;
 using Microsoft.AspNetCore.Razor.LanguageServer.Common;
 using Microsoft.AspNetCore.Razor.Telemetry;
@@ -260,7 +261,7 @@ internal class RazorLanguageServerClient : ILanguageClient, ILanguageClientCusto
         serviceCollection.AddLogging(logging =>
         {
             logging.AddFilter<LogHubLoggerProvider>(level => true);
-            logging.AddProvider(_loggerProvider);
+            logging.AddProvider(_loggerProvider.AssumeNotNull());
         });
 
         if (_vsHostWorkspaceServicesProvider is not null)
