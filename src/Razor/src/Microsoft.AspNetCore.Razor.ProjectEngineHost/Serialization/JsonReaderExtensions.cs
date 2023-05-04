575dffd2d3d08deafe905430e24a13ef9e710a56
Dustin Campbell <dustin@teamcampbell.org>
2023-05-04 13:27:25 -0700

    Add helper methods for reading/writing JSON

--- src/Razor/src/Microsoft.AspNetCore.Razor.ProjectEngineHost/Serialization/JsonReaderExtensions.cs
+++ src/Razor/src/Microsoft.AspNetCore.Razor.ProjectEngineHost/Serialization/JsonReaderExtensions.cs
@@ -1,15 +1,178 @@
 ﻿// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the MIT license. See License.txt in the project root for license information.
 
-#nullable disable
-
 using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Runtime.CompilerServices;
 using Newtonsoft.Json;
 
 namespace Microsoft.AspNetCore.Razor.ProjectEngineHost.Serialization;
 
+internal delegate void ReadPropertyValue<TData>(JsonReader reader, ref TData data);
+
+internal sealed class PropertyMap<TData>
+    where TData : struct
+{
+    private readonly Dictionary<string, ReadPropertyValue<TData>> _map;
+
+    public PropertyMap(params (string, ReadPropertyValue<TData>)[] pairs)
+    {
+        var map = new Dictionary<string, ReadPropertyValue<TData>>(capacity: pairs.Length);
+
+        foreach (var (key, value) in pairs)
+        {
+            map.Add(key, value);
+        }
+
+        _map = map;
+    }
+
+    public bool TryGetPropertyReader(string key, [MaybeNullWhen(false)] out ReadPropertyValue<TData> value)
+        => _map.TryGetValue(key, out value);
+}
+
 internal static class JsonReaderExtensions
 {
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static void CheckToken(this JsonReader reader, JsonToken expectedToken)
+    {
+        if (reader.TokenType != expectedToken)
+        {
+            ThrowUnexpectedTokenException(expectedToken, reader.TokenType);
+        }
+
+        [DoesNotReturn]
+        static void ThrowUnexpectedTokenException(JsonToken expectedToken, JsonToken actualToken)
+        {
+            throw new InvalidOperationException(
+                SR.FormatExpected_JSON_token_0_but_it_was_1(expectedToken, actualToken));
+        }
+    }
+
+    [MethodImpl(MethodImplOptions.AggressiveInlining)]
+    public static void ReadToken(this JsonReader reader, JsonToken expectedToken)
+    {
+        reader.CheckToken(expectedToken);
+        reader.Read();
+    }
+
+    public static bool TryReadNull(this JsonReader reader)
+    {
+        if (reader.TokenType == JsonToken.Null)
+        {
+            reader.Read();
+            return true;
+        }
+
+        return false;
+    }
+
+    public static bool ReadBoolean(this JsonReader reader)
+    {
+        reader.CheckToken(JsonToken.Boolean);
+
+        var result = Convert.ToBoolean(reader.Value);
+        reader.Read();
+
+        return result;
+    }
+
+    public static int ReadInt32(this JsonReader reader)
+    {
+        reader.CheckToken(JsonToken.Integer);
+
+        var result = Convert.ToInt32(reader.Value);
+        reader.Read();
+
+        return result;
+    }
+
+    public static string? ReadString(this JsonReader reader)
+    {
+        if (reader.TryReadNull())
+        {
+            return null;
+        }
+
+        reader.CheckToken(JsonToken.String);
+
+        var result = Convert.ToString(reader.Value);
+        reader.Read();
+
+        return result;
+    }
+
+    public static string ReadNonNullString(this JsonReader reader)
+    {
+        reader.CheckToken(JsonToken.String);
+
+        var result = Convert.ToString(reader.Value).AssumeNotNull();
+        reader.Read();
+
+        return result;
+    }
+
+    public static TData ReadObjectData<TData>(this JsonReader reader, PropertyMap<TData> propertyMap)
+        where TData : struct
+    {
+        reader.ReadToken(JsonToken.StartObject);
+        var result = reader.ReadProperties(propertyMap);
+        reader.ReadToken(JsonToken.EndObject);
+
+        return result;
+    }
+
+    public static void ReadObjectData<TData>(this JsonReader reader, ref TData data, PropertyMap<TData> propertyMap)
+        where TData : struct
+    {
+        reader.ReadToken(JsonToken.StartObject);
+        reader.ReadProperties(ref data, propertyMap);
+        reader.ReadToken(JsonToken.EndObject);
+    }
+
+    public static TData ReadProperties<TData>(this JsonReader reader, PropertyMap<TData> propertyMap)
+        where TData : struct
+    {
+        TData result = default;
+        reader.ReadProperties(ref result, propertyMap);
+
+        return result;
+    }
+
+    public static void ReadProperties<TData>(this JsonReader reader, ref TData data, PropertyMap<TData> propertyMap)
+        where TData : struct
+    {
+        while (true)
+        {
+            switch (reader.TokenType)
+            {
+                case JsonToken.PropertyName:
+                    var propertyName = (string)reader.Value.AssumeNotNull();
+
+                    if (!propertyMap.TryGetPropertyReader(propertyName, out var readProperty))
+                    {
+                        throw new InvalidOperationException(
+                            SR.FormatEncountered_unexpected_JSON_property_0(propertyName));
+                    }
+
+                    reader.Read();
+                    readProperty(reader, ref data);
+
+                    break;
+
+                case JsonToken.EndObject:
+                    return;
+
+                case var token:
+                    throw new InvalidOperationException(
+                        SR.FormatEncountered_unexpected_JSON_token_0(token));
+            }
+        }
+    }
+
+#nullable disable
+
     public static bool ReadTokenAndAdvance(this JsonReader reader, JsonToken expectedTokenType, out object value)
     {
         value = reader.Value;
--- src/Razor/src/Microsoft.AspNetCore.Razor.ProjectEngineHost/Serialization/JsonWriterExtensions.cs
+++ src/Razor/src/Microsoft.AspNetCore.Razor.ProjectEngineHost/Serialization/JsonWriterExtensions.cs
@@ -1,15 +1,54 @@
 ﻿// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the MIT license. See License.txt in the project root for license information.
 
-#nullable disable
-
 using System.Collections.Generic;
 using Newtonsoft.Json;
 
 namespace Microsoft.AspNetCore.Razor.ProjectEngineHost.Serialization;
 
+internal delegate void WriteProperties<T>(JsonWriter writer, T value);
+
 internal static class JsonWriterExtensions
 {
+    public static void Write(this JsonWriter writer, string propertyName, bool value)
+    {
+        writer.WritePropertyName(propertyName);
+        writer.WriteValue(value);
+    }
+
+    public static void Write(this JsonWriter writer, string propertyName, int value)
+    {
+        writer.WritePropertyName(propertyName);
+        writer.WriteValue(value);
+    }
+
+    public static void Write(this JsonWriter writer, string propertyName, string? value)
+    {
+        writer.WritePropertyName(propertyName);
+        writer.WriteValue(value);
+    }
+
+    public static void WriteObject<T>(this JsonWriter writer, string propertyName, T? value, WriteProperties<T> writeProperties)
+    {
+        writer.WritePropertyName(propertyName);
+        writer.WriteObject(value, writeProperties);
+    }
+
+    public static void WriteObject<T>(this JsonWriter writer, T? value, WriteProperties<T> writeProperties)
+    {
+        if (value is null)
+        {
+            writer.WriteNull();
+            return;
+        }
+
+        writer.WriteStartObject();
+        writeProperties(writer, value);
+        writer.WriteEndObject();
+    }
+
+#nullable disable
+
     public static void WritePropertyArray<T>(this JsonWriter writer, string propertyName, IEnumerable<T> collection, JsonSerializer serializer)
     {
         writer.WritePropertyName(propertyName);
