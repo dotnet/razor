137fd4b0167cc4a56ebd5620e6fe7831a424b8e6
Dustin Campbell <dustin@teamcampbell.org>
2023-05-04 13:27:58 -0700

    Add ObjectJsonConverter<T> base class

---
+++ src/Razor/src/Microsoft.AspNetCore.Razor.ProjectEngineHost/Serialization/ObjectJsonConverter`1.cs
@@ -0,0 +1,45 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the MIT license. See License.txt in the project root for license information.
+
+using System;
+using Newtonsoft.Json;
+
+namespace Microsoft.AspNetCore.Razor.ProjectEngineHost.Serialization;
+
+internal abstract class ObjectJsonConverter<T> : JsonConverter<T>
+    where T : class
+{
+    protected abstract T? ReadFromProperties(JsonReader reader);
+    protected abstract void WriteProperties(JsonWriter writer, T value);
+
+    public sealed override T? ReadJson(JsonReader reader, Type objectType, T? existingValue, bool hasExistingValue, JsonSerializer serializer)
+    {
+        if (reader.TokenType == JsonToken.Null)
+        {
+            return null;
+        }
+
+        reader.ReadToken(JsonToken.StartObject);
+
+        var result = ReadFromProperties(reader);
+
+        // JSON.NET serialization expects that we don't advance passed the end object token,
+        // but we should verify that it's there.
+        reader.CheckToken(JsonToken.EndObject);
+
+        return result;
+    }
+
+    public sealed override void WriteJson(JsonWriter writer, T? value, JsonSerializer serializer)
+    {
+        if (value is null)
+        {
+            writer.WriteNull();
+            return;
+        }
+
+        writer.WriteStartObject();
+        WriteProperties(writer, value);
+        writer.WriteEndObject();
+    }
+}
