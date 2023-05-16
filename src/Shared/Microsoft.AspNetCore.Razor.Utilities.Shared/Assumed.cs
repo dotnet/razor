95ab8ad0bb710bff10d64dee9885782a8da168f1
Dustin Campbell <dustin@teamcampbell.org>
2023-05-16 11:09:55 -0700

    Update Assumed.Unreachable() to include file/line info

--- src/Shared/Microsoft.AspNetCore.Razor.Utilities.Shared/Assumed.cs
+++ src/Shared/Microsoft.AspNetCore.Razor.Utilities.Shared/Assumed.cs
@@ -3,27 +3,27 @@
 
 using System;
 using System.Diagnostics.CodeAnalysis;
+using System.Runtime.CompilerServices;
 
 namespace Microsoft.AspNetCore.Razor;
 
 internal static class Assumed
 {
-    private static readonly Exception s_unreachable
-        = new InvalidOperationException(SR.This_program_location_is_throught_to_be_unreachable);
+    /// <summary>
+    ///  Can be called at points that are assumed to be unreachable at runtime.
+    /// </summary>
+    /// <exception cref="InvalidOperationException"/>
+    [DoesNotReturn]
+    public static void Unreachable([CallerFilePath] string? path = null, [CallerLineNumber] int line = 0)
+        => throw new InvalidOperationException(
+            SR.FormatThis_program_location_is_throught_to_be_unreachable_File_0_Line_1(path, line));
 
     /// <summary>
     ///  Can be called at points that are assumed to be unreachable at runtime.
     /// </summary>
     /// <exception cref="InvalidOperationException"/>
     [DoesNotReturn]
-    public static void Unreachable()
-        => throw s_unreachable;
-
-    /// <summary>
-    ///  Can be called at points that are assumed to be unreachable at runtime.
-    /// </summary>
-    /// <exception cref="InvalidOperationException"/>
-    [DoesNotReturn]
-    public static T Unreachable<T>()
-        => throw s_unreachable;
+    public static T Unreachable<T>([CallerFilePath] string? path = null, [CallerLineNumber] int line = 0)
+        => throw new InvalidOperationException(
+            SR.FormatThis_program_location_is_throught_to_be_unreachable_File_0_Line_1(path, line));
 }
