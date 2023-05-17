c82595c6481209081f143f8609fd823c0d15dd62
Jan Jones <janjones@microsoft.com>
2023-05-17 11:50:57 +0200

    Add a test

---
+++ src/Compiler/test/Microsoft.NET.Sdk.Razor.SourceGenerators.Tests/RazorSourceGeneratorComponentTests.cs
@@ -0,0 +1,52 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Razor.Test.Common;
+using Xunit;
+
+namespace Microsoft.NET.Sdk.Razor.SourceGenerators;
+
+public sealed class RazorSourceGeneratorComponentTests : RazorSourceGeneratorTestsBase
+{
+    [Fact, WorkItem("https://github.com/dotnet/razor/issues/8718")]
+    public async Task PartialClass()
+    {
+        // Arrange
+        var project = CreateTestProject(new()
+        {
+            ["Shared/Component1.razor"] = """
+                <Component2 />
+                """,
+            ["Shared/Component2.razor"] = """
+                @inherits ComponentBase
+
+                @code {
+                    [Parameter]
+                    public RenderFragment? ChildContent { get; set; }
+                }
+                """
+        }, new()
+        {
+            ["Component2.razor.cs"] = """
+                using Microsoft.AspNetCore.Components;
+
+                namespace MyApp.Shared;
+
+                public partial class Component2 : ComponentBase
+                {
+
+                }
+                """
+        });
+        var compilation = await project.GetCompilationAsync();
+        var driver = await GetDriverAsync(project);
+
+        // Act
+        var result = RunGenerator(compilation!, ref driver);
+
+        // Assert
+        Assert.Empty(result.Diagnostics);
+        Assert.Equal(2, result.GeneratedSources.Length);
+    }
+}
