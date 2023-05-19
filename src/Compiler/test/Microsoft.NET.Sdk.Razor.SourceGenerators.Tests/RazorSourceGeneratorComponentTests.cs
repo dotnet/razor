0c926dc3298d56ceef3c1bb13f7c1851f326a6b2
Jan Jones <janjones@microsoft.com>
2023-05-19 14:02:59 +0200

    Add more tests

---
+++ src/Compiler/test/Microsoft.NET.Sdk.Razor.SourceGenerators.Tests/RazorSourceGeneratorComponentTests.cs
@@ -0,0 +1,141 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Razor.Test.Common;
+using Xunit;
+
+namespace Microsoft.NET.Sdk.Razor.SourceGenerators;
+
+public sealed class RazorSourceGeneratorComponentTests : RazorSourceGeneratorTestsBase
+{
+    [Fact, WorkItem("https://github.com/dotnet/razor/issues/8718")]
+    public async Task PartialClass()
+    {
+        // Arrange
+        var project = CreateTestProject(new()
+        {
+            ["Views/Home/Index.cshtml"] = """
+                @(await Html.RenderComponentAsync<MyApp.Shared.Component1>(RenderMode.Static))
+                """,
+            ["Shared/Component1.razor"] = """
+                <Component2 Param="42" />
+                """,
+            ["Shared/Component2.razor"] = """
+                @inherits ComponentBase
+                
+                Value: @(Param + 1)
+
+                @code {
+                    [Parameter]
+                    public int Param { get; set; }
+                }
+                """
+        }, new()
+        {
+            ["Component2.razor.cs"] = """
+                using Microsoft.AspNetCore.Components;
+
+                namespace MyApp.Shared;
+
+                public partial class Component2 : ComponentBase { }
+                """
+        });
+        var compilation = await project.GetCompilationAsync();
+        var driver = await GetDriverAsync(project, options =>
+        {
+            options.TestGlobalOptions["build_property.RazorLangVersion"] = "7.0";
+        });
+
+        // Act
+        var result = RunGenerator(compilation!, ref driver, out compilation);
+
+        // Assert
+        Assert.Empty(result.Diagnostics);
+        Assert.Equal(3, result.GeneratedSources.Length);
+        await VerifyRazorPageMatchesBaselineAsync(compilation, "Views_Home_Index");
+    }
+
+    [Fact, WorkItem("https://github.com/dotnet/razor/issues/8718")]
+    public async Task PartialClass_NoBaseInCSharp()
+    {
+        // Arrange
+        var project = CreateTestProject(new()
+        {
+            ["Views/Home/Index.cshtml"] = """
+                @(await Html.RenderComponentAsync<MyApp.Shared.Component1>(RenderMode.Static))
+                """,
+            ["Shared/Component1.razor"] = """
+                <Component2 Param="42" />
+                """,
+            ["Shared/Component2.razor"] = """
+                @inherits ComponentBase
+
+                Value: @(Param + 1)
+
+                @code {
+                    [Parameter]
+                    public int Param { get; set; }
+                }
+                """
+        }, new()
+        {
+            ["Component2.razor.cs"] = """
+                using Microsoft.AspNetCore.Components;
+
+                namespace MyApp.Shared;
+
+                public partial class Component2 { }
+                """
+        });
+        var compilation = await project.GetCompilationAsync();
+        var driver = await GetDriverAsync(project, options =>
+        {
+            options.TestGlobalOptions["build_property.RazorLangVersion"] = "7.0";
+        });
+
+        // Act
+        var result = RunGenerator(compilation!, ref driver, out compilation);
+
+        // Assert
+        Assert.Empty(result.Diagnostics);
+        Assert.Equal(3, result.GeneratedSources.Length);
+        await VerifyRazorPageMatchesBaselineAsync(compilation, "Views_Home_Index");
+    }
+
+    [Fact, WorkItem("https://github.com/dotnet/razor/issues/8718")]
+    public async Task ComponentInheritsFromComponent()
+    {
+        // Arrange
+        var project = CreateTestProject(new()
+        {
+            ["Views/Home/Index.cshtml"] = """
+                @(await Html.RenderComponentAsync<MyApp.Shared.Component1>(RenderMode.Static))
+                """,
+            ["Shared/Component1.razor"] = """
+                Hello from Component1
+                <DerivedComponent />
+                """,
+            ["Shared/BaseComponent.razor"] = """
+                Hello from Base
+                """,
+            ["Shared/DerivedComponent.razor"] = """
+                @inherits BaseComponent
+                Hello from Derived
+                """
+        });
+        var compilation = await project.GetCompilationAsync();
+        var driver = await GetDriverAsync(project, options =>
+        {
+            options.TestGlobalOptions["build_property.RazorLangVersion"] = "7.0";
+        });
+
+        // Act
+        var result = RunGenerator(compilation!, ref driver, out compilation);
+
+        // Assert
+        Assert.Empty(result.Diagnostics);
+        Assert.Equal(4, result.GeneratedSources.Length);
+        await VerifyRazorPageMatchesBaselineAsync(compilation, "Views_Home_Index");
+    }
+}
