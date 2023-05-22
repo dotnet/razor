a6a61fdfa748eaa65aab53dab583276e26af4a3e
Jan Jones <janjones@microsoft.com>
2023-05-22 18:07:23 +0200

    [17.6] Revert "Perf/generator (#8212)" (#8742)
    * Revert "Perf/generator (#8212)"
    
    This reverts commit 4ab512e7f0690cafa6e1515667d88625dc24f015.
    
    * Add more tests

--- src/Compiler/test/Microsoft.NET.Sdk.Razor.SourceGenerators.Tests/RazorSourceGeneratorTagHelperTests.cs
+++ src/Compiler/test/Microsoft.NET.Sdk.Razor.SourceGenerators.Tests/RazorSourceGeneratorTagHelperTests.cs
@@ -5,6 +5,7 @@
 
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Razor.Test.Common;
 using Xunit;
 
 namespace Microsoft.NET.Sdk.Razor.SourceGenerators;
@@ -86,6 +87,109 @@ public sealed class RazorSourceGeneratorTagHelperTests : RazorSourceGeneratorTes
         // Assert
         Assert.Contains("HtmlTargetElementAttribute(\"vc:test\")", result.GeneratedSources.Single().SourceText.ToString());
         result.VerifyOutputsMatchBaseline();
+        await VerifyRazorPageMatchesBaselineAsync(compilation, "Views_Home_Index");
+    }
+
+    [Fact, WorkItem("https://github.com/dotnet/razor/issues/8718")]
+    public async Task ComponentAndTagHelper()
+    {
+        // Arrange
+        var project = CreateTestProject(new()
+        {
+            ["Views/Home/Index.cshtml"] = """
+                @addTagHelper *, TestProject
+
+                <email mail="example">custom tag helper</email>
+                """,
+            ["Shared/EmailTagHelper.razor"] = """
+                @inherits ComponentAndTagHelper
+                @code {
+                    public string? Mail { get; set; }
+                }
+                """,
+        }, new()
+        {
+            ["EmailTagHelper.cs"] = """
+                using Microsoft.AspNetCore.Razor.TagHelpers;
+                namespace MyApp.Shared;
+
+                public abstract class ComponentAndTagHelper : TagHelper
+                {
+                    protected abstract void BuildRenderTree(Microsoft.AspNetCore.Components.Rendering.RenderTreeBuilder __builder);
+                }
+
+                public partial class EmailTagHelper : ComponentAndTagHelper
+                {
+                    public override void Process(TagHelperContext context, TagHelperOutput output)
+                    {
+                        output.TagName = "a";
+                        output.Attributes.SetAttribute("href", $"mailto:{Mail}");
+                    }
+                }
+                """
+        });
+        var compilation = await project.GetCompilationAsync();
+        var driver = await GetDriverAsync(project);
+
+        // Act
+        var result = RunGenerator(compilation!, ref driver, out compilation);
+
+        // Assert
+        Assert.Empty(result.Diagnostics);
+        Assert.Equal(2, result.GeneratedSources.Length);
+        await VerifyRazorPageMatchesBaselineAsync(compilation, "Views_Home_Index");
+    }
+
+    [Fact, WorkItem("https://github.com/dotnet/razor/issues/8718")]
+    public async Task ComponentAndTagHelper_HtmlTargetElement()
+    {
+        // Arrange
+        var project = CreateTestProject(new()
+        {
+            ["Views/Home/Index.cshtml"] = """
+                @addTagHelper *, TestProject
+
+                <email mail="example1">inside email</email>
+                <mail mail="example2">inside mail</mail>
+                """,
+            ["Shared/EmailTagHelper.razor"] = """
+                @using Microsoft.AspNetCore.Razor.TagHelpers;
+                @attribute [HtmlTargetElement("mail")]
+                @inherits ComponentAndTagHelper
+                @code {
+                    public string? Mail { get; set; }
+                }
+                """,
+        }, new()
+        {
+            ["EmailTagHelper.cs"] = """
+                using Microsoft.AspNetCore.Razor.TagHelpers;
+                namespace MyApp.Shared;
+
+                public abstract class ComponentAndTagHelper : TagHelper
+                {
+                    protected abstract void BuildRenderTree(Microsoft.AspNetCore.Components.Rendering.RenderTreeBuilder __builder);
+                }
+
+                public partial class EmailTagHelper : ComponentAndTagHelper
+                {
+                    public override void Process(TagHelperContext context, TagHelperOutput output)
+                    {
+                        output.TagName = "a";
+                        output.Attributes.SetAttribute("href", $"mailto:{Mail}");
+                    }
+                }
+                """
+        });
+        var compilation = await project.GetCompilationAsync();
+        var driver = await GetDriverAsync(project);
+
+        // Act
+        var result = RunGenerator(compilation!, ref driver, out compilation);
+
+        // Assert
+        Assert.Empty(result.Diagnostics);
+        Assert.Equal(2, result.GeneratedSources.Length);
         await VerifyRazorPageMatchesBaselineAsync(compilation, "Views_Home_Index");
     }
 }
