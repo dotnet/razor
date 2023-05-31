50c3b2b8025b7618e3b917636172c17f881dba2b
Dustin Campbell <dustin@teamcampbell.org>
2023-05-31 10:54:17 -0700

    Add a handful of unit tests

--- src/Compiler/Microsoft.AspNetCore.Razor.Language/test/DefaultBoundAttributeDescriptorBuilderTest.cs
+++ src/Compiler/Microsoft.AspNetCore.Razor.Language/test/DefaultBoundAttributeDescriptorBuilderTest.cs
@@ -1,8 +1,6 @@
 ﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-#nullable disable
-
 using Xunit;
 using static Microsoft.AspNetCore.Razor.Language.CommonMetadata;
 
@@ -45,5 +43,67 @@ public class DefaultBoundAttributeDescriptorBuilderTest
 
         // Assert
         Assert.Equal("int TestTagHelper.SomeProperty", descriptor.DisplayName);
+    }
+
+    [Fact]
+    public void Metadata_Same()
+    {
+        // When SetMetadata is called on multiple builders with the same metadata collection,
+        // they should share the instance.
+
+        // Arrange
+        var tagHelperBuilder = new DefaultTagHelperDescriptorBuilder(TagHelperConventions.DefaultKind, "TestTagHelper", "Test");
+
+        var metadata = MetadataCollection.Create(PropertyName("SomeProperty"));
+
+        var builder1 = new DefaultBoundAttributeDescriptorBuilder(tagHelperBuilder, TagHelperConventions.DefaultKind)
+        {
+            TypeName = typeof(int).FullName
+        };
+
+        var builder2 = new DefaultBoundAttributeDescriptorBuilder(tagHelperBuilder, TagHelperConventions.DefaultKind)
+        {
+            TypeName = typeof(int).FullName
+        };
+
+        builder1.SetMetadata(metadata);
+        builder2.SetMetadata(metadata);
+
+        // Act
+        var descriptor1 = builder1.Build();
+        var descriptor2 = builder2.Build();
+
+        // Assert
+        Assert.Same(descriptor1.Metadata, descriptor2.Metadata);
+    }
+
+    [Fact]
+    public void Metadata_NotSame()
+    {
+        // When Metadata is accessed on multiple builders with the same metadata,
+        // they do not share the instance.
+
+        // Arrange
+        var tagHelperBuilder = new DefaultTagHelperDescriptorBuilder(TagHelperConventions.DefaultKind, "TestTagHelper", "Test");
+
+        var builder1 = new DefaultBoundAttributeDescriptorBuilder(tagHelperBuilder, TagHelperConventions.DefaultKind)
+        {
+            TypeName = typeof(int).FullName
+        };
+
+        var builder2 = new DefaultBoundAttributeDescriptorBuilder(tagHelperBuilder, TagHelperConventions.DefaultKind)
+        {
+            TypeName = typeof(int).FullName
+        };
+
+        builder1.Metadata.Add(PropertyName("SomeProperty"));
+        builder2.Metadata.Add(PropertyName("SomeProperty"));
+
+        // Act
+        var descriptor1 = builder1.Build();
+        var descriptor2 = builder2.Build();
+
+        // Assert
+        Assert.NotSame(descriptor1.Metadata, descriptor2.Metadata);
     }
 }
