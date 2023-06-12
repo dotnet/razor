9f48d5ea0f5d825cee9c8cfee437cef2f1b09a63
Dustin Campbell <dustin@teamcampbell.org>
2023-06-12 08:58:50 -0700

    Interpolated string handler attributes should be internal

--- src/Shared/Microsoft.AspNetCore.Razor.Utilities.Shared/LanguageSupport/InterpolatedStringHandlerArgumentAttribute.cs
+++ src/Shared/Microsoft.AspNetCore.Razor.Utilities.Shared/LanguageSupport/InterpolatedStringHandlerArgumentAttribute.cs
@@ -9,7 +9,7 @@ namespace System.Runtime.CompilerServices;
 
 /// <summary>Indicates which arguments to a method involving an interpolated string handler should be passed to that handler.</summary>
 [AttributeUsage(AttributeTargets.Parameter, AllowMultiple = false, Inherited = false)]
-public sealed class InterpolatedStringHandlerArgumentAttribute : Attribute
+internal sealed class InterpolatedStringHandlerArgumentAttribute : Attribute
 {
     /// <summary>Initializes a new instance of the <see cref="InterpolatedStringHandlerArgumentAttribute"/> class.</summary>
     /// <param name="argument">The name of the argument that should be passed to the handler.</param>
--- src/Shared/Microsoft.AspNetCore.Razor.Utilities.Shared/LanguageSupport/InterpolatedStringHandlerAttribute.cs
+++ src/Shared/Microsoft.AspNetCore.Razor.Utilities.Shared/LanguageSupport/InterpolatedStringHandlerAttribute.cs
@@ -9,7 +9,7 @@ namespace System.Runtime.CompilerServices;
 
 /// <summary>Indicates the attributed type is to be used as an interpolated string handler.</summary>
 [AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct, AllowMultiple = false, Inherited = false)]
-public sealed class InterpolatedStringHandlerAttribute : Attribute
+internal sealed class InterpolatedStringHandlerAttribute : Attribute
 {
     /// <summary>Initializes the <see cref="InterpolatedStringHandlerAttribute"/>.</summary>
     public InterpolatedStringHandlerAttribute() { }
