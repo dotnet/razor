f9aece03c52eb1302f8d9bcdf47a27f263333e69
David Wengier <david.wengier@microsoft.com>
2023-06-22 14:09:25 +1000

    Skip flaky test

--- src/Razor/test/Microsoft.VisualStudio.Razor.IntegrationTests/WrapWithTagTests.cs
+++ src/Razor/test/Microsoft.VisualStudio.Razor.IntegrationTests/WrapWithTagTests.cs
@@ -98,7 +98,7 @@ public class WrapWithTagTests : AbstractRazorEditorTest
             """, ControlledHangMitigatingCancellationToken);
     }
 
-    [IdeFact]
+    [ConditionalSkipIdeFact(Issue = "https://github.com/dotnet/razor/issues/8856")]
     public async Task WrapWithTag_SelfClosingTag()
     {
         // Open the file
