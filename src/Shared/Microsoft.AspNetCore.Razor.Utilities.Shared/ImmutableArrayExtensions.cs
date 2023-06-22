a975f824be0c822baf7759eb205de0d62e4c9971
Dustin Campbell <dustin@teamcampbell.org>
2023-06-22 13:31:49 -0700

    Add ImmutableArray<T>.SelectAsArray(...) extension method

--- src/Shared/Microsoft.AspNetCore.Razor.Utilities.Shared/ImmutableArrayExtensions.cs
+++ src/Shared/Microsoft.AspNetCore.Razor.Utilities.Shared/ImmutableArrayExtensions.cs
@@ -1,6 +1,8 @@
 ﻿// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the MIT license. See License.txt in the project root for license information.
 
+using Microsoft.AspNetCore.Razor.PooledObjects;
+
 namespace System.Collections.Immutable;
 
 /// <summary>
@@ -55,5 +57,30 @@ internal static class ImmutableArrayExtensions
         builder.Clear();
         return result;
 #endif
+    }
+
+    public static ImmutableArray<TResult> SelectAsArray<T, TResult>(this ImmutableArray<T> source, Func<T, TResult> selector)
+    {
+        return source switch
+        {
+            [] => ImmutableArray<TResult>.Empty,
+            [var item] => ImmutableArray.Create(selector(item)),
+            [var item1, var item2] => ImmutableArray.Create(selector(item1), selector(item2)),
+            [var item1, var item2, var item3] => ImmutableArray.Create(selector(item1), selector(item2), selector(item3)),
+            [var item1, var item2, var item3, var item4] => ImmutableArray.Create(selector(item1), selector(item2), selector(item3), selector(item4)),
+            var items => BuildResult(items, selector)
+        };
+
+        static ImmutableArray<TResult> BuildResult(ImmutableArray<T> items, Func<T, TResult> selector)
+        {
+            using var _ = ArrayBuilderPool<TResult>.GetPooledObject(out var result);
+
+            foreach (var item in items)
+            {
+                result.Add(selector(item));
+            }
+
+            return result.DrainToImmutable();
+        }
     }
 }
