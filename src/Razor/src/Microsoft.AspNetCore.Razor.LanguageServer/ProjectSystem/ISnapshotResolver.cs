a87f5cb0caa1c7508f43f095889268f83d37a423
Andrew Hall <ryzngard@live.com>
2023-07-11 11:39:35 -0700

    Update src/Razor/src/Microsoft.AspNetCore.Razor.LanguageServer/ProjectSystem/ISnapshotResolver.cs
    Co-authored-by: David Wengier <david.wengier@microsoft.com>

--- src/Razor/src/Microsoft.AspNetCore.Razor.LanguageServer/ProjectSystem/ISnapshotResolver.cs
+++ src/Razor/src/Microsoft.AspNetCore.Razor.LanguageServer/ProjectSystem/ISnapshotResolver.cs
@@ -6,6 +6,7 @@ using System.Diagnostics.CodeAnalysis;
 using Microsoft.CodeAnalysis.Razor.ProjectSystem;
 
 namespace Microsoft.AspNetCore.Razor.LanguageServer.ProjectSystem;
+
 internal interface ISnapshotResolver
 {
     /// <summary>
