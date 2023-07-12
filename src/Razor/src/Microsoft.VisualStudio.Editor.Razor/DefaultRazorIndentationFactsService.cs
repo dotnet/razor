ad4b9f05493826942de06adae1eb3185a561fbc8
Dustin Campbell <dustin@teamcampbell.org>
2023-07-12 11:37:58 -0700

    Add RazorIndentationFactsService back to avoid temporary breaking change

---
+++ src/Razor/src/Microsoft.VisualStudio.Editor.Razor/DefaultRazorIndentationFactsService.cs
@@ -0,0 +1,23 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the MIT license. See License.txt in the project root for license information.
+
+using System.ComponentModel.Composition;
+using Microsoft.AspNetCore.Razor.Language;
+using Microsoft.VisualStudio.Text;
+
+namespace Microsoft.VisualStudio.Editor.Razor;
+
+[System.Composition.Shared]
+[Export(typeof(RazorIndentationFactsService))]
+internal class DefaultRazorIndentationFactsService : RazorIndentationFactsService
+{
+    public override int? GetDesiredIndentation(
+        RazorSyntaxTree syntaxTree,
+        ITextSnapshot syntaxTreeSnapshot,
+        ITextSnapshotLine line,
+        int indentSize,
+        int tabSize)
+    {
+        return RazorIndentationFacts.GetDesiredIndentation(syntaxTree, syntaxTreeSnapshot, line, indentSize, tabSize);
+    }
+}
