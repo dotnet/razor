37642c85c0fd0f4acbc03a0cedcdab05e0423857
David Wengier <david.wengier@microsoft.com>
2023-07-25 12:24:15 +1000

    Include a suffix from the project as part of the generated document
    Also fixed GetRazorFilePath so its better.

--- src/Razor/src/Microsoft.CodeAnalysis.Razor.Workspaces/DocumentFilePathProvider.cs
+++ src/Razor/src/Microsoft.CodeAnalysis.Razor.Workspaces/DocumentFilePathProvider.cs
@@ -3,6 +3,8 @@
 
 using System;
 using System.Composition;
+using System.Diagnostics;
+using Microsoft.CodeAnalysis.Razor.ProjectSystem;
 
 namespace Microsoft.CodeAnalysis.Razor.Workspaces;
 
@@ -18,9 +20,11 @@ internal class DocumentFilePathProvider
         _languageServerFeatureOptions = languageServerFeatureOptions;
     }
 
-    public string GetRazorCSharpFilePath(string razorFilePath) => razorFilePath + _languageServerFeatureOptions.CSharpVirtualDocumentSuffix;
+    public string GetRazorCSharpFilePath(ProjectKey projectKey, string razorFilePath)
+        => GetGeneratedFilePath(projectKey, razorFilePath, _languageServerFeatureOptions.CSharpVirtualDocumentSuffix);
 
-    public string GetRazorHtmlFilePath(string razorFilePath) => razorFilePath + _languageServerFeatureOptions.HtmlVirtualDocumentSuffix;
+    public string GetRazorHtmlFilePath(ProjectKey projectKey, string razorFilePath)
+        => GetGeneratedFilePath(projectKey, razorFilePath, _languageServerFeatureOptions.HtmlVirtualDocumentSuffix);
 
     public Uri GetRazorDocumentUri(Uri virtualDocumentUri)
     {
@@ -44,9 +48,44 @@ internal class DocumentFilePathProvider
 
     private string GetRazorFilePath(string filePath)
     {
-        filePath = filePath.Replace(_languageServerFeatureOptions.CSharpVirtualDocumentSuffix, string.Empty);
-        filePath = filePath.Replace(_languageServerFeatureOptions.HtmlVirtualDocumentSuffix, string.Empty);
+        var trimIndex = filePath.LastIndexOf(_languageServerFeatureOptions.CSharpVirtualDocumentSuffix);
+        if (trimIndex == -1)
+        {
+            trimIndex = filePath.LastIndexOf(_languageServerFeatureOptions.HtmlVirtualDocumentSuffix);
+        }
+
+        if (trimIndex != -1 && _languageServerFeatureOptions.IncludeProjectKeyInGeneratedFilePath)
+        {
+            trimIndex = filePath.LastIndexOf('.', trimIndex);
+            Debug.Assert(trimIndex != -1, "There was no project element to the generated file name?");
+        }
+
+        if (trimIndex != -1)
+        {
+            return filePath.Substring(0, trimIndex);
+        }
 
         return filePath;
+    }
+
+    private string GetGeneratedFilePath(ProjectKey projectKey, string razorFilePath, string suffix)
+    {
+        var projectSuffix = GetProjectSuffix(projectKey);
+
+        return razorFilePath + projectSuffix + suffix;
+    }
+
+    private string GetProjectSuffix(ProjectKey _)
+    {
+        if (!_languageServerFeatureOptions.IncludeProjectKeyInGeneratedFilePath)
+        {
+            return string.Empty;
+        }
+
+        // TODO: Use projectKey to make this dynamic
+        var projectToken = "project";
+
+        Debug.Assert(!projectToken.Contains("."), "Project token can't contain a dot or the GetRazorFilePath method will fail.");
+        return "." + projectToken;
     }
 }
