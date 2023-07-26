c1f770b456dda4712a8b39a8820c78f08968dc3b
Allison Chou <allichou@microsoft.com>
2023-07-25 18:41:22 -0700

    Revert "Remove Razor.VSCode.Grammar.Test"
    This reverts commit bc4a68cccea1c2940f2360de9b688deb49917a83.

---
+++ src/Razor/test/Microsoft.AspNetCore.Razor.VSCode.Grammar.Test/jest.config.js
@@ -0,0 +1,15 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the MIT license. See License.txt in the project root for license information.
+
+module.exports = {
+  globals: {
+    "ts-jest": {
+      "tsConfig": "./tsconfig.json",
+      "babeConfig": true,
+      "diagnostics": true
+    }
+  },
+  testPathIgnorePatterns: [ 'dist' ],
+  preset: 'ts-jest',
+  testEnvironment: 'jsdom'
+};
