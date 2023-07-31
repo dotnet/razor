22e411c87e80935d7f3317753c0962cf2c6edf2a
David Wengier <david.wengier@microsoft.com>
2023-07-31 19:35:09 +1000

    Update various code paths to allow for multiple virtual documents
    This started as pseudocode to allow me to reason about the shape of the API I'd need, and ended up being something that is pretty close to done, and more importantly, something that should work fine with our current system. This could have been part of a subsequent PR, but I though it might help you all understand the API better.

--- src/Razor/src/Microsoft.VisualStudio.LanguageServerClient.Razor/CSharpVirtualDocument.cs
+++ src/Razor/src/Microsoft.VisualStudio.LanguageServerClient.Razor/CSharpVirtualDocument.cs
@@ -2,6 +2,7 @@
 // Licensed under the MIT license. See License.txt in the project root for license information.
 
 using System;
+using Microsoft.CodeAnalysis.Razor.ProjectSystem;
 using Microsoft.VisualStudio.LanguageServer.ContainedLanguage;
 using Microsoft.VisualStudio.Text;
 
@@ -9,9 +10,13 @@ namespace Microsoft.VisualStudio.LanguageServerClient.Razor;
 
 internal class CSharpVirtualDocument : VirtualDocumentBase<CSharpVirtualDocumentSnapshot>
 {
-    public CSharpVirtualDocument(Uri uri, ITextBuffer textBuffer) : base(uri, textBuffer)
+    private readonly ProjectKey _projectKey;
+
+    public CSharpVirtualDocument(ProjectKey projectKey, Uri uri, ITextBuffer textBuffer)
+        : base(uri, textBuffer)
     {
+        _projectKey = projectKey;
     }
 
-    protected override CSharpVirtualDocumentSnapshot GetUpdatedSnapshot(object? state) => new(Uri, TextBuffer.CurrentSnapshot, HostDocumentVersion);
+    protected override CSharpVirtualDocumentSnapshot GetUpdatedSnapshot(object? state) => new(_projectKey, Uri, TextBuffer.CurrentSnapshot, HostDocumentVersion);
 }
