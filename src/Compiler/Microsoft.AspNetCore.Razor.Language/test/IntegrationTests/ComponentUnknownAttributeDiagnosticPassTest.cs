6fa6f0b86d2a1010878b15647cfed1a632ab73f8
SparkyTD <45818400+SparkyTD@users.noreply.github.com>
2023-08-21 20:49:05 +0300

    Added test cases for ComponentUnknownAttributeDiagnosticPass

---
+++ src/Compiler/Microsoft.AspNetCore.Razor.Language/test/IntegrationTests/ComponentUnknownAttributeDiagnosticPassTest.cs
@@ -0,0 +1,148 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Razor.Language.Components;
+using Microsoft.AspNetCore.Razor.Language.IntegrationTests;
+using Microsoft.AspNetCore.Razor.Language.Intermediate;
+using Xunit;
+
+namespace Microsoft.AspNetCore.Razor.Language.IntegrationTests;
+
+public class ComponentUnknownAttributeDiagnosticPassTest : RazorIntegrationTestBase
+{
+    public ComponentUnknownAttributeDiagnosticPassTest()
+    {
+        Pass = new ComponentUnknownAttributeDiagnosticPass();
+        ProjectEngine = (DefaultRazorProjectEngine)RazorProjectEngine.Create(
+            RazorConfiguration.Default,
+            RazorProjectFileSystem.Create(Environment.CurrentDirectory),
+            b =>
+            {
+                // Don't run the markup mutating passes.
+                b.Features.Remove(b.Features.OfType<ComponentMarkupDiagnosticPass>().Single());
+                b.Features.Remove(b.Features.OfType<ComponentMarkupBlockPass>().Single());
+                b.Features.Remove(b.Features.OfType<ComponentMarkupEncodingPass>().Single());
+            });
+        Engine = ProjectEngine.Engine;
+
+        Pass.Engine = Engine;
+    }
+
+    private DefaultRazorProjectEngine ProjectEngine { get; }
+    private RazorEngine Engine { get; }
+    private ComponentUnknownAttributeDiagnosticPass Pass { get; set; }
+    internal override string FileKind => FileKinds.Component;
+    internal override bool UseTwoPhaseCompilation => true;
+
+    [Fact]
+    public void Execute_NoInvalidAttributes()
+    {
+        // Arrange
+        AdditionalSyntaxTrees.Add(Parse(@"
+using System;
+using Microsoft.AspNetCore.Components;
+
+namespace Test
+{
+    public class MyComponent : ComponentBase
+    {
+        [Parameter] public int Value { get; set; }
+    }
+}
+"));
+        var result = CompileToCSharp(@"<MyComponent Value=""123"" />");
+        var document = result.CodeDocument;
+        var documentNode = Lower(document);
+
+        // Act
+        Pass.Execute(document, documentNode);
+
+        // Assert
+        Assert.Empty(documentNode.GetAllDiagnostics());
+    }
+
+    [Fact]
+    public void Execute_OneInvalidAttribute()
+    {
+        // Arrange
+        AdditionalSyntaxTrees.Add(Parse(@"
+using System;
+using Microsoft.AspNetCore.Components;
+
+namespace Test
+{
+    public class MyComponent : ComponentBase
+    {
+        [Parameter] public int Value { get; set; }
+    }
+}
+"));
+        var result = CompileToCSharp(@"<MyComponent InvalidAttribute=""123"" />");
+        var document = result.CodeDocument;
+        var documentNode = Lower(document);
+
+        // Act
+        Pass.Execute(document, documentNode);
+
+        // Assert
+        var diagnostic = Assert.Single(documentNode.GetAllDiagnostics());
+        Assert.Equal(ComponentDiagnosticFactory.UnknownMarkupAttribute.Id, diagnostic.Id);
+
+        var node = documentNode.FindDescendantNodes<ComponentAttributeIntermediateNode>().Where(n => n.HasDiagnostics).Single();
+        Assert.Equal("InvalidAttribute", node.AttributeName);
+    }
+
+    [Fact]
+    public void Execute_CaptureAdditionalAttributes()
+    {
+        // Arrange
+        AdditionalSyntaxTrees.Add(Parse(@"
+using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Components;
+
+namespace Test
+{
+    public class MyComponent : ComponentBase
+    {
+        [Parameter] public int Value { get; set; }
+
+        [Parameter(CaptureUnmatchedValues = true)]
+        public IDictionary<string, object> AdditionalAttributes { get; set; }
+    }
+}
+"));
+        var result = CompileToCSharp(@"<MyComponent InvalidAttribute=""123"" />");
+        var document = result.CodeDocument;
+        var documentNode = Lower(document);
+
+        // Act
+        Pass.Execute(document, documentNode);
+
+        // Assert
+        Assert.Empty(documentNode.GetAllDiagnostics());
+    }
+
+    private DocumentIntermediateNode Lower(RazorCodeDocument codeDocument)
+    {
+        for (var i = 0; i < Engine.Phases.Count; i++)
+        {
+            var phase = Engine.Phases[i];
+            if (phase is IRazorCSharpLoweringPhase)
+            {
+                break;
+            }
+
+            phase.Execute(codeDocument);
+        }
+
+        var document = codeDocument.GetDocumentIntermediateNode();
+        Engine.Features.OfType<ComponentDocumentClassifierPass>().Single().Execute(codeDocument, document);
+        return document;
+    }
+}
