c8754b61ea44e433f58d16591448779101441cc5
SparkyTD <45818400+SparkyTD@users.noreply.github.com>
2023-08-21 20:48:55 +0300

    Introduced ComponentUnknownAttributeDiagnosticPass
    This new intermediate pass will check for any component attributes that cannot be mapped to a valid property in the underlying component type, for the purpose of issuing a warning diagnostic.

---
+++ src/Compiler/Microsoft.AspNetCore.Razor.Language/src/Components/ComponentUnknownAttributeDiagnosticPass.cs
@@ -0,0 +1,47 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using Microsoft.AspNetCore.Razor.Language.Intermediate;
+
+namespace Microsoft.AspNetCore.Razor.Language.Components;
+internal sealed class ComponentUnknownAttributeDiagnosticPass : ComponentIntermediateNodePassBase, IRazorOptimizationPass
+{
+    protected override void ExecuteCore(RazorCodeDocument codeDocument, DocumentIntermediateNode documentNode)
+    {
+        var visitor = new Visitor();
+        visitor.Visit(documentNode);
+    }
+
+    private class Visitor : IntermediateNodeWalker
+    {
+        public override void VisitComponent(ComponentIntermediateNode node)
+        {
+            // First, check if there is a property of type 'IDictionary<string, object>'
+            // with 'CaptureUnmatchedValues' set to 'true'
+            var component = node.Component;
+            var boundComponentAttributes = component.BoundAttributes;
+            for (var i = 0; i < boundComponentAttributes.Count; i++)
+            {
+                var attribute = boundComponentAttributes[i];
+                // [HELP NEEDED] I would need to access component Type information here in order to check for CaptureUnmatchedValues
+            }
+
+
+            // If no arbitrary attributes can be accepted by the component, check if all
+            // the user-specified attribute names map to an underlying property
+            for (var i = 0; i < node.Children.Count; i++)
+            {
+                if (node.Children[i] is ComponentAttributeIntermediateNode attribute && attribute.AttributeName != null)
+                {
+                    if (attribute.BoundAttribute == null)
+                    {
+                        attribute.Diagnostics.Add(ComponentDiagnosticFactory.Create_UnknownMarkupAttribute(
+                            attribute.AttributeName, attribute.Source));
+                    }
+                }
+            }
+
+            base.VisitComponent(node);
+        }
+    }
+}
