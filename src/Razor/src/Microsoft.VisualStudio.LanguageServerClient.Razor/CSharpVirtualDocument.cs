3096735c4dea11a9288ba6658939cb1d902fa55f
David Wengier <david.wengier@microsoft.com>
2023-08-21 16:54:30 +1000

    Make sure we don't introduce double content in generated C# files

--- src/Razor/src/Microsoft.VisualStudio.LanguageServerClient.Razor/CSharpVirtualDocument.cs
+++ src/Razor/src/Microsoft.VisualStudio.LanguageServerClient.Razor/CSharpVirtualDocument.cs
@@ -2,6 +2,8 @@
 // Licensed under the MIT license. See License.txt in the project root for license information.
 
 using System;
+using System.Collections.Generic;
+using System.Diagnostics;
 using Microsoft.CodeAnalysis.Razor.ProjectSystem;
 using Microsoft.VisualStudio.LanguageServer.ContainedLanguage;
 using Microsoft.VisualStudio.Text;
@@ -19,4 +21,20 @@ internal class CSharpVirtualDocument : VirtualDocumentBase<CSharpVirtualDocument
     }
 
     protected override CSharpVirtualDocumentSnapshot GetUpdatedSnapshot(object? state) => new(_projectKey, Uri, TextBuffer.CurrentSnapshot, HostDocumentVersion);
+
+    public override VirtualDocumentSnapshot Update(IReadOnlyList<ITextChange> changes, int hostDocumentVersion, object? state)
+    {
+        var result = base.Update(changes, hostDocumentVersion, state);
+
+#if DEBUG
+        var text = TextBuffer.CurrentSnapshot.GetText();
+
+        var generatedFileStartIndex = text.IndexOf("#pragma warning disable 1591");
+        var secondGeneratedFileStartIndex = text.IndexOf("#pragma warning disable 1591", generatedFileStartIndex + 20);
+
+        Debug.Assert(secondGeneratedFileStartIndex == -1, "Generated C# file appears to have duplicated file contents. This could indicate a sync problem between language server and client.");
+#endif
+
+        return result;
+    }
 }
