6f119bf91e451aff92ef4c9d8d0ef11dd4bbdfe3
Jan Jones <janjones@microsoft.com>
2023-08-23 15:18:25 +0200

    Add compiler support for `@formname` attribute

--- src/Compiler/Microsoft.AspNetCore.Razor.Language/src/Components/ComponentNodeWriter.cs
+++ src/Compiler/Microsoft.AspNetCore.Razor.Language/src/Components/ComponentNodeWriter.cs
@@ -5,6 +5,7 @@
 
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using Microsoft.AspNetCore.Razor.Language.CodeGeneration;
 using Microsoft.AspNetCore.Razor.Language.Extensions;
