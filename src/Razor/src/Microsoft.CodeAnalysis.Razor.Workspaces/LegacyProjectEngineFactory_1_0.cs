92e7e566b83a3801fca35424320de386722b947f
Jan Jones <janjones@microsoft.com>
2023-08-23 10:32:32 +0200

    Rename VersionY_X DLLs

--- src/Razor/src/Microsoft.CodeAnalysis.Razor.Workspaces/LegacyProjectEngineFactory_1_0.cs
+++ src/Razor/src/Microsoft.CodeAnalysis.Razor.Workspaces/LegacyProjectEngineFactory_1_0.cs
@@ -12,7 +12,7 @@ namespace Microsoft.CodeAnalysis.Razor.Workspaces;
 [ExportCustomProjectEngineFactory("MVC-1.0", SupportsSerialization = true)]
 internal class LegacyProjectEngineFactory_1_0 : IProjectEngineFactory
 {
-    private const string AssemblyName = "Microsoft.AspNetCore.Mvc.Razor.Extensions.Version1_X";
+    private const string AssemblyName = "Microsoft.CodeAnalysis.Razor.Compiler.Mvc.Version1_X";
 
     public RazorProjectEngine Create(RazorConfiguration configuration, RazorProjectFileSystem fileSystem, Action<RazorProjectEngineBuilder> configure)
     {
