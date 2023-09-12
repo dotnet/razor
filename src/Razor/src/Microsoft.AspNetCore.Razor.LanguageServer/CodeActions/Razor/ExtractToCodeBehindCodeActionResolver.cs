b62fe90002131e37fc3b418940917ef9adebef8d
David Wengier <david.wengier@microsoft.com>
2023-09-12 13:38:37 +1000

    Whitespace

--- src/Razor/src/Microsoft.AspNetCore.Razor.LanguageServer/CodeActions/Razor/ExtractToCodeBehindCodeActionResolver.cs
+++ src/Razor/src/Microsoft.AspNetCore.Razor.LanguageServer/CodeActions/Razor/ExtractToCodeBehindCodeActionResolver.cs
@@ -178,8 +178,8 @@ internal sealed class ExtractToCodeBehindCodeActionResolver : IRazorCodeActionRe
         using var _ = StringBuilderPool.GetPooledObject(out var builder);
 
         var usingDirectives = razorCodeDocument
-                    .GetDocumentIntermediateNode()
-                    .FindDescendantNodes<UsingDirectiveIntermediateNode>();
+            .GetDocumentIntermediateNode()
+            .FindDescendantNodes<UsingDirectiveIntermediateNode>();
         foreach (var usingDirective in usingDirectives)
         {
             builder.Append("using ");
