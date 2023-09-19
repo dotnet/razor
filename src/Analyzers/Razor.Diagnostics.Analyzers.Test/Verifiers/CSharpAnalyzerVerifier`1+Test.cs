757afa11a04a9cee24d7e1f8cd3cb2cd1ccd65e0
Dustin Campbell <dustin@teamcampbell.org>
2023-09-18 21:13:31 -0700

    Move analyzers into separate solution

--- src/Analyzers/Razor.Diagnostics.Analyzers.Test/Verifiers/CSharpAnalyzerVerifier`1+Test.cs
+++ src/Analyzers/Razor.Diagnostics.Analyzers.Test/Verifiers/CSharpAnalyzerVerifier`1+Test.cs
@@ -1,7 +1,6 @@
 ﻿// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the MIT license. See License.txt in the project root for license information.
 
-using Microsoft.AspNetCore.Razor;
 using Microsoft.CodeAnalysis.CSharp.Testing;
 using Microsoft.CodeAnalysis.Diagnostics;
 using Microsoft.CodeAnalysis.Testing.Verifiers;
@@ -17,8 +16,8 @@ public static partial class CSharpAnalyzerVerifier<TAnalyzer>
         {
             SolutionTransforms.Add((solution, projectId) =>
             {
-                var compilationOptions = solution.GetProject(projectId).AssumeNotNull().CompilationOptions;
-                compilationOptions = compilationOptions.AssumeNotNull().WithSpecificDiagnosticOptions(
+                var compilationOptions = solution.GetProject(projectId)!.CompilationOptions;
+                compilationOptions = compilationOptions!.WithSpecificDiagnosticOptions(
                     compilationOptions.SpecificDiagnosticOptions.SetItems(CSharpVerifierHelper.NullableWarnings));
                 solution = solution.WithProjectCompilationOptions(projectId, compilationOptions);
 
