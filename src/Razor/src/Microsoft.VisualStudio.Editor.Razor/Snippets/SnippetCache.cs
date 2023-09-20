83226d2678de1ce2d6f4fe34d44e146f1331b36f
Andrew Hall <andrha@microsoft.com>
2023-09-20 16:30:38 -0700

    Get completions populating the cache

---
+++ src/Razor/src/Microsoft.VisualStudio.Editor.Razor/Snippets/SnippetCache.cs
@@ -0,0 +1,29 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the MIT license. See License.txt in the project root for license information.
+
+using System.Collections.Immutable;
+using System.Composition;
+using Microsoft.AspNetCore.Razor;
+
+namespace Microsoft.VisualStudio.Editor.Razor.Snippets;
+
+[Export(typeof(SnippetCache)), Shared]
+internal class SnippetCache
+{
+    private ReadWriterLocker _lock = new();
+    private ImmutableArray<SnippetInfo> _snippets;
+
+    internal void Update(ImmutableArray<SnippetInfo> snippets)
+    {
+        using (_lock.EnterWriteLock())
+        {
+            _snippets = snippets;
+        }
+    }
+
+    public ImmutableArray<SnippetInfo> GetSnippets()
+    {
+        using var _ = _lock.EnterReadLock();
+        return _snippets;
+    }
+}
---
+++ src/Razor/src/Microsoft.VisualStudio.Editor.Razor/Snippets/SnippetInfo.cs
@@ -0,0 +1,15 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the MIT license. See License.txt in the project root for license information.
+
+namespace Microsoft.VisualStudio.Editor.Razor.Snippets;
+
+internal record SnippetInfo(string Shortcut, string Title, string Description, string Path, SnippetLanguage Language)
+{
+}
+
+internal enum SnippetLanguage
+{
+    CSharp,
+    Html,
+    Razor
+}
