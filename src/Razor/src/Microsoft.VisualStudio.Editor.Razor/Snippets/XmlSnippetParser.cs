2646be0c81a0de627f69c4ce804f63d004ab06db
Andrew Hall <andrha@microsoft.com>
2023-10-03 13:44:41 -0700

    Remove usings

--- src/Razor/src/Microsoft.VisualStudio.Editor.Razor/Snippets/XmlSnippetParser.cs
+++ src/Razor/src/Microsoft.VisualStudio.Editor.Razor/Snippets/XmlSnippetParser.cs
@@ -2,8 +2,6 @@
 // Licensed under the MIT license. See License.txt in the project root for license information.
 
 using System;
-using System.Collections.Concurrent;
-using System.Composition;
 using System.Diagnostics;
 using System.IO;
 using Microsoft.Extensions.Logging;
