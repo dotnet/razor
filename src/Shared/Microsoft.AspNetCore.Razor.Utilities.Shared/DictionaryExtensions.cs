4e14ca135965681d348862b501ef9feb653e21f2
Andrew Hall <andrha@microsoft.com>
2023-10-06 13:03:34 -0700

    Fix warnings and change namespace

--- src/Shared/Microsoft.AspNetCore.Razor.Utilities.Shared/DictionaryExtensions.cs
+++ src/Shared/Microsoft.AspNetCore.Razor.Utilities.Shared/DictionaryExtensions.cs
@@ -1,11 +1,9 @@
 ﻿// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the MIT license. See License.txt in the project root for license information.
 
-using System;
-using System.Collections.Generic;
 using Microsoft.AspNetCore.Razor.PooledObjects;
 
-namespace Microsoft.AspNetCore.Razor;
+namespace System.Collections.Generic;
 
 internal static class DictionaryExtensions
 {
