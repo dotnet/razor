5373d374145e9cebaa766d3a68e5d19b10e813c0
Dustin Campbell <dustin@teamcampbell.org>
2023-10-11 15:06:57 -0700

    Introduce infrastructure to collect tag helpers from symbols with caching

---
+++ src/Compiler/Microsoft.AspNetCore.Razor.Language/src/TagHelperCollector.cs
@@ -0,0 +1,67 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using Microsoft.AspNetCore.Razor.PooledObjects;
+using Microsoft.CodeAnalysis;
+
+namespace Microsoft.AspNetCore.Razor.Language;
+
+internal abstract class TagHelperCollector
+{
+    private static readonly ConditionalWeakTable<IAssemblySymbol, TagHelperDescriptor[]> s_perAssemblyTagHelperCache = new();
+
+    private readonly Compilation _compilation;
+    private readonly ISymbol? _targetSymbol;
+
+    protected TagHelperCollector(Compilation compilation, ISymbol? targetSymbol)
+    {
+        _compilation = compilation;
+        _targetSymbol = targetSymbol;
+    }
+
+    protected abstract void Collect(ISymbol symbol, ICollection<TagHelperDescriptor> results);
+
+    public void Collect(ICollection<TagHelperDescriptor> results)
+    {
+        if (_targetSymbol is not null)
+        {
+            Collect(_targetSymbol, results);
+        }
+        else
+        {
+            Collect(_compilation.Assembly.GlobalNamespace, results);
+
+            foreach (var reference in _compilation.References)
+            {
+                if (_compilation.GetAssemblyOrModuleSymbol(reference) is IAssemblySymbol assembly)
+                {
+                    TagHelperDescriptor[] tagHelpers;
+
+                    lock (s_perAssemblyTagHelperCache)
+                    {
+                        // Check to see if we already have tag helpers cached for this assembly
+                        // and use them cached versions if we do. Roslyn shares PE assembly symbols
+                        // across compilations, so this ensures that we don't produce new tag helpers
+                        // for the same assemblies over and over again.
+                        if (!s_perAssemblyTagHelperCache.TryGetValue(assembly, out tagHelpers))
+                        {
+                            using var _ = ListPool<TagHelperDescriptor>.GetPooledObject(out var referenceTagHelpers);
+                            Collect(assembly.GlobalNamespace, referenceTagHelpers);
+
+                            tagHelpers = referenceTagHelpers.ToArrayOrEmpty();
+
+                            s_perAssemblyTagHelperCache.Add(assembly, tagHelpers);
+                        }
+                    }
+
+                    foreach (var tagHelper in tagHelpers)
+                    {
+                        results.Add(tagHelper);
+                    }
+                }
+            }
+        }
+    }
+}
