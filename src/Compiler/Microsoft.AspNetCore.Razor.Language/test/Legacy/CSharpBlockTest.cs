9f5c9adf65bc503ecbb417ce23b2fd3ff636e27a
Fredric Silberberg <frsilb@microsoft.com>
2023-10-26 10:53:10 -0700

    Fix another BOM change

--- src/Compiler/Microsoft.AspNetCore.Razor.Language/test/Legacy/CSharpBlockTest.cs
+++ src/Compiler/Microsoft.AspNetCore.Razor.Language/test/Legacy/CSharpBlockTest.cs
@@ -1,4 +1,4 @@
-﻿// Licensed to the .NET Foundation under one or more agreements.
+// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
 #nullable disable
@@ -44,7 +44,7 @@ public class CSharpBlockTest() : ParserTestBase(layerName: "Compiler")
     {
         ParseDocumentTest(
 @"@{
-    void Foo()
+    void Foo() 
     {
         var time = DateTime.Now
         <strong>Hello the time is @time</strong>
