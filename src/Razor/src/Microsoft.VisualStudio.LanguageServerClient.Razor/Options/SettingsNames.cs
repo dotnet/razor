f30281733890e4643da2fe36e2acb1eebda0b53c
Andrew Hall <ryzngard@live.com>
2023-12-18 13:32:49 -0800

    Update src/Razor/src/Microsoft.VisualStudio.LanguageServerClient.Razor/Options/SettingsNames.cs
    Co-authored-by: David Wengier <david.wengier@microsoft.com>

--- src/Razor/src/Microsoft.VisualStudio.LanguageServerClient.Razor/Options/SettingsNames.cs
+++ src/Razor/src/Microsoft.VisualStudio.LanguageServerClient.Razor/Options/SettingsNames.cs
@@ -2,6 +2,7 @@
 // Licensed under the MIT license. See License.txt in the project root for license information.
 
 namespace Microsoft.VisualStudio.LanguageServerClient.Razor.Options;
+
 internal static class SettingsNames
 {
     public record Setting(string LegacyName, string UnifiedName);
