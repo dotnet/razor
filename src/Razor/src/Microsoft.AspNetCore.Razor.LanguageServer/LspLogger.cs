c16abd1fbbad72992aeb9fc210d63541eb6b3632
David Wengier <david.wengier@microsoft.com>
2023-12-21 14:50:12 +1100

    Make VS Code logging actually honour the setting

--- src/Razor/src/Microsoft.AspNetCore.Razor.LanguageServer/LspLogger.cs
+++ src/Razor/src/Microsoft.AspNetCore.Razor.LanguageServer/LspLogger.cs
@@ -33,12 +33,12 @@ internal class LspLogger : IRazorLogger
 
     public bool IsEnabled(LogLevel logLevel)
     {
-        return true;
+        return logLevel >= _logLevel;
     }
 
     public void Log<TState>(LogLevel logLevel, EventId eventId, TState state, Exception? exception, Func<TState, Exception?, string> formatter)
     {
-        if (logLevel is LogLevel.None)
+        if (!IsEnabled(logLevel))
         {
             return;
         }
