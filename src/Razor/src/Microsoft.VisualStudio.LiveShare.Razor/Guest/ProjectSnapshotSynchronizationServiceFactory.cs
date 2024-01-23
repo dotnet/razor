9d36e13a5ccd655421955f123a86337f186ac8ad
Dustin Campbell <dustin@teamcampbell.org>
2024-01-23 13:45:29 -0800

    Use IProjectSnapshotManagerAccessor in ProjectSnapshotSynchronizationService

--- src/Razor/src/Microsoft.VisualStudio.LiveShare.Razor/Guest/ProjectSnapshotSynchronizationServiceFactory.cs
+++ src/Razor/src/Microsoft.VisualStudio.LiveShare.Razor/Guest/ProjectSnapshotSynchronizationServiceFactory.cs
@@ -1,14 +1,11 @@
 ﻿// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the MIT license. See License.txt in the project root for license information.
 
-using System;
 using System.ComponentModel.Composition;
 using System.Threading;
 using System.Threading.Tasks;
-using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.Razor;
-using Microsoft.CodeAnalysis.Razor.ProjectSystem;
-using Microsoft.VisualStudio.LanguageServices;
+using Microsoft.CodeAnalysis.Razor.Workspaces;
 using Microsoft.VisualStudio.LiveShare.Razor.Serialization;
 using Microsoft.VisualStudio.Threading;
 using Newtonsoft.Json;
@@ -16,38 +13,13 @@ using Newtonsoft.Json;
 namespace Microsoft.VisualStudio.LiveShare.Razor.Guest;
 
 [ExportCollaborationService(typeof(ProjectSnapshotSynchronizationService), Scope = SessionScope.Guest)]
-internal class ProjectSnapshotSynchronizationServiceFactory : ICollaborationServiceFactory
+[method: ImportingConstructor]
+internal class ProjectSnapshotSynchronizationServiceFactory(
+    JoinableTaskContext joinableTaskContext,
+    IProjectSnapshotManagerAccessor projectManagerAccessor,
+    IErrorReporter errorReporter,
+    ProjectSnapshotManagerDispatcher dispatcher) : ICollaborationServiceFactory
 {
-    private readonly ProxyAccessor _proxyAccessor;
-    private readonly JoinableTaskContext _joinableTaskContext;
-    private readonly Workspace _workspace;
-
-    [ImportingConstructor]
-    public ProjectSnapshotSynchronizationServiceFactory(
-        ProxyAccessor proxyAccessor,
-        JoinableTaskContext joinableTaskContext,
-        [Import(typeof(VisualStudioWorkspace))] Workspace workspace)
-    {
-        if (proxyAccessor is null)
-        {
-            throw new ArgumentNullException(nameof(proxyAccessor));
-        }
-
-        if (joinableTaskContext is null)
-        {
-            throw new ArgumentNullException(nameof(joinableTaskContext));
-        }
-
-        if (workspace is null)
-        {
-            throw new ArgumentNullException(nameof(workspace));
-        }
-
-        _proxyAccessor = proxyAccessor;
-        _joinableTaskContext = joinableTaskContext;
-        _workspace = workspace;
-    }
-
     public async Task<ICollaborationService> CreateServiceAsync(CollaborationSession sessionContext, CancellationToken cancellationToken)
     {
         // This collaboration service depends on these serializers being immediately available so we need to register these now so the
@@ -55,15 +27,14 @@ internal class ProjectSnapshotSynchronizationServiceFactory : ICollaborationServ
         var serializer = (JsonSerializer)sessionContext.GetService(typeof(JsonSerializer));
         serializer.Converters.RegisterRazorLiveShareConverters();
 
-        var languageServices = _workspace.Services.GetLanguageServices(RazorLanguage.Name);
-        var projectManager = (ProjectSnapshotManagerBase)languageServices.GetRequiredService<ProjectSnapshotManager>();
-
         var projectSnapshotManagerProxy = await sessionContext.GetRemoteServiceAsync<IProjectSnapshotManagerProxy>(typeof(IProjectSnapshotManagerProxy).Name, cancellationToken);
         var synchronizationService = new ProjectSnapshotSynchronizationService(
-            _joinableTaskContext.Factory,
+            joinableTaskContext.Factory,
             sessionContext,
             projectSnapshotManagerProxy,
-            projectManager);
+            projectManagerAccessor,
+            errorReporter,
+            dispatcher);
 
         await synchronizationService.InitializeAsync(cancellationToken);
 
