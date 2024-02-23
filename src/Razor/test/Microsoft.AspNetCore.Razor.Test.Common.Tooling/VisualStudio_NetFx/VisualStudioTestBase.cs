4520cd1cb4ae12351465aca43761e755d813d09c
Dustin Campbell <dustin@teamcampbell.org>
2024-02-23 12:45:21 -0800

    ImportDocumentManagerIntegrationTest: Update to use dispatcher

--- src/Razor/test/Microsoft.AspNetCore.Razor.Test.Common.Tooling/VisualStudio_NetFx/VisualStudioTestBase.cs
+++ src/Razor/test/Microsoft.AspNetCore.Razor.Test.Common.Tooling/VisualStudio_NetFx/VisualStudioTestBase.cs
@@ -1,13 +1,14 @@
 ﻿// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the MIT license. See License.txt in the project root for license information.
 
+using Microsoft.AspNetCore.Razor.Language.Legacy;
 using Microsoft.CodeAnalysis.Razor;
 using Microsoft.VisualStudio.LanguageServices.Razor;
 using Xunit.Abstractions;
 
 namespace Microsoft.AspNetCore.Razor.Test.Common.VisualStudio;
 
-public abstract class VisualStudioTestBase(ITestOutputHelper testOutput) : ToolingTestBase(testOutput)
+public abstract class VisualStudioTestBase(ITestOutputHelper testOutput) : ToolingParserTestBase(testOutput)
 {
     private protected override ProjectSnapshotManagerDispatcher CreateDispatcher()
     {
