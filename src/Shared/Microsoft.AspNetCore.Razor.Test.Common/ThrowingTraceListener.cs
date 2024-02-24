034589e58c7efbfe2e75cc6be396dfcd608e3758
Fredric Silberberg <frsilb@microsoft.com>
2024-02-23 16:58:10 -0800

    Add test trace listener
    This add a test trace listener that will throw an InvalidOperationException when a Debug.Assert or Fail is hit, with a module initializer to automatically add it to our tests.

---
+++ src/Shared/Microsoft.AspNetCore.Razor.Test.Common/ThrowingTraceListener.cs
@@ -0,0 +1,57 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System;
+using System.Diagnostics;
+using System.Runtime.CompilerServices;
+
+namespace Microsoft.AspNetCore.Razor.Test.Common;
+
+public sealed class ThrowingTraceListener : TraceListener
+{
+    [ModuleInitializer]
+    internal static void Initialize()
+    {
+        Trace.Listeners.Clear();
+        Trace.Listeners.Add(new ThrowingTraceListener());
+    }
+
+    public override void Fail(string? message, string? detailMessage)
+    {
+        throw new InvalidOperationException(
+            (string.IsNullOrEmpty(message) ? "Assertion failed" : message) +
+            (string.IsNullOrEmpty(detailMessage) ? "" : Environment.NewLine + detailMessage));
+    }
+
+    public override void Write(object? o)
+    {
+    }
+
+    public override void Write(object? o, string? category)
+    {
+    }
+
+    public override void Write(string? message)
+    {
+    }
+
+    public override void Write(string? message, string? category)
+    {
+    }
+
+    public override void WriteLine(object? o)
+    {
+    }
+
+    public override void WriteLine(object? o, string? category)
+    {
+    }
+
+    public override void WriteLine(string? message)
+    {
+    }
+
+    public override void WriteLine(string? message, string? category)
+    {
+    }
+}
