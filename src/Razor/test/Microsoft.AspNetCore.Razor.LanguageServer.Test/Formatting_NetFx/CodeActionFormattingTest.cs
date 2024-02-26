0edc8440c861d2503d7afce4a1f11053bcd0b7e1
David Wengier <david.wengier@microsoft.com>
2024-02-26 22:10:03 +1100

    Update code action formatting tests
    These tests were really annoying to update, and I don't think they necessarily add much value, but I've done it now so they may as well stay.

--- src/Razor/test/Microsoft.AspNetCore.Razor.LanguageServer.Test/Formatting_NetFx/CodeActionFormattingTest.cs
+++ src/Razor/test/Microsoft.AspNetCore.Razor.LanguageServer.Test/Formatting_NetFx/CodeActionFormattingTest.cs
@@ -26,12 +26,12 @@ input: @"
 codeActionEdits: new[]
 {
 Edit(7, 6, 7, 6, "System.Diagnostics;\r\nusing "),
-Edit(67, 0, 67, 8, ""),
-Edit(69, 34, 70, 7, "\r\n\r\n        [DebuggerDisplay($\"{{{nameof(GetDebuggerDisplay)}(),nq}}\")]"),
-Edit(71, 0, 71, 4, "        "),
-Edit(72, 5, 72, 5, "\r\n            private string GetDebuggerDisplay()\r\n            {"),
-Edit(73, 0, 73, 0, "                return ToString();\r\n            }\r\n"),
-Edit(73, 8, 74, 4, "")
+Edit(55, 0, 55, 8, ""),
+Edit(57, 34, 58, 7, "\r\n\r\n        [DebuggerDisplay($\"{{{nameof(GetDebuggerDisplay)}(),nq}}\")]"),
+Edit(59, 0, 59, 4, "        "),
+Edit(60, 5, 60, 5, "\r\n            private string GetDebuggerDisplay()\r\n            {"),
+Edit(61, 0, 61, 0, "                return ToString();\r\n            }\r\n"),
+Edit(61, 8, 62, 4, "")
 },
 expected: @"@using System.Diagnostics
 
@@ -62,14 +62,14 @@ input: @"
 ",
 codeActionEdits: new[]
 {
-Edit(67, 0, 67, 8, ""),
-Edit(69, 33, 69, 33, "\r\n\r\n        class Goo"),
-Edit(70, 0, 70, 12, "        {"),
-Edit(71, 0, 71, 9, "            public"),
-Edit(71, 13, 71, 13, "()"),
-Edit(72, 0, 72, 4, "            "),
-Edit(73, 0, 73, 4, "            }"),
-Edit(74, 0, 74, 4, "        "),
+Edit(55, 0, 55, 8, ""),
+Edit(57, 33, 57, 33, "\r\n\r\n        class Goo"),
+Edit(58, 0, 58, 12, "        {"),
+Edit(59, 0, 59, 9, "            public"),
+Edit(59, 13, 59, 13, "()"),
+Edit(60, 0, 60, 4, "            "),
+Edit(61, 0, 61, 4, "            }"),
+Edit(62, 0, 62, 4, "        "),
 },
 expected: @"
 @functions {
@@ -94,7 +94,7 @@ input: @"
 ",
 codeActionEdits: new[]
 {
-Edit(65, 0, 72, 0, "        {\r\n        }\r\n#pragma warning restore 1998\r\n#nullable restore\r\n#line 2 \"e:/Scratch/BlazorApp13/BlazorApp13/Client/Pages/Test.razor\"\r\n\r\n        protected override void OnAfterRender(bool firstRender)\r\n        {\r\n            base.OnAfterRender(firstRender);/*$0*/\r\n        }\r\n"),
+Edit(53, 0, 60, 0, "        {\r\n        }\r\n#pragma warning restore 1998\r\n#nullable restore\r\n#line 2 \"e:/Scratch/BlazorApp13/BlazorApp13/Client/Pages/Test.razor\"\r\n\r\n        protected override void OnAfterRender(bool firstRender)\r\n        {\r\n            base.OnAfterRender(firstRender);/*$0*/\r\n        }\r\n"),
 },
 expected: @"
 @functions {
