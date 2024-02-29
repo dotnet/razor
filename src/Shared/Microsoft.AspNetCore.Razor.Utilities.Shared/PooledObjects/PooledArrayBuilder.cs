cee959b9d75a318163582ab3bb5770de419db2cd
Fredric Silberberg <frsilb@microsoft.com>
2024-02-29 10:56:07 -0800

    Allow PooledArrayBuilder to be constructed with collection expressions.

---
+++ src/Shared/Microsoft.AspNetCore.Razor.Utilities.Shared/PooledObjects/PooledArrayBuilder.cs
@@ -0,0 +1,16 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the MIT license. See License.txt in the project root for license information.
+
+using System;
+
+namespace Microsoft.AspNetCore.Razor.PooledObjects;
+
+internal static class PooledArrayBuilder
+{
+    public static PooledArrayBuilder<T> Create<T>(ReadOnlySpan<T> source)
+    {
+        var pooledArray = new PooledArrayBuilder<T>(source.Length);
+        pooledArray.AddRange(source);
+        return pooledArray;
+    }
+}
