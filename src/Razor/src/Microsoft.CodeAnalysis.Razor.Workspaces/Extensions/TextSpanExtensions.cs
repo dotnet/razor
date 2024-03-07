41895ce7734316bb713b5bb4fe9939e0f7b9fd7f
David Wengier <git@wengier.com>
2024-03-07 14:35:57 +1100

    Merge the remaining two extension classes

--- src/Razor/src/Microsoft.CodeAnalysis.Razor.Workspaces/Extensions/TextSpanExtensions.cs
+++ src/Razor/src/Microsoft.CodeAnalysis.Razor.Workspaces/Extensions/TextSpanExtensions.cs
@@ -2,7 +2,9 @@
 // Licensed under the MIT license. See License.txt in the project root for license information.
 
 using System;
+using Microsoft.AspNetCore.Razor.LanguageServer.Extensions;
 using Microsoft.CodeAnalysis.Text;
+using Microsoft.VisualStudio.LanguageServer.Protocol;
 
 namespace Microsoft.CodeAnalysis.Razor.Workspaces.Extensions;
 
@@ -24,5 +26,23 @@ internal static class TextSpanExtensions
         }
 
         return span;
+    }
+
+    public static Range ToRange(this TextSpan span, SourceText sourceText)
+    {
+        if (sourceText is null)
+        {
+            throw new ArgumentNullException(nameof(sourceText));
+        }
+
+        sourceText.GetLinesAndOffsets(span, out var startLine, out var startChar, out var endLine, out var endChar);
+
+        var range = new Range
+        {
+            Start = new Position(startLine, startChar),
+            End = new Position(endLine, endChar)
+        };
+
+        return range;
     }
 }
