9de3c1751e33a86c142fafc812966dfa1d6b93ed
Jan Jones <janjones@microsoft.com>
2024-03-13 16:13:28 +0100

    Update existing tests

--- src/Compiler/Microsoft.AspNetCore.Mvc.Razor.Extensions/test/TestFiles/IntegrationTests/CodeGenerationIntegrationTest/BasicComponent_Runtime.codegen.cs
+++ src/Compiler/Microsoft.AspNetCore.Mvc.Razor.Extensions/test/TestFiles/IntegrationTests/CodeGenerationIntegrationTest/BasicComponent_Runtime.codegen.cs
@@ -12,7 +12,7 @@ namespace __GeneratedComponent
     #line default
     #line hidden
     #nullable restore
-    public partial class AspNetCore_3708c9fcd2e1ecb6cbaba92bcc60927f830690f42092c4aa1a7275c2f37020fd : global::Microsoft.AspNetCore.Components.ComponentBase, IDisposable
+    public partial class BasicComponent : global::Microsoft.AspNetCore.Components.ComponentBase, IDisposable
     #nullable disable
     {
         #pragma warning disable 1998
