b078e98a4cfa24908d0851c85cc2dacd29c249a4
Dustin Campbell <dustin@teamcampbell.org>
2024-03-14 11:16:43 -0700

    Add TestProjectSnapshotManager Update(...) and UpdateAsync(...) methods

--- src/Razor/test/Microsoft.AspNetCore.Razor.Test.Common.Tooling/LanguageServer/LanguageServerTestBase.cs
+++ src/Razor/test/Microsoft.AspNetCore.Razor.Test.Common.Tooling/LanguageServer/LanguageServerTestBase.cs
@@ -58,7 +58,7 @@ public abstract class LanguageServerTestBase : ToolingTestBase
         => CreateProjectSnapshotManager(ProjectEngineFactories.DefaultProvider);
 
     private protected TestProjectSnapshotManager CreateProjectSnapshotManager(IProjectEngineFactoryProvider projectEngineFactoryProvider)
-        => new(projectEngineFactoryProvider, Dispatcher);
+        => new(projectEngineFactoryProvider, Dispatcher, DisposalToken);
 
     internal RazorRequestContext CreateRazorRequestContext(VersionedDocumentContext? documentContext, ILspServices? lspServices = null)
     {
--- src/Razor/test/Microsoft.AspNetCore.Razor.Test.Common.Tooling/ProjectSystem/TestProjectSnapshotManager.cs
+++ src/Razor/test/Microsoft.AspNetCore.Razor.Test.Common.Tooling/ProjectSystem/TestProjectSnapshotManager.cs
@@ -1,6 +1,9 @@
 ﻿// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the MIT license. See License.txt in the project root for license information.
 
+using System;
+using System.Threading;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Razor.LanguageServer;
 using Microsoft.AspNetCore.Razor.ProjectEngineHost;
 using Microsoft.CodeAnalysis.Razor;
@@ -10,9 +13,12 @@ namespace Microsoft.AspNetCore.Razor.Test.Common.ProjectSystem;
 
 internal partial class TestProjectSnapshotManager(
     IProjectEngineFactoryProvider projectEngineFactoryProvider,
-    ProjectSnapshotManagerDispatcher dispatcher)
+    ProjectSnapshotManagerDispatcher dispatcher,
+    CancellationToken disposalToken)
     : ProjectSnapshotManager(projectEngineFactoryProvider, dispatcher)
 {
+    private readonly CancellationToken _disposalToken = disposalToken;
+
     public TestDocumentSnapshot CreateAndAddDocument(ProjectSnapshot projectSnapshot, string filePath)
     {
         var documentSnapshot = TestDocumentSnapshot.Create(projectSnapshot, filePath);
@@ -30,4 +36,28 @@ internal partial class TestProjectSnapshotManager(
     }
 
     public Listener ListenToNotifications() => new(this);
+
+    public Task UpdateAsync(Action<Updater> updater)
+        => UpdateAsync(updater, _disposalToken);
+
+    public Task UpdateAsync<TState>(Action<Updater, TState> updater, TState state)
+        => UpdateAsync(updater, state, _disposalToken);
+
+    public Task<TResult> UpdateAsync<TResult>(Func<Updater, TResult> updater)
+        => UpdateAsync(updater, _disposalToken);
+
+    public Task<TResult> UpdateAsync<TState, TResult>(Func<Updater, TState, TResult> updater, TState state)
+        => UpdateAsync(updater, state, _disposalToken);
+
+    public Task UpdateAsync(Func<Updater, Task> updater)
+        => UpdateAsync(updater, _disposalToken);
+
+    public Task UpdateAsync<TState>(Func<Updater, TState, Task> updater, TState state)
+        => UpdateAsync(updater, state, _disposalToken);
+
+    public Task<TResult> UpdateAsync<TResult>(Func<Updater, Task<TResult>> updater)
+        => UpdateAsync(updater, _disposalToken);
+
+    public Task<TResult> UpdateAsync<TState, TResult>(Func<Updater, TState, Task<TResult>> updater, TState state)
+        => UpdateAsync(updater, state, _disposalToken);
 }
