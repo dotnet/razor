c5d83547f728b75432f35818ef32271eb5f0d023
David Wengier <david.wengier@microsoft.com>
2024-03-18 12:24:07 +1100

    Create concrete services in each "head" project

---
+++ src/Razor/src/Microsoft.VisualStudio.Editor.Razor/VisualStudioFilePathService.cs
@@ -0,0 +1,13 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the MIT license. See License.txt in the project root for license information.
+
+using System.Composition;
+using Microsoft.CodeAnalysis.Razor.Workspaces;
+
+namespace Microsoft.VisualStudio.Editor.Razor;
+
+[Export(typeof(IFilePathService)), Shared]
+[method: ImportingConstructor]
+internal sealed class VisualStudioFilePathService(LanguageServerFeatureOptions options) : AbstractFilePathService(options)
+{
+}
