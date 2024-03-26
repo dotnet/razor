087ffed04ffe771b701ae918545ab0cdb1e5ce7c
Jan Jones <janjones@microsoft.com>
2024-03-26 13:17:28 +0100

    Add RazorLangVersion 9

--- src/Compiler/Microsoft.CodeAnalysis.Razor.Compiler/src/Language/RazorLanguageVersion.cs
+++ src/Compiler/Microsoft.CodeAnalysis.Razor.Compiler/src/Language/RazorLanguageVersion.cs
@@ -23,7 +23,8 @@ public sealed record RazorLanguageVersion : IComparable<RazorLanguageVersion>
     public static readonly RazorLanguageVersion Version_6_0 = new(6, 0);
     public static readonly RazorLanguageVersion Version_7_0 = new(7, 0);
     public static readonly RazorLanguageVersion Version_8_0 = new(8, 0);
-    public static readonly RazorLanguageVersion Latest = Version_8_0;
+    public static readonly RazorLanguageVersion Version_9_0 = new(9, 0);
+    public static readonly RazorLanguageVersion Latest = Version_9_0;
     public static readonly RazorLanguageVersion Experimental = new(1337, 1337);
 
     private static readonly FrozenDictionary<string, RazorLanguageVersion> s_knownVersions = BuildKnownVersions();
@@ -50,6 +51,7 @@ public sealed record RazorLanguageVersion : IComparable<RazorLanguageVersion>
             ["6.0"] = Version_6_0,
             ["7.0"] = Version_7_0,
             ["8.0"] = Version_8_0,
+            ["9.0"] = Version_9_0,
             ["latest"] = Latest,
             ["experimental"] = Experimental,
         };
