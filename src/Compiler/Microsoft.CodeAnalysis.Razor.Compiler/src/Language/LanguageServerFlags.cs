ddad5fd2f06a7b66e3b69210e2ffb1d2330d04cc
Andrew Hall (METAL) <andrha@microsoft.com>
2024-03-26 16:47:50 -0700

    Add a comment

--- src/Compiler/Microsoft.CodeAnalysis.Razor.Compiler/src/Language/LanguageServerFlags.cs
+++ src/Compiler/Microsoft.CodeAnalysis.Razor.Compiler/src/Language/LanguageServerFlags.cs
@@ -3,6 +3,11 @@
 
 namespace Microsoft.AspNetCore.Razor.Language;
 
+/// <summary>
+/// This class represents flags provided by the language server instead of project configuration.
+/// They should not be serialized as part of the configuration, as they instead change runtime behavior
+/// impacted by LSP configuration rather than any project configuration
+/// </summary>
 public sealed record class LanguageServerFlags
     (bool ForceRuntimeCodeGeneration)
 {
