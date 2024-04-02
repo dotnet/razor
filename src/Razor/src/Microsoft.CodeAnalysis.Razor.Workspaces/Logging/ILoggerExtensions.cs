dc4f835e2656a5521538b1d75b3f2e671772999d
David Wengier <david.wengier@microsoft.com>
2024-04-02 15:43:20 +1100

    Implement a proper log message formatting routine
    Copies from MS.Ext.Logging :) In future we should use a proper interpolated string handler

--- src/Razor/src/Microsoft.CodeAnalysis.Razor.Workspaces/Logging/ILoggerExtensions.cs
+++ src/Razor/src/Microsoft.CodeAnalysis.Razor.Workspaces/Logging/ILoggerExtensions.cs
@@ -2,6 +2,7 @@
 // Licensed under the MIT license. See License.txt in the project root for license information.
 
 using System;
+using Microsoft.AspNetCore.Razor.PooledObjects;
 
 namespace Microsoft.CodeAnalysis.Razor.Logging;
 
@@ -54,6 +55,97 @@ internal static class ILoggerExtensions
 
     private static string FormatMessage((string? message, object?[] args) s, Exception? exception)
     {
-        return s.message is null ? "[null]" : string.Format(s.message, s.args);
+        var (format, args) = s;
+        if (format is null)
+        {
+            return "[null]";
+        }
+
+        // from: https://github.com/dotnet/runtime/blob/ec4437be46d8b90bc9fa6740c556bd860d9fe5ab/src/libraries/Microsoft.Extensions.Logging.Abstractions/src/LogValuesFormatter.cs
+        using var _ = StringBuilderPool.GetPooledObject(out var vsb);
+        var scanIndex = 0;
+        var endIndex = format.Length;
+
+        var count = 0;
+
+        while (scanIndex < endIndex)
+        {
+            var openBraceIndex = FindBraceIndex(format, '{', scanIndex, endIndex);
+            if (scanIndex == 0 && openBraceIndex == endIndex)
+            {
+                // No holes found.
+                return format;
+            }
+
+            var closeBraceIndex = FindBraceIndex(format, '}', openBraceIndex, endIndex);
+
+            if (closeBraceIndex == endIndex)
+            {
+                vsb.Append(format, scanIndex, endIndex - scanIndex);
+                scanIndex = endIndex;
+            }
+            else
+            {
+                // Format item syntax : { index[,alignment][ :formatString] }.
+                var formatDelimiterIndex = format.AsSpan(openBraceIndex, closeBraceIndex - openBraceIndex).IndexOfAny(',', ':');
+                formatDelimiterIndex = formatDelimiterIndex < 0 ? closeBraceIndex : formatDelimiterIndex + openBraceIndex;
+
+                vsb.Append(format, scanIndex, openBraceIndex - scanIndex + 1);
+                vsb.Append(count++);
+                vsb.Append(format, formatDelimiterIndex, closeBraceIndex - formatDelimiterIndex + 1);
+
+                scanIndex = closeBraceIndex + 1;
+            }
+        }
+
+        return string.Format(vsb.ToString(), args);
+    }
+
+    private static int FindBraceIndex(string format, char brace, int startIndex, int endIndex)
+    {
+        // Example: {{prefix{{{Argument}}}suffix}}.
+        var braceIndex = endIndex;
+        var scanIndex = startIndex;
+        var braceOccurrenceCount = 0;
+
+        while (scanIndex < endIndex)
+        {
+            if (braceOccurrenceCount > 0 && format[scanIndex] != brace)
+            {
+                if (braceOccurrenceCount % 2 == 0)
+                {
+                    // Even number of '{' or '}' found. Proceed search with next occurrence of '{' or '}'.
+                    braceOccurrenceCount = 0;
+                    braceIndex = endIndex;
+                }
+                else
+                {
+                    // An unescaped '{' or '}' found.
+                    break;
+                }
+            }
+            else if (format[scanIndex] == brace)
+            {
+                if (brace == '}')
+                {
+                    if (braceOccurrenceCount == 0)
+                    {
+                        // For '}' pick the first occurrence.
+                        braceIndex = scanIndex;
+                    }
+                }
+                else
+                {
+                    // For '{' pick the last occurrence.
+                    braceIndex = scanIndex;
+                }
+
+                braceOccurrenceCount++;
+            }
+
+            scanIndex++;
+        }
+
+        return braceIndex;
     }
 }
