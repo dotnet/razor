05aadfa6ee661c074d51d95cc8264f2d98ce83ca
Jared Parsons <jared@paranoidcoding.org>
2024-04-03 20:12:53 -0700

    more

--- src/Shared/Microsoft.AspNetCore.Razor.Test.Common/Language/SyntaxTreeVerifier.cs
+++ src/Shared/Microsoft.AspNetCore.Razor.Test.Common/Language/SyntaxTreeVerifier.cs
@@ -40,7 +40,7 @@ internal class SyntaxTreeVerifier
 
         if (tokens.Count == 0)
         {
-            Assert.Fail("No tokens found in the syntax tree. There should at least be an EOF token.");
+            Assert.False(true, "No tokens found in the syntax tree. There should at least be an EOF token.");
         }
 
         var root = syntaxTree.Root;
@@ -104,7 +104,7 @@ internal class SyntaxTreeVerifier
                 return;
             }
 
-            Assert.Fail("Did not find the non-zero width token in the list of tokens.");
+            Assert.False(true, "Did not find the non-zero width token in the list of tokens.");
         }
     }
 
