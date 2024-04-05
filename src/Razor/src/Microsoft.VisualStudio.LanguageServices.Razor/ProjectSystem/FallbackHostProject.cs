4e221d05c25d237a794cd3641da8ce40c25793a9
Dustin Campbell <dustin@teamcampbell.org>
2024-04-05 10:14:19 -0700

    Fix up for code review feedback

--- src/Razor/src/Microsoft.VisualStudio.LanguageServices.Razor/ProjectSystem/FallbackHostProject.cs
+++ src/Razor/src/Microsoft.VisualStudio.LanguageServices.Razor/ProjectSystem/FallbackHostProject.cs
@@ -1,10 +1,6 @@
 ﻿// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the MIT license. See License.txt in the project root for license information.
 
-
-// Copyright (c) .NET Foundation. All rights reserved.
-// Licensed under the MIT license. See License.txt in the project root for license information.
-
 using Microsoft.AspNetCore.Razor.Language;
 using Microsoft.CodeAnalysis.Razor.ProjectSystem;
 
--- src/Razor/src/Microsoft.VisualStudio.LanguageServices.Razor/VisualStudioHostServicesProvider.cs
+++ src/Razor/src/Microsoft.VisualStudio.LanguageServices.Razor/VisualStudioHostServicesProvider.cs
@@ -1,10 +1,6 @@
 ﻿// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the MIT license. See License.txt in the project root for license information.
 
-
-// Copyright (c) .NET Foundation. All rights reserved.
-// Licensed under the MIT license. See License.txt in the project root for license information.
-
 using Microsoft.CodeAnalysis.Host;
 
 namespace Microsoft.VisualStudio.Razor;
