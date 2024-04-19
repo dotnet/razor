b9b3b48afcb01787fc99a252dd058f1c8e220928
Dustin Campbell <dustin@teamcampbell.org>
2024-04-19 11:01:25 -0700

    Clean up ProjectConfigurationFileChangeDetectorTest

--- src/Razor/test/Microsoft.AspNetCore.Razor.LanguageServer.Test/ProjectConfigurationFileChangeDetectorTest.cs
+++ src/Razor/test/Microsoft.AspNetCore.Razor.LanguageServer.Test/ProjectConfigurationFileChangeDetectorTest.cs
@@ -1,12 +1,11 @@
 ﻿// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the MIT license. See License.txt in the project root for license information.
 
-#nullable disable
-
 using System.Collections.Generic;
 using System.Collections.Immutable;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Razor.LanguageServer.Common;
+using Microsoft.AspNetCore.Razor.Test.Common;
 using Microsoft.AspNetCore.Razor.Test.Common.LanguageServer;
 using Microsoft.AspNetCore.Razor.Test.Common.Workspaces;
 using Microsoft.CodeAnalysis.Razor.Logging;
@@ -16,28 +15,28 @@ using Xunit.Abstractions;
 
 namespace Microsoft.AspNetCore.Razor.LanguageServer;
 
-public class ProjectConfigurationFileChangeDetectorTest : LanguageServerTestBase
+public class ProjectConfigurationFileChangeDetectorTest(ITestOutputHelper testOutput) : LanguageServerTestBase(testOutput)
 {
-    public ProjectConfigurationFileChangeDetectorTest(ITestOutputHelper testOutput)
-        : base(testOutput)
-    {
-    }
-
     [Fact]
     public async Task StartAsync_NotifiesListenersOfExistingConfigurationFiles()
     {
         // Arrange
         var eventArgs1 = new List<ProjectConfigurationFileChangeEventArgs>();
-        var listener1 = new Mock<IProjectConfigurationFileChangeListener>(MockBehavior.Strict);
-        listener1.Setup(l => l.ProjectConfigurationFileChanged(It.IsAny<ProjectConfigurationFileChangeEventArgs>()))
-            .Callback<ProjectConfigurationFileChangeEventArgs>(args => eventArgs1.Add(args));
+        var listenerMock1 = new StrictMock<IProjectConfigurationFileChangeListener>();
+        listenerMock1
+            .Setup(l => l.ProjectConfigurationFileChanged(It.IsAny<ProjectConfigurationFileChangeEventArgs>()))
+            .Callback<ProjectConfigurationFileChangeEventArgs>(eventArgs1.Add);
+
         var eventArgs2 = new List<ProjectConfigurationFileChangeEventArgs>();
-        var listener2 = new Mock<IProjectConfigurationFileChangeListener>(MockBehavior.Strict);
-        listener2.Setup(l => l.ProjectConfigurationFileChanged(It.IsAny<ProjectConfigurationFileChangeEventArgs>()))
-            .Callback<ProjectConfigurationFileChangeEventArgs>(args => eventArgs2.Add(args));
+        var listenerMock2 = new StrictMock<IProjectConfigurationFileChangeListener>();
+        listenerMock2
+            .Setup(l => l.ProjectConfigurationFileChanged(It.IsAny<ProjectConfigurationFileChangeEventArgs>()))
+            .Callback<ProjectConfigurationFileChangeEventArgs>(eventArgs2.Add);
+
         ImmutableArray<string> existingConfigurationFiles = ["c:/path/to/project.razor.json", "c:/other/path/project.razor.bin"];
+
         var detector = new TestProjectConfigurationFileChangeDetector(
-            new[] { listener1.Object, listener2.Object },
+            [listenerMock1.Object, listenerMock2.Object],
             existingConfigurationFiles,
             LoggerFactory);
 
