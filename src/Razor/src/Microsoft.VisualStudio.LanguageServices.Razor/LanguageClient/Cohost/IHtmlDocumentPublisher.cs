5a67db7927ccffa440172c77c97037fd72dfbc78
David Wengier <david.wengier@microsoft.com>
2024-05-08 16:33:05 +1000

    Create a service to request Html from OOP, and publish it to VS buffers

---
+++ src/Razor/src/Microsoft.VisualStudio.LanguageServices.Razor/LanguageClient/Cohost/IHtmlDocumentPublisher.cs
@@ -0,0 +1,14 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the MIT license. See License.txt in the project root for license information.
+
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis;
+
+namespace Microsoft.VisualStudio.Razor.LanguageClient.Cohost;
+
+internal interface IHtmlDocumentPublisher
+{
+    Task<string?> GetHtmlSourceFromOOPAsync(TextDocument document, CancellationToken cancellationToken);
+    Task PublishAsync(TextDocument document, string htmlText, CancellationToken cancellationToken);
+}
