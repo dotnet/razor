d7c1841b53e97ac1d86b840c4d127f2596481e27
David Wengier <david.wengier@microsoft.com>
2024-05-20 14:23:28 +1000

    Copy assembly loading from Roslyn
    I don't actually know why, but the OOPFoldingRangeService breaks out MEF composition without this.

--- src/Razor/src/Microsoft.CodeAnalysis.Remote.Razor/RemoteMefComposition.cs
+++ src/Razor/src/Microsoft.CodeAnalysis.Remote.Razor/RemoteMefComposition.cs
@@ -23,7 +23,7 @@ internal static class RemoteMefComposition
     // Inspired by https://github.com/dotnet/roslyn/blob/25aa74d725e801b8232dbb3e5abcda0fa72da8c5/src/Workspaces/Remote/ServiceHub/Host/RemoteWorkspaceManager.cs#L77
     private static async Task<ExportProvider> CreateExportProviderAsync()
     {
-        var resolver = Resolver.DefaultInstance;
+        var resolver = new Resolver(SimpleAssemblyLoader.Instance);
         var discovery = new AttributedPartDiscovery(resolver, isNonPublicSupported: true); // MEFv2 only
         var parts = await discovery.CreatePartsAsync(RemoteHostAssemblies).ConfigureAwait(false);
         var catalog = ComposableCatalog.Create(resolver).AddParts(parts);
@@ -32,5 +32,26 @@ internal static class RemoteMefComposition
         var runtimeComposition = RuntimeComposition.CreateRuntimeComposition(configuration);
         var exportProviderFactory = runtimeComposition.CreateExportProviderFactory();
         return exportProviderFactory.CreateExportProvider();
+    }
+
+    private sealed class SimpleAssemblyLoader : IAssemblyLoader
+    {
+        public static readonly IAssemblyLoader Instance = new SimpleAssemblyLoader();
+
+        public Assembly LoadAssembly(AssemblyName assemblyName)
+            => Assembly.Load(assemblyName);
+
+        public Assembly LoadAssembly(string assemblyFullName, string? codeBasePath)
+        {
+            var assemblyName = new AssemblyName(assemblyFullName);
+            if (!string.IsNullOrEmpty(codeBasePath))
+            {
+#pragma warning disable SYSLIB0044 // https://github.com/dotnet/roslyn/issues/71510
+                assemblyName.CodeBase = codeBasePath;
+#pragma warning restore SYSLIB0044
+            }
+
+            return LoadAssembly(assemblyName);
+        }
     }
 }
