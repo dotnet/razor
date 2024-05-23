7975f94b1c578084b9a7170d183d7f9fc6a9a42f
Andrew Hall <ryzngard@live.com>
2024-05-22 17:34:33 -0700

    Update FormatDocumentTests.cs

--- src/Razor/test/Microsoft.VisualStudio.Razor.IntegrationTests/Formatting/FormatDocumentTests.cs
+++ src/Razor/test/Microsoft.VisualStudio.Razor.IntegrationTests/Formatting/FormatDocumentTests.cs
@@ -7,6 +7,7 @@ using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Razor.Language;
+using Roslyn.Test.Utilities;
 using Xunit;
 using Xunit.Abstractions;
 
