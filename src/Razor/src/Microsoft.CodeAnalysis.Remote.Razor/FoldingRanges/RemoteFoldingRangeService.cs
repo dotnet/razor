d25399f637022ea52582f856f85d38f80f32ac6f
David Wengier <david.wengier@microsoft.com>
2024-05-24 12:03:10 +1000

    Fixup:service

--- src/Razor/src/Microsoft.CodeAnalysis.Remote.Razor/FoldingRanges/RemoteFoldingRangeService.cs
+++ src/Razor/src/Microsoft.CodeAnalysis.Remote.Razor/FoldingRanges/RemoteFoldingRangeService.cs
@@ -6,8 +6,6 @@ using System.Collections.Immutable;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.CodeAnalysis.ExternalAccess.Razor;
-using Microsoft.CodeAnalysis.ExternalAccess.Razor.Api;
-using Microsoft.CodeAnalysis.Host.Mef;
 using Microsoft.CodeAnalysis.Razor.FoldingRanges;
 using Microsoft.CodeAnalysis.Razor.Protocol.Folding;
 using Microsoft.CodeAnalysis.Razor.Remote;
@@ -28,29 +26,23 @@ internal sealed class RemoteFoldingRangeService(
     private readonly IFoldingRangeService _foldingRangeService = foldingRangeService;
     private readonly IFilePathService _filePathService = filePathService;
 
-    public ValueTask<ImmutableArray<RemoteFoldingRange>?> GetFoldingRangesAsync(RazorPinnedSolutionInfoWrapper solutionInfo, DocumentId id, ImmutableArray<RemoteFoldingRange> htmlRanges, CancellationToken cancellationToken)
-        => RazorBrokeredServiceImplementation.RunServiceAsync(
+    public ValueTask<ImmutableArray<RemoteFoldingRange>?> GetFoldingRangesAsync(RazorPinnedSolutionInfoWrapper solutionInfo, DocumentId documentId, ImmutableArray<RemoteFoldingRange> htmlRanges, CancellationToken cancellationToken)
+        => RunServiceAsync(
             solutionInfo,
-            ServiceBrokerClient,
-            solution => GetFoldingRangesAsync(solution, id, htmlRanges, cancellationToken),
+            documentId,
+            context => GetFoldingRangesAsync(context, htmlRanges, cancellationToken),
             cancellationToken);
 
-    private async ValueTask<ImmutableArray<RemoteFoldingRange>?> GetFoldingRangesAsync(Solution solution, DocumentId razorDocumentId, ImmutableArray<RemoteFoldingRange> htmlRanges, CancellationToken cancellationToken)
+    private async ValueTask<ImmutableArray<RemoteFoldingRange>?> GetFoldingRangesAsync(RemoteDocumentContext context, ImmutableArray<RemoteFoldingRange> htmlRanges, CancellationToken cancellationToken)
     {
-        var documentContext = CreateRazorDocumentContext(solution, razorDocumentId);
-        if (documentContext is null)
-        {
-            return null;
-        }
-
-        var generatedDocument = await documentContext.GetGeneratedDocumentAsync(_filePathService, cancellationToken).ConfigureAwait(false);
+        var generatedDocument = await context.GetGeneratedDocumentAsync(_filePathService, cancellationToken).ConfigureAwait(false);
 
         var csharpRanges = await ExternalAccess.Razor.Cohost.Handlers.FoldingRanges.GetFoldingRangesAsync(generatedDocument, cancellationToken).ConfigureAwait(false);
 
         var convertedCSharp = csharpRanges.SelectAsArray(ToFoldingRange);
         var convertedHtml = htmlRanges.SelectAsArray(RemoteFoldingRange.ToFoldingRange);
 
-        var codeDocument = await documentContext.GetCodeDocumentAsync(cancellationToken).ConfigureAwait(false);
+        var codeDocument = await context.GetCodeDocumentAsync(cancellationToken).ConfigureAwait(false);
         return _foldingRangeService.GetFoldingRanges(codeDocument, convertedCSharp, convertedHtml, cancellationToken)
             .SelectAsArray(RemoteFoldingRange.FromFoldingRange);
     }
