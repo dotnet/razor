191adb6d9b2894cfc580eafdde686f31af48b365
David Wengier <david.wengier@microsoft.com>
2024-05-31 17:06:45 +1000

    Found a sneaky params object

--- src/Razor/src/Microsoft.AspNetCore.Razor.LanguageServer/UpdateBufferRequest.cs
+++ src/Razor/src/Microsoft.AspNetCore.Razor.LanguageServer/UpdateBufferRequest.cs
@@ -2,19 +2,25 @@
 // Licensed under the MIT license. See License.txt in the project root for license information.
 
 using System.Collections.Generic;
+using System.Text.Json.Serialization;
 using Microsoft.CodeAnalysis.Text;
 
 namespace Microsoft.AspNetCore.Razor.LanguageServer;
 
 internal class UpdateBufferRequest
 {
+    [JsonPropertyName("hostDocumentVersion")]
     public int? HostDocumentVersion { get; set; }
 
+    [JsonPropertyName("projectKeyId")]
     public string? ProjectKeyId { get; set; }
 
+    [JsonPropertyName("hostDocumentFilePath")]
     public required string HostDocumentFilePath { get; set; }
 
+    [JsonPropertyName("changes")]
     public required IReadOnlyList<TextChange> Changes { get; set; }
 
+    [JsonPropertyName("previousWasEmpty")]
     public bool PreviousWasEmpty { get; set; }
 }
--- src/Razor/src/Microsoft.VisualStudio.LanguageServices.Razor/LanguageClient/Endpoints/UpdateBufferRequest.cs
+++ src/Razor/src/Microsoft.VisualStudio.LanguageServices.Razor/LanguageClient/Endpoints/UpdateBufferRequest.cs
@@ -2,19 +2,25 @@
 // Licensed under the MIT license. See License.txt in the project root for license information.
 
 using System.Collections.Generic;
+using System.Text.Json.Serialization;
 using Microsoft.CodeAnalysis.Text;
 
 namespace Microsoft.VisualStudio.Razor.LanguageClient.Endpoints;
 
 internal class UpdateBufferRequest
 {
+    [JsonPropertyName("hostDocumentVersion")]
     public int? HostDocumentVersion { get; init; }
 
+    [JsonPropertyName("projectKeyId")]
     public string? ProjectKeyId { get; init; }
 
+    [JsonPropertyName("hostDocumentFilePath")]
     public string? HostDocumentFilePath { get; init; }
 
+    [JsonPropertyName("changes")]
     public required IReadOnlyList<TextChange> Changes { get; init; }
 
+    [JsonPropertyName("previousWasEmpty")]
     public bool PreviousWasEmpty { get; set; }
 }
