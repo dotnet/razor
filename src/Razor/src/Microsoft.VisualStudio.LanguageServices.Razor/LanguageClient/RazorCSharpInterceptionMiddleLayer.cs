9641e7057e96a8b996f52fa6a9f0da7fc7883964
David Wengier <david.wengier@microsoft.com>
2024-06-07 04:22:42 +1000

    Update src/Razor/src/Microsoft.VisualStudio.LanguageServices.Razor/LanguageClient/RazorCSharpInterceptionMiddleLayer.cs

--- src/Razor/src/Microsoft.VisualStudio.LanguageServices.Razor/LanguageClient/RazorCSharpInterceptionMiddleLayer.cs
+++ src/Razor/src/Microsoft.VisualStudio.LanguageServices.Razor/LanguageClient/RazorCSharpInterceptionMiddleLayer.cs
@@ -35,6 +35,12 @@ internal class RazorCSharpInterceptionMiddleLayer : IRazorCSharpInterceptionMidd
         // as Roslyn moves away from Newtonsoft.Json. Do not use these parameters without designing a new API contract.
         Debug.Assert(CanHandle(methodName), "Got a call to intercept a message we were not expecting");
 
+        // Normally we use the LSPRequestInvoker when we're on the client side of things, and want
+        // to send to a server, so at first glance this might seem redundant. However, we're actually
+        // intercepting a message from the Roslyn server, so need to call into our server, so we can turn
+        // around and call the client from our server, because its our server we want it to refresh
+        // semantic tokens from.
+
         var refreshParams = new SemanticTokensRefreshParams();
         return _requestInvoker.ReinvokeRequestOnServerAsync<SemanticTokensRefreshParams, Unit>(
             CustomMessageNames.RazorSemanticTokensRefreshEndpoint,
