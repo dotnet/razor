d76a63d8231ea8710ad135593fdc83c684ac3c98
Alex Gavrilov <alexgav@microsoft.com>
2024-07-02 12:49:08 -0700

    Removing support for legacy completion endpoint, tests, and option to turn it on

--- src/Razor/src/Microsoft.AspNetCore.Razor.LanguageServer/DefaultLanguageServerFeatureOptions.cs
+++ src/Razor/src/Microsoft.AspNetCore.Razor.LanguageServer/DefaultLanguageServerFeatureOptions.cs
@@ -17,8 +17,6 @@ internal class DefaultLanguageServerFeatureOptions : LanguageServerFeatureOption
 
     public override string HtmlVirtualDocumentSuffix => DefaultHtmlVirtualDocumentSuffix;
 
-    public override bool SingleServerCompletionSupport => false;
-
     public override bool SingleServerSupport => false;
 
     public override bool DelegateToCSharpOnDiagnosticPublish => false;
