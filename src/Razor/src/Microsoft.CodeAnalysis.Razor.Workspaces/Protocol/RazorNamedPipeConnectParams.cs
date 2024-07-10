fda387efb995f7885043ca4de1857793d41b2377
Andrew Hall <andrha@microsoft.com>
2024-07-10 13:40:41 -0700

    Fix razor/namedPipe params to use STJ. Also rename the endpoint (#10604)

--- src/Razor/src/Microsoft.CodeAnalysis.Razor.Workspaces/Protocol/RazorNamedPipeConnectParams.cs
+++ src/Razor/src/Microsoft.CodeAnalysis.Razor.Workspaces/Protocol/RazorNamedPipeConnectParams.cs
@@ -1,13 +1,12 @@
 ﻿// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the MIT license. See License.txt in the project root for license information.
 
-using System.Runtime.Serialization;
+using System.Text.Json.Serialization;
 
 namespace Microsoft.CodeAnalysis.Razor.Protocol;
 
-[DataContract]
 internal class RazorNamedPipeConnectParams
 {
-    [DataMember(Name = "pipeName")]
+    [JsonPropertyName("pipeName")]
     public required string PipeName { get; set; }
 }
