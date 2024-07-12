c78deb4c25efec5cdb9ed203954cdd283d15537b
David Wengier <david.wengier@microsoft.com>
2024-07-12 11:48:16 +1000

    Tell the compiler that services are disposable

--- src/Razor/src/Microsoft.CodeAnalysis.Razor.Workspaces/Remote/IRemoteFoldingRangeService.cs
+++ src/Razor/src/Microsoft.CodeAnalysis.Razor.Workspaces/Remote/IRemoteFoldingRangeService.cs
@@ -1,6 +1,7 @@
 ﻿// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the MIT license. See License.txt in the project root for license information.
 
+using System;
 using System.Collections.Immutable;
 using System.Threading;
 using System.Threading.Tasks;
@@ -9,7 +10,7 @@ using Microsoft.CodeAnalysis.Razor.Protocol.Folding;
 
 namespace Microsoft.CodeAnalysis.Razor.Remote;
 
-internal interface IRemoteFoldingRangeService
+internal interface IRemoteFoldingRangeService : IDisposable
 {
     ValueTask<ImmutableArray<RemoteFoldingRange>> GetFoldingRangesAsync(RazorPinnedSolutionInfoWrapper solutionInfo, DocumentId documentId, ImmutableArray<RemoteFoldingRange> htmlRanges, CancellationToken cancellationToken);
 }
