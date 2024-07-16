00a11a3ff72132c755d44f2c3333aed38278e9d4
Dustin Campbell <dustin@teamcampbell.org>
2024-07-16 11:41:04 -0700

    Don't inherit EmptyLoggerFactory from AbstractLoggerFactory

--- src/Razor/src/Microsoft.CodeAnalysis.Remote.Razor/Logging/EmptyLoggerFactory.cs
+++ src/Razor/src/Microsoft.CodeAnalysis.Remote.Razor/Logging/EmptyLoggerFactory.cs
@@ -1,17 +1,42 @@
 ﻿// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the MIT license. See License.txt in the project root for license information.
 
-using System.Collections.Immutable;
+using System;
 using Microsoft.CodeAnalysis.Razor.Logging;
 
 namespace Microsoft.CodeAnalysis.Remote.Razor.Logging;
 
-internal sealed class EmptyLoggerFactory : AbstractLoggerFactory
+internal sealed class EmptyLoggerFactory : ILoggerFactory
 {
     public static ILoggerFactory Instance { get; } = new EmptyLoggerFactory();
 
     private EmptyLoggerFactory()
-        : base(ImmutableArray<ILoggerProvider>.Empty)
     {
+    }
+
+    public void AddLoggerProvider(ILoggerProvider provider)
+    {
+        // This is an empty logger factory. Do nothing.
+    }
+
+    public ILogger GetOrCreateLogger(string categoryName)
+    {
+        return Logger.Instance;
+    }
+
+    private sealed class Logger : ILogger
+    {
+        public static readonly ILogger Instance = new Logger();
+
+        private Logger()
+        {
+        }
+
+        public bool IsEnabled(LogLevel logLevel) => false;
+
+        public void Log(LogLevel logLevel, string message, Exception? exception)
+        {
+            // This is an empty logger. Do nothing.
+        }
     }
 }
