d5a6775d1a6d1c855c187e420f66480bd30bfee2
Dustin Campbell <dustin@teamcampbell.org>
2024-07-15 18:35:43 -0700

    Don't force RazorActivityLog onto main thread
    IVsActivityLog is thread-safe so there's no need to switch to the main thread before using it.

--- src/Razor/src/Microsoft.VisualStudio.LanguageServices.Razor/Logging/RazorActivityLog.cs
+++ src/Razor/src/Microsoft.VisualStudio.LanguageServices.Razor/Logging/RazorActivityLog.cs
@@ -20,19 +20,15 @@ internal sealed class RazorActivityLog : IDisposable
     private enum EntryType { Error, Warning, Info }
 
     private readonly IAsyncServiceProvider _serviceProvider;
-    private readonly JoinableTaskFactory _jtf;
 
     private readonly CancellationTokenSource _disposeTokenSource;
     private readonly AsyncBatchingWorkQueue<(EntryType, string)> _loggingQueue;
     private IVsActivityLog? _vsActivityLog;
 
     [ImportingConstructor]
-    public RazorActivityLog(
-        [Import(typeof(SAsyncServiceProvider))] IAsyncServiceProvider serviceProvider,
-        JoinableTaskContext joinableTaskContext)
+    public RazorActivityLog([Import(typeof(SAsyncServiceProvider))] IAsyncServiceProvider serviceProvider)
     {
         _serviceProvider = serviceProvider;
-        _jtf = joinableTaskContext.Factory;
 
         _disposeTokenSource = new();
         _loggingQueue = new AsyncBatchingWorkQueue<(EntryType, string)>(TimeSpan.Zero, ProcessBatchAsync, _disposeTokenSource.Token);
@@ -51,9 +47,7 @@ internal sealed class RazorActivityLog : IDisposable
 
     private async ValueTask ProcessBatchAsync(ImmutableArray<(EntryType, string)> items, CancellationToken token)
     {
-        await _jtf.SwitchToMainThreadAsync(token);
-
-        _vsActivityLog ??= await _serviceProvider.GetServiceAsync<SVsActivityLog, IVsActivityLog>();
+        _vsActivityLog ??= await _serviceProvider.GetServiceAsync<SVsActivityLog, IVsActivityLog>(token).ConfigureAwait(false);
 
         foreach (var (entryType, message) in items)
         {
