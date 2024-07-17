6faa9e93ca2dcd9b698b85ab77f2f430a40af04f
David Wengier <david.wengier@microsoft.com>
2024-07-18 09:18:24 +1000

    File header

--- src/Razor/src/Microsoft.CodeAnalysis.Remote.Razor/RemoteWorkspaceAccessor.cs
+++ src/Razor/src/Microsoft.CodeAnalysis.Remote.Razor/RemoteWorkspaceAccessor.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.CodeAnalysis.ExternalAccess.Razor.Api;
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the MIT license. See License.txt in the project root for license information.
+
+using Microsoft.CodeAnalysis.ExternalAccess.Razor.Api;
 
 namespace Microsoft.CodeAnalysis.Remote.Razor;
 
