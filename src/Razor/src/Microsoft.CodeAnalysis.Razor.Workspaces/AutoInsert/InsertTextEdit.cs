7d63bd49e84eb17db6750949c5271db7b4867034
Alex Gavrilov (DEV PROD) <alexgav@microsoft.com>
2024-07-19 06:13:15 -0700

    Factoring out AutoInsertService

---
+++ src/Razor/src/Microsoft.CodeAnalysis.Razor.Workspaces/AutoInsert/InsertTextEdit.cs
@@ -0,0 +1,10 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the MIT license. See License.txt in the project root for license information.
+
+using Microsoft.VisualStudio.LanguageServer.Protocol;
+
+namespace Microsoft.CodeAnalysis.Razor.AutoInsert;
+
+internal record struct InsertTextEdit(TextEdit TextEdit, InsertTextFormat InsertTextFormat)
+{
+}
