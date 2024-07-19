b9abac630a003e39da13fac2bc869b37cc6cd381
Alex Gavrilov (DEV PROD) <alexgav@microsoft.com>
2024-07-19 06:13:15 -0700

    Factoring out AutoInsertService

---
+++ src/Razor/src/Microsoft.CodeAnalysis.Razor.Workspaces/AutoInsert/IOnAutoInsertProvider.cs
@@ -0,0 +1,15 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the MIT license. See License.txt in the project root for license information.
+
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis.Razor.ProjectSystem;
+using Microsoft.VisualStudio.LanguageServer.Protocol;
+
+namespace Microsoft.CodeAnalysis.Razor.AutoInsert;
+
+internal interface IOnAutoInsertProvider
+{
+    string TriggerCharacter { get; }
+
+    public ValueTask<InsertTextEdit?> TryResolveInsertionAsync(Position position, IDocumentSnapshot documentSnapshot, bool autoClosingTagsOption);
+}
