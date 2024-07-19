5a828ee212e0a6c7ef24eab44140dde252146131
Dustin Campbell <dustin@teamcampbell.org>
2024-07-19 13:56:28 -0700

    Use ArgHelper in TextSpanExtensions

--- src/Razor/src/Microsoft.CodeAnalysis.Razor.Workspaces/Extensions/TextSpanExtensions.cs
+++ src/Razor/src/Microsoft.CodeAnalysis.Razor.Workspaces/Extensions/TextSpanExtensions.cs
@@ -1,7 +1,7 @@
 ﻿// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the MIT license. See License.txt in the project root for license information.
 
-using System;
+using Microsoft.AspNetCore.Razor;
 using Microsoft.CodeAnalysis.Text;
 using Microsoft.VisualStudio.LanguageServer.Protocol;
 using Range = Microsoft.VisualStudio.LanguageServer.Protocol.Range;
@@ -12,10 +12,7 @@ internal static class TextSpanExtensions
 {
     internal static TextSpan TrimLeadingWhitespace(this TextSpan span, SourceText text)
     {
-        if (text is null)
-        {
-            throw new ArgumentNullException(nameof(text));
-        }
+        ArgHelper.ThrowIfNull(text);
 
         for (var i = 0; i < span.Length; ++i)
         {
@@ -30,10 +27,7 @@ internal static class TextSpanExtensions
 
     public static Range ToRange(this TextSpan span, SourceText sourceText)
     {
-        if (sourceText is null)
-        {
-            throw new ArgumentNullException(nameof(sourceText));
-        }
+        ArgHelper.ThrowIfNull(sourceText);
 
         sourceText.GetLinesAndOffsets(span, out var startLine, out var startChar, out var endLine, out var endChar);
 
