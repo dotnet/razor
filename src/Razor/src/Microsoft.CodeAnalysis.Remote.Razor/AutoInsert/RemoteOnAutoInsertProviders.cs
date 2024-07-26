3f74c033b77f398e9572dcab1031725df2965cd3
Alex Gavrilov (DEV PROD) <alexgav@microsoft.com>
2024-07-25 20:32:56 -0700

    Add cohost OnAutoInsert endpoint

--- src/Razor/src/Microsoft.CodeAnalysis.Remote.Razor/AutoInsert/RemoteOnAutoInsertProviders.cs
+++ src/Razor/src/Microsoft.CodeAnalysis.Remote.Razor/AutoInsert/RemoteOnAutoInsertProviders.cs
@@ -1,14 +1,11 @@
 ﻿// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the MIT license. See License.txt in the project root for license information.
 
-using System;
-using System.Collections.Generic;
 using System.Composition;
-using System.Text;
 using Microsoft.CodeAnalysis.Razor.AutoInsert;
 using Microsoft.CodeAnalysis.Razor.Logging;
 
-namespace Microsoft.CodeAnalysis.Remote.Razor.AutoInsert;
+namespace Microsoft.CodBeAnalysis.Remote.Razor.AutoInsert;
 
 [Shared]
 [Export(typeof(IOnAutoInsertProvider))]
