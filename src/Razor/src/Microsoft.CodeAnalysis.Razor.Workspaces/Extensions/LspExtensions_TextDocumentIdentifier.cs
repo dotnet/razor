e5721227a98eb904ccc5f23b90dd1ace39c1fe77
David Wengier <david.wengier@microsoft.com>
2024-07-31 15:45:45 +1000

    Fixes after merge

--- src/Razor/src/Microsoft.CodeAnalysis.Razor.Workspaces/Extensions/LspExtensions_TextDocumentIdentifier.cs
+++ src/Razor/src/Microsoft.CodeAnalysis.Razor.Workspaces/Extensions/LspExtensions_TextDocumentIdentifier.cs
@@ -1,6 +1,9 @@
 ﻿// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the MIT license. See License.txt in the project root for license information.
 
+using System;
+using Microsoft.CodeAnalysis.ExternalAccess.Razor.Cohost;
+
 namespace Roslyn.LanguageServer.Protocol;
 
 internal static partial class LspExtensions
