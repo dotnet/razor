f65c6b609a9b650775a610087e01c7a7194061c0
phil-allen-msft <phillipa@microsoft.com>
2024-08-06 22:00:13 -0700

    Add simple telemetry to initialize endpoint

--- src/Razor/src/Microsoft.AspNetCore.Razor.LanguageServer/RazorInitializeEndpoint.cs
+++ src/Razor/src/Microsoft.AspNetCore.Razor.LanguageServer/RazorInitializeEndpoint.cs
@@ -4,14 +4,20 @@
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Razor.LanguageServer.EndpointContracts;
+using Microsoft.AspNetCore.Razor.Telemetry;
+using Microsoft.CodeAnalysis.Razor.Logging;
+using Microsoft.CodeAnalysis.Razor.Workspaces;
 using Microsoft.CommonLanguageServerProtocol.Framework;
 using Microsoft.VisualStudio.LanguageServer.Protocol;
 
 namespace Microsoft.AspNetCore.Razor.LanguageServer;
 
 [RazorLanguageServerEndpoint(Methods.InitializeName)]
-internal class RazorInitializeEndpoint : IRazorDocumentlessRequestHandler<InitializeParams, InitializeResult>
+internal class RazorInitializeEndpoint(
+        LanguageServerFeatureOptions languageServerFeatureOptions) : IRazorDocumentlessRequestHandler<InitializeParams, InitializeResult>
 {
+    private readonly LanguageServerFeatureOptions _languageServerFeatureOptions = languageServerFeatureOptions;
+
     public bool MutatesSolutionState { get; } = true;
 
     public Task<InitializeResult> HandleRequestAsync(InitializeParams request, RazorRequestContext requestContext, CancellationToken cancellationToken)
@@ -20,6 +26,12 @@ internal class RazorInitializeEndpoint : IRazorDocumentlessRequestHandler<Initia
 
         capabilitiesManager.SetInitializeParams(request);
         var serverCapabilities = capabilitiesManager.GetInitializeResult();
+
+        var telemetryReporter = requestContext.GetRequiredService<ITelemetryReporter>();
+
+        telemetryReporter.ReportEvent("initialize", Severity.Normal, new
+            Property(nameof(LanguageServerFeatureOptions.ForceRuntimeCodeGeneration), _languageServerFeatureOptions.ForceRuntimeCodeGeneration)
+            );
 
         return Task.FromResult(serverCapabilities);
     }
