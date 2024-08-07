325b59fdc5db8bb1021a7c30ec457de088e57645
Alex Gavrilov (DEV PROD) <alexgav@microsoft.com>
2024-08-06 20:34:02 -0700

    Switch to using RemoteResponse

--- src/Razor/src/Microsoft.CodeAnalysis.Remote.Razor/AutoInsert/RemoteAutoInsertService.cs
+++ src/Razor/src/Microsoft.CodeAnalysis.Remote.Razor/AutoInsert/RemoteAutoInsertService.cs
@@ -1,17 +1,20 @@
 ﻿// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the MIT license. See License.txt in the project root for license information.
 
-using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.CodeAnalysis.ExternalAccess.Razor;
 using Microsoft.CodeAnalysis.Razor.AutoInsert;
+using Microsoft.CodeAnalysis.Razor.DocumentMapping;
+using Microsoft.CodeAnalysis.Razor.Protocol;
 using Microsoft.CodeAnalysis.Razor.Protocol.AutoInsert;
-using Microsoft.CodeAnalysis.Razor.Remote;
 using Microsoft.CodeAnalysis.Remote.Razor.ProjectSystem;
 using Microsoft.VisualStudio.LanguageServer.Protocol;
 
+using Response = Microsoft.CodeAnalysis.Razor.Remote.RemoteResponse<Microsoft.CodeAnalysis.Razor.Protocol.AutoInsert.RemoteInsertTextEdit?>;
+
 namespace Microsoft.CodeAnalysis.Remote.Razor;
+
 internal class RemoteAutoInsertService(in ServiceArgs args)
     : RazorDocumentServiceBase(in args), IRemoteAutoInsertService
 {
@@ -23,8 +26,10 @@ internal class RemoteAutoInsertService(in ServiceArgs args)
 
     private readonly IAutoInsertService _autoInsertService
         = args.ExportProvider.GetExportedValue<IAutoInsertService>();
+    private readonly IRazorDocumentMappingService _documentMappingService
+        = args.ExportProvider.GetExportedValue<IRazorDocumentMappingService>();
 
-    public ValueTask<RemoteInsertTextEdit?> TryResolveInsertionAsync(
+    public ValueTask<Response> TryResolveInsertionAsync(
         RazorPinnedSolutionInfoWrapper solutionInfo,
         DocumentId documentId,
         Position position,
@@ -42,19 +47,41 @@ internal class RemoteAutoInsertService(in ServiceArgs args)
                 cancellationToken),
             cancellationToken);
 
-    private async ValueTask<RemoteInsertTextEdit?> TryResolveInsertionAsync(
+    private async ValueTask<Response> TryResolveInsertionAsync(
         RemoteDocumentContext remoteDocumentContext,
         Position position,
         string character,
         bool autoCloseTags,
         CancellationToken cancellationToken)
     {
-        var insertTextEdit = await _autoInsertService.TryResolveInsertionAsync(
-            remoteDocumentContext.Snapshot,
-            position,
-            character,
-            autoCloseTags,
-            cancellationToken);
-        return insertTextEdit.HasValue ? RemoteInsertTextEdit.FromLspInsertTextEdit(insertTextEdit.Value) : null;
+        var sourceText = await remoteDocumentContext.GetSourceTextAsync(cancellationToken).ConfigureAwait(false);
+        if (!sourceText.TryGetAbsoluteIndex(position, out var index))
+        {
+            return Response.NoFurtherHandling;
+        }
+
+        var codeDocument = await remoteDocumentContext.GetCodeDocumentAsync(cancellationToken).ConfigureAwait(false);
+
+        var languageKind = _documentMappingService.GetLanguageKind(codeDocument, index, rightAssociative: true);
+        if (languageKind is RazorLanguageKind.Html)
+        {
+            return Response.CallHtml;
+        }
+        else if (languageKind is RazorLanguageKind.Razor)
+        {
+            var insertTextEdit = await _autoInsertService.TryResolveInsertionAsync(
+                remoteDocumentContext.Snapshot,
+                position,
+                character,
+                autoCloseTags,
+                cancellationToken);
+            return insertTextEdit.HasValue
+                ? Response.Results(RemoteInsertTextEdit.FromLspInsertTextEdit(insertTextEdit.Value))
+                : Response.NoFurtherHandling;
+        }
+
+        // TODO: handle C# case
+
+        return Response.NoFurtherHandling;
     }
 }
