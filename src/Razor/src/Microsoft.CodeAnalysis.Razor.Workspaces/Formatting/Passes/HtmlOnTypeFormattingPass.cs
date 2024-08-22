ef44f4fe61d22c31e3ceea649d51ed76b60187bf
David Wengier <david.wengier@microsoft.com>
2024-08-22 15:12:55 +1000

    Separate out the files in the Html formatter passes.
    Doing this separately, and purely mechanically, so make review easier

---
+++ src/Razor/src/Microsoft.CodeAnalysis.Razor.Workspaces/Formatting/Passes/HtmlOnTypeFormattingPass.cs
@@ -0,0 +1,26 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the MIT license. See License.txt in the project root for license information.
+
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis.Razor.Logging;
+using Microsoft.CodeAnalysis.Razor.Protocol;
+
+namespace Microsoft.CodeAnalysis.Razor.Formatting;
+
+internal sealed class HtmlOnTypeFormattingPass(ILoggerFactory loggerFactory) : HtmlFormattingPassBase(loggerFactory.GetOrCreateLogger<HtmlOnTypeFormattingPass>())
+{
+    public override Task<FormattingResult> ExecuteAsync(FormattingContext context, FormattingResult result, CancellationToken cancellationToken)
+    {
+        Debug.Assert(result.Kind == RazorLanguageKind.Html);
+
+        if (result.Edits.Length == 0)
+        {
+            // There are no HTML edits for us to apply. No op.
+            return Task.FromResult(new FormattingResult([]));
+        }
+
+        return base.ExecuteAsync(context, result, cancellationToken);
+    }
+}
