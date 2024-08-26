2476b630ef69aeb2ea45cf4243f298fc240a8f35
Dustin Campbell <dustin@teamcampbell.org>
2024-08-26 12:59:20 -0700

    Refactor tests to share ordering test data

---
+++ src/Shared/Microsoft.AspNetCore.Razor.Utilities.Shared.Test/PooledObjects/PooledArrayBuilderOrderingTests.cs
@@ -0,0 +1,188 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the MIT license. See License.txt in the project root for license information.
+
+using System.Collections.Immutable;
+using Microsoft.AspNetCore.Razor.PooledObjects;
+using Microsoft.AspNetCore.Razor.Utilities.Shared.Test.TestData;
+using Xunit;
+
+namespace Microsoft.AspNetCore.Razor.Utilities.Shared.Test.PooledObjects;
+
+public class PooledArrayBuilderOrderingTests : ImmutableArrayOrderingTestBase
+{
+    [Theory]
+    [MemberData(nameof(OrderTestData))]
+    public void ToImmutableOrdered(ImmutableArray<int> data, ImmutableArray<int> expected)
+    {
+        using var builder = new PooledArrayBuilder<int>(capacity: data.Length);
+        builder.AddRange(data);
+
+        var sorted = builder.ToImmutableOrdered();
+        AssertEqual(expected, sorted);
+    }
+
+    [Theory]
+    [MemberData(nameof(OrderTestData_OddBeforeEven))]
+    public void ToImmutableOrdered_OddBeforeEven(ImmutableArray<int> data, ImmutableArray<int> expected)
+    {
+        using var builder = new PooledArrayBuilder<int>(capacity: data.Length);
+        builder.AddRange(data);
+
+        var sorted = builder.ToImmutableOrdered(OddBeforeEven);
+        AssertEqual(expected, sorted);
+    }
+
+    [Theory]
+    [MemberData(nameof(OrderDescendingTestData))]
+    public void ToImmutableOrderedDescending(ImmutableArray<int> data, ImmutableArray<int> expected)
+    {
+        using var builder = new PooledArrayBuilder<int>(capacity: data.Length);
+        builder.AddRange(data);
+
+        var sorted = builder.ToImmutableOrderedDescending();
+        AssertEqual(expected, sorted);
+    }
+
+    [Theory]
+    [MemberData(nameof(OrderDescendingTestData_OddBeforeEven))]
+    public void ToImmutableOrderedDescending_OddBeforeEven(ImmutableArray<int> data, ImmutableArray<int> expected)
+    {
+        using var builder = new PooledArrayBuilder<int>(capacity: data.Length);
+        builder.AddRange(data);
+
+        var sorted = builder.ToImmutableOrderedDescending(OddBeforeEven);
+        AssertEqual(expected, sorted);
+    }
+
+    [Theory]
+    [MemberData(nameof(OrderByTestData))]
+    public void ToImmutableOrderedBy(ImmutableArray<ValueHolder<int>> data, ImmutableArray<ValueHolder<int>> expected)
+    {
+        using var builder = new PooledArrayBuilder<ValueHolder<int>>(capacity: data.Length);
+        builder.AddRange(data);
+
+        var sorted = builder.ToImmutableOrderedBy(static x => x.Value);
+        AssertEqual(expected, sorted);
+    }
+
+    [Theory]
+    [MemberData(nameof(OrderByTestData_OddBeforeEven))]
+    public void ToImmutableOrderedBy_OddBeforeEven(ImmutableArray<ValueHolder<int>> data, ImmutableArray<ValueHolder<int>> expected)
+    {
+        using var builder = new PooledArrayBuilder<ValueHolder<int>>(capacity: data.Length);
+        builder.AddRange(data);
+
+        var sorted = builder.ToImmutableOrderedBy(static x => x.Value, OddBeforeEven);
+        AssertEqual(expected, sorted);
+    }
+
+    [Theory]
+    [MemberData(nameof(OrderByDescendingTestData))]
+    public void ToImmutableOrderedByDescending(ImmutableArray<ValueHolder<int>> data, ImmutableArray<ValueHolder<int>> expected)
+    {
+        using var builder = new PooledArrayBuilder<ValueHolder<int>>(capacity: data.Length);
+        builder.AddRange(data);
+
+        var sorted = builder.ToImmutableOrderedByDescending(static x => x.Value);
+        AssertEqual(expected, sorted);
+    }
+
+    [Theory]
+    [MemberData(nameof(OrderByDescendingTestData_OddBeforeEven))]
+    public void ToImmutableOrderedByDescending_OddBeforeEven(ImmutableArray<ValueHolder<int>> data, ImmutableArray<ValueHolder<int>> expected)
+    {
+        using var builder = new PooledArrayBuilder<ValueHolder<int>>(capacity: data.Length);
+        builder.AddRange(data);
+
+        var sorted = builder.ToImmutableOrderedByDescending(static x => x.Value, OddBeforeEven);
+        AssertEqual(expected, sorted);
+    }
+
+    [Theory]
+    [MemberData(nameof(OrderTestData))]
+    public void DrainToImmutableOrdered(ImmutableArray<int> data, ImmutableArray<int> expected)
+    {
+        using var builder = new PooledArrayBuilder<int>(capacity: data.Length);
+        builder.AddRange(data);
+
+        var sorted = builder.DrainToImmutableOrdered();
+        AssertEqual(expected, sorted);
+    }
+
+    [Theory]
+    [MemberData(nameof(OrderTestData_OddBeforeEven))]
+    public void DrainToImmutableOrdered_OddBeforeEven(ImmutableArray<int> data, ImmutableArray<int> expected)
+    {
+        using var builder = new PooledArrayBuilder<int>(capacity: data.Length);
+        builder.AddRange(data);
+
+        var sorted = builder.DrainToImmutableOrdered(OddBeforeEven);
+        AssertEqual(expected, sorted);
+    }
+
+    [Theory]
+    [MemberData(nameof(OrderDescendingTestData))]
+    public void DrainToImmutableOrderedDescending(ImmutableArray<int> data, ImmutableArray<int> expected)
+    {
+        using var builder = new PooledArrayBuilder<int>(capacity: data.Length);
+        builder.AddRange(data);
+
+        var sorted = builder.DrainToImmutableOrderedDescending();
+        AssertEqual(expected, sorted);
+    }
+
+    [Theory]
+    [MemberData(nameof(OrderDescendingTestData_OddBeforeEven))]
+    public void DrainToImmutableOrderedDescending_OddBeforeEven(ImmutableArray<int> data, ImmutableArray<int> expected)
+    {
+        using var builder = new PooledArrayBuilder<int>(capacity: data.Length);
+        builder.AddRange(data);
+
+        var sorted = builder.DrainToImmutableOrderedDescending(OddBeforeEven);
+        AssertEqual(expected, sorted);
+    }
+
+    [Theory]
+    [MemberData(nameof(OrderByTestData))]
+    public void DrainToImmutableOrderedBy(ImmutableArray<ValueHolder<int>> data, ImmutableArray<ValueHolder<int>> expected)
+    {
+        using var builder = new PooledArrayBuilder<ValueHolder<int>>(capacity: data.Length);
+        builder.AddRange(data);
+
+        var sorted = builder.DrainToImmutableOrderedBy(static x => x.Value);
+        AssertEqual(expected, sorted);
+    }
+
+    [Theory]
+    [MemberData(nameof(OrderByTestData_OddBeforeEven))]
+    public void DrainToImmutableOrderedBy_OddBeforeEven(ImmutableArray<ValueHolder<int>> data, ImmutableArray<ValueHolder<int>> expected)
+    {
+        using var builder = new PooledArrayBuilder<ValueHolder<int>>(capacity: data.Length);
+        builder.AddRange(data);
+
+        var sorted = builder.DrainToImmutableOrderedBy(static x => x.Value, OddBeforeEven);
+        AssertEqual(expected, sorted);
+    }
+
+    [Theory]
+    [MemberData(nameof(OrderByDescendingTestData))]
+    public void DrainToImmutableOrderedByDescending(ImmutableArray<ValueHolder<int>> data, ImmutableArray<ValueHolder<int>> expected)
+    {
+        using var builder = new PooledArrayBuilder<ValueHolder<int>>(capacity: data.Length);
+        builder.AddRange(data);
+
+        var sorted = builder.DrainToImmutableOrderedByDescending(static x => x.Value);
+        AssertEqual(expected, sorted);
+    }
+
+    [Theory]
+    [MemberData(nameof(OrderByDescendingTestData_OddBeforeEven))]
+    public void DrainToImmutableOrderedByDescending_OddBeforeEven(ImmutableArray<ValueHolder<int>> data, ImmutableArray<ValueHolder<int>> expected)
+    {
+        using var builder = new PooledArrayBuilder<ValueHolder<int>>(capacity: data.Length);
+        builder.AddRange(data);
+
+        var sorted = builder.DrainToImmutableOrderedByDescending(static x => x.Value, OddBeforeEven);
+        AssertEqual(expected, sorted);
+    }
+}
---
+++ src/Shared/Microsoft.AspNetCore.Razor.Utilities.Shared.Test/TestData/ReadOnlyListOrderingTestBase.cs
@@ -0,0 +1,10 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the MIT license. See License.txt in the project root for license information.
+
+using System.Collections.Generic;
+
+namespace Microsoft.AspNetCore.Razor.Utilities.Shared.Test.TestData;
+
+public abstract class ReadOnlyListOrderingTestBase : OrderingTestBase<IReadOnlyList<int>, IReadOnlyList<ValueHolder<int>>, OrderingCaseConverters.ReadOnlyList>
+{
+}
