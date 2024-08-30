8eb3a41c78057279dd4ead01d51ed2fc9e949a07
David Wengier <david.wengier@microsoft.com>
2024-08-30 14:42:53 +1000

    First test just to prove it works

---
+++ src/Razor/test/Microsoft.VisualStudio.LanguageServices.Razor.Test/Cohost/CohostDocumentFormattingEndpointTest.cs
@@ -0,0 +1,100 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the MIT license. See License.txt in the project root for license information.
+
+using System;
+using System.Collections.Immutable;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Razor.Test.Common;
+using Microsoft.AspNetCore.Razor.Test.Common.Mef;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.ExternalAccess.Razor;
+using Microsoft.CodeAnalysis.Razor.Formatting;
+using Microsoft.CodeAnalysis.Testing;
+using Microsoft.CodeAnalysis.Text;
+using Microsoft.VisualStudio.LanguageServer.ContainedLanguage;
+using Microsoft.VisualStudio.LanguageServer.Protocol;
+using Microsoft.VisualStudio.Threading;
+using Roslyn.Test.Utilities;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Microsoft.VisualStudio.Razor.LanguageClient.Cohost;
+
+[UseExportProvider]
+public class CohostDocumentFormattingEndpointTest(ITestOutputHelper testOutputHelper) : CohostEndpointTestBase(testOutputHelper)
+{
+    [Fact]
+    public Task Formatting()
+        => VerifyDocumentFormattingAsync(
+            input: """
+
+                        <div></div>
+
+            @code {
+                            private void M(string thisIsMyString)
+                {
+                    var x = 5;
+
+                                var y = "Hello";
+
+                    M({|thisIsMyString:"Hello"|});
+                }
+            }
+
+            """,
+            expected: """
+
+            <div></div>
+
+            @code {
+                private void M(string thisIsMyString)
+                {
+                    var x = 5;
+
+                    var y = "Hello";
+
+                    M("Hello");
+                }
+            }
+
+            """);
+
+    private async Task VerifyDocumentFormattingAsync(string input, string expected)
+    {
+        TestFileMarkupParser.GetSpans(input, out input, out ImmutableDictionary<string, ImmutableArray<TextSpan>> spansDict);
+        var document = CreateProjectAndRazorDocument(input);
+        var inputText = await document.GetTextAsync(DisposalToken);
+
+        var htmlDocumentPublisher = new HtmlDocumentPublisher(RemoteServiceInvoker, StrictMock.Of<TrackingLSPDocumentManager>(), StrictMock.Of<JoinableTaskContext>(), LoggerFactory);
+        var generatedHtml = await htmlDocumentPublisher.GetHtmlSourceFromOOPAsync(document, DisposalToken);
+        Assert.NotNull(generatedHtml);
+
+        var uri = new Uri(document.CreateUri(), $"{document.FilePath}{FeatureOptions.HtmlVirtualDocumentSuffix}");
+        var htmlEdits = await HtmlFormatting.GetDocumentFormattingEditsAsync(LoggerFactory, uri, generatedHtml, insertSpaces: true, tabSize: 4);
+
+        var requestInvoker = new TestLSPRequestInvoker([(Methods.TextDocumentFormattingName, htmlEdits)]);
+
+        var endpoint = new CohostDocumentFormattingEndpoint(RemoteServiceInvoker, TestHtmlDocumentSynchronizer.Instance, requestInvoker, LoggerFactory);
+
+        var request = new DocumentFormattingParams()
+        {
+            TextDocument = new TextDocumentIdentifier() { Uri = document.CreateUri() },
+            Options = new FormattingOptions()
+            {
+                TabSize = 4,
+                InsertSpaces = true
+            }
+        };
+
+        var edits = await endpoint.GetTestAccessor().HandleRequestAsync(request, document, DisposalToken);
+
+        var changes = edits
+            .OrderByDescending(e => e.Range.Start.Line)
+            .ThenByDescending(e => e.Range.Start.Character)
+            .Select(inputText.GetTextChange);
+        inputText = inputText.WithChanges(changes);
+
+        AssertEx.EqualOrDiff(expected, inputText.ToString());
+    }
+}
