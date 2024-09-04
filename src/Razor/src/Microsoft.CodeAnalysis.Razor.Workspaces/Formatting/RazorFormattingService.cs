95d7a72b40b579f9575b303225b52241366a74ec
Jordi Ramos <jordi1215.jrc@gmail.com>
2024-09-04 11:07:26 -0700

    updated NormalizeLineEndings comment into XML format

--- src/Razor/src/Microsoft.CodeAnalysis.Razor.Workspaces/Formatting/RazorFormattingService.cs
+++ src/Razor/src/Microsoft.CodeAnalysis.Razor.Workspaces/Formatting/RazorFormattingService.cs
@@ -281,9 +281,11 @@ internal class RazorFormattingService : IRazorFormattingService
         }
     }
 
-    // This method counts the occurrences of CRLF and LF line endings in the original text. If LF line endings are more
-    // prevalent, it removes any CR characters from the text edits to ensure consistency with the LF style.
-    // This can be removed once we figure out how to generate the formatted CSharp file with the correct line endings.
+    /// <summary>
+    /// This method counts the occurrences of CRLF and LF line endings in the original text. 
+    /// If LF line endings are more prevalent, it removes any CR characters from the text edits 
+    /// to ensure consistency with the LF style.
+    /// </summary>
     private TextEdit[] NormalizeLineEndings(SourceText originalText, TextEdit[] edits)
     {
         if (originalText.HasLFLineEndings())
