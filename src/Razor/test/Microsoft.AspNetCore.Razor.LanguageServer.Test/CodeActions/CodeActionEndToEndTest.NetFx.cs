ea556b90f97b0462eb3314f78db2abf3284bb500
Andrew Hall <andrha@microsoft.com>
2024-10-01 14:55:20 -0700

    Fix merge

--- src/Razor/test/Microsoft.AspNetCore.Razor.LanguageServer.Test/CodeActions/CodeActionEndToEndTest.NetFx.cs
+++ src/Razor/test/Microsoft.AspNetCore.Razor.LanguageServer.Test/CodeActions/CodeActionEndToEndTest.NetFx.cs
@@ -1300,7 +1300,7 @@ public class CodeActionEndToEndTest(ITestOutputHelper testOutput) : SingleServer
 
         Assert.NotNull(codeActionToRun);
 
-        var formattingService = await TestRazorFormattingService.CreateWithFullSupportAsync(LoggerFactory, codeDocument, documentContext.Snapshot, optionsMonitor?.CurrentValue);
+        var formattingService = await TestRazorFormattingService.CreateWithFullSupportAsync(LoggerFactory, codeDocument, optionsMonitor?.CurrentValue);
         var changes = await GetEditsAsync(
             codeActionToRun,
             requestContext,
@@ -1477,9 +1477,8 @@ public class CodeActionEndToEndTest(ITestOutputHelper testOutput) : SingleServer
         public ExtractToComponentResolverDocumentContextFactory
             (string filePath,
             RazorCodeDocument codeDocument,
-            TagHelperDescriptor[]? tagHelpers = null,
-            int? version = null)
-            : base(filePath, codeDocument, version)
+            TagHelperDescriptor[]? tagHelpers = null)
+            : base(filePath, codeDocument)
         {
             _tagHelperDescriptors = CreateTagHelperDescriptors();
             if (tagHelpers is not null)
@@ -1491,7 +1490,6 @@ public class CodeActionEndToEndTest(ITestOutputHelper testOutput) : SingleServer
         public override bool TryCreate(
             Uri documentUri,
             VSProjectContext? projectContext,
-            bool versioned,
             [NotNullWhen(true)] out DocumentContext? context)
         {
             if (FilePath is null || CodeDocument is null)
