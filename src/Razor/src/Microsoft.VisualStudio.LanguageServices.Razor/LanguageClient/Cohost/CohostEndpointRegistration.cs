d7775c0eaced8e7ad0c6b028f6e9e18f87664c53
David Wengier <david.wengier@microsoft.com>
2024-10-02 17:25:04 +1000

    Register for the endpoints Roslyn implements

---
+++ src/Razor/src/Microsoft.VisualStudio.LanguageServices.Razor/LanguageClient/Cohost/CohostEndpointRegistration.cs
@@ -0,0 +1,61 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the MIT license. See License.txt in the project root for license information.
+
+using System.Collections.Immutable;
+using System.Composition;
+using Microsoft.CodeAnalysis.ExternalAccess.Razor.Cohost;
+using Microsoft.VisualStudio.LanguageServer.Protocol;
+
+namespace Microsoft.VisualStudio.Razor.LanguageClient.Cohost;
+
+/// <summary>
+/// This class provides dynamic registration for Razor files, for LSP methods where the endpoint implementation
+/// is provided by Roslyn
+/// </summary>
+#pragma warning disable RS0030 // Do not use banned APIs
+[Shared]
+[Export(typeof(IDynamicRegistrationProvider))]
+#pragma warning restore RS0030 // Do not use banned APIs
+internal sealed class CohostEndpointRegistration : IDynamicRegistrationProvider
+{
+    public ImmutableArray<Registration>GetRegistrations(VSInternalClientCapabilities clientCapabilities, DocumentFilter[] filter, RazorCohostRequestContext requestContext)
+    {
+        return [
+            // Project Context, for the nav bar
+            new Registration
+            {
+                Method = VSMethods.GetProjectContextsName,
+                RegisterOptions = new TextDocumentRegistrationOptions()
+                {
+                    DocumentSelector = filter
+                }
+            },
+            // DidOpen, DidChange, DidClose, for document synchronization
+            new Registration
+            {
+                Method = Methods.TextDocumentDidOpenName,
+                RegisterOptions = new TextDocumentRegistrationOptions()
+                {
+                    DocumentSelector = filter
+                }
+            },
+            new Registration
+            {
+                Method = Methods.TextDocumentDidChangeName,
+                RegisterOptions = new TextDocumentChangeRegistrationOptions()
+                {
+                    DocumentSelector = filter,
+                    SyncKind = TextDocumentSyncKind.Incremental
+                }
+            },
+            new Registration
+            {
+                Method = Methods.TextDocumentDidCloseName,
+                RegisterOptions = new TextDocumentRegistrationOptions()
+                {
+                    DocumentSelector = filter
+                }
+            },
+        ];
+    }
+}
