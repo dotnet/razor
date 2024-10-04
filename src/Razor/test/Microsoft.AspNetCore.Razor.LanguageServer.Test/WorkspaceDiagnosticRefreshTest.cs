bfda0df2b350e55c5e8a56d16feb87d9975816fa
Andrew Hall <andrha@microsoft.com>
2024-10-04 15:28:37 -0700

    Refresh diagnostics on project changes (#10964)
    This notifies a client to refresh diagnostics when we experience project snapshot changes (except for document changed). This should help for cases where the project engine is updating but doesn't have all taghelper information yet.

---
+++ src/Razor/test/Microsoft.AspNetCore.Razor.LanguageServer.Test/WorkspaceDiagnosticRefreshTest.cs
@@ -0,0 +1,188 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the MIT license. See License.txt in the project root for license information.
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Razor.LanguageServer.Hosting;
+using Microsoft.AspNetCore.Razor.Test.Common;
+using Microsoft.AspNetCore.Razor.Test.Common.LanguageServer;
+using Microsoft.AspNetCore.Razor.Test.Common.ProjectSystem;
+using Microsoft.CodeAnalysis.Razor.ProjectSystem;
+using Microsoft.VisualStudio.LanguageServer.Protocol;
+using Moq;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Microsoft.AspNetCore.Razor.LanguageServer.Test;
+
+public class WorkspaceDiagnosticRefreshTest(ITestOutputHelper testOutputHelper) : LanguageServerTestBase(testOutputHelper)
+{
+    private static readonly TimeSpan s_delay = TimeSpan.FromMilliseconds(10);
+
+    [Fact]
+    public async Task WorkspaceRefreshSent()
+    {
+        var projectSnapshotManager = CreateProjectSnapshotManager();
+        var clientConnection = new StrictMock<IClientConnection>();
+        clientConnection
+            .Setup(c => c.SendNotificationAsync(Methods.WorkspaceDiagnosticRefreshName, It.IsAny<CancellationToken>()))
+            .Returns(Task.CompletedTask)
+            .Verifiable();
+
+        using var publisher = new WorkspaceDiagnosticsRefresher(
+            projectSnapshotManager,
+            new TestClientCapabilitiesService(new()
+            {
+                Workspace = new()
+                {
+                    Diagnostics = new()
+                    {
+                        RefreshSupport = true
+                    }
+                }
+            }),
+            clientConnection.Object,
+            s_delay);
+
+        var testAccessor = publisher.GetTestAccessor();
+
+        await projectSnapshotManager.UpdateAsync(
+            static updater =>
+            {
+                updater.CreateAndAddProject("C:/path/to/project.csproj");
+            });
+
+        await testAccessor.WaitForRefreshAsync();
+
+        clientConnection.Verify();
+    }
+
+    [Fact]
+    public async Task WorkspaceRefreshSent_MultipleTimes()
+    {
+        var projectSnapshotManager = CreateProjectSnapshotManager();
+        var clientConnection = new StrictMock<IClientConnection>();
+        clientConnection
+            .Setup(c => c.SendNotificationAsync(Methods.WorkspaceDiagnosticRefreshName, It.IsAny<CancellationToken>()))
+            .Returns(Task.CompletedTask);
+
+        using var publisher = new WorkspaceDiagnosticsRefresher(
+            projectSnapshotManager,
+            new TestClientCapabilitiesService(new()
+            {
+                Workspace = new()
+                {
+                    Diagnostics = new()
+                    {
+                        RefreshSupport = true
+                    }
+                }
+            }),
+            clientConnection.Object,
+            s_delay);
+
+        var testAccessor = publisher.GetTestAccessor();
+
+        await projectSnapshotManager.UpdateAsync(
+            static updater =>
+            {
+                updater.CreateAndAddProject("C:/path/to/project.csproj");
+            });
+
+        await testAccessor.WaitForRefreshAsync();
+
+        await projectSnapshotManager.UpdateAsync(
+            static updater =>
+            {
+                var project = (ProjectSnapshot)updater.GetProjects().First();
+                var directory = Path.GetDirectoryName(project.FilePath);
+                Assert.NotNull(directory);
+
+                var filePath = Path.Combine(directory, "document.razor");
+                updater.CreateAndAddDocument(project, filePath);
+            });
+
+        await testAccessor.WaitForRefreshAsync();
+
+        clientConnection.Verify(
+            c => c.SendNotificationAsync(Methods.WorkspaceDiagnosticRefreshName, It.IsAny<CancellationToken>()),
+            Times.Exactly(2));
+    }
+
+    [Fact]
+    public async Task WorkspaceRefreshNotSent_ClientDoesNotSupport()
+    {
+        var projectSnapshotManager = CreateProjectSnapshotManager();
+        var clientConnection = new StrictMock<IClientConnection>();
+
+        using var publisher = new WorkspaceDiagnosticsRefresher(
+            projectSnapshotManager,
+            new TestClientCapabilitiesService(new()
+            {
+                Workspace = new()
+                {
+                    Diagnostics = new()
+                    {
+                        RefreshSupport = false
+                    }
+                }
+            }),
+            clientConnection.Object,
+            s_delay);
+
+        var testAccessor = publisher.GetTestAccessor();
+
+        await projectSnapshotManager.UpdateAsync(
+            static updater =>
+            {
+                updater.CreateAndAddProject("C:/path/to/project.csproj");
+            });
+
+        await testAccessor.WaitForRefreshAsync();
+
+        clientConnection
+            .Verify(c => c.SendNotificationAsync(It.IsAny<string>(), It.IsAny<CancellationToken>()),
+                    Times.Never);
+    }
+
+    [Fact]
+    public async Task WorkspaceRefreshNotSent_RefresherDisposed()
+    {
+        var projectSnapshotManager = CreateProjectSnapshotManager();
+        var clientConnection = new StrictMock<IClientConnection>();
+
+        var publisher = new WorkspaceDiagnosticsRefresher(
+            projectSnapshotManager,
+            new TestClientCapabilitiesService(new()
+            {
+                Workspace = new()
+                {
+                    Diagnostics = new()
+                    {
+                        RefreshSupport = false
+                    }
+                }
+            }),
+            clientConnection.Object,
+            s_delay);
+
+        var testAccessor = publisher.GetTestAccessor();
+
+        publisher.Dispose();
+
+        await projectSnapshotManager.UpdateAsync(
+            static updater =>
+            {
+                updater.CreateAndAddProject("C:/path/to/project.csproj");
+            });
+
+        await testAccessor.WaitForRefreshAsync();
+
+        clientConnection
+            .Verify(c => c.SendNotificationAsync(It.IsAny<string>(), It.IsAny<CancellationToken>()),
+                    Times.Never);
+    }
+}
