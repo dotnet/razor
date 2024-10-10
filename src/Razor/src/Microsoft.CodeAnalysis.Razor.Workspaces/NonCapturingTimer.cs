6e4d125aaa17c1fb0908506f3d708c66d40359c3
Jan Jones <janjones@microsoft.com>
2024-10-10 17:28:44 +0200

    Remove source-only package

---
+++ src/Razor/src/Microsoft.CodeAnalysis.Razor.Workspaces/NonCapturingTimer.cs
@@ -0,0 +1,44 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the MIT license. See License.txt in the project root for license information.
+
+// https://github.com/dotnet/runtime/blob/11c86d8acba2f248b3afb5e8594f5f41ceebf098/src/libraries/Common/src/Extensions/NonCapturingTimer/NonCapturingTimer.cs
+
+using System;
+using System.Threading;
+
+namespace Microsoft.Extensions.Internal;
+
+// A convenience API for interacting with System.Threading.Timer in a way
+// that doesn't capture the ExecutionContext. We should be using this (or equivalent)
+// everywhere we use timers to avoid rooting any values stored in asynclocals.
+internal static class NonCapturingTimer
+{
+    public static Timer Create(TimerCallback callback, object state, TimeSpan dueTime, TimeSpan period)
+    {
+        if (callback is null)
+        {
+            throw new ArgumentNullException(nameof(callback));
+        }
+
+        // Don't capture the current ExecutionContext and its AsyncLocals onto the timer
+        var restoreFlow = false;
+        try
+        {
+            if (!ExecutionContext.IsFlowSuppressed())
+            {
+                ExecutionContext.SuppressFlow();
+                restoreFlow = true;
+            }
+
+            return new Timer(callback, state, dueTime, period);
+        }
+        finally
+        {
+            // Restore the current ExecutionContext
+            if (restoreFlow)
+            {
+                ExecutionContext.RestoreFlow();
+            }
+        }
+    }
+}
