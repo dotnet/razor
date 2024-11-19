3fce67da5bba3d9767ae90a5e569676fdc0c30fb
Andrew Hall <andrha@microsoft.com>
2024-11-18 16:28:21 -0800

    Missing using

--- src/Razor/src/rzls/Endpoints/RazorUpdateLogLevelParams.cs
+++ src/Razor/src/rzls/Endpoints/RazorUpdateLogLevelParams.cs
@@ -2,6 +2,7 @@
 // Licensed under the MIT license. See License.txt in the project root for license information.
 
 using System.Text.Json.Serialization;
+using Microsoft.CodeAnalysis.Razor.Logging;
 
 namespace Microsoft.AspNetCore.Razor.LanguageServer.Endpoints;
 
