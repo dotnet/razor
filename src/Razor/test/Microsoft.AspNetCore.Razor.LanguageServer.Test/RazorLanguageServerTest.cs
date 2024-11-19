7cfaabb9c94c96d850b3a408fe10c8c16a32ffdc
Andrew Hall <andrha@microsoft.com>
2024-11-18 18:01:31 -0800

    Test

--- src/Razor/test/Microsoft.AspNetCore.Razor.LanguageServer.Test/RazorLanguageServerTest.cs
+++ src/Razor/test/Microsoft.AspNetCore.Razor.LanguageServer.Test/RazorLanguageServerTest.cs
@@ -8,8 +8,8 @@ using System.Linq;
 using System.Reflection;
 using System.Text.Json;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Razor.LanguageServer.Extensions;
 using Microsoft.AspNetCore.Razor.LanguageServer.Hosting;
+using Microsoft.AspNetCore.Razor.LanguageServer.Hosting.Logging;
 using Microsoft.AspNetCore.Razor.LanguageServer.Hosting.NamedPipes;
 using Microsoft.AspNetCore.Razor.ProjectSystem;
 using Microsoft.AspNetCore.Razor.Telemetry;
@@ -124,8 +124,9 @@ public class RazorLanguageServerTest(ITestOutputHelper testOutput) : ToolingTest
             {
                 s.AddSingleton<IRazorProjectInfoDriver, TestProjectInfoDriver>();
 
-                // VS Code only handler is added by rzls, but add here for testing purposes
+                // VS Code only handlers are added by rzls, but add here for testing purposes
                 s.AddHandler<RazorNamedPipeConnectEndpoint>();
+                s.AddHandler<UpdateLogLevelEndpoint>();
             });
     }
 
