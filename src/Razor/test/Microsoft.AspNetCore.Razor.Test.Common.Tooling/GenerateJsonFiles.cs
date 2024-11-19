43f4c54d874e8fcc21530e422af4edadb508330a
Dustin Campbell <dustin@teamcampbell.org>
2024-11-19 11:08:01 -0800

    Add GenerateJsonFiles in MS.ANC.Razor.Test.Common.Tooling

---
+++ src/Razor/test/Microsoft.AspNetCore.Razor.Test.Common.Tooling/GenerateJsonFiles.cs
@@ -0,0 +1,23 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+// Uncomment to easily generate baselines for tests
+//#define GENERATE_JSON_FILES
+
+using Xunit;
+
+public static class GenerateJsonFiles
+{
+#if GENERATE_JSON_FILES
+    internal static readonly bool ShouldGenerate = true;
+#else
+    internal static readonly bool ShouldGenerate = false;
+#endif
+
+    // This is to prevent you from accidentally checking in with GenerateJsonFiles = true
+    [Fact]
+    public static void GenerateJsonFilesMustBeFalse()
+    {
+        Assert.False(ShouldGenerate, "GenerateJsonFiles should be set back to false before you check in!");
+    }
+}
