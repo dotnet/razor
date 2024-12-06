ed40963968b0af88491031fe2585f5f71b3e0ce6
David Wengier <david.wengier@microsoft.com>
2024-12-06 16:01:32 +1100

    Unused usings....

--- src/Razor/src/Microsoft.CodeAnalysis.Razor.Workspaces/Formatting/SnippetFormatter.cs
+++ src/Razor/src/Microsoft.CodeAnalysis.Razor.Workspaces/Formatting/SnippetFormatter.cs
@@ -4,7 +4,6 @@
 using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 using System.Linq;
-using Microsoft.CodeAnalysis.Razor.Formatting;
 using Microsoft.CodeAnalysis.Text;
 
 namespace Microsoft.CodeAnalysis.Razor.Formatting;
