102473920062907c2d3aa80d024bcf25a57fea02
Dustin Campbell <dustin@teamcampbell.org>
2024-12-11 12:01:04 -0800

    Introduce GetProject and GetRequiredProject extension methods

---
+++ src/Razor/src/Microsoft.CodeAnalysis.Razor.Workspaces/ProjectSystem/IProjectSnapshotManagerExtensions.cs
@@ -0,0 +1,26 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the MIT license. See License.txt in the project root for license information.
+
+using Microsoft.AspNetCore.Razor;
+using Microsoft.AspNetCore.Razor.ProjectSystem;
+
+namespace Microsoft.CodeAnalysis.Razor.ProjectSystem;
+
+internal static class IProjectSnapshotManagerExtensions
+{
+    public static IProjectSnapshot? GetProject(this IProjectSnapshotManager projectManager, ProjectKey projectKey)
+        => projectManager.TryGetProject(projectKey, out var result)
+            ? result
+            : null;
+
+    public static IProjectSnapshot GetRequiredProject(this IProjectSnapshotManager projectManager, ProjectKey projectKey)
+        => projectManager.GetProject(projectKey).AssumeNotNull();
+
+    public static IProjectSnapshot? GetProject(this ProjectSnapshotManager.Updater updater, ProjectKey projectKey)
+        => updater.TryGetProject(projectKey, out var result)
+            ? result
+            : null;
+
+    public static IProjectSnapshot GetRequiredProject(this ProjectSnapshotManager.Updater updater, ProjectKey projectKey)
+        => updater.GetProject(projectKey).AssumeNotNull();
+}
