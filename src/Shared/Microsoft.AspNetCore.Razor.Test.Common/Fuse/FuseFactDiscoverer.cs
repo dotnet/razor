6013045d338653db4a4e2055a1e55b1e18e1bdbc
David Wengier <david.wengier@microsoft.com>
2024-12-20 17:03:31 +1100

    Create test infra for running all tests on fuse and non-fuse

---
+++ src/Shared/Microsoft.AspNetCore.Razor.Test.Common/Fuse/FuseFactDiscoverer.cs
@@ -0,0 +1,28 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System.Collections.Generic;
+using Xunit.Abstractions;
+using Xunit.Sdk;
+
+namespace Microsoft.AspNetCore.Razor.Test.Common;
+
+internal sealed class FuseFactDiscoverer(IMessageSink diagnosticMessageSink)
+    : FactDiscoverer(diagnosticMessageSink)
+{
+    public override IEnumerable<IXunitTestCase> Discover(ITestFrameworkDiscoveryOptions discoveryOptions, ITestMethod testMethod, IAttributeInfo factAttribute)
+    {
+        return CreateTestCases(discoveryOptions, testMethod, DiagnosticMessageSink);
+    }
+
+    public static IEnumerable<IXunitTestCase> CreateTestCases(ITestFrameworkDiscoveryOptions discoveryOptions, ITestMethod testMethod, IMessageSink messageSink, object[]? dataRow = null)
+    {
+        yield return CreateTestCase(forceRuntimeCodeGeneration: false);
+        yield return CreateTestCase(forceRuntimeCodeGeneration: true);
+
+        FuseTestCase CreateTestCase(bool forceRuntimeCodeGeneration)
+        {
+            return new FuseTestCase(forceRuntimeCodeGeneration, messageSink, discoveryOptions.MethodDisplayOrDefault(), discoveryOptions.MethodDisplayOptionsOrDefault(), testMethod, dataRow);
+        }
+    }
+}
