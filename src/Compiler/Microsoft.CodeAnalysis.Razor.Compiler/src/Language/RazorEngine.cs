3e8210c24012b62440eaded011cbd0c95a44b50d
Dustin Campbell <dustin@teamcampbell.org>
2024-12-26 06:47:16 -0800

    RazorEngine: Use FeatureCache<T>

--- src/Compiler/Microsoft.CodeAnalysis.Razor.Compiler/src/Language/RazorEngine.cs
+++ src/Compiler/Microsoft.CodeAnalysis.Razor.Compiler/src/Language/RazorEngine.cs
@@ -1,7 +1,6 @@
 ﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-using System;
 using System.Collections.Immutable;
 using System.Diagnostics.CodeAnalysis;
 
@@ -12,10 +11,14 @@ public sealed class RazorEngine
     public ImmutableArray<IRazorEngineFeature> Features { get; }
     public ImmutableArray<IRazorEnginePhase> Phases { get; }
 
+    private readonly FeatureCache<IRazorEngineFeature> _featureCache;
+
     internal RazorEngine(ImmutableArray<IRazorEngineFeature> features, ImmutableArray<IRazorEnginePhase> phases)
     {
         Features = features;
         Phases = phases;
+
+        _featureCache = new(features);
 
         foreach (var feature in features)
         {
@@ -38,19 +41,20 @@ public sealed class RazorEngine
         }
     }
 
-    internal bool TryGetFeature<TFeature>([NotNullWhen(true)] out TFeature? feature)
+    public ImmutableArray<TFeature> GetFeatures<TFeature>()
+        where TFeature : class, IRazorEngineFeature
+        => _featureCache.GetFeatures<TFeature>();
+
+    public bool TryGetFeature<TFeature>([NotNullWhen(true)] out TFeature? result)
         where TFeature : class, IRazorEngineFeature
     {
-        foreach (var item in Features)
+        if (GetFeatures<TFeature>() is [var feature, ..])
         {
-            if (item is TFeature result)
-            {
-                feature = result;
-                return true;
-            }
+            result = feature;
+            return true;
         }
 
-        feature = null;
+        result = null;
         return false;
     }
 }
