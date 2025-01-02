4d742ad17311185ca16a5425917bd7284c4afcc7
David Wengier <david.wengier@microsoft.com>
2025-01-02 15:05:53 +1100

    Fix using

--- src/Razor/src/Microsoft.CodeAnalysis.Razor.Workspaces/CodeActions/Razor/ExtractToComponentCodeActionResolver.cs
+++ src/Razor/src/Microsoft.CodeAnalysis.Razor.Workspaces/CodeActions/Razor/ExtractToComponentCodeActionResolver.cs
@@ -1,7 +1,9 @@
 ﻿// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the MIT license. See License.txt in the project root for license information.
 
+#if !NET
 using System;
+#endif
 using System.IO;
 using System.Text.Json;
 using System.Threading;
