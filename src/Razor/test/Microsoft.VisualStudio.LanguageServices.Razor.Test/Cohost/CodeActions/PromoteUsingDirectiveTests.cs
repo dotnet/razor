ec24b69aede60bcaf57ae4adce659aedd0e332b0
David Wengier <david.wengier@microsoft.com>
2025-02-05 11:05:21 +1100

    Break up tests across multiple files
    Basically entirely mechanical copy and paste commit

---
+++ src/Razor/test/Microsoft.VisualStudio.LanguageServices.Razor.Test/Cohost/CodeActions/PromoteUsingDirectiveTests.cs
@@ -0,0 +1,185 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the MIT license. See License.txt in the project root for license information.
+
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Razor.Language;
+using Microsoft.AspNetCore.Razor.Test.Common;
+using Microsoft.CodeAnalysis.Razor.Protocol;
+using Xunit.Abstractions;
+
+namespace Microsoft.VisualStudio.Razor.LanguageClient.Cohost.CodeActions;
+
+public class PromoteUsingDirectiveTests(FuseTestContext context, ITestOutputHelper testOutputHelper) : CohostCodeActionsEndpointTestBase(context, testOutputHelper)
+{
+    [FuseFact]
+    public async Task PromoteUsingDirective()
+    {
+        await VerifyCodeActionAsync(
+            input: """
+                @using [||]System
+
+                <div>
+                    Hello World
+                </div>
+                """,
+            expected: """
+
+                <div>
+                    Hello World
+                </div>
+                """,
+            codeActionName: LanguageServerConstants.CodeActions.PromoteUsingDirective,
+            additionalExpectedFiles: [
+                (FileUri(@"..\_Imports.razor"), """
+                    @using System
+                    """)]);
+    }
+
+    [FuseFact]
+    public async Task PromoteUsingDirective_Indented()
+    {
+        await VerifyCodeActionAsync(
+            input: """
+                <div>
+                    @using [||]System
+                </div>
+
+                <div>
+                    Hello World
+                </div>
+                """,
+            expected: """
+                <div>
+                </div>
+
+                <div>
+                    Hello World
+                </div>
+                """,
+            codeActionName: LanguageServerConstants.CodeActions.PromoteUsingDirective,
+            additionalExpectedFiles: [
+                (FileUri(@"..\_Imports.razor"), """
+                    @using System
+                    """)]);
+    }
+
+    [FuseFact]
+    public async Task PromoteUsingDirective_Mvc()
+    {
+        await VerifyCodeActionAsync(
+            input: """
+                @using [||]System
+
+                <div>
+                    Hello World
+                </div>
+                """,
+            expected: """
+
+                <div>
+                    Hello World
+                </div>
+                """,
+            codeActionName: LanguageServerConstants.CodeActions.PromoteUsingDirective,
+            fileKind: FileKinds.Legacy,
+            additionalExpectedFiles: [
+                (FileUri(@"..\_ViewImports.cshtml"), """
+                    @using System
+                    """)]);
+    }
+
+    [FuseFact]
+    public async Task PromoteUsingDirective_ExistingImports()
+    {
+        await VerifyCodeActionAsync(
+            input: """
+                @using [||]System
+
+                <div>
+                    Hello World
+                </div>
+                """,
+            additionalFiles: [
+                (FilePath(@"..\_Imports.razor"), """
+                    @using System.Text
+                    @using Foo.Bar
+                    """)],
+            expected: """
+
+                <div>
+                    Hello World
+                </div>
+                """,
+            codeActionName: LanguageServerConstants.CodeActions.PromoteUsingDirective,
+            additionalExpectedFiles: [
+                (FileUri(@"..\_Imports.razor"), """
+                    @using System.Text
+                    @using Foo.Bar
+                    @using System
+                    """)]);
+    }
+
+    [FuseFact]
+    public async Task PromoteUsingDirective_ExistingImports_BlankLineAtEnd()
+    {
+        await VerifyCodeActionAsync(
+            input: """
+                @using [||]System
+
+                <div>
+                    Hello World
+                </div>
+                """,
+            additionalFiles: [
+                (FilePath(@"..\_Imports.razor"), """
+                    @using System.Text
+                    @using Foo.Bar
+                    
+                    """)],
+            expected: """
+
+                <div>
+                    Hello World
+                </div>
+                """,
+            codeActionName: LanguageServerConstants.CodeActions.PromoteUsingDirective,
+            additionalExpectedFiles: [
+                (FileUri(@"..\_Imports.razor"), """
+                    @using System.Text
+                    @using Foo.Bar
+                    @using System
+                    """)]);
+    }
+
+    [FuseFact]
+    public async Task PromoteUsingDirective_ExistingImports_WhitespaceLineAtEnd()
+    {
+        await VerifyCodeActionAsync(
+            input: """
+                @using [||]System
+
+                <div>
+                    Hello World
+                </div>
+                """,
+            additionalFiles: [
+                (FilePath(@"..\_Imports.razor"), """
+                    @using System.Text
+                    @using Foo.Bar
+                        
+                    """)],
+            expected: """
+
+                <div>
+                    Hello World
+                </div>
+                """,
+            codeActionName: LanguageServerConstants.CodeActions.PromoteUsingDirective,
+            additionalExpectedFiles: [
+                (FileUri(@"..\_Imports.razor"), """
+                    @using System.Text
+                    @using Foo.Bar
+                    @using System    
+                    """)]);
+    }
+}
---
+++ src/Razor/test/Microsoft.VisualStudio.LanguageServices.Razor.Test/Cohost/CodeActions/WrapAttributeTests.cs
@@ -0,0 +1,104 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the MIT license. See License.txt in the project root for license information.
+
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Razor.Test.Common;
+using Microsoft.CodeAnalysis.Razor.Protocol;
+using Xunit.Abstractions;
+
+namespace Microsoft.VisualStudio.Razor.LanguageClient.Cohost.CodeActions;
+
+public class WrapAttributeTests(FuseTestContext context, ITestOutputHelper testOutputHelper) : CohostCodeActionsEndpointTestBase(context, testOutputHelper)
+{
+    [FuseFact]
+    public async Task WrapAttributes()
+    {
+        await VerifyCodeActionAsync(
+            input: """
+                <div>
+                    <div [||]bar="Baz" Zip="Zap" checked @onclick="foo" Pop="Pap">
+                        <div></div>
+                    </div>
+                </div>
+                """,
+            expected: """
+                <div>
+                    <div bar="Baz"
+                         Zip="Zap"
+                         checked
+                         @onclick="foo"
+                         Pop="Pap">
+                        <div></div>
+                    </div>
+                </div>
+                """,
+            codeActionName: LanguageServerConstants.CodeActions.WrapAttributes);
+    }
+
+    [FuseFact]
+    public async Task WrapAttributes_Component()
+    {
+        await VerifyCodeActionAsync(
+            input: """
+                <div>
+                    <EditForm [||]bar="Baz" Zip="Zap" checked @onclick="foo" Pop="Pap" />
+                </div>
+                """,
+            expected: """
+                <div>
+                    <EditForm bar="Baz"
+                              Zip="Zap"
+                              checked
+                              @onclick="foo"
+                              Pop="Pap" />
+                </div>
+                """,
+            codeActionName: LanguageServerConstants.CodeActions.WrapAttributes);
+    }
+
+    [FuseFact]
+    public async Task WrapAttributes_Whitespace()
+    {
+        await VerifyCodeActionAsync(
+            input: """
+                <div>
+                    <Foo Bar="Baz"        Zip="Za[||]p"               Pop="Pap" />
+                </div>
+                """,
+            expected: """
+                <div>
+                    <Foo Bar="Baz"
+                         Zip="Zap"
+                         Pop="Pap" />
+                </div>
+                """,
+            codeActionName: LanguageServerConstants.CodeActions.WrapAttributes);
+    }
+
+    [FuseFact]
+    public async Task WrapAttributes_MultiLine()
+    {
+        await VerifyCodeActionAsync(
+            input: """
+                <div>
+                    <Foo Bar="Baz" Zip="Za[||]p"
+                         Pop="Pap" />
+                </div>
+                """,
+            expected: null,
+            codeActionName: LanguageServerConstants.CodeActions.WrapAttributes);
+    }
+
+    [FuseFact]
+    public async Task WrapAttributes_OneAttribute()
+    {
+        await VerifyCodeActionAsync(
+            input: """
+                <div>
+                    <Foo Zip="Za[||]p" />
+                </div>
+                """,
+            expected: null,
+            codeActionName: LanguageServerConstants.CodeActions.WrapAttributes);
+    }
+}
