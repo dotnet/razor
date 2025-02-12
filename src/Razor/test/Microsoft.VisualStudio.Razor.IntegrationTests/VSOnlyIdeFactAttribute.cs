914ace6f9dad31187812fbb18f56db31adda6f1b
David Wengier <david.wengier@microsoft.com>
2025-02-12 12:33:36 +1100

    Allow for tests that only can run in VS

---
+++ src/Razor/test/Microsoft.VisualStudio.Razor.IntegrationTests/VSOnlyIdeFactAttribute.cs
@@ -0,0 +1,19 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the MIT license. See License.txt in the project root for license information.
+
+using System;
+using Xunit;
+
+namespace Microsoft.VisualStudio.Razor.IntegrationTests;
+
+[AttributeUsage(AttributeTargets.Method, AllowMultiple = false)]
+public class VSOnlyIdeFactAttribute : IdeFactAttribute
+{
+    public VSOnlyIdeFactAttribute()
+    {
+        if (Environment.GetEnvironmentVariable("VisualStudioVersion") is null)
+        {
+            Skip = "This test can only run in Visual Studio";
+        }
+    }
+}
