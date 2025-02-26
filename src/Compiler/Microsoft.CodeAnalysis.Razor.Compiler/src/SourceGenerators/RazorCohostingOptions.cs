440c05cfe89c96c59ee3c7e73c78d07325cd110e
Chris Sienkiewicz <chsienki@microsoft.com>
2025-02-26 15:36:00 -0800

    Move cohost options to explicit class

---
+++ src/Compiler/Microsoft.CodeAnalysis.Razor.Compiler/src/SourceGenerators/RazorCohostingOptions.cs
@@ -0,0 +1,9 @@
+﻿namespace Microsoft.NET.Sdk.Razor.SourceGenerators;
+
+internal static class RazorCohostingOptions
+{
+    /// <summary>
+    /// True if razor is running in the cohosting mode
+    /// </summary>
+    internal static bool UseRazorCohostServer { get; set; } = false;
+}
