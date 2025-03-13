89b6765a82954394c12affc62f76696ffaf9d3a6
Chris Sienkiewicz <chsienki@microsoft.com>
2025-03-13 10:58:20 -0700

    Update a single test

--- src/Compiler/Microsoft.AspNetCore.Razor.Language/test/TestFiles/IntegrationTests/ComponentRuntimeCodeGenerationTest/GenericComponentTypeUsage/TestComponent.codegen.cs
+++ src/Compiler/Microsoft.AspNetCore.Razor.Language/test/TestFiles/IntegrationTests/ComponentRuntimeCodeGenerationTest/GenericComponentTypeUsage/TestComponent.codegen.cs
@@ -25,7 +25,15 @@ TItem
         #pragma warning disable 1998
         protected override void BuildRenderTree(global::Microsoft.AspNetCore.Components.Rendering.RenderTreeBuilder __builder)
         {
-            __builder.OpenComponent<global::Test.TestComponent<string>>(0);
+            __builder.OpenComponent<global::Test.TestComponent<
+#nullable restore
+#line (7,23)-(7,29) "x:\dir\subdir\Test\TestComponent.cshtml"
+string
+
+#line default
+#line hidden
+#nullable disable
+            >>(0);
             __builder.CloseComponent();
         }
         #pragma warning restore 1998
