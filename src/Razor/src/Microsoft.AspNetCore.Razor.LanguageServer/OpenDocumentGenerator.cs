da6d4c2a8c9a251dfd6640e0f4caf3d84240edca
Dustin Campbell <dustin@teamcampbell.org>
2025-03-17 10:04:41 -0700

    OpenDocumentGenerator: Use HashSet<DocumentKey> for unique items
    Update OpenDocumentGenerator to use a dedicated HashSet instance to compute the most recent unique items. In addition, make the following changes:
    
    - Add a DocumentSnapshot.Key property that produces a DocumentKey.
    - Update OpenDocumentGenerator's async batching work queue to track DocumentKeys rather than DocumentSnapshots.
    - Prefer faster boolean tests first, but checking _options.UpdateBuffersForCLosedDocuments before _projectManager.IsDocumentOpen(...)

--- src/Razor/src/Microsoft.AspNetCore.Razor.LanguageServer/OpenDocumentGenerator.cs
+++ src/Razor/src/Microsoft.AspNetCore.Razor.LanguageServer/OpenDocumentGenerator.cs
@@ -6,6 +6,7 @@ using System.Collections.Generic;
 using System.Collections.Immutable;
 using System.Threading;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Razor.ProjectSystem;
 using Microsoft.AspNetCore.Razor.Utilities;
 using Microsoft.CodeAnalysis.Razor.Logging;
 using Microsoft.CodeAnalysis.Razor.ProjectSystem;
@@ -32,8 +33,9 @@ internal partial class OpenDocumentGenerator : IRazorStartupService, IDisposable
     private readonly LanguageServerFeatureOptions _options;
     private readonly ILogger _logger;
 
-    private readonly AsyncBatchingWorkQueue<DocumentSnapshot> _workQueue;
+    private readonly AsyncBatchingWorkQueue<DocumentKey> _workQueue;
     private readonly CancellationTokenSource _disposeTokenSource;
+    private readonly HashSet<DocumentKey> _workerSet;
 
     public OpenDocumentGenerator(
         IEnumerable<IDocumentProcessedListener> listeners,
@@ -41,12 +43,13 @@ internal partial class OpenDocumentGenerator : IRazorStartupService, IDisposable
         LanguageServerFeatureOptions options,
         ILoggerFactory loggerFactory)
     {
-        _listeners = listeners.ToImmutableArray();
+        _listeners = [.. listeners];
         _projectManager = projectManager;
         _options = options;
 
+        _workerSet = [];
         _disposeTokenSource = new();
-        _workQueue = new AsyncBatchingWorkQueue<DocumentSnapshot>(
+        _workQueue = new AsyncBatchingWorkQueue<DocumentKey>(
             s_delay,
             ProcessBatchAsync,
             _disposeTokenSource.Token);
@@ -66,13 +69,20 @@ internal partial class OpenDocumentGenerator : IRazorStartupService, IDisposable
         _disposeTokenSource.Dispose();
     }
 
-    private async ValueTask ProcessBatchAsync(ImmutableArray<DocumentSnapshot> items, CancellationToken token)
+    private async ValueTask ProcessBatchAsync(ImmutableArray<DocumentKey> items, CancellationToken token)
     {
-        foreach (var document in items.GetMostRecentUniqueItems(Comparer.Instance))
+        _workerSet.Clear();
+
+        foreach (var key in items.GetMostRecentUniqueItems(_workerSet))
         {
             if (token.IsCancellationRequested)
             {
                 return;
+            }
+
+            if (!_projectManager.TryGetDocument(key, out var document))
+            {
+                continue;
             }
 
             var codeDocument = await document.GetGeneratedOutputAsync(token).ConfigureAwait(false);
@@ -109,7 +119,7 @@ internal partial class OpenDocumentGenerator : IRazorStartupService, IDisposable
                     {
                         if (newProject.TryGetDocument(documentFilePath, out var document))
                         {
-                            EnqueueIfNecessary(document);
+                            EnqueueIfNecessary(document.Key, document.Version);
                         }
                     }
 
@@ -127,11 +137,11 @@ internal partial class OpenDocumentGenerator : IRazorStartupService, IDisposable
 
                     if (newProject.TryGetDocument(documentFilePath, out var document))
                     {
-                        EnqueueIfNecessary(document);
+                        EnqueueIfNecessary(document.Key, document.Version);
 
                         foreach (var relatedDocument in newProject.GetRelatedDocuments(document))
                         {
-                            EnqueueIfNecessary(relatedDocument);
+                            EnqueueIfNecessary(relatedDocument.Key, relatedDocument.Version);
                         }
                     }
 
@@ -152,7 +162,7 @@ internal partial class OpenDocumentGenerator : IRazorStartupService, IDisposable
 
                             if (newProject.TryGetDocument(relatedDocumentFilePath, out var newRelatedDocument))
                             {
-                                EnqueueIfNecessary(newRelatedDocument);
+                                EnqueueIfNecessary(newRelatedDocument.Key, newRelatedDocument.Version);
                             }
                         }
                     }
@@ -167,17 +177,17 @@ internal partial class OpenDocumentGenerator : IRazorStartupService, IDisposable
                 }
         }
 
-        void EnqueueIfNecessary(DocumentSnapshot document)
+        void EnqueueIfNecessary(DocumentKey documentKey, int documentVersion)
         {
-            if (!_projectManager.IsDocumentOpen(document.FilePath) &&
-                !_options.UpdateBuffersForClosedDocuments)
+            if (!_options.UpdateBuffersForClosedDocuments &&
+                !_projectManager.IsDocumentOpen(documentKey.FilePath))
             {
                 return;
             }
 
-            _logger.LogDebug($"Enqueuing generation of {document.FilePath} in {document.Project.Key.Id} at version {document.Version}");
+            _logger.LogDebug($"Enqueuing generation of {documentKey.FilePath} in {documentKey.ProjectKey.Id} at version {documentVersion}");
 
-            _workQueue.AddWork(document);
+            _workQueue.AddWork(documentKey);
         }
     }
 }
