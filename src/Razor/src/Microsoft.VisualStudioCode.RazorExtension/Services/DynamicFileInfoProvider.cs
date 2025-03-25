39a5c88ab3e5e0cbbf03f2358b17609fe4e7524c
Andrew Hall <andrha@microsoft.com>
2025-03-25 11:54:04 -0700

    Consolidate code

--- src/Razor/src/Microsoft.VisualStudioCode.RazorExtension/Services/DynamicFileInfoProvider.cs
+++ src/Razor/src/Microsoft.VisualStudioCode.RazorExtension/Services/DynamicFileInfoProvider.cs
@@ -1,24 +1,13 @@
 ﻿// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the MIT license. See License.txt in the project root for license information.
 
-using System.Composition;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.ExternalAccess.Razor;
 using Microsoft.CodeAnalysis.ExternalAccess.Razor.Features;
 
 namespace Microsoft.VisualStudioCode.RazorExtension.Services;
 
-[ExportRazorLspServiceFactory(typeof(RazorLspDynamicFileInfoProvider)), Shared]
-internal sealed class DynamicFileProviderFactory : AbstractRazorLspServiceFactory
-{
-    protected override AbstractRazorLspService CreateService(IRazorLspServices lspServices)
-    {
-        var clientLanguageServerManager = lspServices.GetRequiredService<IRazorClientLanguageServerManager>();
-        return new LspDynamicFileProvider(clientLanguageServerManager);
-    }
-}
-
-file sealed class LspDynamicFileProvider(IRazorClientLanguageServerManager clientLanguageServerManager) : RazorLspDynamicFileInfoProvider
+internal sealed partial class LspDynamicFileProvider(IRazorClientLanguageServerManager clientLanguageServerManager) : RazorLspDynamicFileInfoProvider
 {
     private const string ProvideRazorDynamicFileInfoMethodName = "razor/provideDynamicFileInfo";
     private const string RemoveRazorDynamicFileInfoMethodName = "razor/removeDynamicFileInfo";
