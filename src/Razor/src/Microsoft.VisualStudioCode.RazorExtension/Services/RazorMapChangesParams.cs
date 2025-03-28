bd1fc09c17754be3956cbbc514926ee1fe6cba12
Andrew Hall <andrha@microsoft.com>
2025-03-28 12:33:04 -0700

    Add lsp mapping service

---
+++ src/Razor/src/Microsoft.VisualStudioCode.RazorExtension/Services/RazorMapChangesParams.cs
@@ -0,0 +1,17 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the MIT license. See License.txt in the project root for license information.
+
+using System.Text.Json.Serialization;
+using Microsoft.CodeAnalysis.Razor.Protocol;
+using Microsoft.VisualStudio.LanguageServer.Protocol;
+
+namespace Microsoft.VisualStudioCode.RazorExtension.Services;
+
+internal sealed class RazorMapChangesParams
+{
+    [JsonPropertyName("csharpDocument")]
+    public required TextDocumentIdentifier CSharpDocument { get; set; }
+
+    [JsonPropertyName("textChanges")]
+    public required RazorTextChange[] TextChanges { get; set; }
+}
---
+++ src/Razor/src/Microsoft.VisualStudioCode.RazorExtension/Services/RazorMapSpansParams.cs
@@ -0,0 +1,17 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the MIT license. See License.txt in the project root for license information.
+
+using System.Text.Json.Serialization;
+using Microsoft.CodeAnalysis.Razor.Protocol;
+using Microsoft.VisualStudio.LanguageServer.Protocol;
+
+namespace Microsoft.VisualStudioCode.RazorExtension.Services;
+
+internal sealed class RazorMapSpansParams
+{
+    [JsonPropertyName("csharpDocument")]
+    public required TextDocumentIdentifier CSharpDocument { get; set; }
+
+    [JsonPropertyName("spans")]
+    public required RazorTextSpan[] Spans { get; set; }
+}
