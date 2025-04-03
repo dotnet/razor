6549f7cd29bdb817742dea9f11d802aeed760168
David Wengier <david.wengier@microsoft.com>
2025-04-03 14:12:50 +1100

    Attempt number 1 at writing powershell

--- eng/scripts/CreateVSHive.ps1
+++ eng/scripts/CreateVSHive.ps1
@@ -31,8 +31,13 @@ if($success -eq $false){
   throw "Failed to create hive"
 }
 
-Write-Host "-- VS Info --"
 $vsDir = Split-Path -Parent $devenvExePath
+
+$vsRegEdit = Join-Path (Join-Path (Join-Path $vsDir 'Common7') 'IDE') 'VsRegEdit.exe'
+
+&$vsRegEdit set "$vsDir" $hive HKLM "Profile" DisableFirstLaunchDialog dword 1
+
+Write-Host "-- VS Info --"
 $isolationIni = Join-Path $vsDir 'devenv.isolation.ini'
 Get-Content $isolationIni | Write-Host
 Write-Host "-- /VS Info --"
