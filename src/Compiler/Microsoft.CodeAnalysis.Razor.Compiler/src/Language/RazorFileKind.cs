24faf00933cf6372ace1b73adc9f270fb72c97aa
Dustin Campbell <dustin@teamcampbell.org>
2025-04-07 14:40:29 -0700

    Add RazorFileKind enum
    Add RazorFileKind enum that provides the allowable values from FileKinds. In addition, include a None value for scenarios where a file kind would be null.

---
+++ src/Compiler/Microsoft.CodeAnalysis.Razor.Compiler/src/Language/RazorFileKind.cs
@@ -0,0 +1,12 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+namespace Microsoft.AspNetCore.Razor.Language;
+
+public enum RazorFileKind : byte
+{
+    None = 0,
+    Component = 1,
+    ComponentImport = 2,
+    Legacy = 3
+}
