0b043d8bdae5d1d98c490d69c9d2a4b99bded4ab
Dustin Campbell <dustin@teamcampbell.org>
2025-04-10 11:33:21 -0700

    RazorFileKind: Add doc comments

--- src/Compiler/Microsoft.CodeAnalysis.Razor.Compiler/src/Language/RazorFileKind.cs
+++ src/Compiler/Microsoft.CodeAnalysis.Razor.Compiler/src/Language/RazorFileKind.cs
@@ -3,10 +3,25 @@
 
 namespace Microsoft.AspNetCore.Razor.Language;
 
+/// <summary>
+///  The type of Razor file.
+/// </summary>
 public enum RazorFileKind : byte
 {
     None = 0,
+
+    /// <summary>
+    ///  A file containing a Razor component, i.e. has a '.razor' file extension.
+    /// </summary>
     Component = 1,
+
+    /// <summary>
+    ///  A file containing a Razor component import, i.e. file name is '_Imports.razor'.
+    /// </summary>
     ComponentImport = 2,
+
+    /// <summary>
+    ///  A file containing legacy Razor code, i.e. has a '.cshtml' file extension.
+    /// </summary>
     Legacy = 3
 }
