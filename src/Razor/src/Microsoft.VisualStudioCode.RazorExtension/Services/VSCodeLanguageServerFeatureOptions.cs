9ae25ffb26c36cea27a9ed9a962684db10fe0f7b
David Wengier <david.wengier@microsoft.com>
2025-04-14 08:09:02 +1000

    Add a few required services for cohosting, without real implementations yet

---
+++ src/Razor/src/Microsoft.VisualStudioCode.RazorExtension/Services/VSCodeLanguageServerFeatureOptions.cs
@@ -0,0 +1,48 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the MIT license. See License.txt in the project root for license information.
+
+using System;
+using System.Composition;
+using Microsoft.AspNetCore.Razor.Utilities;
+using Microsoft.CodeAnalysis.Razor.Workspaces;
+
+namespace Microsoft.VisualStudioCode.RazorExtension.Services;
+
+// TODO:
+
+[Shared]
+[Export(typeof(LanguageServerFeatureOptions))]
+internal class VSCodeLanguageServerFeatureOptions : LanguageServerFeatureOptions
+{
+    public override bool SupportsFileManipulation => false;
+
+    public override string CSharpVirtualDocumentSuffix => throw new InvalidOperationException("This property is not valid in OOP");
+
+    public override string HtmlVirtualDocumentSuffix => "*.html";
+
+    public override bool SingleServerSupport => throw new InvalidOperationException("This option has not been synced to OOP.");
+
+    public override bool DelegateToCSharpOnDiagnosticPublish => throw new InvalidOperationException("This option has not been synced to OOP.");
+
+    public override bool UsePreciseSemanticTokenRanges => false;
+
+    public override bool ShowAllCSharpCodeActions => false;
+
+    public override bool UpdateBuffersForClosedDocuments => throw new InvalidOperationException("This option has not been synced to OOP.");
+
+    public override bool ReturnCodeActionAndRenamePathsWithPrefixedSlash => PlatformInformation.IsWindows;
+
+    public override bool IncludeProjectKeyInGeneratedFilePath => throw new InvalidOperationException("This option does not apply in cohosting.");
+
+    public override bool UseRazorCohostServer => true;
+
+    public override bool ForceRuntimeCodeGeneration => true;
+
+    public override bool UseNewFormattingEngine => true;
+
+    public override bool SupportsSoftSelectionInCompletion => false;
+
+    public override bool UseVsCodeCompletionTriggerCharacters => true;
+
+    public override bool DoNotInitializeMiscFilesProjectFromWorkspace => throw new NotImplementedException("This option has not been synced to OOP.");
+}
---
+++ src/Razor/src/Microsoft.VisualStudioCode.RazorExtension/Services/VSCodeRemoteServiceInvoker.cs
@@ -0,0 +1,24 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the MIT license. See License.txt in the project root for license information.
+
+using System;
+using System.Composition;
+using System.Runtime.CompilerServices;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.ExternalAccess.Razor;
+using Microsoft.CodeAnalysis.Razor.Remote;
+
+namespace Microsoft.VisualStudioCode.RazorExtension.Services;
+
+[Shared]
+[Export(typeof(IRemoteServiceInvoker))]
+internal class VSCodeRemoteServiceInvoker : IRemoteServiceInvoker
+{
+    public ValueTask<TResult?> TryInvokeAsync<TService, TResult>(Solution solution, Func<TService, RazorPinnedSolutionInfoWrapper, CancellationToken, ValueTask<TResult>> invocation, CancellationToken cancellationToken, [CallerFilePath] string? callerFilePath = null, [CallerMemberName] string? callerMemberName = null) where TService : class
+    {
+        // TODO:
+        throw new NotImplementedException();
+    }
+}
