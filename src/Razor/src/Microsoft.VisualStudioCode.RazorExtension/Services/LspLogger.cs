2ee87a37aa8363d48913d4035aaf94a0e15937ad
David Wengier <david.wengier@microsoft.com>
2025-04-15 14:13:25 +1000

    Add logger factory infra to VS Code extension to log via LSP

---
+++ src/Razor/src/Microsoft.VisualStudioCode.RazorExtension/Services/LspLogger.cs
@@ -0,0 +1,54 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the MIT license. See License.txt in the project root for license information.
+
+using System;
+using System.Threading;
+using Microsoft.CodeAnalysis.Razor.Logging;
+using Microsoft.VisualStudio.Threading;
+
+namespace Microsoft.AspNetCore.Razor.LanguageServer;
+
+/// <summary>
+/// ILogger implementation that logs via the window/logMessage LSP method
+/// </summary>
+internal class LspLogger(string categoryName, RazorClientServerManagerProvider razorClientServerManagerProvider) : ILogger
+{
+    private readonly string _categoryName = categoryName;
+    private readonly RazorClientServerManagerProvider _razorClientServerManagerProvider = razorClientServerManagerProvider;
+
+    public bool IsEnabled(LogLevel logLevel) => true;
+
+    public void Log(LogLevel logLevel, string message, Exception? exception)
+    {
+        if (_razorClientServerManagerProvider.ClientLanguageServerManager is not { } clientLanguageServerManager)
+        {
+            return;
+        }
+
+        if (!IsEnabled(logLevel))
+        {
+            return;
+        }
+
+        var messageType = logLevel switch
+        {
+            LogLevel.Critical => MessageType.Error,
+            LogLevel.Error => MessageType.Error,
+            LogLevel.Warning => MessageType.Warning,
+            LogLevel.Information => MessageType.Info,
+            LogLevel.Debug => MessageType.Log,
+            LogLevel.Trace => MessageType.Log,
+            _ => throw new NotImplementedException(),
+        };
+
+        var formattedMessage = LogMessageFormatter.FormatMessage(message, _categoryName, exception, includeTimeStamp: false);
+
+        var @params = new LogMessageParams
+        {
+            MessageType = messageType,
+            Message = formattedMessage,
+        };
+
+        clientLanguageServerManager.SendNotificationAsync(Methods.WindowLogMessageName, @params, CancellationToken.None).Forget();
+    }
+}
---
+++ src/Razor/src/Microsoft.VisualStudioCode.RazorExtension/Services/RazorClientServerManagerProvider.cs
@@ -0,0 +1,27 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the MIT license. See License.txt in the project root for license information.
+
+using System.Composition;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis.ExternalAccess.Razor;
+using Microsoft.CodeAnalysis.ExternalAccess.Razor.Cohost;
+
+namespace Microsoft.AspNetCore.Razor.LanguageServer;
+
+[Shared]
+[Export(typeof(ICohostStartupService))]
+[Export(typeof(RazorClientServerManagerProvider))]
+[method: ImportingConstructor]
+internal class RazorClientServerManagerProvider() : ICohostStartupService
+{
+    private IRazorClientLanguageServerManager? _razorClientLanguageServerManager;
+
+    public IRazorClientLanguageServerManager? ClientLanguageServerManager => _razorClientLanguageServerManager;
+
+    public Task StartupAsync(string serializedClientCapabilities, RazorCohostRequestContext requestContext, CancellationToken cancellationToken)
+    {
+        _razorClientLanguageServerManager = requestContext.GetRequiredService<IRazorClientLanguageServerManager>();
+        return Task.CompletedTask;
+    }
+}
