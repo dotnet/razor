8cbd21730fd63ce4c9f2517793dd347a079ab5b6
Andrew Hall <andrha@microsoft.com>
2025-05-05 15:50:45 -0700

    Fix import and add function to interface

--- src/Razor/src/Microsoft.CodeAnalysis.Razor.Workspaces/Completion/ISnippetCompletionItemProvider.cs
+++ src/Razor/src/Microsoft.CodeAnalysis.Razor.Workspaces/Completion/ISnippetCompletionItemProvider.cs
@@ -1,6 +1,7 @@
 ﻿// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the MIT license. See License.txt in the project root for license information.
 
+using System.Diagnostics.CodeAnalysis;
 using Microsoft.AspNetCore.Razor.PooledObjects;
 using Microsoft.CodeAnalysis.Razor.Protocol;
 
@@ -9,4 +10,5 @@ namespace Microsoft.CodeAnalysis.Razor.Completion;
 internal interface ISnippetCompletionItemProvider
 {
     void AddSnippetCompletions(ref PooledArrayBuilder<VSInternalCompletionItem> builder, RazorLanguageKind projectedKind, VSInternalCompletionInvokeKind invokeKind, string? triggerCharacter);
+    bool TryResolveInsertString(VSInternalCompletionItem completionItem, [NotNullWhen(true)] out string? insertString);
 }
