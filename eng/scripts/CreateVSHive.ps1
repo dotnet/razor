cc511b7d8a09c0037289025bcf297eeb098f90b4
akhera99 <ankitakhera@microsoft.com>
2025-05-21 14:22:53 -0700

    whoops

--- eng/scripts/CreateVSHive.ps1
+++ eng/scripts/CreateVSHive.ps1
@@ -18,15 +18,6 @@ Set-StrictMode -Version 1
 $success=$false
 for($i=0; $i -le 3; $i++)
 {
-  Write-Host "Searching for 'RoslynDev' under $env:LocalAppData\Microsoft"
-  $roslynDevPaths = Get-ChildItem -Path "$env:LocalAppData\Microsoft" -Recurse -Filter *RoslynDev* -ErrorAction SilentlyContinue | Select-Object -ExpandProperty FullName
-  if ($roslynDevPaths) {
-    Write-Host "Found the following 'RoslynDev' paths:"
-    $roslynDevPaths | ForEach-Object { Write-Host $_ }
-  } else {
-    Write-Host "'RoslynDev' not found under $env:LocalAppData\Microsoft"
-  }
-
   & $devenvExePath /rootsuffix $rootSuffix /updateConfiguration
   if(Test-Path -Path $env:LocalAppData\Microsoft\VisualStudio\18.0*RoslynDev)
   {
@@ -36,16 +27,8 @@ for($i=0; $i -le 3; $i++)
   }
 }
 
-if($success -eq $false){
-  Write-Host "Searching for 'devenv.exe' under C:\ ..."
-  $devenvPaths = Get-ChildItem -Path C:\ -Filter devenv.exe -Recurse -ErrorAction SilentlyContinue | Select-Object -ExpandProperty FullName
-  if ($devenvPaths) {
-    Write-Host "Found the following 'devenv.exe' paths:"
-    $devenvPaths | ForEach-Object { Write-Host $_ }
-  } else {
-    Write-Host "'devenv.exe' not found under C:\"
-  }
-
+if($success -eq $false)
+{
   throw "Failed to create hive"
 }
 
