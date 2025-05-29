dotnet/razor

commit ac6e32a73fb93b3c699269fa432efac38fe4d5fe

Use RazorProjectItem.GetSource() in tooling project system
This change uses RazorProjectItem.GetSource() in tooling, which avoids creating the same RazorSourceDocuments over and over again for default imports. This allows a lot of duplicated code to process imports to be removed and simplified.

src/Razor/src/Microsoft.CodeAnalysis.Razor.Workspaces/ProjectSystem/CompilationHelpers.cs
@@ -2,9 +2,11 @@
 // Licensed under the MIT license. See License.txt in the project root for license information.
 
 using System.Collections.Immutable;
+using System.Diagnostics;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Razor.Language;
+using Microsoft.AspNetCore.Razor.PooledObjects;
 using Microsoft.AspNetCore.Razor.ProjectSystem;
 
 namespace Microsoft.CodeAnalysis.Razor.ProjectSystem;
@@ -17,22 +19,9 @@ internal static class CompilationHelpers
         RazorCompilerOptions compilerOptions,
         CancellationToken cancellationToken)
     {
-        var importItems = await ImportHelpers.GetImportItemsAsync(document, projectEngine, cancellationToken).ConfigureAwait(false);
-
-        return await GenerateCodeDocumentAsync(
-            document, projectEngine, importItems, compilerOptions, cancellationToken).ConfigureAwait(false);
-    }
-
-    internal static async Task<RazorCodeDocument> GenerateCodeDocumentAsync(
-        IDocumentSnapshot document,
-        RazorProjectEngine projectEngine,
-        ImmutableArray<ImportItem> imports,
-        RazorCompilerOptions compilerOptions,
-        CancellationToken cancellationToken)
-    {
-        var importSources = ImportHelpers.GetImportSources(imports, projectEngine);
+        var importSources = await GetImportSourcesAsync(document, projectEngine, cancellationToken).ConfigureAwait(false);
         var tagHelpers = await document.Project.GetTagHelpersAsync(cancellationToken).ConfigureAwait(false);
-        var source = await ImportHelpers.GetSourceAsync(document, projectEngine, cancellationToken).ConfigureAwait(false);
+        var source = await document.GetSourceAsync(projectEngine, cancellationToken).ConfigureAwait(false);
 
         var generator = new CodeDocumentGenerator(projectEngine, compilerOptions);
         return generator.Generate(source, document.FileKind, importSources, tagHelpers, cancellationToken);
@@ -41,28 +30,66 @@ internal static class CompilationHelpers
     internal static async Task<RazorCodeDocument> GenerateDesignTimeCodeDocumentAsync(
         IDocumentSnapshot document,
         RazorProjectEngine projectEngine,
-        ImmutableArray<ImportItem> imports,
         CancellationToken cancellationToken)
     {
-        var importSources = ImportHelpers.GetImportSources(imports, projectEngine);
+        var importSources = await GetImportSourcesAsync(document, projectEngine, cancellationToken).ConfigureAwait(false);
         var tagHelpers = await document.Project.GetTagHelpersAsync(cancellationToken).ConfigureAwait(false);
-        var source = await ImportHelpers.GetSourceAsync(document, projectEngine, cancellationToken).ConfigureAwait(false);
+        var source = await document.GetSourceAsync(projectEngine, cancellationToken).ConfigureAwait(false);
 
         var generator = new CodeDocumentGenerator(projectEngine, RazorCompilerOptions.None);
         return generator.GenerateDesignTime(source, document.FileKind, importSources, tagHelpers, cancellationToken);
     }
 
-    internal static async Task<RazorCodeDocument> GenerateDesignTimeCodeDocumentAsync(
-        IDocumentSnapshot document,
-        RazorProjectEngine projectEngine,
-        CancellationToken cancellationToken)
+    private static async Task<ImmutableArray<RazorSourceDocument>> GetImportSourcesAsync(IDocumentSnapshot document, RazorProjectEngine projectEngine, CancellationToken cancellationToken)
     {
-        var importItems = await ImportHelpers.GetImportItemsAsync(document, projectEngine, cancellationToken).ConfigureAwait(false);
-        var importSources = ImportHelpers.GetImportSources(importItems, projectEngine);
-        var tagHelpers = await document.Project.GetTagHelpersAsync(cancellationToken).ConfigureAwait(false);
-        var source = await ImportHelpers.GetSourceAsync(document, projectEngine, cancellationToken).ConfigureAwait(false);
+        var projectItem = projectEngine.FileSystem.GetItem(document.FilePath, document.FileKind);
 
-        var generator = new CodeDocumentGenerator(projectEngine, RazorCompilerOptions.None);
-        return generator.GenerateDesignTime(source, document.FileKind, importSources, tagHelpers, cancellationToken);
+        using var importProjectItems = new PooledArrayBuilder<RazorProjectItem>();
+
+        foreach (var feature in projectEngine.GetFeatures<IImportProjectFeature>())
+        {
+            if (feature.GetImports(projectItem) is { } featureImports)
+            {
+                importProjectItems.AddRange(featureImports);
+            }
+        }
+
+        if (importProjectItems.Count == 0)
+        {
+            return [];
+        }
+
+        var project = document.Project;
+
+        using var importSources = new PooledArrayBuilder<RazorSourceDocument>(capacity: importProjectItems.Count);
+
+        foreach (var importProjectItem in importProjectItems)
+        {
+            if (importProjectItem is NotFoundProjectItem)
+            {
+                continue;
+            }
+
+            if (importProjectItem.PhysicalPath is null)
+            {
+                // This is a default import.
+                var importSource = importProjectItem.GetSource();
+                importSources.Add(importSource);
+            }
+            else if (project.TryGetDocument(importProjectItem.PhysicalPath, out var importDocument))
+            {
+                var text = await importDocument.GetTextAsync(cancellationToken).ConfigureAwait(false);
+                var properties = RazorSourceDocumentProperties.Create(importProjectItem.FilePath, importProjectItem.RelativePhysicalPath);
+                var importSource = RazorSourceDocument.Create(text, properties);
+
+                importSources.Add(importSource);
+            }
+            else if (importProjectItem.Exists)
+            {
+                Debug.Fail($"Encountered an import that was not a default import or tracked in the project system: {importProjectItem.PhysicalPath}.");
+            }
+        }
+
+        return importSources.DrainToImmutable();
     }
 }

src/Razor/src/Microsoft.CodeAnalysis.Razor.Workspaces/ProjectSystem/DocumentSnapshot.cs
@@ -66,15 +66,8 @@ internal sealed class DocumentSnapshot(ProjectSnapshot project, DocumentState st
     }
 
 #if !FORMAT_FUSE
-    public async Task<RazorCodeDocument> GenerateDesignTimeOutputAsync(CancellationToken cancellationToken)
-    {
-        var importItems = await Project.GetImportItemsAsync(FilePath, cancellationToken).ConfigureAwait(false);
-        var projectEngine = Project.ProjectEngine;
-
-        return await CompilationHelpers
-            .GenerateDesignTimeCodeDocumentAsync(this, projectEngine, importItems, cancellationToken)
-            .ConfigureAwait(false);
-    }
+    public Task<RazorCodeDocument> GenerateDesignTimeOutputAsync(CancellationToken cancellationToken)
+        => CompilationHelpers.GenerateDesignTimeCodeDocumentAsync(this, Project.ProjectEngine, cancellationToken);
 #endif
 
     #region ILegacyDocumentSnapshot support

src/Razor/src/Microsoft.CodeAnalysis.Razor.Workspaces/ProjectSystem/IDocumentSnapshotExtensions.cs
@@ -12,6 +12,17 @@ namespace Microsoft.CodeAnalysis.Razor.ProjectSystem;
 
 internal static class IDocumentSnapshotExtensions
 {
+    public static async Task<RazorSourceDocument> GetSourceAsync(this IDocumentSnapshot document, RazorProjectEngine projectEngine, CancellationToken cancellationToken)
+    {
+        var projectItem = document is { FilePath: string filePath, FileKind: var fileKind }
+            ? projectEngine.FileSystem.GetItem(filePath, fileKind)
+            : null;
+
+        var text = await document.GetTextAsync(cancellationToken).ConfigureAwait(false);
+        var properties = RazorSourceDocumentProperties.Create(document.FilePath, projectItem?.RelativePhysicalPath);
+        return RazorSourceDocument.Create(text, properties);
+    }
+
     public static async Task<TagHelperDescriptor?> TryGetTagHelperDescriptorAsync(
         this IDocumentSnapshot documentSnapshot,
         CancellationToken cancellationToken)

src/Razor/src/Microsoft.CodeAnalysis.Razor.Workspaces/ProjectSystem/ImportHelpers.cs
@@ -1,97 +1,0 @@
-﻿// Copyright (c) .NET Foundation. All rights reserved.
-// Licensed under the MIT license. See License.txt in the project root for license information.
-
-using System.Collections.Immutable;
-using System.Threading;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Razor.Language;
-using Microsoft.AspNetCore.Razor.PooledObjects;
-using Microsoft.AspNetCore.Razor.ProjectSystem;
-using Microsoft.CodeAnalysis.Text;
-
-namespace Microsoft.CodeAnalysis.Razor.ProjectSystem;
-
-internal static class ImportHelpers
-{
-    public static async Task<ImmutableArray<ImportItem>> GetImportItemsAsync(IDocumentSnapshot document, RazorProjectEngine projectEngine, CancellationToken cancellationToken)
-    {
-        var projectItem = projectEngine.FileSystem.GetItem(document.FilePath, document.FileKind);
-
-        using var importProjectItems = new PooledArrayBuilder<RazorProjectItem>();
-
-        foreach (var feature in projectEngine.GetFeatures<IImportProjectFeature>())
-        {
-            if (feature.GetImports(projectItem) is { } featureImports)
-            {
-                importProjectItems.AddRange(featureImports);
-            }
-        }
-
-        if (importProjectItems.Count == 0)
-        {
-            return [];
-        }
-
-        var project = document.Project;
-
-        using var importItems = new PooledArrayBuilder<ImportItem>(capacity: importProjectItems.Count);
-
-        foreach (var importProjectItem in importProjectItems)
-        {
-            if (importProjectItem is NotFoundProjectItem)
-            {
-                continue;
-            }
-
-            if (importProjectItem.PhysicalPath is null)
-            {
-                // This is a default import.
-                using var stream = importProjectItem.Read();
-                var text = SourceText.From(stream);
-                var defaultImport = ImportItem.CreateDefault(text);
-
-                importItems.Add(defaultImport);
-            }
-            else if (project.TryGetDocument(importProjectItem.PhysicalPath, out var importDocument))
-            {
-                var text = await importDocument.GetTextAsync(cancellationToken).ConfigureAwait(false);
-                var versionStamp = await importDocument.GetTextVersionAsync(cancellationToken).ConfigureAwait(false);
-                var importItem = new ImportItem(importDocument.FilePath, importDocument.FileKind, text, versionStamp);
-
-                importItems.Add(importItem);
-            }
-        }
-
-        return importItems.DrainToImmutable();
-    }
-
-    public static ImmutableArray<RazorSourceDocument> GetImportSources(ImmutableArray<ImportItem> importItems, RazorProjectEngine projectEngine)
-    {
-        using var importSources = new PooledArrayBuilder<RazorSourceDocument>(importItems.Length);
-
-        foreach (var importItem in importItems)
-        {
-            var importProjectItem = importItem is { FilePath: string filePath, FileKind: var fileKind }
-                ? projectEngine.FileSystem.GetItem(filePath, fileKind)
-                : null;
-
-            var properties = RazorSourceDocumentProperties.Create(importItem.FilePath, importProjectItem?.RelativePhysicalPath);
-            var importSource = RazorSourceDocument.Create(importItem.Text, properties);
-
-            importSources.Add(importSource);
-        }
-
-        return importSources.DrainToImmutable();
-    }
-
-    public static async Task<RazorSourceDocument> GetSourceAsync(IDocumentSnapshot document, RazorProjectEngine projectEngine, CancellationToken cancellationToken)
-    {
-        var projectItem = document is { FilePath: string filePath, FileKind: var fileKind }
-            ? projectEngine.FileSystem.GetItem(filePath, fileKind)
-            : null;
-
-        var text = await document.GetTextAsync(cancellationToken).ConfigureAwait(false);
-        var properties = RazorSourceDocumentProperties.Create(document.FilePath, projectItem?.RelativePhysicalPath);
-        return RazorSourceDocument.Create(text, properties);
-    }
-}

src/Razor/src/Microsoft.CodeAnalysis.Razor.Workspaces/ProjectSystem/ImportItem.cs
@@ -1,17 +1,0 @@
-﻿// Copyright (c) .NET Foundation. All rights reserved.
-// Licensed under the MIT license. See License.txt in the project root for license information.
-
-using Microsoft.CodeAnalysis.Text;
-
-namespace Microsoft.CodeAnalysis.Razor.ProjectSystem;
-
-internal readonly record struct ImportItem(
-    string? FilePath,
-    string? FileKind,
-    SourceText Text,
-    VersionStamp Version)
-{
-    // Note: The default import does not have file path, file kind, or version stamp.
-    public static ImportItem CreateDefault(SourceText text)
-        => new(FilePath: null, FileKind: null, text, Version: default);
-}

src/Razor/src/Microsoft.CodeAnalysis.Razor.Workspaces/ProjectSystem/ProjectSnapshot.cs
@@ -12,7 +12,6 @@ using Microsoft.AspNetCore.Razor.ProjectSystem;
 using Microsoft.AspNetCore.Razor.Utilities;
 using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.Razor.ProjectSystem.Legacy;
-using Microsoft.CodeAnalysis.Text;
 
 namespace Microsoft.CodeAnalysis.Razor.ProjectSystem;
 
@@ -126,64 +125,6 @@ internal sealed class ProjectSnapshot(ProjectState state) : IProjectSnapshot, IL
 
             return builder.DrainToImmutable();
         }
-    }
-
-    public ValueTask<ImmutableArray<ImportItem>> GetImportItemsAsync(string filePath, CancellationToken cancellationToken)
-    {
-        return _state.Documents.TryGetValue(filePath, out var state)
-            ? new(GetImportItemsAsync(state.HostDocument, cancellationToken))
-            : new([]);
-    }
-
-    public async Task<ImmutableArray<ImportItem>> GetImportItemsAsync(HostDocument hostDocument, CancellationToken cancellationToken)
-    {
-        var projectEngine = ProjectEngine;
-
-        var projectItem = projectEngine.FileSystem.GetItem(hostDocument.FilePath, hostDocument.FileKind);
-
-        using var importProjectItems = new PooledArrayBuilder<RazorProjectItem>();
-
-        foreach (var feature in projectEngine.GetFeatures<IImportProjectFeature>())
-        {
-            if (feature.GetImports(projectItem) is { } featureImports)
-            {
-                importProjectItems.AddRange(featureImports);
-            }
-        }
-
-        if (importProjectItems.Count == 0)
-        {
-            return [];
-        }
-
-        using var importItems = new PooledArrayBuilder<ImportItem>(capacity: importProjectItems.Count);
-
-        foreach (var importProjectItem in importProjectItems)
-        {
-            if (importProjectItem is NotFoundProjectItem)
-            {
-                continue;
-            }
-
-            if (importProjectItem.PhysicalPath is null)
-            {
-                // This is a default import.
-                using var stream = importProjectItem.Read();
-                var text = SourceText.From(stream);
-                var defaultImport = ImportItem.CreateDefault(text);
-
-                importItems.Add(defaultImport);
-            }
-            else if (_state.Documents.TryGetValue(importProjectItem.PhysicalPath, out var importDocumentState))
-            {
-                var textAndVersion = await importDocumentState.GetTextAndVersionAsync(cancellationToken).ConfigureAwait(false);
-                var importItem = new ImportItem(importDocumentState.HostDocument.FilePath, importDocumentState.HostDocument.FileKind, textAndVersion.Text, textAndVersion.Version);
-
-                importItems.Add(importItem);
-            }
-        }
-
-        return importItems.DrainToImmutable();
     }
 
     #region ILegacyProjectSnapshot support

src/Razor/src/Microsoft.CodeAnalysis.Razor.Workspaces/ProjectSystem/Sources/GeneratedOutputSource.cs
@@ -39,10 +39,8 @@ internal sealed class GeneratedOutputSource
             var projectEngine = project.ProjectEngine;
             var compilerOptions = project.CompilerOptions;
 
-            var importItems = await project.GetImportItemsAsync(document.HostDocument, cancellationToken).ConfigureAwait(false);
-
             _output = await CompilationHelpers
-                .GenerateCodeDocumentAsync(document, projectEngine, importItems, compilerOptions, cancellationToken)
+                .GenerateCodeDocumentAsync(document, projectEngine, compilerOptions, cancellationToken)
                 .ConfigureAwait(false);
 
             return _output;

src/Razor/test/Microsoft.CodeAnalysis.Razor.Workspaces.Test/ProjectSystem/DefaultDocumentSnapshotTest.cs
@@ -35,17 +35,17 @@ public class DefaultDocumentSnapshotTest : WorkspaceTestBase
 
         var textLoader = TestMocks.CreateTextLoader(_sourceText);
 
-        var documentState = DocumentState.Create(s_legacyHostDocument, textLoader);
-        _legacyDocument = new DocumentSnapshot(project, documentState);
+        _legacyDocument = new ProjectSnapshot(projectState.AddDocument(s_legacyHostDocument, textLoader))
+            .GetRequiredDocument(s_legacyHostDocument.FilePath);
 
-        documentState = DocumentState.Create(s_componentHostDocument, textLoader);
-        _componentDocument = new DocumentSnapshot(project, documentState);
+        _componentDocument = new ProjectSnapshot(projectState.AddDocument(s_componentHostDocument, textLoader))
+            .GetRequiredDocument(s_componentHostDocument.FilePath);
 
-        documentState = DocumentState.Create(s_componentCshtmlHostDocument, textLoader);
-        _componentCshtmlDocument = new DocumentSnapshot(project, documentState);
+        _componentCshtmlDocument = new ProjectSnapshot(projectState.AddDocument(s_componentCshtmlHostDocument, textLoader))
+            .GetRequiredDocument(s_componentCshtmlHostDocument.FilePath);
 
-        documentState = DocumentState.Create(s_nestedComponentHostDocument, textLoader);
-        _nestedComponentDocument = new DocumentSnapshot(project, documentState);
+        _nestedComponentDocument = new ProjectSnapshot(projectState.AddDocument(s_nestedComponentHostDocument, textLoader))
+            .GetRequiredDocument(s_nestedComponentHostDocument.FilePath);
     }
 
     [Fact(Skip = "Weak cache removed")]

commit a200418cb64a9a402070464eed120ddf457f89f2

Fix #11712
A bug introduced in ac6e32a73fb93b3c699269fa432efac38fe4d5fe causes RazorSourceDocuments for imports to be created with the wrong file paths. Instead of a pointing to a physical file path on disk, they contain a relative path, which causes incorrect code generation. To fix this, call the GetSourceAsync(...) helper for the import. That will populate the RazorSourceDocument with the correct paths.

src/Razor/src/Microsoft.CodeAnalysis.Razor.Workspaces/ProjectSystem/CompilationHelpers.cs
@@ -1,4 +1,4 @@
-// Copyright (c) .NET Foundation. All rights reserved.
+﻿// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the MIT license. See License.txt in the project root for license information.
 
 using System.Collections.Immutable;
@@ -74,9 +74,7 @@ internal static class CompilationHelpers
             }
             else if (project.TryGetDocument(importProjectItem.PhysicalPath, out var importDocument))
             {
-                var text = await importDocument.GetTextAsync(cancellationToken).ConfigureAwait(false);
-                var properties = RazorSourceDocumentProperties.Create(importProjectItem.FilePath, importProjectItem.RelativePhysicalPath);
-                var importSource = RazorSourceDocument.Create(text, properties);
+                var importSource = await importDocument.GetSourceAsync(cancellationToken).ConfigureAwait(false);
 
                 importSources.Add(importSource);
             }