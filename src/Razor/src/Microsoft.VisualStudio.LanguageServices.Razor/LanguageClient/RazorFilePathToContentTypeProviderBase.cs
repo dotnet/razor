5280119f86f96cf5bcceb470e21ef4c3b8fe6795
David Wengier <david.wengier@microsoft.com>
2025-06-18 16:54:06 +1000

    Extract method

--- src/Razor/src/Microsoft.VisualStudio.LanguageServices.Razor/LanguageClient/RazorFilePathToContentTypeProviderBase.cs
+++ src/Razor/src/Microsoft.VisualStudio.LanguageServices.Razor/LanguageClient/RazorFilePathToContentTypeProviderBase.cs
@@ -18,23 +18,33 @@ internal abstract class RazorFilePathToContentTypeProviderBase(
 
     public bool TryGetContentTypeForFilePath(string filePath, [NotNullWhen(true)] out IContentType? contentType)
     {
-        // When cohosting is on, it's on for all non .NET Framework projects, regardless of feature flags or
-        // project capabilities.
-        if (_options.UseRazorCohostServer &&
-            _lspEditorFeatureDetector.IsDotNetCoreProject(filePath))
-        {
-            contentType = _contentTypeRegistryService.GetContentType(RazorConstants.RazorLSPContentTypeName);
-            return true;
-        }
-
-        if (_lspEditorFeatureDetector.IsLspEditorEnabled() &&
-            _lspEditorFeatureDetector.IsLspEditorSupported(filePath))
+        if (UseLSPEditor(filePath))
         {
             contentType = _contentTypeRegistryService.GetContentType(RazorConstants.RazorLSPContentTypeName);
             return true;
         }
 
         contentType = null;
+        return false;
+    }
+
+    private bool UseLSPEditor(string filePath)
+    {
+        // When cohosting is on, it's on for all non .NET Framework projects, regardless of feature flags or
+        // project capabilities.
+        if (_options.UseRazorCohostServer &&
+            _lspEditorFeatureDetector.IsDotNetCoreProject(filePath))
+        {
+            return true;
+        }
+
+        // Otherwise, we just check for the lack of feature flag feature or project capability.
+        if (_lspEditorFeatureDetector.IsLspEditorEnabled() &&
+            _lspEditorFeatureDetector.IsLspEditorSupported(filePath))
+        {
+            return true;
+        }
+
         return false;
     }
 }
