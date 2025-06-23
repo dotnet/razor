8b82e0a1d8e926f0ff1ba91fe468d16f70f8f7ff
David Wengier <david.wengier@microsoft.com>
2025-06-23 16:18:45 +1000

    Add 120 auto-generated tests to cover the CSharp formatting newline options exhaustively-ish

---
+++ src/Shared/Microsoft.AspNetCore.Razor.Test.Common/ManualRunOnlyFactAttribute.cs
@@ -0,0 +1,19 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the MIT license. See License.txt in the project root for license information.
+
+using System;
+using Xunit;
+
+namespace Microsoft.AspNetCore.Razor.Test.Common;
+
+[AttributeUsage(AttributeTargets.Method, AllowMultiple = false)]
+public class ManualRunOnlyFactAttribute : FactAttribute
+{
+    public ManualRunOnlyFactAttribute()
+    {
+        if (Environment.GetEnvironmentVariable("_IntegrationTestsRunningInCI") is not null)
+        {
+            Skip = "This test can only run manually";
+        }
+    }
+}
