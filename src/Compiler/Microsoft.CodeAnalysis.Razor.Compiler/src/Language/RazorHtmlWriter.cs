c359c03c439ad6b174e7c87009b0114e4e1e1256
Dustin Campbell <dustin@teamcampbell.org>
2025-06-24 13:03:32 -0700

    Reduce allocations when writing '~' spans for C# content
    Don't allocate new strings when writing spans of '~' characters for C# content. Instead, write `ReadOnlyMemory<char>`s sliced from the same string.

--- src/Compiler/Microsoft.CodeAnalysis.Razor.Compiler/src/Language/RazorHtmlWriter.cs
+++ src/Compiler/Microsoft.CodeAnalysis.Razor.Compiler/src/Language/RazorHtmlWriter.cs
@@ -1,6 +1,7 @@
 ﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+using System;
 using System.Collections.Immutable;
 using System.Diagnostics;
 using Microsoft.AspNetCore.Razor.Language.CodeGeneration;
@@ -15,6 +16,13 @@ namespace Microsoft.AspNetCore.Razor.Language;
 // not all characters in the document are included in the ClassifiedSpans.
 internal sealed class RazorHtmlWriter : SyntaxWalker
 {
+    // 32 '~' characters followed by comment start and end text.
+    private const string KnownText = "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~/**/";
+
+    private static ReadOnlyMemory<char> Tildes => KnownText.AsMemory(0, 32);
+    private static ReadOnlyMemory<char> CommentStart => KnownText.AsMemory(32, 2);
+    private static ReadOnlyMemory<char> CommentEnd => KnownText.AsMemory(34, 2);
+
     private readonly RazorSourceDocument _source;
     private readonly CodeWriter _codeWriter;
     private readonly ImmutableArray<SourceMapping>.Builder _sourceMappings;
@@ -27,7 +35,7 @@ internal sealed class RazorHtmlWriter : SyntaxWalker
     // can be written as a block, allowing any block of 4 characters or more to be written as a comment (ie '/**/`)
     // which takes pressure off the TypeScript/JavaScript compiler. Doing this per token means we can end up with
     // "@className" being written as '~/*~~~~~*/', which means Html formatting will insert a space which breaks things.
-    private int _csharpCharacterCount;
+    private int _tildesToWrite;
 
     private RazorHtmlWriter(RazorSourceDocument source, CodeWriter codeWriter, ImmutableArray<SourceMapping>.Builder sourceMappings)
     {
@@ -63,7 +71,7 @@ internal sealed class RazorHtmlWriter : SyntaxWalker
     {
         Visit(syntaxTree.Root);
 
-        WriteDeferredCSharpContent();
+        WriteCSharpContentPlaceholder();
 
         if (_lastGeneratedSourceSpan != SourceSpan.Undefined)
         {
@@ -226,7 +234,7 @@ internal sealed class RazorHtmlWriter : SyntaxWalker
         var content = token.Content;
         if (_isWritingHtml)
         {
-            WriteDeferredCSharpContent();
+            WriteCSharpContentPlaceholder();
 
             var source = token.GetSourceSpan(_source);
 
@@ -292,52 +300,69 @@ internal sealed class RazorHtmlWriter : SyntaxWalker
         {
             if (char.IsWhiteSpace(c))
             {
-                WriteDeferredCSharpContent();
+                WriteCSharpContentPlaceholder();
                 _codeWriter.Write(c.ToString());
             }
             else
             {
-                _csharpCharacterCount++;
+                _tildesToWrite++;
             }
         }
     }
 
-    private void WriteDeferredCSharpContent()
+    private void WriteCSharpContentPlaceholder()
     {
-        if (_csharpCharacterCount == 0)
+        var tildesToWrite = _tildesToWrite;
+
+        if (tildesToWrite == 0)
         {
+            // Nothing to write, so just return
             return;
         }
 
-        _codeWriter.Write(_csharpCharacterCount switch
+        _tildesToWrite = 0;
+
+        var writeComment = false;
+
+        // When writing 4 or more tildes, we write them as a comment to relieve pressure on the JS/TS compiler.
+
+        if (tildesToWrite >= 4)
         {
-            // Less than 4 chars, just use tildes. We can't do anything more fancy in a small space
-            1 => "~",
-            2 => "~~",
-            3 => "~~~",
+            // SPECIAL CASE: If the next character is a greater than symbol ('>'), we don't write a comment because
+            // the forward slash in the comment would be interpreted as the end of a tag, resulting in incorrect HTML.
+            // For example, `<div class=@className>` should not be written as `<div class=/*~~~~~*/>`.
 
-            // Special case for unquoted attributes that appear at the end of a tag. eg `<div class=@className>`
-            // Without this special handling, our replacement would result in `<div class=/*~~~~~*/>` which differs
-            // from the original meaning, as the div is now self-closing.
-            // Note that we don't actually know if we're in an attribute here, or some other construct, but false
-            // positives are totally fine in this scenario.
-            _ when NextCharacterIsGreaterThanSymbol() => new string('~', _csharpCharacterCount),
+            var nextIndex = _codeWriter.Location.AbsoluteIndex + tildesToWrite;
+            var text = _source.Text;
 
-            // All other cases, use a comment to relieve pressure on the JS/TS compiler
-            _ => "/*" + new string('~', _csharpCharacterCount - 4) + "*/",
-        });
-        _csharpCharacterCount = 0;
+            Debug.Assert(nextIndex <= text.Length, "The next index should not exceed the length of the source text.");
 
-        bool NextCharacterIsGreaterThanSymbol()
-        {
-            var sourceText = _source.Text;
-            var index = _codeWriter.Location.AbsoluteIndex + _csharpCharacterCount;
-            if (sourceText.Length <= index)
+            if (nextIndex >= text.Length || text[nextIndex] != '>')
             {
-                return false;
+                // We can write a comment
+                writeComment = true;
+                tildesToWrite -= 4; // We need to reserve 4 characters for the comment start and end
             }
+        }
 
-            return sourceText[index] == '>';
+        if (writeComment)
+        {
+            _codeWriter.Write(CommentStart);
+        }
+
+        while (tildesToWrite > 0)
+        {
+            var tildes = tildesToWrite < Tildes.Length
+                ? Tildes[..tildesToWrite]
+                : Tildes;
+
+            _codeWriter.Write(tildes);
+            tildesToWrite -= tildes.Length;
+        }
+
+        if (writeComment)
+        {
+            _codeWriter.Write(CommentEnd);
         }
     }
 }
