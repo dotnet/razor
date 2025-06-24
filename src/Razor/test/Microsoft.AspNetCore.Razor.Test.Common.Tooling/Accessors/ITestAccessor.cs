7979cc9b2bd538a332c228172360ed91d5eefacf
David Wengier <david.wengier@microsoft.com>
2025-06-24 12:19:17 +1000

    Update file header in all affected files
    This was done mechanically with the codefix

--- src/Razor/test/Microsoft.AspNetCore.Razor.Test.Common.Tooling/Accessors/ITestAccessor.cs
+++ src/Razor/test/Microsoft.AspNetCore.Razor.Test.Common.Tooling/Accessors/ITestAccessor.cs
@@ -1,5 +1,5 @@
-﻿// Copyright (c) .NET Foundation. All rights reserved.
-// Licensed under the MIT license. See License.txt in the project root for license information.
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
 
 // Copied from https://github.com/dotnet/winforms/blob/8812d0e14e2cc9fd9870d70e31bfa083add7e541/src/Common/tests/TestUtilities/ITestAccessor.cs
 
