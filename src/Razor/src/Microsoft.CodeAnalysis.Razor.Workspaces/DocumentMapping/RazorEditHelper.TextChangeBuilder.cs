36e32109d9700593373752cfb195e9af5337481f
David Wengier <david.wengier@microsoft.com>
2025-07-02 16:59:45 +1000

    Use property instead of now-deleted method

--- src/Razor/src/Microsoft.CodeAnalysis.Razor.Workspaces/DocumentMapping/RazorEditHelper.TextChangeBuilder.cs
+++ src/Razor/src/Microsoft.CodeAnalysis.Razor.Workspaces/DocumentMapping/RazorEditHelper.TextChangeBuilder.cs
@@ -51,7 +51,7 @@ internal static partial class RazorEditHelper
             var root = codeDocument.GetRequiredSyntaxRoot();
             var razorText = codeDocument.Source.Text;
             var csharpDocument = codeDocument.GetRequiredCSharpDocument();
-            var csharpText = csharpDocument.GetGeneratedSourceText();
+            var csharpText = csharpDocument.Text;
 
             foreach (var edit in csharpEdits)
             {
