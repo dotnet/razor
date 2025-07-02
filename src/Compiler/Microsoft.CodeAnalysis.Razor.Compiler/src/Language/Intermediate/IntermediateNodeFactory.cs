48bca5b2f896b487de71bc19ec33d46e3bc529a8
Dustin Campbell <dustin@teamcampbell.org>
2025-07-02 16:14:33 -0700

    Introduce IntermedateNodeFactory
    Add `IntermediateNodeFactory` static class with factory methods for creating CSharp and Html `IntermediateTokens`.

---
+++ src/Compiler/Microsoft.CodeAnalysis.Razor.Compiler/src/Language/Intermediate/IntermediateNodeFactory.cs
@@ -0,0 +1,21 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System;
+
+namespace Microsoft.AspNetCore.Razor.Language.Intermediate;
+
+internal static class IntermediateNodeFactory
+{
+    public static IntermediateToken CSharpToken(string content, SourceSpan? source = null)
+        => new() { Content = content, Kind = TokenKind.CSharp, Source = source };
+
+    public static LazyIntermediateToken CSharpToken(object factoryArgument, Func<object, string> contentFactory, SourceSpan? source = null)
+        => new() { FactoryArgument = factoryArgument, ContentFactory = contentFactory, Kind = TokenKind.CSharp, Source = source };
+
+    public static IntermediateToken HtmlToken(string content, SourceSpan? source = null)
+        => new() { Content = content, Kind = TokenKind.Html, Source = source };
+
+    public static LazyIntermediateToken HtmlToken(object factoryArgument, Func<object, string> contentFactory, SourceSpan? source = null)
+        => new() { FactoryArgument = factoryArgument, ContentFactory = contentFactory, Kind = TokenKind.Html, Source = source };
+}
