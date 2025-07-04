bccbbf3c1b86fff3c73d09166cb8e6865e184092
David Wengier <david.wengier@microsoft.com>
2025-07-04 10:27:21 +1000

    Report telemetry through Roslyn

--- src/Razor/src/Microsoft.VisualStudioCode.RazorExtension/Services/VSCodeTelemetryReporter.cs
+++ src/Razor/src/Microsoft.VisualStudioCode.RazorExtension/Services/VSCodeTelemetryReporter.cs
@@ -1,61 +1,37 @@
 ﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-using System;
 using System.Composition;
 using Microsoft.CodeAnalysis.Razor.Telemetry;
+using Microsoft.VisualStudio.Razor.Telemetry;
+using Microsoft.VisualStudio.Telemetry;
 
 namespace Microsoft.VisualStudioCode.RazorExtension.Services;
 
-// TODO:
-
 [Shared]
+[Export(typeof(VSCodeTelemetryReporter))]
 [Export(typeof(ITelemetryReporter))]
-internal class VSCodeTelemetryReporter : ITelemetryReporter
+internal sealed class VSCodeTelemetryReporter : TelemetryReporter
 {
-    public TelemetryScope BeginBlock(string name, Severity severity, TimeSpan minTimeToReport)
-        => TelemetryScope.Null;
+    private TelemetryReporterWrapper? _reporter;
 
-    public TelemetryScope BeginBlock(string name, Severity severity, TimeSpan minTimeToReport, Property property)
-        => TelemetryScope.Null;
+    public override bool IsEnabled => true;
 
-    public TelemetryScope BeginBlock(string name, Severity severity, TimeSpan minTimeToReport, Property property1, Property property2)
-        => TelemetryScope.Null;
-
-    public TelemetryScope BeginBlock(string name, Severity severity, TimeSpan minTimeToReport, Property property1, Property property2, Property property3)
-        => TelemetryScope.Null;
-
-    public TelemetryScope BeginBlock(string name, Severity severity, TimeSpan minTimeToReport, params ReadOnlySpan<Property> properties)
-        => TelemetryScope.Null;
-
-    public void ReportEvent(string name, Severity severity)
+    internal void SetTelemetryReporter(TelemetryReporterWrapper reporter)
     {
+        _reporter = reporter;
     }
 
-    public void ReportEvent(string name, Severity severity, Property property)
+    // We override any method in the base class that does actual telemetry reporting, and redirect it
+    // through our wrapper, to the Roslyn reporter.
+
+    protected override void Report(TelemetryEvent telemetryEvent)
     {
+        _reporter?.Report(telemetryEvent.Name, telemetryEvent.Properties);
     }
 
-    public void ReportEvent(string name, Severity severity, Property property1, Property property2)
+    public override void ReportMetric(AggregatingTelemetryLog.TelemetryInstrumentEvent metricEvent)
     {
-    }
-
-    public void ReportEvent(string name, Severity severity, Property property1, Property property2, Property property3)
-    {
-    }
-
-    public void ReportEvent(string name, Severity severity, params ReadOnlySpan<Property> properties)
-    {
-    }
-
-    public void ReportFault(Exception exception, string? message, params object?[] @params)
-    {
-    }
-
-    public TelemetryScope TrackLspRequest(string lspMethodName, string lspServerName, TimeSpan minTimeToReport, Guid correlationId)
-        => TelemetryScope.Null;
-
-    public void ReportRequestTiming(string name, string? language, TimeSpan queuedDuration, TimeSpan requestDuration, TelemetryResult result)
-    {
+        _reporter?.Report(metricEvent.Event.Name, metricEvent.Event.Properties);
     }
 }
