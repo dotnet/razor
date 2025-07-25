ceb15431e42d759ef523e8bdf6234419bd629bb2
David Wengier <david.wengier@microsoft.com>
2025-07-25 14:39:56 +1000

    Add remote service that calls RazorEditHelper to do edit mapping

---
+++ src/Razor/src/Microsoft.CodeAnalysis.Remote.Razor/WrapWithTag/RemoteSpanMappingService.cs
@@ -0,0 +1,87 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System;
+using System.Collections.Generic;
+using System.Collections.Immutable;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.CodeAnalysis.ExternalAccess.Razor;
+using Microsoft.CodeAnalysis.Razor.DocumentMapping;
+using Microsoft.CodeAnalysis.Razor.Protocol;
+using Microsoft.CodeAnalysis.Razor.Remote;
+using Microsoft.CodeAnalysis.Razor.Telemetry;
+using Microsoft.CodeAnalysis.Remote.Razor.ProjectSystem;
+using Microsoft.CodeAnalysis.Text;
+
+namespace Microsoft.CodeAnalysis.Remote.Razor;
+
+internal sealed partial class RemoteSpanMappingService(in ServiceArgs args) : RazorBrokeredServiceBase(in args), IRemoteSpanMappingService
+{
+    internal sealed class Factory : FactoryBase<IRemoteSpanMappingService>
+    {
+        protected override IRemoteSpanMappingService CreateService(in ServiceArgs args)
+            => new RemoteSpanMappingService(in args);
+    }
+
+    private readonly RemoteSnapshotManager _snapshotManager = args.ExportProvider.GetExportedValue<RemoteSnapshotManager>();
+    private readonly IDocumentMappingService _documentMappingService = args.ExportProvider.GetExportedValue<IDocumentMappingService>();
+    private readonly ITelemetryReporter _telemetryReporter = args.ExportProvider.GetExportedValue<ITelemetryReporter>();
+
+    public ValueTask<ImmutableArray<RazorMappedSpanResult>> MapSpansAsync(RazorPinnedSolutionInfoWrapper solutionInfo, DocumentId generatedDocumentId, ImmutableArray<TextSpan> spans, CancellationToken cancellationToken)
+        => throw new NotImplementedException();
+
+    public ValueTask<ImmutableArray<RazorMappedEditResult>> MapTextChangesAsync(RazorPinnedSolutionInfoWrapper solutionInfo, DocumentId generatedDocumentId, ImmutableArray<TextChange> changes, CancellationToken cancellationToken)
+        => RunServiceAsync(
+            solutionInfo,
+            solution => MapTextChangesAsync(solution, generatedDocumentId, changes, cancellationToken),
+            cancellationToken);
+
+    private async ValueTask<ImmutableArray<RazorMappedEditResult>> MapTextChangesAsync(Solution solution, DocumentId generatedDocumentId, ImmutableArray<TextChange> changes, CancellationToken cancellationToken)
+    {
+        try
+        {
+            var generatedDocument = await solution.GetSourceGeneratedDocumentAsync(generatedDocumentId, cancellationToken).ConfigureAwait(false);
+            if (generatedDocument is null)
+            {
+                return [];
+            }
+
+            if (!generatedDocument.Project.TryGetHintNameFromGeneratedDocumentUri(generatedDocument.CreateUri(), out var hintName))
+            {
+                return [];
+            }
+
+            var projectSnapshot = _snapshotManager.GetSnapshot(generatedDocument.Project);
+
+            var razorDocument = await projectSnapshot.TryGetRazorDocumentFromGeneratedHintNameAsync(hintName, cancellationToken).ConfigureAwait(false);
+            if (razorDocument is null)
+            {
+                return [];
+            }
+
+            var documentSnapshot = _snapshotManager.GetSnapshot(razorDocument);
+            var results = await RazorEditHelper.MapCSharpEditsAsync(
+                changes.SelectAsArray(c => c.ToRazorTextChange()),
+                documentSnapshot,
+                _documentMappingService,
+                _telemetryReporter,
+                cancellationToken).ConfigureAwait(false);
+
+            if (results.IsDefaultOrEmpty)
+            {
+                return [];
+            }
+
+            var razorSource = await razorDocument.GetTextAsync(cancellationToken).ConfigureAwait(false);
+            var textChanges = results.SelectAsArray(te => te.ToTextChange());
+
+            return [new RazorMappedEditResult() { FilePath = documentSnapshot.FilePath, TextChanges = [.. textChanges] }];
+        }
+        catch (Exception ex)
+        {
+            _telemetryReporter.ReportFault(ex, "Failed to map edits");
+            return [];
+        }
+    }
+}
