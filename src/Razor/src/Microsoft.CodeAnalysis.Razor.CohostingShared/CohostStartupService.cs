61c2d3cc600fb4b9a59bfece07c7625b53749964
David Wengier <david.wengier@microsoft.com>
2025-08-01 16:50:10 +1000

    Initialize feature flags in OOP early

--- src/Razor/src/Microsoft.CodeAnalysis.Razor.CohostingShared/CohostStartupService.cs
+++ src/Razor/src/Microsoft.CodeAnalysis.Razor.CohostingShared/CohostStartupService.cs
@@ -5,50 +5,60 @@ using System;
 using System.Collections.Generic;
 using System.Collections.Immutable;
 using System.ComponentModel.Composition;
-using System.Text.Json;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.CodeAnalysis.ExternalAccess.Razor.Cohost;
 using Microsoft.CodeAnalysis.Razor.Logging;
-using Microsoft.CodeAnalysis.Razor.Protocol;
+using Microsoft.CodeAnalysis.Razor.Remote;
 
 namespace Microsoft.VisualStudio.Razor.LanguageClient.Cohost;
 
-#pragma warning disable CS0618 // Type or member is obsolete. Will be addressed in https://github.com/dotnet/razor/pull/12079 but Roslyn changes are batched
-[Export(typeof(ICohostStartupService))]
+[Export(typeof(AbstractRazorCohostLifecycleService))]
 [method: ImportingConstructor]
 internal sealed class CohostStartupService(
     [ImportMany] IEnumerable<Lazy<IRazorCohostStartupService>> lazyStartupServices,
-    ILoggerFactory loggerFactory) : ICohostStartupService
-#pragma warning restore CS0618 // Type or member is obsolete
+    IRemoteServiceInvoker remoteServiceInvoker,
+    ILoggerFactory loggerFactory) : AbstractRazorCohostLifecycleService
 {
     private readonly ImmutableArray<Lazy<IRazorCohostStartupService>> _lazyStartupServices = [.. lazyStartupServices];
-    private readonly ILogger _logger = loggerFactory.GetOrCreateLogger<CohostStartupService>();
+    private readonly IRemoteServiceInvoker _remoteServiceInvoker = remoteServiceInvoker;
 
-    public async Task StartupAsync(string clientCapabilitiesString, RazorCohostRequestContext requestContext, CancellationToken cancellationToken)
+    public override Task LspServerIntializedAsync(CancellationToken cancellationToken)
     {
-        var clientCapabilities = JsonSerializer.Deserialize<VSInternalClientCapabilities>(clientCapabilitiesString, JsonHelpers.JsonSerializerOptions) ?? new();
+        return _remoteServiceInvoker.InitializeAsync().AsTask();
+    }
 
+    public override async Task RazorActivatedAsync(ClientCapabilities clientCapabilities, RazorCohostRequestContext requestContext, CancellationToken cancellationToken)
+    {
+        // Normally loggers are fields, but this service gets created early so it's better to avoid the work in case Razor
+        // never gets activated.
+        var logger = loggerFactory.GetOrCreateLogger<CohostStartupService>();
+
+        var capabilities = clientCapabilities.ToVSInternalClientCapabilities();
         var providers = _lazyStartupServices.SelectAndOrderByAsArray(p => p.Value, p => p.Order);
 
         foreach (var provider in providers)
         {
             if (cancellationToken.IsCancellationRequested)
             {
-                _logger.LogInformation($"Razor extension startup cancelled.");
+                logger.LogInformation($"Razor extension startup cancelled.");
                 return;
             }
 
             try
             {
-                await provider.StartupAsync(clientCapabilities, requestContext, cancellationToken).ConfigureAwait(false);
+                await provider.StartupAsync(capabilities, requestContext, cancellationToken).ConfigureAwait(false);
             }
             catch (Exception ex) when (ex is not OperationCanceledException)
             {
-                _logger.LogError(ex, $"Error initializing Razor startup service '{provider.GetType().Name}'");
+                logger.LogError(ex, $"Error initializing Razor startup service '{provider.GetType().Name}'");
             }
         }
 
-        _logger.LogInformation($"Razor extension startup finished.");
+        logger.LogInformation($"Razor extension startup finished.");
+    }
+
+    public override void Dispose()
+    {
     }
 }
