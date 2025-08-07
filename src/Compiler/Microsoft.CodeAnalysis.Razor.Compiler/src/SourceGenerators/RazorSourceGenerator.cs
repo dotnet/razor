7c8749e7cdef446b7de4bcde505c4457b4115f06
Chris Sienkiewicz <chsienki@microsoft.com>
2025-08-06 17:43:11 -0700

    Update src/Compiler/Microsoft.CodeAnalysis.Razor.Compiler/src/SourceGenerators/RazorSourceGenerator.cs
    Co-authored-by: David Wengier <david.wengier@microsoft.com>

--- src/Compiler/Microsoft.CodeAnalysis.Razor.Compiler/src/SourceGenerators/RazorSourceGenerator.cs
+++ src/Compiler/Microsoft.CodeAnalysis.Razor.Compiler/src/SourceGenerators/RazorSourceGenerator.cs
@@ -38,7 +38,7 @@ namespace Microsoft.NET.Sdk.Razor.SourceGenerators
             var parseOptions = context.ParseOptionsProvider;
             var compilation = context.CompilationProvider;
 
-            // We might get initialized before razor tooling has a chance to set this, so check it any time somthing changes. It's extremely cheap and we'll stop propagating directly below if it didn't changed.
+            // We might get initialized before razor tooling has a chance to set this, so check it any time something changes. It's extremely cheap and we'll stop propagating directly below if it didn't changed.
             var useRazorCohostServer = context.AdditionalTextsProvider.Collect().Combine(context.CompilationProvider).Select((_, _) => RazorCohostingOptions.UseRazorCohostServer);
 
             // determine if we should suppress this run and filter out all the additional files and references if so
