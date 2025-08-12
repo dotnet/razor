19134e74dcdef4a8ae6340939b13ac25b3d60796
David Wengier <git@wengier.com>
2025-08-12 22:27:03 +1000

    Create tag helper search engine interface and impl

---
+++ src/Razor/src/Microsoft.CodeAnalysis.Remote.Razor/RemoteTagHelperSearchEngine.cs
@@ -0,0 +1,60 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System.Composition;
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Razor.Language;
+using Microsoft.CodeAnalysis.ExternalAccess.Razor;
+using Microsoft.CodeAnalysis.Razor.ProjectSystem;
+using Microsoft.CodeAnalysis.Razor.Workspaces;
+using Microsoft.CodeAnalysis.Remote.Razor.ProjectSystem;
+
+namespace Microsoft.CodeAnalysis.Remote.Razor;
+
+[Export(typeof(ITagHelperSearchEngine)), Shared]
+internal sealed class RemoteTagHelperSearchEngine : ITagHelperSearchEngine
+{
+    public async Task<LspLocation?> TryLocateTagHelperDefinitionAsync(TagHelperDescriptor boundTagHelper, IDocumentSnapshot documentSnapshot, ISolutionQueryOperations solutionQueryOperations, CancellationToken cancellationToken)
+    {
+        if (boundTagHelper.IsComponentTagHelper)
+        {
+            return null;
+        }
+
+        var typeName = boundTagHelper.GetTypeName();
+        if (typeName is null)
+        {
+            return null;
+        }
+
+        Debug.Assert(documentSnapshot is RemoteDocumentSnapshot);
+
+        var project = ((RemoteDocumentSnapshot)documentSnapshot).TextDocument.Project;
+        var compilation = await project.GetCompilationAsync(cancellationToken).ConfigureAwait(false);
+        if (compilation is null)
+        {
+            return null;
+        }
+
+        foreach (var type in compilation.GetTypesByMetadataName(typeName))
+        {
+            foreach (var location in type.Locations)
+            {
+                if (location.IsInSource &&
+                    project.Solution.GetDocument(location.SourceTree) is { } document)
+                {
+                    var text = await document.GetTextAsync(cancellationToken).ConfigureAwait(false);
+                    return new LspLocation
+                    {
+                        DocumentUri = document.CreateDocumentUri(),
+                        Range = text.GetRange(location.SourceSpan)
+                    };
+                }
+            }
+        }
+
+        return null;
+    }
+}
