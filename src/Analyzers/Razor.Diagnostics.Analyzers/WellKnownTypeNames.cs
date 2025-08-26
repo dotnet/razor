17cdc2bf9fdf124651d26e93870bb05d61ce3a1c
Dustin Campbell <dustin@teamcampbell.org>
2025-08-26 11:59:22 -0700

    Add analyzer that flags ImmutableArray<T> boxing to extensions
    Add a Roslyn analyzer for the Razor build that reports diagnostics when an extension method defined in EnumerableExtensions or ReadOnlyListExtensions is called on an ImmutableArray<T> and causes boxing.

--- src/Analyzers/Razor.Diagnostics.Analyzers/WellKnownTypeNames.cs
+++ src/Analyzers/Razor.Diagnostics.Analyzers/WellKnownTypeNames.cs
@@ -5,7 +5,12 @@ namespace Razor.Diagnostics.Analyzers;
 
 internal static class WellKnownTypeNames
 {
+    public const string ImmutableArray_T = "System.Collections.Immutable.ImmutableArray`1";
+
     public const string PooledArrayBuilderExtensions = "Microsoft.AspNetCore.Razor.PooledObjects.PooledArrayBuilderExtensions";
+
+    public const string EnumerableExtensions = "System.Collections.Generic.EnumerableExtensions";
+    public const string ReadOnlyListExtensions = "System.Collections.Generic.ReadOnlyListExtensions";
 
     public const string IRemoteJsonService = "Microsoft.CodeAnalysis.Razor.Remote.IRemoteJsonService";
     public const string RazorPinnedSolutionInfoWrapper = "Microsoft.CodeAnalysis.ExternalAccess.Razor.RazorPinnedSolutionInfoWrapper";
