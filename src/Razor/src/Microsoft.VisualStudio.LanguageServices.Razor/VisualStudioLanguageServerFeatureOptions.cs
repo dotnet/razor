bf76121ee102fb39dfb3a4cd1ed61e5fb1c27fca
David Wengier <david.wengier@microsoft.com>
2025-08-29 14:12:20 +1000

    Default cohosting to on in VS

--- src/Razor/src/Microsoft.VisualStudio.LanguageServices.Razor/VisualStudioLanguageServerFeatureOptions.cs
+++ src/Razor/src/Microsoft.VisualStudio.LanguageServices.Razor/VisualStudioLanguageServerFeatureOptions.cs
@@ -53,7 +53,7 @@ internal class VisualStudioLanguageServerFeatureOptions : LanguageServerFeatureO
         _useRazorCohostServer = new Lazy<bool>(() =>
         {
             var featureFlags = (IVsFeatureFlags)Package.GetGlobalService(typeof(SVsFeatureFlags));
-            var useRazorCohostServer = featureFlags.IsFeatureEnabled(WellKnownFeatureFlagNames.UseRazorCohostServer, defaultValue: false);
+            var useRazorCohostServer = featureFlags.IsFeatureEnabled(WellKnownFeatureFlagNames.UseRazorCohostServer, defaultValue: true);
             return useRazorCohostServer;
         });
 
