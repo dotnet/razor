b8ca9efadc5558a81240c815a5dcedc6cb93ba3c
David Wengier <david.wengier@microsoft.com>
2025-08-29 17:10:40 +1000

    Remove feature flag entirely

--- src/Razor/src/Microsoft.CodeAnalysis.Razor.Workspaces/ProjectSystem/RazorCompilerOptionsExtensions.cs
+++ src/Razor/src/Microsoft.CodeAnalysis.Razor.Workspaces/ProjectSystem/RazorCompilerOptionsExtensions.cs
@@ -1,7 +1,6 @@
 ﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-using Microsoft.AspNetCore.Razor;
 using Microsoft.CodeAnalysis.Razor.Workspaces;
 
 namespace Microsoft.CodeAnalysis.Razor.ProjectSystem;
@@ -11,11 +10,6 @@ internal static class RazorCompilerOptionsExtensions
     public static RazorCompilerOptions ToCompilerOptions(this LanguageServerFeatureOptions languageServerFeatureOptions)
     {
         var options = RazorCompilerOptions.None;
-
-        if (languageServerFeatureOptions.ForceRuntimeCodeGeneration)
-        {
-            options.SetFlag(RazorCompilerOptions.ForceRuntimeCodeGeneration);
-        }
 
         return options;
     }
