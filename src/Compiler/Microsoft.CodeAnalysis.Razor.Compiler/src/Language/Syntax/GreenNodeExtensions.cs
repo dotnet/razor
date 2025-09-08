29ae3c7cdad3c9fef7b0718d512bca05131a8303
Todd Grunke <toddgrun@microsoft.com>
2025-09-08 16:43:49 -0700

    Small allocation improvement to GreenNodeExtensions.WithAnnotationsGreenNode
    The WithAnnotationsGreen method shows up as about 0.7% of allocations in the RoslynCodeAnalysisService process during the C# completions scenario in the razor cohosting completion test.
    
    These changes should get rid of about 0.2% of that (The top two blue highlighted lines in the profile image).

--- src/Compiler/Microsoft.CodeAnalysis.Razor.Compiler/src/Language/Syntax/GreenNodeExtensions.cs
+++ src/Compiler/Microsoft.CodeAnalysis.Razor.Compiler/src/Language/Syntax/GreenNodeExtensions.cs
@@ -3,9 +3,9 @@
 
 #nullable disable
 
-using System.Collections.Generic;
 using System.Collections.Immutable;
 using System.Runtime.InteropServices;
+using Microsoft.AspNetCore.Razor.PooledObjects;
 
 namespace Microsoft.AspNetCore.Razor.Language.Syntax;
 
@@ -25,7 +25,18 @@ internal static class GreenNodeExtensions
 
     public static TNode WithAnnotationsGreen<TNode>(this TNode node, params SyntaxAnnotation[] annotations) where TNode : GreenNode
     {
-        var newAnnotations = new List<SyntaxAnnotation>();
+        if (annotations.Length == 0)
+        {
+            var existingAnnotations = node.GetAnnotations();
+            if (existingAnnotations != null && existingAnnotations.Length > 0)
+            {
+                node = (TNode)node.SetAnnotations(null);
+            }
+
+            return node;
+        }
+
+        using var newAnnotations = new PooledArrayBuilder<SyntaxAnnotation>(annotations.Length);
         foreach (var candidate in annotations)
         {
             if (!newAnnotations.Contains(candidate))
@@ -34,22 +45,7 @@ internal static class GreenNodeExtensions
             }
         }
 
-        if (newAnnotations.Count == 0)
-        {
-            var existingAnnotations = node.GetAnnotations();
-            if (existingAnnotations == null || existingAnnotations.Length == 0)
-            {
-                return node;
-            }
-            else
-            {
-                return (TNode)node.SetAnnotations(null);
-            }
-        }
-        else
-        {
-            return (TNode)node.SetAnnotations(newAnnotations.ToArray());
-        }
+        return (TNode)node.SetAnnotations(newAnnotations.ToArray());
     }
 
     public static TNode WithDiagnosticsGreen<TNode>(this TNode node, RazorDiagnostic[] diagnostics)
