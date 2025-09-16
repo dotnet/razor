1cde74fbb2fac277fc7d821041e30f063b7bec95
David Wengier <david.wengier@microsoft.com>
2025-09-16 12:15:59 +1000

    Tweaks

--- src/Razor/src/Microsoft.CodeAnalysis.Remote.Razor/ProjectSystem/GeneratorRunResult.cs
+++ src/Razor/src/Microsoft.CodeAnalysis.Remote.Razor/ProjectSystem/GeneratorRunResult.cs
@@ -16,15 +16,18 @@ namespace Microsoft.CodeAnalysis.Remote.Razor.ProjectSystem;
 /// <summary>
 /// Wraps the host outputs of the Razor source generator, and the <see cref="CodeAnalysis.Project"/> that produced them.
 /// </summary>
-internal record struct GeneratorRunResult(RazorGeneratorResult GeneratorResult, Project Project)
+internal readonly record struct GeneratorRunResult(RazorGeneratorResult GeneratorResult, Project Project)
 {
     public bool IsDefault => GeneratorResult is null && Project is null;
 
     public IReadOnlyList<TagHelperDescriptor> TagHelpers => GeneratorResult.TagHelpers;
 
-    public RazorCodeDocument? GetCodeDocument(string filePath) => GeneratorResult.GetCodeDocument(filePath);
-    public RazorCodeDocument GetRequiredCodeDocument(string filePath) => GeneratorResult.GetCodeDocument(filePath)
-        ?? throw new InvalidOperationException(SR.FormatGenerator_run_result_did_not_contain_a_code_document(filePath));
+    public RazorCodeDocument? GetCodeDocument(string filePath)
+        => GeneratorResult.GetCodeDocument(filePath);
+
+    public RazorCodeDocument GetRequiredCodeDocument(string filePath)
+        => GeneratorResult.GetCodeDocument(filePath)
+           ?? throw new InvalidOperationException(SR.FormatGenerator_run_result_did_not_contain_a_code_document(filePath));
 
     public string? GetRazorFilePathFromHintName(string generatedDocumentHintName)
         => GeneratorResult.GetFilePath(generatedDocumentHintName);
@@ -81,13 +84,11 @@ internal record struct GeneratorRunResult(RazorGeneratorResult GeneratorResult, 
             // We know the generator is referenced, or we wouldn't have gotten past the above checks. We also know cohosting is turned on, since we got here.
             // There is a race condition that can happen where if Roslyn runs the generator before Razor has a chance to initialize, then Roslyn will have
             // cached the fact that cohosting was off, and any subsequent runs of the generator will not produce a host output. We can work around this by
-            // making an innocuous change to the project, and trying again.
+            // making an innocuous change to the project, and trying again. The snapshot manager makes this change inside a lock, and if we're not the first
+            // ones to try, we'll get the updated project which may have already had generators run on it. We recurse back into ourselves here with the updated
+            // project to try again. TryGetRetryProject also protects against infinite recursion.
             if (snapshotManager.TryGetRetryProject(project) is { } retryProject)
             {
-                // For subsequent requests for this project, the solution snapshot will have updated its view of the world and everything
-                // will be fine. For this request though, the caller of this method is just going to keep querying _project so we need to
-                // update it.
-                //_project = retryProject;
                 return await CreateAsync(throwIfNotFound, retryProject, snapshotManager, cancellationToken).ConfigureAwait(false);
             }
 
