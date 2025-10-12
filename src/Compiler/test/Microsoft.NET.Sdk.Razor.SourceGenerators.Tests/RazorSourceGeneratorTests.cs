bf50b16fa3a9e1d1c403fc9e245f8d870780f81d
Chris Sienkiewicz <chsienki@microsoft.com>
2025-10-12 12:50:29 -0700

    Revert "Revert "Revert "Default the cohosting option in the generator to on. …"
    This reverts commit 06c0bfa232e394fdd691261b26eee19f14b907db.

--- src/Compiler/test/Microsoft.NET.Sdk.Razor.SourceGenerators.Tests/RazorSourceGeneratorTests.cs
+++ src/Compiler/test/Microsoft.NET.Sdk.Razor.SourceGenerators.Tests/RazorSourceGeneratorTests.cs
@@ -10,7 +10,6 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Razor.Language.Syntax;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.Test.Utilities;
@@ -2623,9 +2622,6 @@ namespace AspNetCoreGeneratedDocument
             // start with the generator suppressed (this is the default state in VS)
             driver = SetSuppressionState(true);
 
-            // Disable co-hosting, this test only applies to non-cohosting scenarios
-            RazorCohostingOptions.UseRazorCohostServer = false;
-
             // results should be empty, and no recorded steps should have run
             using var eventListener = new RazorEventListener();
             var result = RunGenerator(compilation!, ref driver).VerifyPageOutput();
@@ -3468,7 +3464,6 @@ namespace MyApp
                 ["Component.Razor"] = "<h1>Hello world</h1>",
             });
             var compilation = await project.GetCompilationAsync();
-            RazorCohostingOptions.UseRazorCohostServer = false;
 
             // Start with the generator suppressed
             var (driver, additionalTexts, optionsProvider) = await GetDriverWithAdditionalTextAndProviderAsync(project, configureGlobalOptions: (o) =>
