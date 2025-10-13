e99545ab366203c7fb0308f645810a39287ce2cb
Dustin Campbell <dustin@teamcampbell.org>
2025-10-13 09:51:38 -0700

    Enable nullability for InternalSyntax.RazorSyntaxNode
    - Remove #nullable disable and add a couple of annotations.

--- src/Compiler/Microsoft.CodeAnalysis.Razor.Compiler/src/Language/Syntax/InternalSyntax/RazorSyntaxNode.cs
+++ src/Compiler/Microsoft.CodeAnalysis.Razor.Compiler/src/Language/Syntax/InternalSyntax/RazorSyntaxNode.cs
@@ -1,13 +1,12 @@
 ﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-#nullable disable
-
 namespace Microsoft.AspNetCore.Razor.Language.Syntax.InternalSyntax;
 
 internal abstract partial class RazorSyntaxNode : GreenNode
 {
-    protected RazorSyntaxNode(SyntaxKind kind) : base(kind)
+    protected RazorSyntaxNode(SyntaxKind kind)
+        : base(kind)
     {
     }
 
@@ -16,12 +15,12 @@ internal abstract partial class RazorSyntaxNode : GreenNode
     {
     }
 
-    protected RazorSyntaxNode(SyntaxKind kind, RazorDiagnostic[] diagnostics, SyntaxAnnotation[] annotations)
+    protected RazorSyntaxNode(SyntaxKind kind, RazorDiagnostic[]? diagnostics, SyntaxAnnotation[]? annotations)
         : base(kind, diagnostics, annotations)
     {
     }
 
-    protected RazorSyntaxNode(SyntaxKind kind, int width, RazorDiagnostic[] diagnostics, SyntaxAnnotation[] annotations)
+    protected RazorSyntaxNode(SyntaxKind kind, int width, RazorDiagnostic[]? diagnostics, SyntaxAnnotation[]? annotations)
         : base(kind, width, diagnostics, annotations)
     {
     }
