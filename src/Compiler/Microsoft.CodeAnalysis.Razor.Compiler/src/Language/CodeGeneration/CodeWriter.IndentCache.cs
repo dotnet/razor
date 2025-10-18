c6cac05ca6dc4f9040fda799b4d2d068334f3025
Todd Grunke <toddgrun@microsoft.com>
2025-10-18 15:34:05 -0700

    Cache indent strings in CodeWriter
    There are typically only a few different sizes requested, and useTabs/tabSize *very* rarely change. Locally, I only saw eight unique combinations of these values when opening and typing in orchardcore.
    
    ComputeIndent accounts for 11.0 MB (0.4%) of total allocations and 27 ms in in the ScrollingAndTypingInCohosting speedometer test. With this change, those numbers go to 0.0 MB (0.0%) and 13 ms respectively.
    
    Test insertion: https://dev.azure.com/devdiv/DevDiv/_git/VS/pullrequest/680751

---
+++ src/Compiler/Microsoft.CodeAnalysis.Razor.Compiler/src/Language/CodeGeneration/CodeWriter.IndentCache.cs
@@ -0,0 +1,91 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System;
+
+namespace Microsoft.AspNetCore.Razor.Language.CodeGeneration;
+
+public sealed partial class CodeWriter
+{
+    private sealed class IndentCache
+    {
+        private const int MaxCachedIndentSize = 80;
+
+        // Caches for combination of common tab sizes (2, 4) and the _useTabs bool
+        private static readonly IndentCache?[] s_caches = new IndentCache?[4];
+
+        private readonly ReadOnlyMemory<char>?[] _indents;
+        private readonly bool _useTabs;
+        private readonly int _tabSize;
+
+        private IndentCache(bool useTabs, int tabSize)
+        {
+            _useTabs = useTabs;
+            _tabSize = tabSize;
+
+            _indents = new ReadOnlyMemory<char>?[MaxCachedIndentSize + 1];
+        }
+
+        public static ReadOnlyMemory<char> GetIndentString(int size, bool useTabs, int tabSize)
+        {
+            var cacheIndex = tabSize switch
+            {
+                2 => useTabs ? 0 : 1,
+                4 => useTabs ? 2 : 3,
+                _ => -1,
+            };
+
+            // Only cache common tab sizes and small indents
+            if (cacheIndex == -1 || size > MaxCachedIndentSize)
+            {
+                return CreateIndent(size, useTabs, tabSize);
+            }
+
+            if (s_caches[cacheIndex] is not IndentCache indentCache)
+            {
+                indentCache = new IndentCache(useTabs, tabSize);
+                s_caches[cacheIndex] = indentCache;
+            }
+
+            return indentCache.GetOrCacheIndent(size);
+        }
+
+        private ReadOnlyMemory<char> GetOrCacheIndent(int size)
+        {
+            if (_indents[size] is not ReadOnlyMemory<char> cachedIndent)
+            {
+                cachedIndent = CreateIndent(size, _useTabs, _tabSize);
+                _indents[size] = cachedIndent;
+            }
+
+            return cachedIndent;
+        }
+
+        private static ReadOnlyMemory<char> CreateIndent(int size, bool useTabs, int tabSize)
+        {
+            if (useTabs)
+            {
+                var tabCount = size / tabSize;
+                var spaceCount = size % tabSize;
+
+                return string.Create(tabCount + spaceCount, (tabCount, spaceCount), static (destination, state) =>
+                {
+                    var (tabCount, spaceCount) = state;
+                    var index = 0;
+
+                    for (var i = 0; i < tabCount; i++)
+                    {
+                        destination[index++] = '\t';
+                    }
+
+                    for (var i = 0; i < spaceCount; i++)
+                    {
+                        destination[index++] = ' ';
+                    }
+                }).AsMemory();
+            }
+
+            return new string(' ', size).AsMemory();
+        }
+    }
+}
