83bc629d981c78048ebaaf956f32ab66235c763e
Todd Grunke <toddgrun@microsoft.com>
2025-10-20 12:08:25 -0700

    Simplify based on Dustin's suggestions

--- src/Compiler/Microsoft.CodeAnalysis.Razor.Compiler/src/Language/CodeGeneration/CodeWriter.IndentCache.cs
+++ src/Compiler/Microsoft.CodeAnalysis.Razor.Compiler/src/Language/CodeGeneration/CodeWriter.IndentCache.cs
@@ -9,70 +9,38 @@ public sealed partial class CodeWriter
 {
     private static class IndentCache
     {
-        private const int MaxCachedIndentSize = 80;
-
-        // Caches for combination of useTabs and common tab sizes (2, 4)
-        private static ReadOnlyMemory<char>?[]? s_tabsTwo;
-        private static ReadOnlyMemory<char>?[]? s_tabsFour;
-        private static ReadOnlyMemory<char>?[]? s_spacesTwo;
-        private static ReadOnlyMemory<char>?[]? s_spacesFour;
+        private static readonly ReadOnlyMemory<char> s_tabs = new string('\t', 64).AsMemory();
+        private static readonly ReadOnlyMemory<char> s_spaces = new string(' ', 128).AsMemory();
 
         public static ReadOnlyMemory<char> GetIndentString(int size, bool useTabs, int tabSize)
         {
-            // Don't initialize anything if we're not going to use it
-            if (size > MaxCachedIndentSize)
+            if (!useTabs)
             {
-                return CreateIndent(size, useTabs, tabSize);
+                return SliceOrCreate(size, s_spaces);
             }
 
-            return (useTabs, tabSize) switch
+            var tabCount = size / tabSize;
+            var spaceCount = size % tabSize;
+
+            if (spaceCount == 0)
             {
-                (true, 2) => GetOrCacheIndent(ref s_tabsTwo, size, useTabs, tabSize),
-                (true, 4) => GetOrCacheIndent(ref s_tabsFour, size, useTabs, tabSize),
-                (false, 2) => GetOrCacheIndent(ref s_spacesTwo, size, useTabs, tabSize),
-                (false, 4) => GetOrCacheIndent(ref s_spacesFour, size, useTabs, tabSize),
-                _ => CreateIndent(size, useTabs, tabSize)
-            };
+                return SliceOrCreate(tabCount, s_tabs);
+            }
+
+            return string.Create(length: tabCount + spaceCount, (tabCount, spaceCount), static (destination, state) =>
+            {
+                var (tabCount, spaceCount) = state;
+
+                s_tabs.Span[..tabCount].CopyTo(destination);
+                s_spaces.Span[..spaceCount].CopyTo(destination[tabCount..]);
+            }).AsMemory();
         }
 
-        private static ReadOnlyMemory<char> GetOrCacheIndent(ref ReadOnlyMemory<char>?[]? indents, int size, bool useTabs, int tabSize)
+        private static ReadOnlyMemory<char> SliceOrCreate(int length, ReadOnlyMemory<char> chars)
         {
-            indents ??= new ReadOnlyMemory<char>?[MaxCachedIndentSize + 1];
-
-            if (indents[size] is not ReadOnlyMemory<char> cachedIndent)
-            {
-                cachedIndent = CreateIndent(size, useTabs, tabSize);
-                indents[size] = cachedIndent;
-            }
-
-            return cachedIndent;
-        }
-
-        private static ReadOnlyMemory<char> CreateIndent(int size, bool useTabs, int tabSize)
-        {
-            if (useTabs)
-            {
-                var tabCount = size / tabSize;
-                var spaceCount = size % tabSize;
-
-                return string.Create(tabCount + spaceCount, (tabCount, spaceCount), static (destination, state) =>
-                {
-                    var (tabCount, spaceCount) = state;
-                    var index = 0;
-
-                    for (var i = 0; i < tabCount; i++)
-                    {
-                        destination[index++] = '\t';
-                    }
-
-                    for (var i = 0; i < spaceCount; i++)
-                    {
-                        destination[index++] = ' ';
-                    }
-                }).AsMemory();
-            }
-
-            return new string(' ', size).AsMemory();
+            return (length <= chars.Length)
+                ? chars[..length]
+                : new string(chars.Span[0], length).AsMemory();
         }
     }
 }
