f6afbc7f22a7901f1da14f3fcba15b9dfa941190
David Wengier <david.wengier@microsoft.com>
2025-10-26 21:18:54 +1100

    Serialize as something that can be deserialized

--- src/Razor/src/Microsoft.CodeAnalysis.Razor.Workspaces/Formatting/RazorFormattingService.cs
+++ src/Razor/src/Microsoft.CodeAnalysis.Razor.Workspaces/Formatting/RazorFormattingService.cs
@@ -92,7 +92,7 @@ internal class RazorFormattingService : IRazorFormattingService
 
         var logger = _formattingLoggerFactory.CreateLogger(documentContext.FilePath, range is null ? "Full" : "Range");
         logger?.LogObject("Options", options);
-        logger?.LogObject("HtmlChanges", htmlChanges);
+        logger?.LogObject("HtmlChanges", htmlChanges.SelectAsArray(e => e.ToRazorTextChange()));
         logger?.LogObject("Range", range);
         logger?.LogSourceText("InitialDocument", sourceText);
 
