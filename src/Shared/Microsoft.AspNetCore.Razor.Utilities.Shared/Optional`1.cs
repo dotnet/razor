4a41c43c3c53d716f5cf0c4c8f86bb71d02ebc70
Dustin Campbell <dustin@teamcampbell.org>
2025-10-28 10:57:53 -0700

    Introduce Optional<T>
    This is based on Roslyn's Optional<T>, which is similar to Nullable<T> except that it can hold reference types and does not throw when `Value` is accessed and `HasValue` is false. It is particular useful for long lists of optional parameters.
    
    Note: This type is public because Razor was currently using Roslyn's Optional<T> in a couple of public places, and those are now using Razor's Optional<T>.

---
+++ src/Shared/Microsoft.AspNetCore.Razor.Utilities.Shared/Optional`1.cs
@@ -0,0 +1,17 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+namespace Microsoft.AspNetCore.Razor;
+
+public readonly struct Optional<T>(T value)
+{
+    public bool HasValue { get; } = true;
+
+    public T Value { get; } = value;
+
+    public static implicit operator Optional<T>(T value)
+        => new(value);
+
+    public override string ToString()
+        => HasValue ? Value?.ToString() ?? "null" : "unspecified";
+}
