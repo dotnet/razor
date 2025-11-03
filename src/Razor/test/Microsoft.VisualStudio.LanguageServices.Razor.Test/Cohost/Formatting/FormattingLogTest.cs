bc90bbc880c6da6cfe37fe277d2948031c943d00
David Wengier <david.wengier@microsoft.com>
2025-11-03 11:12:18 +1100

    Use real types in the formatting log tests

--- src/Razor/test/Microsoft.VisualStudio.LanguageServices.Razor.Test/Cohost/Formatting/FormattingLogTest.cs
+++ src/Razor/test/Microsoft.VisualStudio.LanguageServices.Razor.Test/Cohost/Formatting/FormattingLogTest.cs
@@ -3,7 +3,6 @@
 
 using System.Linq;
 using System.Runtime.CompilerServices;
-using System.Runtime.Serialization;
 using System.Text.Json;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Razor;
@@ -35,7 +34,7 @@ public class FormattingLogTest(FormattingTestContext context, HtmlFormattingFixt
         var document = CreateProjectAndRazorDocument(contents);
 
         var optionsFile = GetResource("Options.json");
-        var options = (TempRazorFormattingOptions)JsonSerializer.Deserialize(optionsFile, typeof(TempRazorFormattingOptions), JsonHelpers.JsonSerializerOptions).AssumeNotNull();
+        var options = (RazorFormattingOptions)JsonSerializer.Deserialize(optionsFile, typeof(RazorFormattingOptions), JsonHelpers.JsonSerializerOptions).AssumeNotNull();
 
         var formattingService = (RazorFormattingService)OOPExportProvider.GetExportedValue<IRazorFormattingService>();
         formattingService.GetTestAccessor().SetFormattingLoggerFactory(new TestFormattingLoggerFactory(TestOutputHelper));
@@ -45,7 +44,7 @@ public class FormattingLogTest(FormattingTestContext context, HtmlFormattingFixt
         var sourceText = await document.GetTextAsync();
         var htmlEdits = htmlChanges.Select(c => sourceText.GetTextEdit(c.ToTextChange())).ToArray();
 
-        await GetFormattingEditsAsync(document, htmlEdits, span: default, options.CodeBlockBraceOnNextLine, options.InsertSpaces, options.TabSize, options.ToRazorFormattingOptions().CSharpSyntaxFormattingOptions.AssumeNotNull());
+        await GetFormattingEditsAsync(document, htmlEdits, span: default, options.CodeBlockBraceOnNextLine, options.InsertSpaces, options.TabSize, options.CSharpSyntaxFormattingOptions.AssumeNotNull());
     }
 
     [Fact]
@@ -72,7 +71,7 @@ public class FormattingLogTest(FormattingTestContext context, HtmlFormattingFixt
     {
         var document = CreateProjectAndRazorDocument(contents);
 
-        var options = new TempRazorFormattingOptions();
+        var options = new RazorFormattingOptions();
 
         var formattingService = (RazorFormattingService)OOPExportProvider.GetExportedValue<IRazorFormattingService>();
         formattingService.GetTestAccessor().SetFormattingLoggerFactory(new TestFormattingLoggerFactory(TestOutputHelper));
@@ -81,7 +80,7 @@ public class FormattingLogTest(FormattingTestContext context, HtmlFormattingFixt
         var sourceText = await document.GetTextAsync();
         var htmlEdits = htmlChanges.Select(c => sourceText.GetTextEdit(c.ToTextChange())).ToArray();
 
-        await GetFormattingEditsAsync(document, htmlEdits, span: default, options.CodeBlockBraceOnNextLine, options.InsertSpaces, options.TabSize, options.ToRazorFormattingOptions().CSharpSyntaxFormattingOptions.AssumeNotNull());
+        await GetFormattingEditsAsync(document, htmlEdits, span: default, options.CodeBlockBraceOnNextLine, options.InsertSpaces, options.TabSize, RazorCSharpSyntaxFormattingOptions.Default);
     }
 
     private string GetResource(string name, [CallerMemberName] string? testName = null)
@@ -92,68 +91,5 @@ public class FormattingLogTest(FormattingTestContext context, HtmlFormattingFixt
         Assert.True(testFile.Exists());
 
         return testFile.ReadAllText();
-    }
-
-    // HACK: Temporary types for deserializing because RazorCSharpSyntaxFormattingOptions doesn't have a parameterless constructor.
-    internal class TempRazorFormattingOptions()
-    {
-        [DataMember(Order = 0)]
-        public bool InsertSpaces { get; init; } = true;
-        [DataMember(Order = 1)]
-        public int TabSize { get; init; } = 4;
-        [DataMember(Order = 2)]
-        public bool CodeBlockBraceOnNextLine { get; init; } = false;
-        [DataMember(Order = 3)]
-        public TempRazorCSharpSyntaxFormattingOptions? CSharpSyntaxFormattingOptions { get; init; }
-
-        public RazorFormattingOptions ToRazorFormattingOptions()
-            => new()
-            {
-                InsertSpaces = InsertSpaces,
-                TabSize = TabSize,
-                CodeBlockBraceOnNextLine = CodeBlockBraceOnNextLine,
-                CSharpSyntaxFormattingOptions = CSharpSyntaxFormattingOptions is not null
-                    ? new RazorCSharpSyntaxFormattingOptions(
-                        CSharpSyntaxFormattingOptions.Spacing,
-                        CSharpSyntaxFormattingOptions.SpacingAroundBinaryOperator,
-                        CSharpSyntaxFormattingOptions.NewLines,
-                        CSharpSyntaxFormattingOptions.LabelPositioning,
-                        CSharpSyntaxFormattingOptions.Indentation,
-                        CSharpSyntaxFormattingOptions.WrappingKeepStatementsOnSingleLine,
-                        CSharpSyntaxFormattingOptions.WrappingPreserveSingleLine,
-                        CSharpSyntaxFormattingOptions.NamespaceDeclarations,
-                        CSharpSyntaxFormattingOptions.PreferTopLevelStatements,
-                        CSharpSyntaxFormattingOptions.CollectionExpressionWrappingLength)
-                    : RazorCSharpSyntaxFormattingOptions.Default
-            };
-    }
-
-    [DataContract]
-    internal sealed record class TempRazorCSharpSyntaxFormattingOptions(
-        [property: DataMember] RazorSpacePlacement Spacing,
-        [property: DataMember] RazorBinaryOperatorSpacingOptions SpacingAroundBinaryOperator,
-        [property: DataMember] RazorNewLinePlacement NewLines,
-        [property: DataMember] RazorLabelPositionOptions LabelPositioning,
-        [property: DataMember] RazorIndentationPlacement Indentation,
-        [property: DataMember] bool WrappingKeepStatementsOnSingleLine,
-        [property: DataMember] bool WrappingPreserveSingleLine,
-        [property: DataMember] RazorNamespaceDeclarationPreference NamespaceDeclarations,
-        [property: DataMember] bool PreferTopLevelStatements,
-        [property: DataMember] int CollectionExpressionWrappingLength)
-    {
-        public TempRazorCSharpSyntaxFormattingOptions()
-            : this(
-                  default,
-                  default,
-                  default,
-                  default,
-                  default,
-                  default,
-                  default,
-                  default,
-                  true,
-                  default)
-        {
-        }
     }
 }
