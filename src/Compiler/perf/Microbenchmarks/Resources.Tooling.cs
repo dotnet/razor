ada358e32316f9b8f8776c838934240fa443ec93
Dustin Campbell <dustin@teamcampbell.org>
2025-11-04 10:40:54 -0800

    Improve TagHelperChecksumBenchmark and move to compiler
    There's also some clean up in this change to the compiler benchmark to read different sets of tag helpers from resources.

---
+++ src/Compiler/perf/Microbenchmarks/Resources.Tooling.cs
@@ -0,0 +1,28 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System.Collections.Immutable;
+using Microsoft.AspNetCore.Razor.Language;
+
+namespace Microsoft.AspNetCore.Razor.Microbenchmarks;
+
+internal static partial class Resources
+{
+    internal static class Tooling
+    {
+        private const string Prefix = $"{Resources.Prefix}.Tooling";
+
+        private static ImmutableArray<TagHelperDescriptor>? s_blazorServerApp;
+        private static ImmutableArray<TagHelperDescriptor>? s_telerikMvc;
+        private static ImmutableArray<TagHelperDescriptor>? s_legacy;
+
+        public static ImmutableArray<TagHelperDescriptor> BlazorServerApp
+            => s_blazorServerApp ??= ReadTagHelpersFromResource($"{Prefix}.BlazorServerApp.TagHelpers.json");
+
+        public static ImmutableArray<TagHelperDescriptor> TelerikMvc
+            => s_telerikMvc ??= ReadTagHelpersFromResource($"{Prefix}.Telerik.Kendo.Mvc.Examples.taghelpers.json");
+
+        public static ImmutableArray<TagHelperDescriptor> Legacy
+            => s_legacy ??= ReadTagHelpersFromResource($"{Prefix}.taghelpers.json");
+    }
+}
---
+++ src/Compiler/perf/Microbenchmarks/Resources.cs
@@ -0,0 +1,27 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+using System;
+using System.Collections.Immutable;
+using System.Reflection;
+using Microsoft.AspNetCore.Razor.Language;
+using Microsoft.AspNetCore.Razor.Serialization.Json;
+
+namespace Microsoft.AspNetCore.Razor.Microbenchmarks;
+
+internal static partial class Resources
+{
+    private const string Prefix = "Microsoft.AspNetCore.Razor.Microbenchmarks.Compiler.Resources";
+
+    private static ImmutableArray<TagHelperDescriptor> ReadTagHelpersFromResource(string resourceName)
+    {
+        using var resourceStream = Assembly.GetExecutingAssembly().GetManifestResourceStream(resourceName);
+        Assumed.NotNull(resourceStream);
+
+        var length = (int)resourceStream.Length;
+        var bytes = new byte[length];
+        resourceStream.ReadExactly(bytes.AsSpan(0, length));
+
+        return JsonDataConvert.DeserializeTagHelperArray(bytes);
+    }
+}
