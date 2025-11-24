c2d2c5c4a2fbc99f061f3e8dd641501296a22695
Todd Grunke <toddgrun@microsoft.com>
2025-11-24 10:23:28 -0800

    Don't reuse DirectiveCompletionItemProvider.ShouldProvideCompletions as that has some filtering going on that we don't want for razor csharp keyword completion

--- src/Razor/src/Microsoft.CodeAnalysis.Razor.Workspaces/Completion/CSharpRazorKeywordCompletionItemProvider.cs
+++ src/Razor/src/Microsoft.CodeAnalysis.Razor.Workspaces/Completion/CSharpRazorKeywordCompletionItemProvider.cs
@@ -1,8 +1,11 @@
 ﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+using System.Collections.Generic;
 using System.Collections.Immutable;
 using System.Runtime.InteropServices;
+using Microsoft.AspNetCore.Razor.Language;
+using Microsoft.AspNetCore.Razor.Language.Syntax;
 
 namespace Microsoft.CodeAnalysis.Razor.Completion;
 
@@ -18,10 +21,48 @@ internal class CSharpRazorKeywordCompletionItemProvider : IRazorCompletionItemPr
 
     public ImmutableArray<RazorCompletionItem> GetCompletionItems(RazorCompletionContext context)
     {
-        // C# Razor keywords are valid in the same locations as directives
-        return DirectiveCompletionItemProvider.ShouldProvideCompletions(context)
+        return ShouldProvideCompletions(context)
             ? GetCSharpRazorKeywordCompletionItems()
             : [];
+    }
+
+    // Internal for testing
+    internal static bool ShouldProvideCompletions(RazorCompletionContext context)
+    {
+        var owner = context.Owner;
+        if (owner is null)
+        {
+            return false;
+        }
+
+        // Do not provide IntelliSense for explicit expressions. Explicit expressions will usually look like:
+        // @(DateTime.Now)
+        var implicitExpression = owner.FirstAncestorOrSelf<CSharpImplicitExpressionSyntax>();
+        if (implicitExpression is null)
+        {
+            return false;
+        }
+
+        if (implicitExpression.Width > 2 && context.Reason != CompletionReason.Invoked)
+        {
+            // We only want to provide razor csharp keyword completions if the implicit expression is empty "@|" or at the beginning of a word "@i|", this ensures
+            // we're consistent with how C# typically provides completion items.
+            return false;
+        }
+
+        if (owner.ChildNodesAndTokens().Any(static x => !x.AsToken(out var token) || !IsCSharpRazorKeywordCompletableToken(token)))
+        {
+            // Implicit expression contains nodes or tokens that aren't completable by a csharp razor keyword
+            return false;
+        }
+
+        return true;
+    }
+
+    private static bool IsCSharpRazorKeywordCompletableToken(AspNetCore.Razor.Language.Syntax.SyntaxToken token)
+    {
+        return token is { Kind: SyntaxKind.Identifier or SyntaxKind.Marker or SyntaxKind.Keyword }
+                     or { Kind: SyntaxKind.Transition, Parent.Kind: SyntaxKind.CSharpTransition };
     }
 
     // Internal for testing
--- src/Razor/test/Microsoft.CodeAnalysis.Razor.Workspaces.Test/Completion/CSharpRazorKeywordCompletionItemProviderTests.cs
+++ src/Razor/test/Microsoft.CodeAnalysis.Razor.Workspaces.Test/Completion/CSharpRazorKeywordCompletionItemProviderTests.cs
@@ -3,6 +3,8 @@
 
 using System;
 using System.Collections.Immutable;
+using Microsoft.AspNetCore.Razor.Language;
+using Microsoft.AspNetCore.Razor.Language.Syntax;
 using Microsoft.AspNetCore.Razor.PooledObjects;
 using Microsoft.AspNetCore.Razor.Test.Common;
 using Xunit;
@@ -27,6 +29,58 @@ public class CSharpRazorKeywordCompletionItemProviderTests(ITestOutputHelper tes
         );
     }
 
+    [Fact]
+    public void ShouldProvideCompletions_ReturnsFalseWhenOwnerIsNotExpression()
+    {
+        // Arrange
+        var context = CreateRazorCompletionContext("@$${");
+
+        // Act
+        var result = CSharpRazorKeywordCompletionItemProvider.ShouldProvideCompletions(context);
+
+        // Assert
+        Assert.False(result);
+    }
+
+    [Fact]
+    public void ShouldProvideCompletions_ReturnsFalseWhenOwnerIsComplexExpression()
+    {
+        // Arrange
+        var context = CreateRazorCompletionContext("@D$$ateTime.Now");
+
+        // Act
+        var result = CSharpRazorKeywordCompletionItemProvider.ShouldProvideCompletions(context);
+
+        // Assert
+        Assert.False(result);
+    }
+
+    [Fact]
+    public void ShouldProvideCompletions_ReturnsFalseWhenOwnerIsExplicitExpression()
+    {
+        // Arrange
+        var context = CreateRazorCompletionContext("@(so$$mething)");
+
+        // Act
+        var result = CSharpRazorKeywordCompletionItemProvider.ShouldProvideCompletions(context);
+
+        // Assert
+        Assert.False(result);
+    }
+
+    [Fact]
+    public void ShouldProvideCompletions_ReturnsTrueForSimpleImplicitExpressions_WhenInvoked()
+    {
+        // Arrange
+        var context = CreateRazorCompletionContext("@w$$hi");
+
+        // Act
+        var result = CSharpRazorKeywordCompletionItemProvider.ShouldProvideCompletions(context);
+
+        // Assert
+        Assert.True(result);
+    }
+
     private static Action<RazorCompletionItem>[] GetKeywordVerifies(ImmutableArray<string> keywords)
     {
         using var builder = new PooledArrayBuilder<Action<RazorCompletionItem>>(keywords.Length);
@@ -48,5 +102,29 @@ public class CSharpRazorKeywordCompletionItemProviderTests(ITestOutputHelper tes
         Assert.Equal(keyword + " Keyword", completionDescription.Description);
 
         Assert.Equal(CSharpRazorKeywordCompletionItemProvider.KeywordCommitCharacters, item.CommitCharacters);
+    }
+
+    private static RazorCompletionContext CreateRazorCompletionContext(TestCode text)
+    {
+        var syntaxTree = CreateSyntaxTree(text);
+        var absoluteIndex = text.Position;
+        var sourceDocument = RazorSourceDocument.Create("", RazorSourceDocumentProperties.Default);
+        var codeDocument = RazorCodeDocument.Create(sourceDocument);
+
+        var tagHelperDocumentContext = TagHelperDocumentContext.Create(prefix: string.Empty, tagHelpers: []);
+        var owner = syntaxTree.Root.FindInnermostNode(absoluteIndex);
+        owner = AbstractRazorCompletionFactsService.AdjustSyntaxNodeForWordBoundary(owner, absoluteIndex);
+
+        return new RazorCompletionContext(codeDocument, absoluteIndex, owner, syntaxTree, tagHelperDocumentContext, CompletionReason.Invoked);
+    }
+
+    private static RazorSyntaxTree CreateSyntaxTree(TestCode text)
+    {
+        var sourceDocument = TestRazorSourceDocument.Create(text.Text);
+
+        var builder = new RazorParserOptions.Builder(RazorLanguageVersion.Latest, RazorFileKind.Legacy);
+        var options = builder.ToOptions();
+
+        return RazorSyntaxTree.Parse(sourceDocument, options);
     }
 }
