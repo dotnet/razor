25ce8d75ce39c9cbdd5997beb75d73daf6bad432
David Wengier <david.wengier@microsoft.com>
2025-12-05 14:41:08 +1100

    Roslyn implements the endpoint, we implement an extension to it

--- src/Razor/src/Microsoft.CodeAnalysis.Razor.CohostingShared/Rename/WorkspaceWillRenameEndpoint.cs
+++ src/Razor/src/Microsoft.CodeAnalysis.Razor.CohostingShared/Rename/WorkspaceWillRenameEndpoint.cs
@@ -1,14 +1,12 @@
 ﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
-using System.Collections.Immutable;
 using System.Composition;
 using System.Threading;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Razor.Threading;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.ExternalAccess.Razor.Cohost;
-using Microsoft.CodeAnalysis.ExternalAccess.Razor.Features;
 using Microsoft.CodeAnalysis.Razor.Logging;
 using Microsoft.CodeAnalysis.Razor.Remote;
 
@@ -16,52 +14,15 @@ namespace Microsoft.VisualStudio.Razor.LanguageClient.Cohost;
 
 #pragma warning disable RS0030 // Do not use banned APIs
 [Shared]
-// NOTE: This has to use RazorMethod, not CohostEndpoint, because it has to use the "default" language,
-// since it has no document associated with it to get any other language. If Roslyn implements their
-// own didRename handler, we'll have to negotiate with them to deal with this.
-[RazorMethod(Methods.WorkspaceWillRenameFilesName)]
-[Export(typeof(IDynamicRegistrationProvider))]
-[ExportRazorStatelessLspService(typeof(WorkspaceWillRenameEndpoint))]
+[ExportRazorWillRenameListener("**/*.razor")]
 [method: ImportingConstructor]
 #pragma warning restore RS0030 // Do not use banned APIs
 internal sealed class WorkspaceWillRenameEndpoint(
     IRemoteServiceInvoker remoteServiceInvoker,
-    ILoggerFactory loggerFactory)
-    : AbstractRazorCohostRequestHandler<RenameFilesParams, WorkspaceEdit?>, IDynamicRegistrationProvider
+    ILoggerFactory loggerFactory) : AbstractRazorWillRenameListener
 {
     private readonly IRemoteServiceInvoker _remoteServiceInvoker = remoteServiceInvoker;
     private readonly ILogger _logger = loggerFactory.GetOrCreateLogger<WorkspaceWillRenameEndpoint>();
-
-    protected override bool MutatesSolutionState => true;
-
-    protected override bool RequiresLSPSolution => true;
-
-    public ImmutableArray<Registration> GetRegistrations(VSInternalClientCapabilities clientCapabilities, RazorCohostRequestContext requestContext)
-    {
-        // This is false in VS, but we have our own polyfill in ProjectItemRenameEventHandler, in the VS layer, that doesn't care about capabilities
-        if (clientCapabilities.Workspace?.FileOperations?.WillRename == true)
-        {
-            return [new Registration
-            {
-                Method = Methods.WorkspaceWillRenameFilesName,
-                RegisterOptions = new FileOperationRegistrationOptions()
-                {
-                    Filters = [new FileOperationFilter()
-                    {
-                        Pattern = new FileOperationPattern()
-                        {
-                            // We don't do anything special for rename of .cshtml files
-                            Glob = "**/*.razor",
-                            Matches = FileOperationPatternKind.File,
-                            Options = new FileOperationPatternOptions() { IgnoreCase = true }
-                        }
-                    }]
-                }
-            }];
-        }
-
-        return [];
-    }
 
     protected override Task<WorkspaceEdit?> HandleRequestAsync(RenameFilesParams request, RazorCohostRequestContext context, CancellationToken cancellationToken)
     {
