45f70d70b2a1c6d4994b20147d9fb157dcadf8de
David Wengier <david.wengier@microsoft.com>
2025-12-11 14:44:59 +1100

    Remove assert and doc
    The old language server tests don't use the source generator, so the assert breaks them.

--- src/Razor/src/Microsoft.CodeAnalysis.Razor.Workspaces/Extensions/CSharpSyntaxNodeExtensions.cs
+++ src/Razor/src/Microsoft.CodeAnalysis.Razor.Workspaces/Extensions/CSharpSyntaxNodeExtensions.cs
@@ -33,10 +33,15 @@ internal static class CSharpSyntaxNodeExtensions
             return sourceText.GetLinePositionSpan(node.Span).SpansMultipleLines();
         }
 
+        /// <summary>
+        /// Attempts to retrieve the first class declaration from the current syntax node, if present.
+        /// </summary>
+        /// <remarks>
+        /// This method only supports the known shape of the Razor compiler generated C# source and should
+        /// not be used for arbitrary C# syntax trees
+        /// </remarks>
         internal bool TryGetClassDeclaration([NotNullWhen(true)] out ClassDeclarationSyntax? classDeclaration)
         {
-            Debug.Assert(node.SyntaxTree.FilePath.Contains("Microsoft.NET.Sdk.Razor.SourceGenerators.RazorSourceGenerator"), "This method only supports finding class declarations for the specific shape of syntax tree that the source generator produces.");
-
             // Since we know how the compiler generates the C# source we can be a little specific here, and avoid
             // long tree walks. If the compiler ever changes how they generate their code, the tests for this will break
             // so we'll know about it.
