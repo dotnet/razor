81c7d8b72f8c20c01f9f1fd6e386ab270ed92d8f
David Wengier <david.wengier@microsoft.com>
2025-12-30 14:57:08 +1100

    Add missing test scenarios from create component tests

--- src/Razor/test/Microsoft.VisualStudioCode.RazorExtension.Test/Endpoints/Shared/CodeActions/CreateComponentFromTagTests.cs
+++ src/Razor/test/Microsoft.VisualStudioCode.RazorExtension.Test/Endpoints/Shared/CodeActions/CreateComponentFromTagTests.cs
@@ -2,6 +2,7 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Razor.Language;
 using Microsoft.CodeAnalysis.Razor.Protocol;
 using Xunit;
 using Xunit.Abstractions;
@@ -10,6 +11,20 @@ namespace Microsoft.VisualStudio.Razor.LanguageClient.Cohost.CodeActions;
 
 public class CreateComponentFromTagTests(ITestOutputHelper testOutputHelper) : CohostCodeActionsEndpointTestBase(testOutputHelper)
 {
+    [Fact]
+    public async Task NotOfferedInLegacy()
+    {
+        await VerifyCodeActionAsync(
+            input: """
+                <div></div>
+
+                <He[||]llo></Hello>
+                """,
+            expected: null,
+            codeActionName: LanguageServerConstants.CodeActions.CreateComponentFromTag,
+            fileKind: RazorFileKind.Legacy);
+    }
+
     [Fact]
     public async Task CreateComponentFromTag()
     {
@@ -27,6 +42,32 @@ public class CreateComponentFromTagTests(ITestOutputHelper testOutputHelper) : C
             codeActionName: LanguageServerConstants.CodeActions.CreateComponentFromTag,
             additionalExpectedFiles: [
                 (FileUri("Hello.razor"), "")]);
+    }
+
+    [Fact]
+    public async Task CreateComponentFromTag_WithNamespace()
+    {
+        await VerifyCodeActionAsync(
+            input: """
+                @namespace MyApp.Components
+
+                <div></div>
+
+                <He[||]llo></Hello>
+                """,
+            expected: """
+                @namespace MyApp.Components
+
+                <div></div>
+
+                <Hello></Hello>
+                """,
+            codeActionName: LanguageServerConstants.CodeActions.CreateComponentFromTag,
+            additionalExpectedFiles: [
+                (FileUri("Hello.razor"), """
+                @namespace MyApp.Components
+                
+                """)]);
     }
 
     [Fact]
