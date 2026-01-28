f9e6239054db7cfd1948c283e28d03f601411d40
David Wengier <david.wengier@microsoft.com>
2026-01-28 14:52:29 +1100

    Remove unused strings

--- src/Razor/src/Microsoft.VisualStudio.LanguageServices.Razor/WellKnownFeatureFlagNames.cs
+++ src/Razor/src/Microsoft.VisualStudio.LanguageServices.Razor/WellKnownFeatureFlagNames.cs
@@ -6,6 +6,4 @@ namespace Microsoft.VisualStudio.Razor;
 internal static class WellKnownFeatureFlagNames
 {
     public const string ShowAllCSharpCodeActions = "Razor.LSP.ShowAllCSharpCodeActions";
-    public const string IncludeProjectKeyInGeneratedFilePath = "Razor.LSP.IncludeProjectKeyInGeneratedFilePath";
-    public const string UseRazorCohostServer = "Razor.LSP.UseRazorCohostServer";
 }
