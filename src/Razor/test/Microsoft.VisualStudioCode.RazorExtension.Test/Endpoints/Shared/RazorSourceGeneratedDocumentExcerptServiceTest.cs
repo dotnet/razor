4e608fd94e106545b96d1c120e733a75e7337bd8
David Wengier <david.wengier@microsoft.com>
2026-01-28 14:30:14 +1100

    Port document excerpt tests to cohosting

--- src/Razor/test/Microsoft.VisualStudioCode.RazorExtension.Test/Endpoints/Shared/RazorSourceGeneratedDocumentExcerptServiceTest.cs
+++ src/Razor/test/Microsoft.VisualStudioCode.RazorExtension.Test/Endpoints/Shared/RazorSourceGeneratedDocumentExcerptServiceTest.cs
@@ -1,24 +1,29 @@
 ﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
 
+using System.Threading;
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Razor.Test.Common.Workspaces;
+using Microsoft.AspNetCore.Razor.Test.Common;
+using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.Classification;
 using Microsoft.CodeAnalysis.ExternalAccess.Razor;
-using Microsoft.CodeAnalysis.Razor.ProjectSystem;
-using Microsoft.CodeAnalysis.Razor.Telemetry;
+using Microsoft.CodeAnalysis.Razor.CohostingShared;
+using Microsoft.CodeAnalysis.Razor.DocumentMapping;
+using Microsoft.CodeAnalysis.Remote.Razor.ProjectSystem;
+using Microsoft.CodeAnalysis.Text;
+using Microsoft.VisualStudio.Razor.LanguageClient.Cohost;
 using Xunit;
 using Xunit.Abstractions;
 
-namespace Microsoft.VisualStudio.Razor.DynamicFiles;
+namespace Microsoft.VisualStudioCode.RazorExtension.Test.Endpoints.Shared;
 
-public class RazorDocumentExcerptServiceTest(ITestOutputHelper testOutput) : DocumentExcerptServiceTestBase(testOutput)
+public class RazorSourceGeneratedDocumentExcerptServiceTest(ITestOutputHelper testOutputHelper) : CohostEndpointTestBase(testOutputHelper)
 {
     [Fact]
-    public async Task TryGetExcerptInternalAsync_SingleLine_CanClassifyCSharp()
+    public async Task TryExcerptAsync_SingleLine_CanClassifyCSharp()
     {
         // Arrange
-        var razorSource = @"
+        TestCode razorSource = @"
 <html>
 @{
     var [|foo|] = ""Hello, World!"";
@@ -34,7 +39,7 @@ public class RazorDocumentExcerptServiceTest(ITestOutputHelper testOutput) : Doc
 
         // Act
         var options = RazorClassificationOptionsWrapper.Default;
-        var result = await service.TryGetExcerptInternalAsync(secondary, secondarySpan, RazorExcerptMode.SingleLine, options, DisposalToken);
+        var result = await service.TryExcerptAsync(secondary, secondarySpan, RazorExcerptMode.SingleLine, options, DisposalToken);
 
         // Assert
         Assert.NotNull(result);
@@ -93,7 +98,7 @@ public class RazorDocumentExcerptServiceTest(ITestOutputHelper testOutput) : Doc
     }
 
     [Fact]
-    public async Task TryGetExcerptInternalAsync_SingleLine_CanClassifyCSharp_ImplicitExpression()
+    public async Task TryExcerptAsync_SingleLine_CanClassifyCSharp_ImplicitExpression()
     {
         // Arrange
         var razorSource = @"
@@ -112,7 +117,7 @@ public class RazorDocumentExcerptServiceTest(ITestOutputHelper testOutput) : Doc
 
         // Act
         var options = RazorClassificationOptionsWrapper.Default;
-        var result = await service.TryGetExcerptInternalAsync(secondary, secondarySpan, RazorExcerptMode.SingleLine, options, DisposalToken);
+        var result = await service.TryExcerptAsync(secondary, secondarySpan, RazorExcerptMode.SingleLine, options, DisposalToken);
 
         // Assert
         Assert.NotNull(result);
@@ -146,18 +151,18 @@ public class RazorDocumentExcerptServiceTest(ITestOutputHelper testOutput) : Doc
     }
 
     [Fact]
-    public async Task TryGetExcerptInternalAsync_SingleLine_CanClassifyCSharp_ComplexLine()
+    public async Task TryExcerptAsync_SingleLine_CanClassifyCSharp_ComplexLine()
     {
         // Arrange
         var razorSource = @"
-<html>
-@{
-    var foo = ""Hello, World!"";
-}
-  <body>@foo</body>
-  <div>@(3 + 4)</div><div>@(foo + [|foo|])</div>
-</html>
-";
+    <html>
+    @{
+        var foo = ""Hello, World!"";
+    }
+      <body>@foo</body>
+      <div>@(3 + 4)</div><div>@(foo + [|foo|])</div>
+    </html>
+    ";
 
         var (primary, secondary, secondarySpan) = await InitializeWithSnapshotAsync(razorSource, DisposalToken);
 
@@ -165,7 +170,7 @@ public class RazorDocumentExcerptServiceTest(ITestOutputHelper testOutput) : Doc
 
         // Act
         var options = RazorClassificationOptionsWrapper.Default;
-        var result = await service.TryGetExcerptInternalAsync(secondary, secondarySpan, RazorExcerptMode.SingleLine, options, DisposalToken);
+        var result = await service.TryExcerptAsync(secondary, secondarySpan, RazorExcerptMode.SingleLine, options, DisposalToken);
 
         // Assert
         Assert.NotNull(result);
@@ -249,22 +254,22 @@ public class RazorDocumentExcerptServiceTest(ITestOutputHelper testOutput) : Doc
     }
 
     [Fact]
-    public async Task TryGetExcerptInternalAsync_MultiLine_MultilineString()
+    public async Task TryExcerptAsync_MultiLine_MultilineString()
     {
         // Arrange
         var razorSource = """
-            <html>
-            @{
-                [|string|] bigString = @"
-                    Razor shows 3 lines in a
-                    tooltip maximum, so this
-                    multi-line verbatim
-                    string must be longer
-                    than that.
-                    ";
-            }
-            </html>
-            """;
+                <html>
+                @{
+                    [|string|] bigString = @"
+                        Razor shows 3 lines in a
+                        tooltip maximum, so this
+                        multi-line verbatim
+                        string must be longer
+                        than that.
+                        ";
+                }
+                </html>
+                """;
 
         var (primary, secondary, secondarySpan) = await InitializeWithSnapshotAsync(razorSource, DisposalToken);
 
@@ -272,7 +277,7 @@ public class RazorDocumentExcerptServiceTest(ITestOutputHelper testOutput) : Doc
 
         // Act
         var options = RazorClassificationOptionsWrapper.Default;
-        var result = await service.TryGetExcerptInternalAsync(secondary, secondarySpan, RazorExcerptMode.Tooltip, options, DisposalToken);
+        var result = await service.TryExcerptAsync(secondary, secondarySpan, RazorExcerptMode.Tooltip, options, DisposalToken);
 
         // Assert
         Assert.NotNull(result);
@@ -286,13 +291,13 @@ public class RazorDocumentExcerptServiceTest(ITestOutputHelper testOutput) : Doc
             ignoreLineEndingDifferences: true);
 
         Assert.Equal("""
-            <html>
-            @{
-                string bigString = @"
-                    Razor shows 3 lines in a
-                    tooltip maximum, so this
-                    multi-line verbatim
-            """,
+                <html>
+                @{
+                    string bigString = @"
+                        Razor shows 3 lines in a
+                        tooltip maximum, so this
+                        multi-line verbatim
+                """,
             result.Value.Content.ToString(), ignoreLineEndingDifferences: true);
 
         Assert.Collection(
@@ -301,9 +306,9 @@ public class RazorDocumentExcerptServiceTest(ITestOutputHelper testOutput) : Doc
             {
                 Assert.Equal(ClassificationTypeNames.Text, c.ClassificationType);
                 Assert.Equal("""
-                    <html>
-                    @{
-                    """,
+                        <html>
+                        @{
+                        """,
                     result.Value.Content.GetSubText(c.TextSpan).ToString(),
                     ignoreLineEndingDifferences: true);
             },
@@ -346,29 +351,29 @@ public class RazorDocumentExcerptServiceTest(ITestOutputHelper testOutput) : Doc
             {
                 Assert.Equal(ClassificationTypeNames.VerbatimStringLiteral, c.ClassificationType);
                 Assert.Equal("""
-                    @"
-                            Razor shows 3 lines in a
-                            tooltip maximum, so this
-                            multi-line verbatim
-                    """, result.Value.Content.GetSubText(c.TextSpan).ToString());
+                        @"
+                                Razor shows 3 lines in a
+                                tooltip maximum, so this
+                                multi-line verbatim
+                        """, result.Value.Content.GetSubText(c.TextSpan).ToString());
             });
     }
 
     [Fact]
-    public async Task TryGetExcerptInternalAsync_SingleLine_MultilineString()
+    public async Task TryExcerptAsync_SingleLine_MultilineString()
     {
         // Arrange
         var razorSource = """
-            <html>
-            @{
-                [|string|] bigString = @"
-                    This is a
-                    multi-line verbatim
-                    string.
-                    ";
-            }
-            </html>
-            """;
+                <html>
+                @{
+                    [|string|] bigString = @"
+                        This is a
+                        multi-line verbatim
+                        string.
+                        ";
+                }
+                </html>
+                """;
 
         var (primary, secondary, secondarySpan) = await InitializeWithSnapshotAsync(razorSource, DisposalToken);
 
@@ -376,7 +381,7 @@ public class RazorDocumentExcerptServiceTest(ITestOutputHelper testOutput) : Doc
 
         // Act
         var options = RazorClassificationOptionsWrapper.Default;
-        var result = await service.TryGetExcerptInternalAsync(secondary, secondarySpan, RazorExcerptMode.SingleLine, options, DisposalToken);
+        var result = await service.TryExcerptAsync(secondary, secondarySpan, RazorExcerptMode.SingleLine, options, DisposalToken);
 
         // Assert
         Assert.NotNull(result);
@@ -450,7 +455,7 @@ public class RazorDocumentExcerptServiceTest(ITestOutputHelper testOutput) : Doc
 
         // Act
         var options = RazorClassificationOptionsWrapper.Default;
-        var result = await service.TryGetExcerptInternalAsync(secondary, secondarySpan, RazorExcerptMode.Tooltip, options, DisposalToken);
+        var result = await service.TryExcerptAsync(secondary, secondarySpan, RazorExcerptMode.Tooltip, options, DisposalToken);
 
         // Assert
         Assert.NotNull(result);
@@ -548,7 +553,7 @@ public class RazorDocumentExcerptServiceTest(ITestOutputHelper testOutput) : Doc
     }
 
     [Fact]
-    public async Task TryGetExcerptInternalAsync_MultiLine_Boundaries_CanClassifyCSharp()
+    public async Task TryExcerptAsync_MultiLine_Boundaries_CanClassifyCSharp()
     {
         // Arrange
         var razorSource = @"@{ var [|foo|] = ""Hello, World!""; }";
@@ -559,7 +564,7 @@ public class RazorDocumentExcerptServiceTest(ITestOutputHelper testOutput) : Doc
 
         // Act
         var options = RazorClassificationOptionsWrapper.Default;
-        var result = await service.TryGetExcerptInternalAsync(secondary, secondarySpan, RazorExcerptMode.Tooltip, options, DisposalToken);
+        var result = await service.TryExcerptAsync(secondary, secondarySpan, RazorExcerptMode.Tooltip, options, DisposalToken);
 
         // Assert
         // Verifies that the right part of the primary document will be highlighted.
@@ -640,8 +645,29 @@ public class RazorDocumentExcerptServiceTest(ITestOutputHelper testOutput) : Doc
             });
     }
 
-    private RazorDocumentExcerptService CreateExcerptService(IDocumentSnapshot document)
+    private RazorSourceGeneratedDocumentExcerptService CreateExcerptService(TextDocument document)
     {
-        return new RazorDocumentExcerptService(document, new RazorMappingService(document, NoOpTelemetryReporter.Instance, LoggerFactory));
+        return new RazorSourceGeneratedDocumentExcerptService(RemoteServiceInvoker);
+    }
+
+    private async Task<(TextDocument primary, SourceGeneratedDocument secondary, TextSpan secondarySpan)> InitializeWithSnapshotAsync(TestCode razorSource, CancellationToken cancellationToken)
+    {
+        var document = CreateProjectAndRazorDocument(razorSource.Text);
+
+        var sourceText = await document.GetTextAsync(cancellationToken);
+
+        var documentMappingService = OOPExportProvider.GetExportedValue<IDocumentMappingService>();
+        var snapshotManager = OOPExportProvider.GetExportedValue<RemoteSnapshotManager>();
+
+        var generatedDocument = await snapshotManager.GetSnapshot(document).GetGeneratedDocumentAsync(cancellationToken);
+        var generatedSourceText = await generatedDocument.GetTextAsync(cancellationToken);
+        var codeDocument = await snapshotManager.GetSnapshot(document).GetGeneratedOutputAsync(cancellationToken);
+        var csharpDocument = codeDocument.GetRequiredCSharpDocument();
+
+        var razorRange = sourceText.GetLinePositionSpan(razorSource.Span);
+        Assert.True(documentMappingService.TryMapToCSharpDocumentRange(csharpDocument, razorRange, out var csharpRange));
+        var csharpSpan = generatedSourceText.GetTextSpan(csharpRange);
+
+        return (document, generatedDocument, csharpSpan);
     }
 }
