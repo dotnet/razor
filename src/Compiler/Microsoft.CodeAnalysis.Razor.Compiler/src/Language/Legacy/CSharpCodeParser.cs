a906eb27d9e06ae81fd0176a207983ed1715da0d
Naresh Joshi <Naresh.Joshi@microsoft.com>
2026-02-12 15:04:27 -0800

    Reduce string allocations in ParseUsingDeclaration by replacing LINQ + Concat(this WorkspaceEdit, WorkspaceEdit?) with StringBuilder loop

--- src/Compiler/Microsoft.CodeAnalysis.Razor.Compiler/src/Language/Legacy/CSharpCodeParser.cs
+++ src/Compiler/Microsoft.CodeAnalysis.Razor.Compiler/src/Language/Legacy/CSharpCodeParser.cs
@@ -7,6 +7,7 @@ using System.Collections.Generic;
 using System.Collections.Immutable;
 using System.Diagnostics;
 using System.Linq;
+using System.Text;
 using Microsoft.AspNetCore.Razor.Language.Syntax.InternalSyntax;
 using Microsoft.AspNetCore.Razor.PooledObjects;
 using static Microsoft.AspNetCore.Razor.Language.Syntax.GreenNodeExtensions;
@@ -2603,10 +2604,6 @@ internal class CSharpCodeParser : TokenizerBackedParser<CSharpTokenizer>
             }
 
             var usingStatementTokens = TokenBuilder.ToList().Nodes;
-            var usingContentTokens = usingStatementTokens.Skip(1);
-            var parsedNamespaceTokens = usingStatementTokens
-                .Skip(nonNamespaceTokenCount)
-                .Where(s => s.Kind != SyntaxKind.CSharpComment && s.Kind != SyntaxKind.Whitespace && s.Kind != SyntaxKind.NewLine);
 
             SetAcceptedCharacters(AcceptedCharactersInternal.AnyExceptNewline);
 
@@ -2617,9 +2614,30 @@ internal class CSharpCodeParser : TokenizerBackedParser<CSharpTokenizer>
                 hasExplicitSemicolon = TryAccept(SyntaxKind.Semicolon);
             }
 
+            var usingContentBuilder = new StringBuilder();
+            var parsedNamespaceBuilder = new StringBuilder();
+
+            for (var i = 0; i < usingStatementTokens.Length; i++)
+            {
+                var token = usingStatementTokens[i];
+
+                if (i >= 1)
+                {
+                    usingContentBuilder.Append(token.Content);
+                }
+
+                if (i >= nonNamespaceTokenCount &&
+                    token.Kind != SyntaxKind.CSharpComment &&
+                    token.Kind != SyntaxKind.Whitespace &&
+                    token.Kind != SyntaxKind.NewLine)
+                {
+                    parsedNamespaceBuilder.Append(token.Content);
+                }
+            }
+
             chunkGenerator = new AddImportChunkGenerator(
-                string.Concat(usingContentTokens.Select(s => s.Content)),
-                string.Concat(parsedNamespaceTokens.Select(s => s.Content)),
+                usingContentBuilder.ToString(),
+                parsedNamespaceBuilder.ToString(),
                 isStatic,
                 hasExplicitSemicolon);
 
