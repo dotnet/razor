964136747e2d423ed1908f14c718c0cc44ec6c82
David Wengier <david.wengier@microsoft.com>
2026-02-17 14:20:38 +1100

    Add extension method for determining unused using directives

--- src/Compiler/Microsoft.CodeAnalysis.Razor.Compiler/src/Language/RazorCodeDocumentExtensions.cs
+++ src/Compiler/Microsoft.CodeAnalysis.Razor.Compiler/src/Language/RazorCodeDocumentExtensions.cs
@@ -2,7 +2,12 @@
 // The .NET Foundation licenses this file to you under the MIT license.
 
 using System;
+using System.Collections.Immutable;
 using System.Diagnostics.CodeAnalysis;
+using System.IO;
+using Microsoft.AspNetCore.Mvc.Razor.Extensions;
+using Microsoft.AspNetCore.Razor.Language.Syntax;
+using Microsoft.AspNetCore.Razor.PooledObjects;
 
 namespace Microsoft.AspNetCore.Razor.Language;
 
@@ -33,4 +38,50 @@ public static class RazorCodeDocumentExtensions
         [NotNullWhen(true)] out string? @namespace,
         out SourceSpan? namespaceSpan)
         => codeDocument.TryGetNamespace(fallbackToRootNamespace, considerImports: true, out @namespace, out namespaceSpan);
+
+    internal static ImmutableArray<BaseRazorDirectiveSyntax> GetUnusedDirectives(this RazorCodeDocument codeDocument)
+    {
+        // Never report unused directives in imports files, as we don't track at that level
+        if (codeDocument.FileKind.IsComponentImport() ||
+            string.Equals(Path.GetFileName(codeDocument.Source.FilePath), MvcImportProjectFeature.ImportsFileName, StringComparison.OrdinalIgnoreCase))
+        {
+            return [];
+        }
+
+        var contributions = codeDocument.GetDirectiveTagHelperContributions();
+        if (contributions.IsDefaultOrEmpty)
+        {
+            return [];
+        }
+
+        var referencedTagHelpers = codeDocument.GetReferencedTagHelpers();
+
+        using var unusedDirectives = new PooledArrayBuilder<BaseRazorDirectiveSyntax>();
+        foreach (var contribution in contributions)
+        {
+            if (referencedTagHelpers is null ||
+                contribution.ContributedTagHelpers.IsEmpty ||
+                !AnyContributedTagHelperIsReferenced(contribution.ContributedTagHelpers, referencedTagHelpers))
+            {
+                unusedDirectives.Add(contribution.Directive);
+            }
+        }
+
+        return unusedDirectives.ToImmutableAndClear();
+
+        static bool AnyContributedTagHelperIsReferenced(
+            TagHelperCollection contributedTagHelpers,
+            TagHelperCollection referencedTagHelpers)
+        {
+            foreach (var contributed in contributedTagHelpers)
+            {
+                if (referencedTagHelpers.Contains(contributed))
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
+    }
 }
