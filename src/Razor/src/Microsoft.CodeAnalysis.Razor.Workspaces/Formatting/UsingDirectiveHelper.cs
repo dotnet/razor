8628b84aae55e2dbd9ae8887e3f2554f94633389
David Wengier <david.wengier@microsoft.com>
2026-03-13 10:44:03 +1100

    Remove the now unused method

--- src/Razor/src/Microsoft.CodeAnalysis.Razor.Workspaces/Formatting/UsingDirectiveHelper.cs
+++ src/Razor/src/Microsoft.CodeAnalysis.Razor.Workspaces/Formatting/UsingDirectiveHelper.cs
@@ -19,7 +19,6 @@ using Microsoft.AspNetCore.Razor.Language.Syntax;
 using Microsoft.AspNetCore.Razor.PooledObjects;
 using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.CodeAnalysis.Razor.DocumentMapping;
-using Microsoft.CodeAnalysis.Razor.ProjectSystem;
 using Microsoft.CodeAnalysis.Text;
 using RazorSyntaxNode = Microsoft.AspNetCore.Razor.Language.Syntax.SyntaxNode;
 
@@ -30,36 +29,6 @@ internal static class UsingDirectiveHelper
     private static readonly Regex s_addUsingVSCodeAction = new Regex("@?using ([^;]+);?$", RegexOptions.Compiled, TimeSpan.FromSeconds(1));
 
     private readonly record struct RazorUsingDirective(RazorUsingDirectiveSyntax Node, AddImportChunkGenerator Statement);
-
-    public static async Task<TextEdit[]> GetUsingStatementEditsAsync(IDocumentSnapshot documentSnapshot, SourceText changedCSharpText, CancellationToken cancellationToken)
-    {
-        // Now that we're done with everything, lets see if there are any using statements to fix up
-        // We do this by comparing the original generated C# code, and the changed C# code, and look for a difference
-        // in using statements. We can't use edits for this for two main reasons:
-        //
-        // 1. Using statements in the generated code might come from _Imports.razor, or from this file, and C# will shove them anywhere
-        // 2. The edit might not be clean. eg given:
-        //      using System;
-        //      using System.Text;
-        //    Adding "using System.Linq;" could result in an insert of "Linq;\r\nusing System." on line 2
-        //
-        // So because of the above, we look for a difference in C# using directive nodes directly from the C# syntax tree, and apply them manually
-        // to the Razor document.
-
-        var codeDocument = await documentSnapshot.GetGeneratedOutputAsync(cancellationToken).ConfigureAwait(false);
-        var originalCSharpSyntaxTree = await documentSnapshot.GetCSharpSyntaxTreeAsync(cancellationToken).ConfigureAwait(false);
-        var changedCSharpSyntaxTree = originalCSharpSyntaxTree.WithChangedText(changedCSharpText);
-        var oldUsings = await FindUsingDirectiveStringsAsync(originalCSharpSyntaxTree, cancellationToken).ConfigureAwait(false);
-        var newUsings = await FindUsingDirectiveStringsAsync(changedCSharpSyntaxTree, cancellationToken).ConfigureAwait(false);
-
-        using var edits = new PooledArrayBuilder<TextEdit>();
-        foreach (var usingStatement in newUsings.Except(oldUsings))
-        {
-            edits.Add(CreateAddUsingTextEdit(usingStatement, codeDocument));
-        }
-
-        return edits.ToArray();
-    }
 
     /// <summary>
     /// Extracts the namespace from a C# add using statement provided by Visual Studio
